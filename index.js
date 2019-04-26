46dbeb4a0ea0d551cbc5a3ffa487052bd0228b0e
Pamela Lovett <pamela.s.lovett@gmail.com>
2019-04-26 13:36:38 -0600

    Add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const http = require('http');
+let app = http.createServer();
+
+// Start the server on port 3000
+app.listen(3000, '127.0.0.1');  
+console.log('Node server running on port 3000'); 
