836696b23c3b285af1d75206b37cba453695e1e0
Alek <alekaker@outlook.com>
2019-04-29 17:52:06 -0600

    adding test to see if turn has a guess

--- test/turn-test.js
+++ test/turn-test.js
@@ -16,5 +16,19 @@ describe('Turn', function() {
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-  
+  it.skip('should have a card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn("object", card)
+    expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
+  });
+
+  it('should have a guess', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn("object", card)
+    expect(turn.guess).to.equal(card.correctAnswer);
+  });
+
+
+
+
 });
