b183330ec2a3ed2a23871d6810aa72088e7d2452
RyanFlachman <ryan@flachman.net>
2019-04-30 12:47:02 -0600

    Add card count functionality

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -9,7 +9,7 @@ describe ('Deck', function() {
   it('should be a function', function() {
     const deck = new Deck();
     expect(Deck).to.be.a('function')
-  })
+  });
 
   it('should be able to hold an array of cards', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
@@ -17,5 +17,13 @@ describe ('Deck', function() {
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     const deck = new Deck([card1,card2,card3]);
     expect(deck.cards.length).to.equal(3)
+  });
+
+  it('should know how many cards are in the deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1,card2,card3]);
+    expect(deck.countCards()).to.equal(3)
   })
 });
