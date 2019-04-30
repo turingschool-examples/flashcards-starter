9b5826e05dd3e17827299740150ada51e896bcd4
Katie Williams <kawilliams8@gmail.com>
2019-04-30 16:25:48 -0600

    Begin Deck files

--- src/turn.js
+++ src/turn.js
@@ -2,10 +2,29 @@ class Turn {
     constructor(card, guess) {
         this.card = card;
         this.guess = guess;
-        // returnGuess() {
-        //     // return this.answer;
-        // }
     }
+
+    returnGuess() {
+        return this.guess;
+    }
+
+    returnCard() {
+        console.log("card, as object: ", this.card);
+        return this.card;
+        //return this.card with id, question, guess, all answers
+    }
+
+    evaluateGuess() {
+        console.log('evaluate for match:', (this.guess === card.correctAnswer ? true : false))
+        return this.guess === this.card.correctAnswer ? true : false;
+        //ternary returns Boolean if this.guess === this.card
+    }
+
+    giveFeedback() {
+        return this.guess === this.card.correctAnswer ? 'correct!' : 'incorrect!';
+        //conditional/ternary returns either ‘incorrect!’ or ‘correct!’
+    }
+
 }
 
 module.exports = Turn;
