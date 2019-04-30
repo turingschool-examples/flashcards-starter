325aa9aa9d754113e1aa7af8c335b0453ff49f3c
Antonio Fry <antoniofry2000@gmail.com>
2019-04-29 20:35:44 -0600

    add card class

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+class Card {
+	constructor(id, question, correctAnswer, answers) {
+		this.id = id;
+		this.question = question;
+		this.answers = answers;
+		// this.correctAnswer = ;
+	}
+
+	
+}
+
+module.exports = Card;
