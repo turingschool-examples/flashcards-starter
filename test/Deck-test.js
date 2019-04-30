e1a784d5f519289e7beeb13367ce40fee7f6090b
Alek <alekaker@outlook.com>
2019-04-30 10:15:17 -0600

    adding Deck and Deck-test files

---
+++ test/Deck-test.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card')
+
+describe('Card', function() {
+
+
+
+
+
+
+
+});
