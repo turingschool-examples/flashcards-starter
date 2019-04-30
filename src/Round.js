716d2fd50788730c582fced60c20b46b6d911278
Alek <alekaker@outlook.com>
2019-04-30 15:35:09 -0600

    adding and testing currentCard method

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,10 @@
 class Round {
-  constructor() {
-
+  constructor(deck) {
+    this.deck = deck || [];
+    this.turns = 0;
+  }
+  returnCurrentCard() {
+    return this.deck.card[this.turns]
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
-const Round = require('../src/Deck');
+const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 const Turn = require('../src/Turn');
 const Card = require('../src/Card')
@@ -15,6 +15,15 @@ describe('Round', function() {
   it('should be an instance of Round', function() {
     const round = new Round();
     expect(round).to.be.an.instanceof(Round);
+  });
+
+  it('should be able to return current card', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'array');
+    const turn = new Turn("object", card1)
+    const deck = new Deck([card1, card2])
+    const round = new Round(deck)
+    expect(round.returnCurrentCard()).to.equal(card1);
   });
 
 
