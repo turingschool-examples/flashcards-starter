35e5a33ece8bbe9eb2e5f1adf3e418d4343b0ef7
Alek <alekaker@outlook.com>
2019-04-30 11:11:52 -0600

    adding deck class and attaching test file

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,10 +6,15 @@ const Card = require('../src/Card')
 
 describe('Card', function() {
 
+  it('should be a function', function() {
+    const deck = new Deck();
+    expect(Deck).to.be.a('function');
+  });
 
-
-
-
+  it('should be an instance of Card', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  });
 
 
 });
