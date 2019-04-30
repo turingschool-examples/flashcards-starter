c6e54b845c9774d71348e1aa9f610eb979ee375c
RyanFlachman <ryan@flachman.net>
2019-04-30 16:28:00 -0600

    Add take turn function

--- src/Round.js
+++ src/Round.js
@@ -1,5 +1,20 @@
-class Round {
+const Turn = require('../src/Turn');
 
+class Round {
+  constructor(deck) {
+    this.deck = deck;
+    this.turns = 0;
+  }
+
+  returnCurrentCard() {
+    return this.deck.cards[0]
+  }
+
+  takeTurn(currentGuess) {
+    const turn = new Turn(currentGuess, this.deck.cards[0]);
+    this.turns++
+    
+  }
 }
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -2,10 +2,21 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Round = require('../src/Round');
+const Card = require('../src/Card')
+const Deck = require('../src/Deck')
 
 describe('Round', function() {
   it('should be a function', function() {
     const round = new Round();
     expect(Round).to.be.a('function')
+  })
+
+  it('should be able to return the current card', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1,card2,card3]);
+    const round = new Round(deck)
+    expect(round.returnCurrentCard()).to.equal(card1)
   })
 });
