5b7091355576ad1700c51083b8653719331b73fe
Antonio Fry <antoniofry2000@gmail.com>
2019-04-30 15:11:19 -0600

    add Turn test

--- src/Turn.js
+++ src/Turn.js
@@ -1,8 +1,15 @@
 class Turn {
-	constructor() {
-
+	constructor(guess, card) {
+		this.guess = guess;
+		this.card = card;
+	}
+	returnGuess() {
+		return this.guess;
 	}
 
+	returnCard() {
+		return this.card;
+	}
 }
 
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,21 +2,45 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card')
 
 describe('Turn', function() {
 
 	it('should be a function', function() {
-    const turn = new Turn();
-    expect(Turn).to.be.a('function');
+		const turn = new Turn();
+		expect(Turn).to.be.a('function');
   });
 
-  it('should be an instance of Card', function() {
-    const turn = new Turn();
-    expect(turn).to.be.an.instanceof(Turn);
-   });
+  it('should be an instance of Turn', function() {
+		const turn = new Turn();
+		expect(turn).to.be.an.instanceof(Turn);
+  });
 
-	// it('Should take an argument that represents the players guess' function() {
+	it('should take an argument that represents the players guess', function() {
+		const turn = new Turn('works');
+		expect(turn.guess).to.equal('works')
+	});
 
-	// })
+	it('should take a card as an argument', function() {
+		const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+		const turn = new Turn('object', card.id);
+		expect(turn.card).to.equal(1);
+	});
+
+	it('should return the players guess', function() {
+		const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+		const turn = new Turn('object', card);
+
+		expect(turn.returnGuess(), card.correctAnswer);
+		console.log(turn.guess)
+	});
+
+	it('should return the card', function() {
+		const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+		const turn = new Turn('object', card1);
+		
+		expect(turn.returnCard(), card1)
+		console.log(card1)
+	});
 
 });
