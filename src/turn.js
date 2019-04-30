cab9facf0989c53d85a2c80ec60b1b4b32357ee7
Katie Williams <kawilliams8@gmail.com>
2019-04-29 18:04:21 -0600

    Add three Turn tests

---
+++ src/turn.js
@@ -0,0 +1,11 @@
+class Turn {
+    constructor(card, guess) {
+        this.card = card;
+        this.guess = guess;
+        // returnGuess() {
+        //     // return this.answer;
+        // }
+    }
+}
+
+module.exports = Turn;
