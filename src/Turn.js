98308b44eb3b804cf7739f64a74a7fc3a75bed40
Jev Forsberg <jev.forsberg@gmail.com>
2019-04-30 16:08:41 -0600

    Update evaluateGuess method

--- src/Turn.js
+++ src/Turn.js
@@ -13,15 +13,11 @@ class Turn {
   }
 
   evaluateGuess() {
-    // let answer;
     if (this.currentCard.correctAnswer === this.userGuess) {
-      // answer = true;
       return true;
     } else {
-      // answer = false;
       return false;
     }
-    // giveFeedback(answer);
   }
 
   giveFeedback(answer) {
