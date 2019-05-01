638494e3552f9459d3e7751244d8505eedb0f567
Antonio Fry <antoniofry2000@gmail.com>
2019-04-30 20:55:19 -0600

    add round class and test file

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,18 +3,28 @@ const expect = chai.expect;
 
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
-const Deck = require('../src/Deck')
+const Deck = require('../src/Deck');
 
 describe('Deck', function() {
 
 	it('should be a function', function() {
 		const deck = new Deck();
 		expect(Deck).to.be.a('function');
-	})
+	});
 
 	it('should be an instance of Deck', function() {
 		const deck = new Deck();
 		expect(deck).to.be.an.instanceof(Deck);
-	})
+	});
+
+	it('should take in cards as an array', function() {
+		const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+		const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+		const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+		
+		const deck = new Deck([card1, card2, card3]);
+
+		expect(deck.countCards()).to.equal(3);
+	});
 
 });
