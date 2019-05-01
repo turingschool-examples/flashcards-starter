3757b50235b67697c513325f1520242bc755e962
Nathan <nathan.froeh@gmail.com>
2019-05-01 12:59:10 -0600

    Add should be instantiated with a Deck test

---
+++ test/Round-test.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Data = require('../src/Data');
+const Round = require('../src/Round');
+
+
+describe('Round', function() {
+
+  it('should be a function', function() {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', function() {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  }); 
+
+  it('should be instantiated with a Deck', () => {
+    const deck = new Deck(Data.prototypeData);
+    const round = new Round(deck);
+    expect(round.deck).to.equal(deck)
+  })
+
+  it.skip('should return current card being played', () => {
+    const round = new Round;
+    expect(round.returnCurrentCard()).to.equal()
+
+  })
+
+});
