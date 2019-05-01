34f9fd685fed876543c5baf37097621a56c3786f
Antonio Fry <antoniofry2000@gmail.com>
2019-04-30 21:45:02 -0600

    add tests and methods to round class files

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,6 +1,9 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
 describe('Round', function() {
@@ -13,6 +16,30 @@ describe('Round', function() {
 	it('should be an instance of Round', function() {
 		const round = new Round();
 		expect(round).to.be.an.instanceof(Round)
-	})
+	});
+
+	it('should have the first card of the deck be the current card', function() {
+		const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+		const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+		const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+		const deck = new Deck([card1, card2, card3]);
+		const round = new Round(deck.cards, deck.cards[0]);
+
+		expect(round.currentCard).to.equal(deck.cards[0]);
+	});
+
+	it('should return the current card being played', function() {
+		const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+		const deck = new Deck([card1]);
+		const round = new Round(deck.cards, deck.cards[0]);
+		
+		expect(round.currentCard).to.equal(deck.cards[0]);
+
+		round.returnCurrentCard();
+
+		expect(round.returnCurrentCard()).to.equal(deck.cards[0])
+	});
+
+	it('should ')
 
 })
