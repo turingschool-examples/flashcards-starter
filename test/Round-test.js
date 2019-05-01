f456e8460b72d6b25fb880d7c67321ba5d0ab045
Jev Forsberg <jev.forsberg@gmail.com>
2019-05-01 15:43:13 -0600

    Add calculatePercentCorrect method

--- test/Round-test.js
+++ test/Round-test.js
@@ -9,7 +9,6 @@ const Card = require('../src/Card');
 describe('Round', function() {
 
   it('should be a function', function() {
-    const round = new Round();
     expect(Round).to.be.a('function');
   });
 
@@ -18,7 +17,7 @@ describe('Round', function() {
     expect(round).to.be.an.instanceof(Round);
   }); 
 
-  it('should return the current card from the deck', function() {
+  it('should have a methods that returns the current card from the deck', function() {
     const card1 = new Card(1, 'Is Jev cool?', ['yeah!', 'fuck yeah!', 'hell yeah, there friend!'], 'fuck yeah!');
     const card2 = new Card(2, 'Does Jev like burgers?', ['yeah!', 'fuck yeah!', 'hell yeah, there friend!'], 'hell yeah, there friend!');
     const cards = [card1, card2];
@@ -29,10 +28,48 @@ describe('Round', function() {
     expect(round.returnCurrentCard()).to.equal(card2);
   });
 
-  it('should increment this.turns by one', function() {
+  it('should have a methods that increments Round.turns by one after takeTurn() is invoked', function() {
     const round = new Round();
     expect(round.turns).to.equal(0);
     round.turnCounter();
     expect(round.turns).to.equal(1);
   }); 
+
+  it('should have a method that creates a new instance of the Turn class', function() {
+    const card1 = new Card(1, 'Is Jev cool?', ['yeah!', 'fuck yeah!', 'hell yeah, there friend!'], 'fuck yeah!');
+    const card2 = new Card(2, 'Does Jev like burgers?', ['yeah!', 'fuck yeah!', 'hell yeah there, friend!'], 'hell yeah there, friend!');
+    const cards = [card1, card2];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    expect(round.createNewTurn('yeah!')).to.be.an.instanceof(Turn);
+  });
+
+  it('should have a method that returns "Correct!" if the guess is correct', function() {
+    const card1 = new Card(1, 'Is Jev cool?', ['yeah!', 'fuck yeah!', 'hell yeah, there friend!'], 'fuck yeah!');
+    const card2 = new Card(2, 'Does Jev like burgers?', ['yeah!', 'fuck yeah!', 'hell yeah there, friend!'], 'hell yeah there, friend!');
+    const cards = [card1, card2];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    expect(round.takeTurn('fuck yeah!')).to.equal('Correct!');
+  });
+
+  it('should have a method that returns "Incorrect!" if the guess is correct', function() {
+    const card1 = new Card(1, 'Is Jev cool?', ['yeah!', 'fuck yeah!', 'hell yeah, there friend!'], 'fuck yeah!');
+    const card2 = new Card(2, 'Does Jev like burgers?', ['yeah!', 'fuck yeah!', 'hell yeah there, friend!'], 'hell yeah there, friend!');
+    const cards = [card1, card2];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    expect(round.takeTurn('yeah!')).to.equal('Incorrect!');
+  });
+
+  it('should have a method that returns the percentage of correct answers', function() {
+    const card1 = new Card(1, 'Is Jev cool?', ['yeah!', 'fuck yeah!', 'hell yeah, there friend!'], 'fuck yeah!');
+    const card2 = new Card(2, 'Does Jev like burgers?', ['yeah!', 'fuck yeah!', 'hell yeah there, friend!'], 'hell yeah there, friend!');
+    const cards = [card1, card2];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    round.takeTurn('yeah!');
+    round.takeTurn('hell yeah there, friend!')
+    expect(round.calculatePercentCorrect()).to.equal('50%');
+  });
 });
