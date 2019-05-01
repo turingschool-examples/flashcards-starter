9d5caf925c647a1c5f2f42177b2438479d34187b
Nathan <nathan.froeh@gmail.com>
2019-04-30 18:42:43 -0600

    Add countCards test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,8 +3,9 @@ const expect = chai.expect;
 
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
+const Data = require('../src/Data')
 
-describe('Turn', function() {
+describe('Deck', function() {
 
   it('should be a function', function() {
     const deck = new Deck();
@@ -15,5 +16,17 @@ describe('Turn', function() {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   }); 
+
+  it('should be instantiated with an array of Card objects', () => {
+    const deck = new Deck(Data.prototypeData);
+    expect(deck.cards).to.be.a('array')
+    expect(deck.cards[0]).to.be.a('object')
+  });
+
+  it('should return number of cards in deck', () => {
+    const deck = new Deck(Data.prototypeData);
+    expect(deck.countCards()).to.equal(Data.prototypeData.length)
+  });
+
 
 });
