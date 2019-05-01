c066142cc103b0cf14c0680f55218bc71903f88f
Kayla <kayla.larson1990@gmail.com>
2019-04-30 19:01:36 -0600

    Add testing to turn class

--- src/Turn.js
+++ src/Turn.js
@@ -1,22 +1,23 @@
 class Turn {
-  constructor() {
-
+  constructor(guess, card) {
+    this.guess = guess;
+    this.card = card
   }
 
   returnGuess() {
-
+    return this.guess;
   }
 
   returnCard() {
-
+    return this.card;
   }
 
   evaluateGuess() {
-
+    return this.guess === this.card.correctAnswer;
   }
 
   giveFeedback() {
-    
+    return this.guess === this.card.correctAnswer ? "correct!" : "incorrect!";
   }
 }
 
--- test/Game-test.js
+++ test/Game-test.js
@@ -2,8 +2,11 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Game = require('../src/Game');
+const Card = require('../src.Card')
 
 describe('Game', function() {
+
+  
 
   
 });
