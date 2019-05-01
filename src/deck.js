348c1bcbe22e097d9454b55ed3ad778576e2b7ad
Katie Williams <kawilliams8@gmail.com>
2019-04-30 20:23:00 -0600

    Draft in the Deck class/test

--- src/deck.js
+++ src/deck.js
@@ -1,5 +1,12 @@
 class Deck {
-    constructor() {
-        
-    }
+  constructor(cards) {
+    this.cards = cards || [];
+    this.count = 0;
+  }  
+
+  countCards() {
+    //count number of cards instances in the array
+    //increment this.count
+    //return the this.count number
+  }
 }
