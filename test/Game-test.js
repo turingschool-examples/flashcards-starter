d6fc6f688e0aa29ce7135da4578f0aff46ae3576
RyanFlachman <ryan@flachman.net>
2019-05-02 11:17:32 -0600

    Add deck argument to game

--- test/Game-test.js
+++ test/Game-test.js
@@ -2,6 +2,9 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Game = require('../src/Game')
+const Round = require('../src/Round')
+const Card = require('../src/Card')
+const Deck = require('../src/Deck')
 
 describe('Game', function() {
 
@@ -10,10 +13,27 @@ describe('Game', function() {
     expect(game.start).to.be.a('function')
   });
 
-  it('should keep track of current round', function() {
+  it('the current round should be an instance of Round', function() {
     const game = new Game();
     game.start();
-    expect(game.currentRound.deck.cards.length).to.equal(30)
+    expect(game.currentRound).to.be.a('object')
+    expect(game.currentRound).to.be.an.instanceOf(Round)
   });
 
+  it('should defualt the deck to the first set of prototypeQuestion', function() {
+    const game = new Game();
+    game.start()
+    expect(game.currentRound.deck.cards.length).to.equal(30)
+  })
+
+  it('should be able to take deck argument in the start function', function() {
+    const game = new Game();
+    const newDeck = [  
+      { "id": 1, "question": "first question", "answers": ["answer 1", "answer 2", "answer 3"], "correctAnswer": "answer 1"},   
+      { "id": 2, "question": "second question", "answers": ["answer 1", "answer 2", "answer 3"], "correctAnswer": "answer 2"},   
+      { "id": 3, "question": "third question", "answers": ["answer 1", "answer 2", "answer 3"], "correctAnswer": "answer 3" },   
+      { "id": 4, "question": "fourth question", "answers": ["answer 1", "answer 2", "answer 3"], "correctAnswer": "answer 1"}]
+    game.start(newDeck)
+    expect(game.currentRound.deck.cards.length).to.equal(4)
+  })
 })
