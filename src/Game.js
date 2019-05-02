56bc31243b56184780c0e76b3dce4715191e4224
Kayla <kayla.larson1990@gmail.com>
2019-05-02 15:58:30 -0600

    Add game functionality and tests to Game class

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,28 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = {};
+  }
+
+  start() {
+    const cards = prototypeQuestions.map(function(element){
+      let card = new Card(element.id, element.question, element.answers, element.correctAnswer)
+      return card;
+    });
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    this.currentRound = round;
+    this.printMessage(deck, round);
+    this.printQuestion(round);
+
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
