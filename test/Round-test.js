860eaf42a31a9e8028f74c7e52a4f5ce85302ea3
Nathan <nathan.froeh@gmail.com>
2019-05-02 13:01:59 -0600

    Add confirm number of cards in deck test

--- test/Round-test.js
+++ test/Round-test.js
@@ -108,4 +108,15 @@ describe('Round', function() {
     expect(round.correctGuesses).to.equal(2)
   });
 
+  it('should be number of cards in deck', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.deck.cards.length).to.equal(3)
+  });
+
+  
+
 });
