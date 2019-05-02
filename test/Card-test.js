a596d4c816f233d2fafc3b59f1605abbd525ef42
RyanFlachman <ryan@flachman.net>
2019-05-02 13:32:34 -0600

    Add turn test file

--- test/Card-test.js
+++ test/Card-test.js
@@ -2,7 +2,6 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Card = require('../src/Card');
-const Turn = require('../src/Turn');
 
 describe('Card', function() {
   const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
@@ -27,51 +26,3 @@ describe('Card', function() {
     expect(card.correctAnswer).to.equal('object');
   });
 });
-
-describe('Turn', function() {
-  const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
-  const turn = new Turn('my guess', card);
-
-  it('should be a function', function() {
-    expect(Turn).to.be.a('function')
-  });
-
-  it('should be an instance of Turn', function() {
-    expect(turn).to.be.an.instanceOf(Turn)
-  });
-
-  it('should store the user\'s guess', function() {
-    expect(turn.userGuess).to.equal('my guess')
-  });
-
-  it('should store the current card object', function() {
-    expect(turn.currentCard).to.equal(card)
-  });
-  
-  it('should have a function called returnGuess', function() {
-    expect(turn.returnGuess).to.be.a('function')
-  });
-
-  it('should be able to return the users guess', function() {
-    expect(turn.returnGuess()).to.equal('my guess')
-  });
-
-  it('should be able to return the card object', function() {
-    expect(turn.returnCard()).to.equal(card)
-  });
-
-  it('should evaluate the current guess and return a boolean', function() {
-    const turn1 = new Turn('my guess', card);
-    const turn2 = new Turn('object', card)
-    expect(turn1.evaluateGuess()).to.equal(false)
-    expect(turn2.evaluateGuess()).to.equal(true)
-  });
-
-  it('should be able to give feedback depending on if the answer is correct', function() {
-    const turn1 = new Turn('my guess', card);
-    const turn2 = new Turn('object', card)
-    expect(turn1.giveFeedback()).to.equal('incorrect!')
-    expect(turn2.giveFeedback()).to.equal('correct!')
-  });
-});
-
