b2ece7863b89d5e226e6115b94f4578d9c472830
Vinton Teo <taulago@ymail.com>
2019-05-04 13:33:27 -0600

    Create test for Turn class.

---
+++ test/Turn-test.js
@@ -0,0 +1,55 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it('should be instantiated with two arguments', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+    expect(turn.guess).to.equal('pug');
+    expect(turn.card).to.equal(card);
+  });
+
+  it('should be able to return the guess', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+    expect(turn.returnGuess()).to.equal('pug');
+  });
+
+  it('should be able to return the card', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+    expect(turn.returnCard()).to.equal(card);
+  });
+
+  it('should return true if the guess is correct', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn1 = new Turn('sea otter', card);
+    const turn2 = new Turn('pug', card);
+    expect(turn1.evaluateGuess()).to.equal(true);
+    expect(turn2.evaluateGuess()).to.equal(false);
+  });
+
+  it('should return a response based on guess evaluation', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn1 = new Turn('sea otter', card);
+    const turn2 = new Turn('pug', card);
+    expect(turn1.evaluateGuess()).to.equal(true);
+    expect(turn1.giveFeedback()).to.equal('correct!');
+    expect(turn2.evaluateGuess()).to.equal(false);
+    expect(turn2.giveFeedback()).to.equal('incorrect!');
+  });
+});
