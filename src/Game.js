d8af6b9be52bc0c5c99445cdbd488d2d167adda0
Taylor Jordan <taylornoel418@icloud.com>
2019-05-04 16:58:09 -0600

    Add start method on Game

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,20 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Card = require('../src/Card');
+const Deck = require('../src/Card');
+const Round = require('../src/Round');
+
 class Game {
-  constructor() {}
+  constructor(currentRound) {
+  	this.currentRound = currentRound;
+  }
+
+  start() {
+  	const card = new Card();
+  	const deck = new Deck(card);
+  	const round = new Round(deck);
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
--- test/Game-test.js
+++ test/Game-test.js
@@ -5,19 +5,53 @@ const Card = 	require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 const Turn = require('../src/Round');
+const Game = require('../src/Game')
 
-describe('Game', function() {
+describe.only('Game', function() {
 
 	it('should be a function', function() {
 		const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
 		const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
 		const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
 		const deck = new Deck([card1, card2, card3]);
-		const round = new Round(deck);		const game = new Game()
-	})
+		const round = new Round(deck);
+		const game = new Game(round);
+
+		expect(Game).to.be.a('function');
+	});
+
+	it('should be an instance of Game', function() {
+		const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+		const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+		const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+		const deck = new Deck([card1, card2, card3]);
+		const round = new Round(deck);
+		const game = new Game(round);
+
+    expect(game).to.be.an.instanceof(Game);
+	});
 
 	it('should keep track of the current round', function() {
+		const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+		const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+		const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+		const deck = new Deck([card1, card2, card3]);
+		const round = new Round(deck);
+		const game = new Game(round);
 
+		expect(game.currentRound).to.equal(round)
+	});
+
+	it('should create instances of Cards, a Deck, and a current Round', function() {
+		let game = new Game();
+		game.start();
+		const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+		const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+		const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+		const deck = new Deck([card1, card2, card3]);
+		const round = new Round(deck);
+		
+		expect(game).to.equal(game(round));
 	})
 
 })
