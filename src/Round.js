5f1ebf75833b8790b94e1afad1cf8bb3c43286d1
Ann Cerveny <cervann11@gmail.com>
2019-05-04 16:08:51 -0600

    Amend functionality of takeTurn method of Round

--- src/Round.js
+++ src/Round.js
@@ -7,18 +7,24 @@ class Round {
     this.turns = 0;
   }
   returnCurrentCard() {
+    console.log(this.incorrectGuesses)
     return this.deck[this.turns];
   }
   takeTurn(guess) {
     const turn = new Turn(guess, this.deck[this.turns]);
     this.turns++;
-    if (turn.evaluateGuess() === null) {
-      this.incorrectGuesses.push()
+    if (!turn.evaluateGuess()) {
+      this.incorrectGuesses.push(turn.card.id)
       return turn.giveFeedback()
     } else {
       return turn.giveFeedback();
     }
   }
+  calculatePercentCorrect() {
+    return (this.incorrectGuesses.length / this.turns) * 100
+  }
+
 }
 
 module.exports = Round;
+
