5f1a0efed2e06098f7164a1788fa2a914ed3597e
Vinton Teo <taulago@ymail.com>
2019-05-04 21:39:11 -0600

    Add new instance of Game and invoke start method.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const http = require('http');
 let app = http.createServer();
+const Game = require('./src/Game');
+
+let game = new Game();
+game.start();
 
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');  
