553aa1547394c227a1c2c4c3fa0a8f9588861d55
Vinton Teo <taulago@ymail.com>
2019-05-05 00:40:41 -0600

    Create function to instantiate Game and run start method. Create module export.

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const http = require('http');
 let app = http.createServer();
 const Game = require('./src/Game');
 
-let game = new Game();
-game.start();
+function runner() {
+  let game = new Game();
+  game.start();
+}
+
+runner();
 
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');  
 console.log('Node server running on port 3000'); 
+
+module.exports.runner = runner;
