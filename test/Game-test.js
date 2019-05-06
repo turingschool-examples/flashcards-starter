e4b44a184d741cee7e33bd5bd80db4ce05e56112
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-05 18:54:35 -0600

    created test for Game class

---
+++ test/Game-test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Game = require('../src/Game');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+const Round = require('../src/Round');
+
+describe('Game', function () {
+  
+  it('should be a function', function () {
+    const game = new Game();
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Game', function () {
+    const game = new Game();
+    expect(game).to.be.an.instanceof(Game);
+  }); 
+
+  it('should start a new game', function () {
+       
+  })
+});
