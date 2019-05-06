08b26a7df1bad7e394eb817a673701c377721966
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-06 12:47:32 -0600

    refactored round test to be more SRP

--- test/Round-test.js
+++ test/Round-test.js
@@ -25,26 +25,49 @@ describe('Round', function () {
     expect(round.returnCurrentCard()).to.equal(deck.card[0])
   })
 
-  it('should update turns count, evaluates guesses, gives feedback, and stores ids of incorrect guesses', function () {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+  it('should update turns count', function () {
+    const card1 = new Card();
+    const card2 = new Card();
+    const card3 = new Card();
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
     const turn = new Turn(card1, 'sea otter');
 
     expect(round.turns).to.equal(0);
-    expect(round.returnCurrentCard()).to.equal(deck.card[0]);
-    expect(round.takeTurn('sea otter')).to.equal('correct!');
+    round.takeTurn()
     expect(round.turns).to.equal(1);
-    expect(round.returnCurrentCard()).to.equal(deck.card[1])
-    round.takeTurn('spleen')
-    expect(round.incorrectGuesses[0]).to.equal(2)
-    expect(round.returnCurrentCard()).to.equal(deck.card[2])
-    round.takeTurn('Fitzgerald');
-    expect(round.incorrectGuesses.length).to.equal(1)
-
+    round.takeTurn()
+    expect(round.turns).to.equal(2);
+    round.takeTurn()
+    expect(round.turns).to.equal(3)
   })
+
+  it('should evaluate guesses, and give feedback', function () {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(round.takeTurn('sea otter')).to.equal('correct!');
+    expect(round.takeTurn('spleen')).to.equal('incorrect!')
+    expect(round.incorrectGuesses[0]).to.equal(card2.id)
+    expect(round.takeTurn('Fitzgerald')).to.equal('correct!');
+  })
+
+  it('should store ids of incorrect guesses', function () {
+    const card1 = new Card(1, 'question 1', ['incorrect', 'incorrect', 'correctAnswer'], 'correctAnswer')
+    const card2 = new Card(2, 'question 2', ['incorrect', 'incorrect', 'correctAnswer'], 'correctAnswer')
+    const card3 = new Card(3, 'question 3', ['incorrect', 'incorrect', 'correctAnswer'], 'correctAnswer')
+    const deck = new Deck([card1, card2, card3])
+    const round = new Round(deck)
+
+    round.takeTurn('correctAnswer')
+    round.takeTurn('incorrect answer')
+    round.takeTurn('correctAnswer')
+    expect(round.incorrectGuesses).to.deep.equal([card2.id])
+  })
+
   it('should calculate and return the percentage of correct guesses', function () {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
@@ -58,6 +81,7 @@ describe('Round', function () {
     round.takeTurn('Fitzgerald')
     expect(round.calculatePercentCorrect()).to.equal(66)
   })
+
   it('should print a message with the percent correct to the console once you run out of cards in the deck', function () {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
@@ -71,18 +95,6 @@ describe('Round', function () {
     round.takeTurn('William')
 
     assert.isFunction(round.endRound)
-
-  })
-
-  it('will end the round once there are no more cards left in the deck', function () {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-    const turn = new Turn(card1, 'sea otter');
-
-
 
   })
 })
