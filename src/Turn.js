f5b33f65cc5d8b24d27ebb6f56ed08e452373cc4
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-05 18:50:26 -0600

    made new turn test and class file.

---
+++ src/Turn.js
@@ -0,0 +1,21 @@
+class Turn {
+  constructor(card, guess) {
+    this.card = card;
+    this.guess = guess;
+    
+  }
+  returnGuess() {
+    return this.guess;
+  }
+  returnCard() {
+    return this.card;
+  }
+  evaluateGuess() {
+    return this.card.correctAnswer === this.guess;
+  }
+  giveFeedback() {
+    return this.evaluateGuess() ? 'correct!' : 'incorrect!';
+  }
+}
+
+module.exports = Turn;
