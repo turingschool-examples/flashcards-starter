d00d1e090d4342630b30e4e7023c433c103bbacc
Brandy Mello <brandyamello@gmail.com>
2019-06-24 16:47:40 -0600

    Add card class

---
+++ src/Card.js
@@ -0,0 +1,10 @@
+class Card {
+  constructor(id, question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+
+  }
+}
+
+module.exports = Card;
