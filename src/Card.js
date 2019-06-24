4f8fc21f79bcf9131d1f5593a5da4fa4c078d7bc
Alan Turing <anderson.gregory.mail@gmail.com>
2019-06-24 16:21:31 -0600

    Initial Commit

---
+++ src/Card.js
@@ -0,0 +1,9 @@
+class Card {
+  constructor (number, question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  }
+}
+
+module.exports = Card
