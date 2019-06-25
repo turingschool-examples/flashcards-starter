8478d80cc482ed13b61e5c4141f4c2645fbfaec6
Alan Turing <anderson.gregory.mail@gmail.com>
2019-06-24 21:14:36 -0600

    Add Deck test and file

---
+++ src/Deck.js
@@ -0,0 +1,15 @@
+class Deck {
+  constructor (cards) {
+    this.cardDeck = cards || [];
+  }
+
+  countCards(){
+    return this.cardDeck.length;
+  }
+
+  addCardToDeck(card) {
+    this.cardDeck.push(card)
+  }
+}
+
+module.exports = Deck
