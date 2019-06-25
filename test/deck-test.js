2ad8da74d238267d473194f8241e017d3b03b3bb
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-06-25 08:15:07 -0600

    All deck tests pass

---
+++ test/deck-test.js
@@ -0,0 +1,32 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+
+describe('Deck', function() {
+
+	it('should be a function', function() {
+    	const deck = new Deck();
+   		expect(Deck).to.be.a('function');
+  	});
+
+  	it('should be an instance of Deck', function() {
+    	const deck = new Deck();
+    	expect(deck).to.be.an.instanceof(Deck);
+  	});
+
+  	it('should have a method to determine the number of cards within the deck', function() {
+    	const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+		const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+		const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+		const deck = new Deck([card1, card2, card3]);
+
+		expect(deck.countCards()).to.be.equal(3);
+  	});
+
+});
+
+
