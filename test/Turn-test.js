270684b2b403a1b599505a33a75e10fe913dde4c
Jessie Le-Ho <jessiele-ho@Jessies-MacBook-Pro.local>
2019-06-25 20:27:44 -0600

    Finish turn

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -16,35 +16,29 @@ describe('Turn', function(){
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-  it.skip('should return the guess', function(){
+  it('should return the guess', function(){
     
     const card = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method','accessor method','iteration method'],'mutator method');
-    const turn = new Turn('mutator method',card);
+    const turn = new Turn('mutator method', card);
     expect(turn.returnGuess()).to.equal('mutator method');
   });
 
-  it.skip('should return the card', function(){
-    const card = new Card(//id, 'question in a string','array of answers','correct answer');
-    const turn = new Turn();
-    expect(turn.returnCard()).to.deep.equal(//card#);
+  it('should return the card', function(){
+    const card = new Card(14, 'Which iteration method can turn an array into a single value of any data type?', ['reduce()', 'map()', 'filter()'],'reduce()');
+    const turn = new Turn('reduce()', card);
+    expect(turn.returnCard()).to.deep.equal(card);
   });
 
-  it.skip('should ', function(){
-    const card = new Card();
-    const turn = new Turn();
-    expect(turn.returnGuess).to.equal(//string);
+  it('should return boolean if user matches the answer', function(){
+    const card = new Card(16, 'What does the callback function for reduce() return?', ['the accumulator', 'the current element', 'the initializer'],'the accumulator');
+    const turn = new Turn('the accumulator', card);
+    expect(turn.evaluateGuess()).to.equal(true);
   });
 
-  it.skip('should ', function(){
-    const card = new Card();
-    const turn = new Turn();
-    expect(turn.returnGuess).to.equal(//string);
-  });
-
-    it.skip('should ', function(){
-    const card = new Card();
-    const turn = new Turn();
-    expect(turn.returnGuess).to.equal(//string);
+  it('should return either incorrect or correct based the guess is correct or not', function(){
+    const card = new Card(11, 'Which iteration method returns a new array with all elements that match a given condition?', ['filter()', 'find()', 'map()'],'filter()');
+    const turn = new Turn('filter()', card);
+    expect(turn.giveFeedback()).to.equal('correct');
   });
 
 });
