1d4117841973e82b3ba896516812c1976063481c
Amy Rippeto <amyrippeto@gmail.com>
2019-06-26 08:56:49 -0600

    Start writing round tests

---
+++ test/Round-test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+
+describe('Round', function() {
+
+  it('should be a function', function() {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  });
+
+  it('should', function() {
+    const round = new Round();
+    
+  })
+})
