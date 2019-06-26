31306232dd11f4a87044b2fa8db3cb172a7ebe5a
Amy Rippeto <amyrippeto@gmail.com>
2019-06-25 19:49:40 -0600

    Complete deck test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,17 +1,27 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Deck = require('../src/Card');
-const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const data = require('../src/data');
+const allCards = data.prototypeData;
 
 describe('Deck', function(){
 
   it('should be a function', function() {
     const deck = new Deck();
+    
     expect(Deck).to.be.a('function');
   });
 
   it('should be an instance of Deck', function(){
+    const deck = new Deck(allCards);
+    
+    expect(deck).to.be.an.instanceOf(Deck);
+  });
 
+  it('should count number of cards in deck', function() {
+    const deck = new Deck(allCards);
+   
+    expect(deck.countCards()).to.equal(30);
   });
 });
