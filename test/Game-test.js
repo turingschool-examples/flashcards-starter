92ad13baaa4afcbd00644da3761dd34fd1d3be0a
dawnlunacy <alanturing@example.com>
2019-06-27 14:53:19 -0600

    Add game is set up

--- test/Game-test.js
+++ test/Game-test.js
@@ -10,27 +10,25 @@ const prototypeData = require('../src/data.js')
 
 var card, deck, round, game;
 
+
 describe('Game', function() {
 
   it('should be a function', () => {
-    const game = new Game();
+    game = new Game();
     expect(Game).to.be.a('function');
   });
 
   it('should be an instance of game', () => {
-    const game = new Game();
+    game = new Game();
     expect(game).to.be.an.instanceof(Game);
   });
+
   describe('currentRound', () => { 
   it('should keep track of the current round', () => {
-    const card = new Card(prototypeData[1]);
-    console.log(prototypeData)
-    console.log(prototypeData[{}])
-    const deck = new Deck(card);
-    const round = new Round(deck);
-    const game = new Game(round);
-    
-    expect(game.currentRound()).to.eql(1);
+    game = new Game();
+
+    game.startRound();
+    expect(game.currentRound).to.eql(1);
   });
 });
 });
