5f717b2c8e39568c97cfda2d08d8ee590a8c3d56
davidagitlen <davidagitlen@gmail.com>
2019-06-27 23:36:37 -0600

    Refactor tests with beforeEach

--- test/Game-test.js
+++ test/Game-test.js
@@ -7,27 +7,29 @@ const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 const Game = require('../src/Game');
 
+let card1, card2, card3, turn, deck, round, game; 
+
 describe('Game', function(){
 
+	beforeEach(function(){
+		card1 = new Card({id: 1, question: 'What is Robbie\'s favorite animal', answers: ['sea otter', 'pug', 'capybara'], correctAnswer: 'sea otter'});
+		card2 = new Card({id: 14, question: 'What organ is Khalid missing?', answers: ['spleen', 'appendix', 'gallbladder'], correctAnswer: 'gallbladder'});
+		card3 = new Card({id: 12, question: 'What is Travis\'s middle name?', answers: ['Lex', 'William', 'Fitzgerald'], correctAnswer: 'Fitzgerald'});
+		deck = new Deck([card1, card2, card3]);
+		turn = new Turn();
+		round = new Round(deck);
+		game = new Game();
+	});
+
 	it('should be a function', function(){
-		const game = new Game();
 		expect(Game).to.be.a('function');
 	});
 
 	it('should be an instance of Game', function(){
-		const game = new Game();
 		expect(game).to.be.an.instanceOf(Game);
 	});
 
 	it('should create cards and put them in a deck', function(){
-		const card1 = new Card({id: 1, question: 'What is Robbie\'s favorite animal', answers: ['sea otter', 'pug', 'capybara'], correctAnswer: 'sea otter'});
-		const card2 = new Card({id: 14, question: 'What organ is Khalid missing?', answers: ['spleen', 'appendix', 'gallbladder'], correctAnswer: 'gallbladder'});
-		const card3 = new Card({id: 12, question: 'What is Travis\'s middle name?', answers: ['Lex', 'William', 'Fitzgerald'], correctAnswer: 'Fitzgerald'});
-		const deck1 = new Deck([card1, card2, card3]);
-		const turn = new Turn();
-		const round1 = new Round(deck1);
-		const game = new Game();
-
 		// game.printMessage(deck1, round1);
 		
 	});
