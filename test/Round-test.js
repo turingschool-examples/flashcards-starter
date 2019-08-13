aabf882b64dfc40ecbfd2bf4fb98163acf77d14e
“Elyse <elysemyers1@gmail.com>
2019-08-12 21:36:47 -0600

    Adds testing functionality for turns property and currentCard method

---
+++ test/Round-test.js
@@ -0,0 +1,38 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
+describe('Round', function () {
+
+  it('should be a function', function () {
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of a Round', function () {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  }); 
+
+  it('should start with zero turns', function () {
+    const round = new Round();
+    expect(round.turns).to.equal(0);
+  });
+
+  it('should return the current card', function () {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, 'What type of prototype method directly modifies the existing array ?', 'mutator method', 'accessor method', 'iteration method', 'mutator method');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.currentCard()).to.equal({
+      id: 1,
+      question: 'What allows you to define a set of related information using key-value pairs?',
+      answers: ['object', 'array', 'function'],
+      correctAnswer: 'object'
+    })
+  })
+});
