0e5920bf3157d9ffebd15f31e4a00e2e869046b8
“Elyse <elysemyers1@gmail.com>
2019-08-12 20:26:02 -0600

    Adds class and constructor to Turn.js file and stubs required methods

---
+++ src/Turn.js
@@ -0,0 +1,26 @@
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess;
+    this.card = card;
+  }
+
+  returnGuess() {
+
+  }
+
+  returnCard() {
+
+  }
+
+  evaluateGuess() {
+
+  }
+
+  giveFeedback() {
+
+  }
+}
+
+
+
+module.exports = Turn;
