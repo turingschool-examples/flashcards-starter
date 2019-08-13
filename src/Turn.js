5f67c0b9f2df2c19c36211d1f9b8f53c58afc2bc
VPAbraham <vpa456@gmail.com>
2019-08-12 20:44:15 -0600

    Create turn class

---
+++ src/Turn.js
@@ -0,0 +1,21 @@
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess;
+    this.card = card;
+  }
+
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.card;
+  }
+
+  evaluateGuess() {
+    return (this.guess === this.card.correctAnswer ? true : false);
+  }
+
+  giveFeedback() {
+    return (this.guess === this.card.correctAnswer ? 'Correct!' : 'Incorrect!');
+}
