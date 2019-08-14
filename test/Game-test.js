c93650b7b1e0c227e6126be7fede930f118f4227
“Elyse <elysemyers1@gmail.com>
2019-08-13 21:31:55 -0600

    Adds functionality to test Game class as function and instance

---
+++ test/Game-test.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Game = require('../src/Game')
+
+describe('Game', function () {
+
+  it('should be a function', function () {
+    expect(Game).to.be.a('function')
+  }); 
+
+  it('should be an instance of a Game', function () {
+    const game = new Game();
+    expect(game).to.be.an.instanceof(Game);
+  }); 
+});
