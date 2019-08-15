2c222e06cc61457d8e762bc83cd4399b7f3ef574
“Foster <fosterataylor@yahoo.com>
2019-08-15 14:28:20 -0600

    Finished 2nd and 3rd iteration

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,14 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('../src/Round');
+// const Card = require('../src/Card');
+const Deck = require('../src/Deck');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = 0;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -13,6 +18,31 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
-}
+
+  start() {
+    this.currentRound++;
+    var deck = new Deck(prototypeQuestions);
+    var round = new Round(deck);
+    this.printMessage(deck,round);
+    this.printQuestion(round);
+    console.log(deck)
+  }
+ }
+
 
 module.exports = Game;
+
+// Should keep track of the currentRound
+
+// start: method that starts everything
+// Creates Cards
+
+// Puts Cards in a Deck
+
+// Creates a new Round using the Deck
+
+// invokes printMessage to display the message in the CLI
+
+// invokes printQuestion to kick off our helper functions that allow interaction via the CLI
+
+// Note: The helper functions are all fleshed out and fit to work with classes/methods that meet the requirements in the past iterations.
--- src/Round.js
+++ src/Round.js
@@ -1,13 +1,35 @@
+const Turn = require('../src/Turn');
+
 class Round {
   constructor(deck) {
-    this.deck = deck;
+    this.deck = deck.cards;
     this.turns = 0;
+    this.currentCard;
     this.incorrectGuesses = [];
   }
 
   returnCurrentCard() {
-    return this.deck.cards[this.turns];
+   this.currentCard = this.deck[this.turns];
+   return this.currentCard;
     
+  }
+
+  takeTurn(guess) {
+    let turn = new Turn(guess, this.returnCurrentCard())
+    this.turns ++;
+    if(turn.evaluateGuess()=== false) {
+      this.incorrectGuesses.push(this.currentCard.id)
+    }
+    return turn.giveFeedback();
+  }
+
+  calculatePercentCorrect() {
+    return Math.round(100-((this.incorrectGuesses.length/this.turns) * 100))
+
+  }
+
+  endRound() {
+    return 'Round over! You answered ' + this.calculatePercentCorrect() + '%' + ' of the questions correctly!'
   }
 }
 
@@ -17,3 +39,15 @@ class Round {
 
 
 module.exports = Round;
+
+// endRound: method that prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!’
+
+// takeTurn: method that updates turns count, evaluates guesses, gives feedback, and stores ids of incorrect guesses
+
+// When a guess is made, a new Turn instance is created.
+
+// The turns count is updated, regardless of whether the guess is correct or incorrect
+
+// The next card becomes current card
+// Guess is evaluated/recorded. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses
+// Feedback is returned regarding whether the guess is incorrect or correct
