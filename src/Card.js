bc0f17d2a79ce451b8c17e10c9814a31ac2d3695
sertmer <scottertmer@gmail.com>
2019-09-30 18:21:44 -0600

    Build inital method skeleton within Turn.js

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ class Card {
     this.question = card.question;
     this.answers = card.answers;
     this.correctAnswer = card.correctAnswer;
-  }
-}
+  };
+};
 
 module.exports = Card;
---
+++ src/Turn.js
@@ -0,0 +1,23 @@
+class Turn {
+  constructor(guess, card) {
+
+  };
+
+  returnGuess() {
+
+  };
+
+  returnCard() {
+
+  };
+
+  evaluateGuess() {
+
+  };
+
+  giveFeedback() {
+
+  };
+};
+
+module.exports = Turn;
