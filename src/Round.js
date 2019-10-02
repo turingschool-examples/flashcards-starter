0f8e03a6c446025057e77aa1d32a505c908ce350
Ben Firth <bfirth93@gmail.com>
2019-10-02 16:40:43 -0600

    Finishe iteration 2

---
+++ src/Round.js
@@ -0,0 +1,34 @@
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
+
+class Round {
+  constructor(deck) {
+    this.deck = deck.cardsInDeck; 
+    this.currentCard = this.deck[0];
+    this.turns = 0;
+    this.incorrectGuesses = [];
+  }
+  returnCurrentCard() {
+    return this.currentCard;
+  }
+  takeTurn(guess) {
+    const turn = new Turn(guess, this.currentCard);
+    this.turns++;
+
+    if (turn.evaluateGuess() === false) {
+      this.incorrectGuesses.push(this.currentCard.id);
+    }
+    this.currentCard = this.deck[this.turns];
+    return turn.giveFeedback();
+  }
+  calculatePercentageCorrect() {
+    let percentage =  (100 - ((this.incorrectGuesses / this.turns) * 100));
+    return Math.round(percentage);
+  }
+  endRound() {
+    return (`** Round over! ** You answered ${this.calculatePercentageCorrect()}% of the questions correctly!`)
+  }
+}
+
+module.exports = Round;
---
+++ test/Round-test.js
@@ -0,0 +1,97 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
+const Round = require('../src/Round');
+
+
+describe('Round', function() {
+
+  it('should be a function', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(round).to.be.an.instanceof(Round);
+  }); 
+
+  it('currentCard should be the first card in Deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(round.currentCard).to.equal(round.deck[0]);
+  })
+
+  it('returnCurrentCard method should give the card at index zero', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(round.returnCurrentCard()).to.equal(round.currentCard);
+  })
+
+  it('take turn', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(round.takeTurn('sea otter')).to.equal('correct!');
+  })
+
+  it('takeTurn method should update the turns count, evaluate guesses, give feedback, and stores ids of incorrect guesses', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(round.takeTurn('pug')).to.equal('incorrect!');
+    expect(round.incorrectGuesses).to.deep.equal([ 1 ]);
+  })
+
+  it('should be a method that calculates and returns the percentage of correct guesses', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    round.takeTurn('pug');
+    round.takeTurn('gallbladder');
+    expect(round.calculatePercentageCorrect()).to.equal(50)
+  })
+
+  it('should be a method that calculates and returns the percentage of correct guesses', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    round.takeTurn('pug');
+    round.takeTurn('gallbladder');
+    expect(round.endRound()).to.equal('** Round over! ** You answered 50% of the questions correctly!')
+  })
+
+}); 
