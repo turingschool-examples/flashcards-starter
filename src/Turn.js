56f9594f21089eb031f5480118a3d6d48e50fbe0
Karen Nguyen <knguyen4184@yahoo.com>
2019-11-18 17:56:51 -0700

    Add Turn class object file

---
+++ src/Turn.js
@@ -0,0 +1,32 @@
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess;
+    this.card = card;
+  }
+
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.card;
+  }
+
+  evaluateGuess() {
+    if (this.guess === this.card.correctAnswer) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  giveFeedback() {
+    if (this.guess === this.card.correctAnswer) {
+      return "Correct!";
+    } else {
+      return "Incorrect!";
+    }
+  }
+}
+
+module.exports = Turn;
