7e0eb61202cf6cd8c236513c80189cd96306e83c
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-11-19 16:16:53 -0700

    Add tests to build functionality

--- test/deck-test.js
+++ test/deck-test.js
@@ -3,10 +3,21 @@ const expect = chai.expect;
 
 const Deck = require('../src/deck');
 const Card = require('../src/Card');
+
 describe('Deck', function() {
 
   it('should be a function', function() {
     const deck = new Deck();
     expect(Deck).to.be.a('function');
   });
+
+  it('should have an array of cards', function() {
+    const deck = new Deck();
+    expect(deck.cards).to.be.an('array');
+  })
+
+  it('should know the length of the array of cards', function() {
+    const deck = new Deck();
+    expect(deck.countCards()).to.equal(deck.cards.length);
+  })
 });
