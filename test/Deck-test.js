79e94abaaa3b356672edb13053747a722b0f66a2
Cody Smith <codysmith2814@gmail.com>
2019-11-19 15:12:01 -0700

    Deck class takes array of cards

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -2,4 +2,24 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
 
+describe('Deck', function() {
+  it('should be a function', function() {
+    const deck = new Deck();
+    expect(Turn).to.be.a('function');
+  });
+  describe('Deck', function() {
+    it('should take an array of cards', function() {
+
+      const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+      const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+      const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+      const deck = new Deck([card1, card2, card3]);
+
+      expect(deck.cards).to.deep.equal([card1, card2, card3])
+    })
+  })
+});
