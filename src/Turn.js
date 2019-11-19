79a2e97b173e366b5acd2695ac368ab0257abc18
Jesse Maxim <jessmaxim303@gmail.com>
2019-11-19 14:48:25 -0700

    Added evaluateGuess and givefeedback methods to the turn class and the matching tests

--- src/Turn.js
+++ src/Turn.js
@@ -13,7 +13,14 @@ class Turn {
 		return this.card;
 	}
 
+	evaluateGuess() {
+		return (this.guess === this.card.correctAnswer) ? 'true' : 'false';
+	}
 
-}
+  giveFeedback() {
+    return this.evaluateGuess() ? 'correct!' : 'incorrect!';
+  }
+
+};
 
 module.exports = Turn;
