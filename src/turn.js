94f28012e690dc52615d0e9642470a6ce920ecf9
Ryan Novak <ryan.novak95@yahoo.com>
2019-11-18 22:38:19 -0700

    Began crating Turn tests

---
+++ src/turn.js
@@ -0,0 +1,25 @@
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess;
+    this.card = card;
+    this.match = false;
+  }
+
+  returnGuess() {
+
+  }
+
+  returnCard() {
+
+  }
+
+  evaluateGuess() {
+
+  }
+
+  giveFeedback() {
+    
+  }
+}
+
+module.exports = Turn;
--- test/turn-test.js
+++ test/turn-test.js
@@ -2,3 +2,22 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/turn');
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should receive a guess from the user', function() {
+    const turn = new Turn('User guess');
+    expect(turn.guess).to.equal('User guess');
+  });
+
+  it('should receive the current card in play', function() {
+    const turn = new Turn('User guess', card);
+    expect(turn.card).to.equal(card);
+  });
+
+});
