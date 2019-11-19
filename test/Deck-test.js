48bc741e5496f202509a7f07f165053ff3c55219
Cody Smith <codysmith2814@gmail.com>
2019-11-19 15:18:40 -0700

    Deck class has countCards method

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -10,16 +10,18 @@ describe('Deck', function() {
     const deck = new Deck();
     expect(Turn).to.be.a('function');
   });
-  describe('Deck', function() {
-    it('should take an array of cards', function() {
-
-      const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-      const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-      const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-
-      const deck = new Deck([card1, card2, card3]);
-
-      expect(deck.cards).to.deep.equal([card1, card2, card3])
-    })
+  it('should take an array of cards', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.cards).to.deep.equal([card1, card2, card3])
+  });
+  it ('has a countCards method which returns the length of the cards array in the deck', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.countCards()).to.equal(3)
   })
 });
