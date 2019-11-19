fc45ef234f7199ee66688ab48427f35ded0e37b4
kevin johnson <kevinj902302@gmail.com>
2019-11-18 19:05:17 -0700

    Created card class and turn files

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+class Card {
+  constructor(number, question, answers, correctAnswer) {
+    this.number = number;
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  }
+
+
+
+}
+
+module.exports = Card
