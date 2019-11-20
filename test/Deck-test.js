37a1d4e228f8e62f324ddd17bb62e667d28d4b12
Noah Gambee <noah@gambee.dev>
2019-11-19 19:32:01 -0700

    Write remaining Deck test cases and pass them

---
+++ test/Deck-test.js
@@ -0,0 +1,74 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+
+describe('Deck', function() {
+
+  it('should be a function', function() {
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', function() {
+    var deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+
+  it('should be initialized with an array of Cards', function() {
+    var card1 = {
+      "id": 1,
+      "question": `A score of one better than par on a golf hole is called what?`,
+      "answers": ['birdie', 'bogey', 'eagle'],
+      "correctAnswer": 'birdie'
+    };
+    var card2 = {
+      "id": 2,
+      "question": `What is my favorite color?`,
+      "answers": ['green', 'blue', 'orange'],
+      "correctAnswer": 'green'
+    };
+    var card3 = {
+      "id": 3,
+      "question": `How many toes do I have on my right foot?`,
+      "answers": [6, 5, 10],
+      "correctAnswer": 5
+    };
+    var deck = new Deck([card1, card2, card3]);
+
+    expect(deck.cards[0]).to.equal(card1);
+    expect(deck.cards[1]).to.equal(card2);
+    expect(deck.cards[2]).to.equal(card3);
+
+  });
+
+  it('should have a countCards method', function() {
+    var deck = new Deck();
+    expect(deck.countCards).to.be.a('function');
+  });
+
+  it('deck.countCards() should return the number of cards in the deck', function() {
+    var card1 = {
+      "id": 1,
+      "question": `A score of one better than par on a golf hole is called what?`,
+      "answers": ['birdie', 'bogey', 'eagle'],
+      "correctAnswer": 'birdie'
+    };
+    var card2 = {
+      "id": 2,
+      "question": `What is my favorite color?`,
+      "answers": ['green', 'blue', 'orange'],
+      "correctAnswer": 'green'
+    };
+    var card3 = {
+      "id": 3,
+      "question": `How many toes do I have on my right foot?`,
+      "answers": [6, 5, 10],
+      "correctAnswer": 5
+    };
+    var deck = new Deck([card1, card2, card3]);
+
+    expect(deck.countCards()).to.equal(3);
+  });
+
+});
