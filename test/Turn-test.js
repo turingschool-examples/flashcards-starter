00d40f2531c76b940a7452d9d3d93b427c0c7894
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-19 17:29:37 -0700

    Create tests for new methods

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
 
@@ -19,5 +20,25 @@ describe('Turn', function() {
     const turn = new Turn('This is a guess');
     expect(turn.returnGuess()).to.equal('This is a guess')
   })
-  
+
+  it('should return the card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('guess', card);
+
+    expect(turn.returnCard()).to.equal(card);
+  })
+
+  it('should return false if the guess does not match the correctAnswer', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('function', card);
+
+    expect(turn.evaluateGuess()).to.equal(false);
+  })
+
+  it('should return incorrect! if the guess does not match the correctAnswer', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('function', card);
+
+    expect(turn.giveFeedback()).to.equal('Incorrect!');
+  })
 });
