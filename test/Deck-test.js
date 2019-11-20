503b3254b29e155e0499b59c5b07337c3893ba96
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-19 20:44:51 -0700

    Create Deck test file and first tests

---
+++ test/Deck-test.js
@@ -0,0 +1,28 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+const Card = require('../src/Card')
+
+describe('Deck', function() {
+
+  it('should be a function', function() {
+    const deck = new Deck();
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  }); 
+
+  it('should be an array of objects', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    console.log(deck.cards);
+    console.log([card1, card2, card3]);
+    expect(deck.cards).to.equal([card1, card2, card3]);
+  })
+});
