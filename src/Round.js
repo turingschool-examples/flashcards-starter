a58b23e2d0d8ed7a6b0375ccf585dd2ab063a3ab
Noah Gambee <noah@gambee.dev>
2019-11-19 20:07:36 -0700

    Add test cases and properties for Round.js

--- src/Round.js
+++ src/Round.js
@@ -1,9 +1,22 @@
 // this object will take in responses and record guesses.
 // methods = returnCurrentCard, takeTurn, calculatePercentCorrect, endRound
+const Turn = require('../src/Turn');
 
 class Round {
-  constructor() {
+  constructor(deck) {
+    this.deck = deck;
+    this.currentCard = deck.cards[0];
+    this.turns = 0;
+    this.incorrectGuesses = [];
+    
 
+  }
+  returnCurrentCard() {
+    return this.currentCard;
+  };
+  takeTurn(guess) {
+    var turn = new Turn(guess, this.currentCard);
+    this.turns++;
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -6,33 +6,67 @@ const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
 describe('Round', function() {
+  var card1;
+  var card2;
+  var card3;
+  var deck;
+  var round;
 
   beforeEach( () => {
-    var card1 = new Card ({
+    card1 = new Card ({
       "id": 1,
       "question": `A score of one better than par on a golf hole is called what?`,
       "answers": ['birdie', 'bogey', 'eagle'],
       "correctAnswer": 'birdie'
     });
-    var card2 = new Card ({
+    card2 = new Card ({
       "id": 2,
       "question": `What is my favorite color?`,
       "answers": ['green', 'blue', 'orange'],
       "correctAnswer": 'green'
     });
-    var card3 = new Card ({
+    card3 = new Card ({
       "id": 3,
       "question": `How many toes do I have on my right foot?`,
       "answers": [6, 5, 10],
       "correctAnswer": 5
     });
 
-    var deck = new Deck([card1, card2, card3]);
-    var round = new Round(deck);
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
   });
 
+  it('should be a function', function() {
+    expect(Round).to.be.a('function');
+  });
 
+  it('should be instantiated with a Deck', function() {
+    expect(round.deck).to.equal(deck);
+  });
 
+  it('deck.currentCard should represent the first card being played', function() {
+    expect(round.currentCard).to.equal(card1);
+  });
 
+  it('should have a returnCurrentCard method', function() {
+    expect(round.returnCurrentCard).to.be.a('function');
+  });
+
+  it('round.returnCurrentCard() should return the card being played', function() {
+    expect(round.returnCurrentCard()).to.deep.equal(card1);
+  });
+
+  it('should have a takeTurn method', function() {
+    expect(round.takeTurn).to.be.a('function');
+  });
+
+  it.skip('round.takeTurn() should create a new instance of Turn', function() {
+    // expect(round.takeTurn('bogey')).to.equal('incorrect!');
+    // expect(round.takeTurn('green')).to.equal('correct!');
+  })
+
+  it('round.takeTurn() should increase round.turns each time it is called', function() {
+    expect(round.takeTurn()).to.deep.equal(1);
+  });
 
 })
