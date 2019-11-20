04efad0da3f2796878407972cb06a05dc2d985e1
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-19 21:05:28 -0700

    Basic outline and first tests

---
+++ test/Round-test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+
+describe('Round', function() {
+
+  it('should be a function', function() {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', function() {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+});
