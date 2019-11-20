d75d551f1d5cb4472ead1531a44f7701f15169a8
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-11-19 17:16:47 -0700

    Adjust code to get first card in array to return

--- src/deck.js
+++ src/deck.js
@@ -1,21 +1,6 @@
 class Deck {
-  constructor() {
-    this.cards = [{
-      "id": 1,
-      "question": "What allows you to define a set of related information using key-value pairs?",
-      "answers": ["object", "array", "function"],
-      "correctAnswer": "object"
-    }, {
-      "id": 2,
-      "question": "What is a comma-separated list of related values?",
-      "answers": ["array", "object", "function"],
-      "correctAnswer": "array"
-    }, {
-      "id": 3,
-      "question": "What type of prototype method directly modifies the existing array?",
-      "answers": ["mutator method", "accessor method", "iteration method"],
-      "correctAnswer": "mutator method"
-    }]
+  constructor(cards) {
+    this.cards = cards
   }
 
   countCards() {
