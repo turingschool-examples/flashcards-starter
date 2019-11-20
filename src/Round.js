a0bad91aa94f54d7c32971e7a64091a3392263b2
Cody Smith <codysmith2814@gmail.com>
2019-11-20 14:44:31 -0700

    Adds evaluation functionality to takeTurns method

--- src/Round.js
+++ src/Round.js
@@ -1,16 +1,26 @@
+/* eslint-disable no-console */
+
+const Turn = require('../src/Turn');
+
+
 class Round {
   constructor(deck) { 
-    this.deck = deck;
+    this.deck = deck.cards;
     this.turns = 0;
+    this.incorrectGuesses = [];
   }
   returnCurrentCard() {
-    for (var i = 0; i < this.deck.length; i++) {
-      return this.deck[i]
+    return this.deck[0]
+  }
+  takeTurn(guess) {   
+    this.turns++;
+    let turn = new Turn(guess, this.deck[0])
+    if (!turn.evaluateGuess()) {
+      this.incorrectGuesses.push(this.deck[0].id);
+      this.deck.shift();
+    } else {
+      this.deck.shift()
     }
   }
-  takeTurn() {
-    this.turns++
-  }
 }
-
 module.exports = Round
--- test/Round-test.js
+++ test/Round-test.js
@@ -7,7 +7,6 @@ const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
 
-
 describe('Round', function() {
   it('should be a function', function() {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
@@ -17,16 +16,17 @@ describe('Round', function() {
     const round = new Round(deck);
     expect(Round).to.be.a('function');
   })
+
   it('should have a method called returnCurrentCard which returns the current card being played', function() {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     const deck = new Deck([card1, card2, card3])
     const round = new Round(deck);
-    for (var i = 0; i < deck.length; i++) {
-      expect(round.returnCurrentCard()).to.equal(deck[i]);
-    }
+    round.returnCurrentCard()
+    expect(round.returnCurrentCard()).to.equal(deck.cards[0]);
   })
+
   it('should be able to take a turn', function() {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
@@ -38,4 +38,54 @@ describe('Round', function() {
     round.takeTurn();
     expect(round.turns).to.equal(2)
   })
+
+  it('when a guess is made, a new turn instance is created', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    const turn = new Turn();
+
+    round.takeTurn();
+    expect(turn).to.be.an.instanceof(Turn);
+  })
+
+  it('evaluates each guess and stores each incorrect guess as an ID in the incorrectGuesses array', () => {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    round.takeTurn('array');
+
+    expect(round.incorrectGuesses.length).to.equal(1)
+
+  })
+
+  it('takes the next card and makes it the current card', () => {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    round.takeTurn();
+    expect(deck.cards[0].id).to.equal(14)
+    round.takeTurn();
+    expect(deck.cards[0].id).to.equal(12)
+
+
+  })
+
 })
