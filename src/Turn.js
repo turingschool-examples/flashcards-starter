021ad64ee4fbb5b5349dd853de06e53debdf6ee8
Noah Gambee <noah@gambee.dev>
2019-11-19 19:04:43 -0700

    Write and pass remaining test cases for Turn.js

--- src/Turn.js
+++ src/Turn.js
@@ -4,16 +4,24 @@ class Turn {
     this.card = cardObj;
   }
   returnGuess() {
-
+    return this.guess;
   }
   returnCard() {
-
+    return this.card;
   }
   evaluateGuess() {
-
+    if (this.guess === this.card.correctAnswer) {
+        return true;
+    } else {
+      return false;
+    }
   }
   giveFeedback() {
-
+    if (this.guess === this.card.correctAnswer) {
+        return 'correct!';
+    } else {
+      return 'incorrect!';
+    }
   }
 }
 
