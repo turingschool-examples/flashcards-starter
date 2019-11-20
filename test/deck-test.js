195d37c07930c55ec0d1960fb0f51b4c3de1c9d6
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-11-19 17:17:24 -0700

    Adjust code to get first card in array to return

--- test/deck-test.js
+++ test/deck-test.js
@@ -12,12 +12,36 @@ describe('Deck', function() {
   });
 
   it('should have an array of cards', function() {
-    const deck = new Deck();
+    const card1 = new Card(1,
+    "What allows you to define a set of related information using key-value pairs?",
+    ["object", "array", "function"],
+    "object")
+    const card2 = new Card(2,
+    "What is a comma-separated list of related values?",
+    ["array", "object", "function"],
+    "array")
+    const card3 = new Card(3,
+    "What type of prototype method directly modifies the existing array?",
+    ["mutator method", "accessor method", "iteration method"],
+    "mutator method")
+    const deck = new Deck([card1, card2, card3]);
     expect(deck.cards).to.be.an('array');
   })
 
   it('should know the length of the array of cards', function() {
-    const deck = new Deck();
+    const card1 = new Card(1,
+    "What allows you to define a set of related information using key-value pairs?",
+    ["object", "array", "function"],
+    "object")
+    const card2 = new Card(2,
+    "What is a comma-separated list of related values?",
+    ["array", "object", "function"],
+    "array")
+    const card3 = new Card(3,
+    "What type of prototype method directly modifies the existing array?",
+    ["mutator method", "accessor method", "iteration method"],
+    "mutator method")
+    const deck = new Deck(card1, card2, card3);
     expect(deck.countCards()).to.equal(deck.cards.length);
   })
 });
