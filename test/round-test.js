578f605e986950ea6400c45efb000ce37f70d8a1
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-11-19 17:36:06 -0700

    Add tests to check turn counter for incrementing

--- test/round-test.js
+++ test/round-test.js
@@ -31,4 +31,26 @@ describe('Round', function() {
     expect(round.returnCurrentCard()).to.equal(card1)
   });
 
+  it('should update turn count', function() {
+    const card1 = new Card(1,
+    "What allows you to define a set of related information using key-value pairs?",
+    ["object", "array", "function"],
+    "object")
+    const card2 = new Card(2,
+    "What is a comma-separated list of related values?",
+    ["array", "object", "function"],
+    "array")
+    const card3 = new Card(3,
+    "What type of prototype method directly modifies the existing array?",
+    ["mutator method", "accessor method", "iteration method"],
+    "mutator method")
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.turns).to.equal(0);
+    round.takeTurn();
+    expect(round.turns).to.equal(1);
+    round.takeTurn();
+    expect(round.turns).to.equal(2);
+  });
+
 });
