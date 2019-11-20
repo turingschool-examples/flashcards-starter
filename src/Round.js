0c2a0cfd51fd77ea2ce195733682d6debcb0dace
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-20 16:25:57 -0700

    Create necessary methods for round class

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,27 @@
-class Round {
-  constructor() {
+const Turn = require('../src/Turn');
 
+class Round {
+  constructor(deck) {
+    this.deck = deck.cards;
+    this.turn = 0;
+    this.incorrectGuesses = [];
+  }
+  returnCurrentCard() {
+    return this.deck[this.turn];
+  }
+  takeTurn(answer) {
+    const turn = new Turn(answer, this.returnCurrentCard());
+    if (turn.giveFeedback() === 'Incorrect!') {
+      this.incorrectGuesses.push(this.returnCurrentCard().id);
+    }
+    this.turn++;
+    return turn.giveFeedback();
+  }
+  calculatePercentCorrect() {
+    return (this.deck.length - this.incorrectGuesses.length) / this.deck.length * 100;
+  }
+  endRound() {
+    return `** Round Over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
   }
 }
 
