c2bfb8da55d5512dba6855c34eb0517defb832db
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-11-19 17:17:01 -0700

    Adjust code to get first card in array to return

--- src/round.js
+++ src/round.js
@@ -1,6 +1,12 @@
 class Round {
-  constructor() {
-
+  constructor(currentDeck) {
+    this.currentDeck = currentDeck;
+    this.turns = 0;
+    this.incorrectGuesses = [];
+    this.correctGuesses = [];
+  }
+  returnCurrentCard() {
+    return this.currentDeck.cards[0];
   }
 }
 
