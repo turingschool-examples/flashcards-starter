7c1669c3ba97af8b11897d6b63e51c624b6c3f79
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-19 20:53:55 -0700

    Wrote test for countCards method

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -21,8 +21,16 @@ describe('Deck', function() {
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     const deck = new Deck([card1, card2, card3]);
-    console.log(deck.cards);
-    console.log([card1, card2, card3]);
-    expect(deck.cards).to.equal([card1, card2, card3]);
+
+    expect(deck.cards).to.deep.equal([card1, card2, card3]);
+  })
+
+  it('should be able to return the number of cards in a deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+
+    expect(deck.countCards()).to.equal(3);
   })
 });
