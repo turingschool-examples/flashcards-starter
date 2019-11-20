9e404ebbeef71180a20d86c025c13834c81333e6
Karen Nguyen <knguyen4184@yahoo.com>
2019-11-20 13:37:56 -0700

    Write test to increment turns and check if its incorrect or correct

--- test/Round-test.js
+++ test/Round-test.js
@@ -64,4 +64,14 @@ describe('Round', function() {
     expect(round.takeTurn("sea otter")).to.equal("Correct!");
     expect(round.takeTurn("spleen")).to.equal("Incorrect!");
     });
+
+  it('should remove a card after a turn', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    round.takeTurn("pug");
+    expect(round.returnCurrentCard()).to.equal(card2);
+    });
 });
