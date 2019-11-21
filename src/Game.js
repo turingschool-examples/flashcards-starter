a9d127df2266d0aa77d47a847c8737a9ec88742b
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-21 14:40:11 -0700

    Create gameStart method

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,14 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('./Card');
+const Deck = require('./Deck');
+const Round = require('./Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -13,6 +18,20 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
+
+  gameStart() {
+    let questions = [];
+    for (var i = 0; i < prototypeQuestions.length; i++) {
+      let card = new Card(prototypeQuestions[i].id, prototypeQuestions[i].question, prototypeQuestions[i].answers, prototypeQuestions[i].correctAnswer)
+      questions.push(card);
+    }
+    const deck = new Deck(questions);
+    const round = new Round(deck);
+    this.currentRound = round;
+    this.printMessage(deck, round);
+    this.printQuestion(round);
+  }
+
 }
 
 module.exports = Game;
