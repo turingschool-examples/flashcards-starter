d061c8e1ac747923c0d90601009cd53fa09cd32e
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-11-20 18:08:54 -0700

    Adjust code to make game playable

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,34 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Deck = require('../src/deck');
+const Card = require('../src/Card');
+const Round = require('../src/round');
+
+
+
 
 class Game {
-  constructor() {}
+  constructor() {
+    var cards = [];
+    for (var i = 0; i < prototypeQuestions.length; i++) {
+      var card = prototypeQuestions[i]
+      cards.push(card)
+    }
+    this.deck = new Deck(cards);
+    this.round = new Round(this.deck);
+
+  }
+
+  start() {
+
+    this.printMessage(this.deck, this.round);
+    this.printQuestion(this.round);
+    // console.log(this.round)
+    console.log('correct', this.round.correctGuesses)
+    console.log('incorrect', this.round.incorrectGuesses)
+    return this.deck.cards.length;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
