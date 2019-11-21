0f951b15a7b18aaaf424da2462368eae7f159087
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2019-11-21 16:48:43 -0700

    Write deck-test

---
+++ test/deck-test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/deck');
+const Card = require('../src/Card');
+
+
+describe('Deck', function() {
+
+  it('should be initialized with an array of Card objects', function() {
+
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+
+    deck.countCards();
+    expect(deck.countCards()).to.equal(3);
+  });
+});
