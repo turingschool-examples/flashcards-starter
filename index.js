6ab6f88274d974d3a438b657fdabbdeca8952ef3
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-11-20 18:53:34 -0700

    Invoke start of game in terminal

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const http = require('http');
 let app = http.createServer();
 
+const Game = require('./src/Game');
+
+var game = new Game();
+game.start();
+
 // Start the server on port 3000
-app.listen(3000, '127.0.0.1');  
-console.log('Node server running on port 3000'); 
+app.listen(3000, '127.0.0.1');
+console.log('Node server running on port 3000');
