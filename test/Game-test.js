83a6d1a64a9721010f457c1466d98ce43c37c776
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-11-20 18:56:27 -0700

    Clean up test page for game

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,20 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Deck = require('../src/deck');
-const Card = require('../src/Card');
-const Round = require('../src/round');
 const Game = require('../src/Game');
-const Turn = require('../src/turn');
-const Data = require('../src/data');
+
 
 describe('Game', function() {
 
+  it('should create cards', function() {
+      const game = new Game();
 
-    it('should create cards', function() {
-        const game = new Game();
-        
-        expect(game.start()).to.equal(30);
-    });
-
+      expect(game.start()).to.equal(30);
+  });
 });
