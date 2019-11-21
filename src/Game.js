302d5a73dc10b7b677f7f7ccdf68088e563fc40c
Noah Gambee <noah@gambee.dev>
2019-11-20 19:27:47 -0700

    Add Game test cases

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,27 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+var Card = require('../src/Card');
+var Deck = require('../src/Deck');
+var Round = require('../src/Round');
+
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = 0;
+  }
+  // returnCurrentRound() {
+  //   return this.currentRound;
+  // }
+  startGame() {
+
+  }
+
+
+
+
+
+
+
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
--- test/Game-test.js
+++ test/Game-test.js
@@ -1,4 +1,55 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-// comment
+var Game = require('../src/Game');
+var Deck = require('../src/Deck');
+
+describe('Game', function() {
+  var game;
+
+  beforeEach( () => {
+    game = new Game();
+  });
+
+  // it('should have the currentRound method', function() {
+  //   expect(game.returnCurrentRound).to.be.a('function');
+  // });
+  //
+  // it('game.currentRound() should keep track of the current round', function() {
+  //   expect(game.returnCurrentRound()).to.equal(0);
+  // });
+
+  it('should keep track of the current round', function() {
+    expect(game.currentRound).to.equal(0);
+  });
+
+  it('should have the startGame method', function() {
+    expect(game.startGame).to.be.a('function');
+  });
+
+  it('game.startGame() should create cards', function() {
+
+  });
+
+  it('game.startGame() should put cards into the deck', function() {
+
+  });
+
+  it('game.startGame() should create a new Round using the deck', function() {
+
+  });
+
+  it('game.startGame() should invoke game.printMessage', function() {
+
+  });
+
+  it('game.startGame() should invoke game.printQuestion', function() {
+
+  });
+
+
+
+
+
+
+})
