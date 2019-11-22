eb62419c49f44fb6c0370f1189480a4c2a1bb916
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2019-11-21 21:52:39 -0700

    Clean up test spacing

--- test/deck-test.js
+++ test/deck-test.js
@@ -8,7 +8,6 @@ const Card = require('../src/Card');
 describe('Deck', function() {
 
   it('should be initialized with an array of Card objects', function() {
-
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
