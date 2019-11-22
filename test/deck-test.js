5c4bb62223840e40ee05f2083aab3e2a653f193e
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-11-21 17:25:38 -0700

    Adjust syntax for linting and add hooks

--- test/deck-test.js
+++ test/deck-test.js
@@ -6,46 +6,37 @@ const Card = require('../src/Card');
 
 describe('Deck', function() {
 
-  it('should be a function', function() {
+  let deck;
+  let card1;
+  let card2;
+  let card3;
 
-    expect(Deck).to.be.a('function');
-  });
-
-  it('should have an array of cards', function() {
-    const card1 = new Card(1,
-      'What allows you to define a set of related information using key-value'
-    + ' pairs?',
-      ['object', 'array', 'function'],
-      'object')
-    const card2 = new Card(2,
-      'What is a comma-separated list of related values?',
-      ['array', 'object', 'function'],
-      'array')
-    const card3 = new Card(3,
-      'What type of prototype method directly modifies the existing array?',
-      ['mutator method', 'accessor method', 'iteration method'],
-      'mutator method')
-    const deck = new Deck([card1, card2, card3]);
-
-    expect(deck.cards).to.be.an('array');
-  })
-
-  it('should know the length of the array of cards', function() {
-    const card1 = new Card(1,
+  beforeEach(() => {
+    card1 = new Card(1,
       'What allows you to define a set of related information using key-value'
       + ' pairs?',
       ['object', 'array', 'function'],
       'object')
-    const card2 = new Card(2,
+    card2 = new Card(2,
       'What is a comma-separated list of related values?',
       ['array', 'object', 'function'],
       'array')
-    const card3 = new Card(3,
+    card3 = new Card(3,
       'What type of prototype method directly modifies the existing array?',
       ['mutator method', 'accessor method', 'iteration method'],
       'mutator method')
-    const deck = new Deck(card1, card2, card3);
+    deck = new Deck([card1, card2, card3]);
+  });
 
+  it('should be a function', function() {
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should have an array of cards', function() {
+    expect(deck.cards).to.be.an('array');
+  })
+
+  it('should know the length of the array of cards', function() {
     expect(deck.countCards()).to.equal(deck.cards.length);
   });
 });
