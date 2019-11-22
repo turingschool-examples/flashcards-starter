7661e20c75116e51b8e53bd9c851b6e134f3d2f2
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2019-11-21 21:50:10 -0700

    Clean up test spacing

--- test/Card-test.js
+++ test/Card-test.js
@@ -11,27 +11,22 @@ beforeEach(function() {
 describe('Card', function() {
 
   it('should be a function', function() {
-
     expect(Card).to.be.a('function');
   });
 
   it('should be an instance of Card', function() {
-
     expect(card).to.be.an.instanceof(Card);
   });
 
   it('should store a question', function() {
-
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
   });
 
   it('should store a list of possible answers', function() {
-
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
   });
 
   it('should store the correct answer', function() {
-
     expect(card.correctAnswer).to.equal('object');
   });
 });
