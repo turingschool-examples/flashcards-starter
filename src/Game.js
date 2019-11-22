c4946baed88a4f1a84678592548645e2b48432bb
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-11-21 20:28:25 -0700

    Adjust code to better match the comp

--- src/Game.js
+++ src/Game.js
@@ -4,21 +4,22 @@ const util = require('./util');
 const Deck = require('../src/deck');
 const Card = require('../src/Card');
 const Round = require('../src/round');
-
+prototypeQuestions.length
 class Game {
   constructor() {
-    var cards = [];
+    this.cards = [];
+
+  }
+
+  start() {
     for (var i = 0; i < prototypeQuestions.length; i++) {
       var card = new Card(prototypeQuestions[i].id,
         prototypeQuestions[i].question, prototypeQuestions[i].answers,
         prototypeQuestions[i].correctAnswer)
-      cards.push(card)
+      this.cards.push(card)
     }
-    this.deck = new Deck(cards);
+    this.deck = new Deck(this.cards);
     this.round = new Round(this.deck);
-  }
-
-  start() {
     this.printMessage(this.deck, this.round);
     this.printQuestion(this.round);
     return this.deck.cards.length;
