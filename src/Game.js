363ded3276f9efa32e8bdf4382a1b19263a47163
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2019-11-21 17:00:47 -0700

    Make start function

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,41 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/deck');
+const Round = require('../src/round');
+
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = 0;
+    this.cards = [];
+  }
 
-  printMessage(deck, round) {
+  printMessage(deck) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start() {
+    this.currentRound++;
+    for (let i = 0; i < prototypeQuestions.length; i++) {
+      let card = new Card(
+        prototypeQuestions[i].id,
+        prototypeQuestions[i].question,
+        prototypeQuestions[i].answers,
+        prototypeQuestions[i].correctAnswer
+      );
+      this.cards.push(card);
+    }
+    const newDeck = new Deck(this.cards)
+    const newRound = new Round(newDeck);
+    this.printMessage(newDeck, newRound);
+    this.printQuestion(newRound);
   }
 }
 
