b75ff59eb775e2251628bfe08555372f93047780
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-26 11:59:17 -0700

    Add deck storage test

--- test/Round-test.js
+++ test/Round-test.js
@@ -14,4 +14,14 @@ describe('Round', function() {
     const round = new Round();
     expect(round).to.be.an.instanceof(Round);
   });
+
+  it('should store a deck of cards', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.deck).to.deep.equal([card1, card2, card3]);
+  });
 });
