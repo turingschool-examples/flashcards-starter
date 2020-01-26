a73a21db2822c0e5fb0365b25bf954ad02288ce0
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-26 12:30:13 -0700

    Write takeTurn tests

--- test/Round-test.js
+++ test/Round-test.js
@@ -33,4 +33,24 @@ describe('Round', function() {
     const currentCard = round.returnCurrentCard();
     expect(currentCard).to.equal(card1);
   });
+
+  it('should be able to take turns', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    const currentCard = round.returnCurrentCard();
+    expect(round.turns).to.equal(0);
+    expect(round.incorrectGuesses).to.deep.equal([])
+    expect(currentCard).to.equal(card1);
+    const feedback1 = round.takeTurn('sea otter');
+    expect(feedback1).to.equal('correct!');
+    const feedback2 = round.takeTurn('spleen');
+    expect(feedback2).to.equal('incorrect!');
+    expect(round.turns).to.equal(2);
+    expect(round.incorrectGuesses).to.deep.equal([14]);
+    currentCard = round.returnCurrentCard();
+    expect(currentCard).to.equal(card3);
+  });
 });
