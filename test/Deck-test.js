8b0857c2859ab656e4d19e4cb2e46797840fa3c5
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-26 11:48:33 -0700

    Add card list test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -15,4 +15,13 @@ describe('Deck', function() {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
+
+  it('should store a list of cards', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.cards).to.deep.equal([card1, card2, card3]);
+  });
 });
