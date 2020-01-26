cf7f94cf09f78b4d8133796d7bc8c717464698ea
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-26 10:39:32 -0700

    Outline constructor turn tests

---
+++ test/Turn-test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it('should store a guess', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.guess).to.equal('object');
+  });
+
+  it('should store a Card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.card).to.equal(card);
+  });
+});
