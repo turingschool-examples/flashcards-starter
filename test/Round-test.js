8d2898d3880718de5805fe1895ca0a27d4f03608
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-26 11:56:01 -0700

    Add instantiator tests

---
+++ test/Round-test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
+
+describe('Round', function() {
+
+  it('should be a function', function() {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', function() {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  });
+});
