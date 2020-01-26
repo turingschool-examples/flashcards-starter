b240de081da60147368ccfd8ad4fa29f3b76b603
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-26 12:55:44 -0700

    Begin take turn functionality

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,17 @@
 class Round {
   constructor(deck) {
-    this.deck = deck.cards || []; 
+    this.deck = deck.cards;
+    this.turns = 0;
+    this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard() {
+    return this.deck[0];
+  }
+
+  takeTurn(guess) {
+    this.turns++;
+
   }
 }
 
