541dcbb7bcd98d49ecd39440bde46b9761cdefd4
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-26 11:29:41 -0700

    Write returnGuess and returnCard functions

--- src/Turn.js
+++ src/Turn.js
@@ -3,6 +3,14 @@ class Turn {
     this.guess = guess;
     this.card = card;
   }
+
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.card;
+  }
 }
 
 module.exports = Turn;
