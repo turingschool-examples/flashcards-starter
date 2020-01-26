5aea78e35c6cb19d239f1f7800aaa1755ce1331c
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-26 11:50:08 -0700

    Add countCards test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -24,4 +24,14 @@ describe('Deck', function() {
     const deck = new Deck([card1, card2, card3]);
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
+
+  it('should be able to count its cards', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const cardCount = deck.countCards();
+    expect(cardCount).to.equal(3)
+  });
 });
