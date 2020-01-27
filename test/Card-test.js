00947f75717688bb318399e1cf9e047b2be02ab3
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-01-27 16:26:28 -0700

    Add test to check for different id

--- test/Card-test.js
+++ test/Card-test.js
@@ -5,28 +5,40 @@ const Card = require('../src/Card');
 
 describe('Card', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const card = new Card();
     expect(Card).to.be.a('function');
   });
 
-  it.skip('should be an instance of Card', function() {
+  it('should be an instance of Card', function() {
     const card = new Card();
     expect(card).to.be.an.instanceof(Card);
-  }); 
+  });
 
-  it.skip('should store a question', function() {
+  it('should store a question', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
-  });  
+  });
 
-  it.skip('should store a list of possible answers', function() {
+
+  it('should store a list of possible answers', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
-  });  
+  });
 
-  it.skip('should store the correct answer', function() {
+  it('should store the correct answer', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.correctAnswer).to.equal('object');
   });
+
+  it('should have an id', function () {
+    const card = new Card(1, 'What is a function defined on an object called?', ['parameter', 'method', 'property'], 'method');
+    expect(card.id).to.equal(1);
+  });
+
+  it('should be able to have a different id', function() {
+    const card = new Card(2, 'What is NOT true of the return keyword?', ['it end the function after it is run', 'it can hold onto a value from the function', 'it always comes back as undefined'], 'it always comes back as undefined');
+    expect(card.id).to.equal(2);
+  });
+
 });
