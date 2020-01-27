50ffab1a031a98c3ca69a2fcfb5ec63b15fa27ea
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-01-27 16:31:45 -0700

    Adjust constructor to take a parameter to assign the value of the this.question key

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,7 @@
 class Card {
-  constructor() {
-    this.id = 1;
-    this.question = 'What allows you to define a set of related information using key-value pairs?';
+  constructor(id, question) {
+    this.id = id;
+    this.question = question;
     this.answers = ['object', 'array', 'function'];
     this.correctAnswer = 'object';
 
