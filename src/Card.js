39870823aaaf081050806a06985ddf5c9973d163
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-01-27 16:19:37 -0700

    Add key-value pair to create an id property

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,6 @@
 class Card {
   constructor() {
+    this.id = 1;
     this.question = 'What allows you to define a set of related information using key-value pairs?';
     this.answers = ['object', 'array', 'function'];
     this.correctAnswer = 'object';
