627b99e809e726e16a149aa4c5f502a085cd6baa
Karen Nguyen <knguyen4184@yahoo.com>
2020-01-27 19:47:08 -0700

    Write methods for the Turn class

--- src/Turn.js
+++ src/Turn.js
@@ -3,6 +3,30 @@ class Turn {
     this.guess = guess;
     this.card = card;
   }
+
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.card;
+  }
+
+  evaluateGuess() {
+    if (this.guess === this.card.correctAnswer) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  giveFeedback() {
+    if (this.evaluateGuess()) {
+      return "Correct!";
+    } else {
+      return "Incorrect!";
+    }
+  }
 }
 
 module.exports = Turn;
