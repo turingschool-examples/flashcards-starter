7de659901c14d90c3e9414ddf2013fa62a5f4f60
Jordan Whitten <jordanwa1947@gmail.com>
2020-01-27 17:44:59 -0700

    Add a method to round that returns the current card

--- src/Round.js
+++ src/Round.js
@@ -2,6 +2,10 @@ class Round {
   constructor(deck) {
     this.deck = deck.cards;
   }
+
+  returnCurrentCard() {
+    return this.deck[0];
+  }
 }
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -20,7 +20,7 @@ describe('Round', function() {
     expect(round).to.be.an.instanceof(Round);
   });
 
-  it('should be a function', function() {
+  it('should store an array of cards', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
@@ -30,6 +30,18 @@ describe('Round', function() {
     const round = new Round(deck);
 
     assert.deepEqual(round.deck, [card1, card2, card3]);
+  });
+
+  it('should have a method to return the current card', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+
+    assert.equal(round.returnCurrentCard(), card1);
   });
 
 });
