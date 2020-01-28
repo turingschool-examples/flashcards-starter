88d5d607d7b397d215206b89346b7c4f1a6d7a7e
Jordan Whitten <jordanwa1947@gmail.com>
2020-01-27 20:12:11 -0700

    Adds the takeTurn method and tests all steps necessary for a new turn to happen

--- src/Round.js
+++ src/Round.js
@@ -1,12 +1,28 @@
+const Turn = require('../src/Turn')
+
 class Round {
   constructor(deck) {
     this.deck = deck.cards;
     this.turns = 0;
-    this.incorrectGuess = [];
+    this.incorrectGuesses = [];
   }
 
   returnCurrentCard() {
     return this.deck[0];
+  }
+
+  movefirstCardToLast() {
+    const firstCard = this.deck.shift();
+    this.deck.push(firstCard);
+  }
+
+  takeTurn(guess) {
+    const card = this.returnCurrentCard();
+    const turn = new Turn(guess, card);
+    this.turns += 1;
+    if (!turn.evaluateGuess()) this.incorrectGuesses.push(card.id);
+    this.movefirstCardToLast();
+    return turn.giveFeedback();
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -27,7 +27,7 @@ describe('Round', function() {
     const round = new Round(deck);
 
     assert.equal(round.turns, 0);
-    assert.deepEqual(round.incorrectGuess, []);
+    assert.deepEqual(round.incorrectGuesses, []);
   });
 
   it('should store an array of cards', function() {
@@ -52,6 +52,26 @@ describe('Round', function() {
     const round = new Round(deck);
 
     assert.equal(round.returnCurrentCard(), card1);
+  });
+
+  it('should be able to take a turn', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+
+    assert.equal(round.turns, 0);
+    assert.deepEqual(round.incorrectGuesses, []);
+
+    assert.equal(round.takeTurn('sea otter'), 'Correct!');
+    assert.equal(round.takeTurn('spleen'), 'Incorrect!');
+
+    assert.equal(round.turns, 2);
+    assert.deepEqual(round.incorrectGuesses, [14]);
+    assert.deepEqual(round.returnCurrentCard(), card3);
   });
 
 });
