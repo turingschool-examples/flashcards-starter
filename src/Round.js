894c846baa9395a6156704f2a3bea506d2cc56d8
Lane Hunter <lanehunter.96@gmail.com>
2020-01-28 16:59:17 -0700

    Update round methods

--- src/Round.js
+++ src/Round.js
@@ -1,15 +1,20 @@
+const Turn = require('./Turn.js');
+
 class Round {
   constructor(deck) {
     this.deck = deck;
     this.incorrectGuesses = [];
     this.turns = 0;
-    this.currentCard = deck.cards[0];
+    this.currentCard = this.turns;
   }
   returnCurrentCard() {
     return this.currentCard;
   }
-  takeTurn() {
-
+  takeTurn(guess, card) {
+    let currentTurn = new Turn(guess, card);
+    this.turns++;
+    
+    return currentTurn;
   }
   calculatePercentCorrect() {
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -37,6 +37,18 @@ describe('Round', function() {
     expect(round.returnCurrentCard()).to.equal(card1)
   });
 
-  it('should')
+  it('should update the turn count', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.turns).to.equal(0)
+    round.takeTurn();
+    expect(round.turns).to.equal(1)
+  });
+
+
 
 });
