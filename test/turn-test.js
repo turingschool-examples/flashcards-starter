ea4edf74044a7e2d72468a06919a71414e391d58
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-01-27 17:32:58 -0700

    Add test to check for user's guess

--- test/turn-test.js
+++ test/turn-test.js
@@ -1,4 +1,19 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const Card = require('../src/Card');
 const Turn = require('../src/turn');
+
+describe('Turn', function() {
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be able to guess an answer', function() {
+    const card = new Card(2, 'Which is of these is a reserved word in Javascript?', ['this', 'never', 'mocha'], 'this');
+    const turn = new Turn('never', card);
+    expect(turn.guess).to.equal('never');
+  })
+
+});
