e1bd7cb148c013a829fb475e93ebd9c20be9b74d
Jordan Whitten <jordanwa1947@gmail.com>
2020-01-27 17:39:54 -0700

    Adds the array of cards stored in a deck to the round class and tests to make sure its there

--- test/Round-test.js
+++ test/Round-test.js
@@ -9,10 +9,27 @@ const Card = require('../src/Card');
 describe('Round', function() {
 
   it('should be a function', function() {
-    const deck = new Round();
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
 
     expect(Round).to.be.a('function');
-    expect(deck).to.be.an.instanceof(Round);
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+  it('should be a function', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+
+    assert.deepEqual(round.deck, [card1, card2, card3]);
   });
 
 });
