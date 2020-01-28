24390d2a1889823478d8b24263afcd5c28842d5a
Jordan Whitten <jordanwa1947@gmail.com>
2020-01-27 17:22:24 -0700

    Adds the countCards method to a deck that return the number of cards in a deck

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -24,4 +24,14 @@ describe('Deck', function() {
     assert.deepEqual(deck.cards, [card1, card2, card3]);
   });
 
+  it('should be able to count its cards', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    assert.equal(deck.countCards(), 3);
+  });
+
 });
