620a601f3e92cb8c4b083dd28b166732cede47f5
Jordan Whitten <jordanwa1947@gmail.com>
2020-01-27 20:14:23 -0700

    Add test for moveFirstCardToLast method

--- test/Round-test.js
+++ test/Round-test.js
@@ -74,4 +74,19 @@ describe('Round', function() {
     assert.deepEqual(round.returnCurrentCard(), card3);
   });
 
+  it('should be able to move to the next card', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+
+    round.takeTurn();
+    round.takeTurn();
+
+    assert.deepEqual(round.returnCurrentCard(), card3);
+  });
+
 });
