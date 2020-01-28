d4e9445c4d904913fef14d93bb336d696acf99e2
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-01-27 19:06:28 -0700

    Add test to check for the number of cards in the deck

--- test/deck-test.js
+++ test/deck-test.js
@@ -23,4 +23,11 @@ describe('Deck', function() {
     expect(deck.cardsInDeck).to.deep.equal([card1, card2, card3]);
   });
 
+  it('should be able to count the numbers of cards in the deck', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(5, 'What is a function defined on an object called?', ['parameter', 'method', 'property'], 'method');
+    const card3 = new Card(6, 'Which is of these is a reserved word in Javascript?', ['this', 'never', 'mocha'], 'this');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.countCards()).to.equal(3);
+  });
 })
