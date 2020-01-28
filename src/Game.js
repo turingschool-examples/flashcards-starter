aa182b911bd07a8db2d8c339e06f8ed576862f29
Ryan Bahan <ryan@ryanbahan.com>
2020-01-27 23:01:55 -0700

    Add basic functionality to track question sets across games

--- src/Game.js
+++ src/Game.js
@@ -25,11 +25,14 @@ class Game {
   }
 
   getQuestions() {
-
+    let questionSet = questions[this.questionsIndex];
+    this.questionsIndex += 1;
+    return questionSet;
   }
 
   start() {
-    var cards = prototypeQuestions.map(item =>
+    var questionSet = this.getQuestions();
+    var cards = questionSet.map(item =>
       new Card(item.id, item.question, item.answers, item.correctAnswer));
     var deck = new Deck(cards);
     var round = new Round(deck);
