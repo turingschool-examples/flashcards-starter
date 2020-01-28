00c8cf1e0a33399f39259a7b1bad8aabcfa7c67b
Erik Raith <raithhtiar@gmail.com>
2020-01-27 21:24:25 -0700

    [feat] Add functionality for Turn This adds turn return guess and card as well as evaluating the guess and give feedback to the player

--- src/Turn.js
+++ src/Turn.js
@@ -1,18 +1,27 @@
 class Turn{
-  constructor(){
-
+  constructor(guess,card){
+    this.guess = guess;
+    this.card = card;
   }
   returnGuess(){
-
+    return this.guess;
   }
   returnCard(){
-
+    return this.card;
   }
   evaluateGuess(){
-
+    return this.card.correctAnswer === this.guess;
   }
   giveFeedback(){
-    
+    switch (this.evaluateGuess()) {
+      case true:
+        return 'Correct!';
+        break;
+      case false:
+        return 'Incorrect!';
+        break;
+      default:
+    }
   }
 }
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -25,7 +25,45 @@ describe('Turn', function() {
   it('should store the current card', function() {
     const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const turn = new Turn('pug',card);
-    expect(turn.guess).to.be.a('object');
+    expect(turn.card).to.be.an('object');
+    expect(turn.card).to.deep.equal(card);
   });
 
+  it('should be able to return the guess', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug',card);
+    var guess = turn.returnGuess();
+    expect(guess).to.deep.equal('pug');
+  });
+
+  it('should be able to return the card', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug',card);
+    var turnCard = turn.returnCard();
+    expect(turnCard).to.deep.equal({ id: 1,
+        question: 'What is Robbie\'s favorite animal',
+        answers: ['sea otter', 'pug', 'capybara'],
+        correctAnswer: 'sea otter'
+      });
+  });
+
+  it('should be able to evaluate the Guess', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug',card);
+    var isCorrect = turn.evaluateGuess();
+    expect(isCorrect).to.equal(false);
+    turn.guess = 'sea otter';
+    isCorrect = turn.evaluateGuess();
+    expect(isCorrect).to.equal(true);
+  });
+
+  it('should be able to give concise feedback', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug',card);
+    var feedback = turn.giveFeedback();
+    expect(feedback).to.equal('Incorrect!');
+    turn.guess = 'sea otter';
+    feedback = turn.giveFeedback();
+    expect(feedback).to.equal('Correct!');
+  });
 });
