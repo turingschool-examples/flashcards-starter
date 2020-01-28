b9759ccb3906ff08edaa14f8892ca07565c28eb0
Karen Nguyen <knguyen4184@yahoo.com>
2020-01-27 20:46:33 -0700

    Write test for deck class

---
+++ test/Deck-test.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Card', function() {
+
+  let card1, card2, card3;
+  let deck;
+
+  beforeEach(() => {
+    
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck = new Deck([card1, card2, card3]);
+  });
+
+  it('should be a function', function() {
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', function() {
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+
+  it('should be able to contain cards', function() {
+    expect(deck.cards).to.deep.equal([card1, card2, card3]);
+  });
+
+  it('should be able to count cards', function() {
+    expect(deck.countCards()).to.equal(3);
+  });
+});
