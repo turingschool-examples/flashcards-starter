a98f9eadae3411150b8612a062f3e46981375c00
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-01-29 13:12:12 -0700

    Refactor round class to take in deck as a parameter

--- src/round.js
+++ src/round.js
@@ -1,6 +1,21 @@
-class Round {
-  constructor() {
+var Turn = require('../src/turn');
 
+class Round {
+  constructor(cardDeck) {
+    this.currentCard = cardDeck.cardsInDeck[0];
+    this.turns = 0;
+    // this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard() {
+    return this.currentCard;
+  }
+
+  takeTurns(userGuess, cardDeck) {
+    var turn = new Turn(userGuess, this.currentCard);
+    this.turns += 1;
+    this.currentCard = cardDeck.cardsInDeck[this.turns];
+    // return turn.giveFeedback();
   }
 }
 
