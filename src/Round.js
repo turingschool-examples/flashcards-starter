aab90d3d46967aefa58f6172fe78c3a5c50c1e03
Megan Venetianer <megan.venetianer@gmail.com>
2020-01-29 13:26:32 -0700

    Game can calculate percentage of correct answers

--- src/Round.js
+++ src/Round.js
@@ -13,19 +13,20 @@ class Round {
   takeTurn(guess) {
     let newTurn = new Turn(guess, this.deck.cards[this.turns]);
     if (newTurn.evaluateGuess() === true) {
-      this.correctGuesses.push(turn.card.id);
+      this.correctGuesses.push(newTurn.card.id);
     } else {
-      this.incorrectGuesses.push(turn.card.id);
+      this.incorrectGuesses.push(newTurn.card.id);
     }
     let result = newTurn.giveFeedback();
     this.turns++;
     return result;
   }
   calculatePercentCorrect() {
-
+    let percentCorrect = (this.correctGuesses.length / (this.correctGuesses.length + this.incorrectGuesses.length))*100;
+    return percentCorrect;
   }
   endRound() {
-
+    return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the answers correctly!`
   }
 };
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -37,7 +37,59 @@ describe('Round', function() {
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
     expect(round.takeTurn('sea otter')).to.equal('correct!');
-  })
+  });
 
-  
+  it('should be able to evaluate/record a correct guess', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const deck = new Deck([card1]);
+    const round = new Round(deck);
+    round.takeTurn('sea otter');
+    expect(round.correctGuesses).to.deep.equal([1]);
+  });
+
+  it('should be able to evaluate/record an incorrect guess', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const deck = new Deck([card1]);
+    const round = new Round(deck);
+    round.takeTurn('pug');
+    expect(round.incorrectGuesses).to.deep.equal([1]);
+  });
+
+  it('should be able to update the turns count when a guess is made', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const deck = new Deck([card1]);
+    const round = new Round(deck);
+    round.takeTurn('sea otter');
+    expect(round.turns).to.equal(1);
+  });
+
+  it('should make the next card the current card after a turn has been made', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const deck = new Deck([card1, card2]);
+    const round = new Round(deck);
+    round.takeTurn('sea otter');
+    expect(round.returnCurrentCard()).to.equal(card2);
+  });
+
+  it('should be able to calculate and return the percentage of correct guesses', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const deck = new Deck([card1, card2]);
+    const round = new Round(deck);
+    round.takeTurn('sea otter');
+    round.takeTurn('appendix');
+    expect(round.calculatePercentCorrect()).to.equal(50);
+  });
+
+  it('should be able to print a message to the console when a the game is over', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const deck = new Deck([card1, card2]);
+    const round = new Round(deck);
+    round.takeTurn('sea otter');
+    round.takeTurn('appendix');
+    expect(round.endRound()).to.equal('** Round over! ** You answered 50% of the answers correctly!');
+  });
+
 });
