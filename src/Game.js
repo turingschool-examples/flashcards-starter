0b75e9b380ac4701525a71a7c0c1ea31b1b8a871
Erik Raith <raithhtiar@gmail.com>
2020-01-29 15:23:08 -0700

    [feat] Add Game Class fully tested
    The game class initiates the game logic after being called from index.js

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,28 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Deck = require('./Deck');
+const Round = require('./Round');
+const Card = require('./Card');
+
 
 class Game {
-  constructor() {}
-
+  constructor() {
+    this.currentRound = 0;
+  }
+  start(){
+    let deck = new Deck(this.newDeck(prototypeQuestions));
+    let round = new Round(deck);
+    this.printMessage(deck,round);
+    this.printQuestion(round);
+  }
+  newDeck(data){
+    let newDeck =[];
+    data.forEach(function(element,index){
+      newDeck.push(new Card(element.id,element.question,element.answers,element.correctAnswer))
+    });
+    return newDeck;
+  }
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
