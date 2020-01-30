d47dc55830151ac1bd4bf84a08adf228ec99dc98
Melissa Steele <melissa.steele09@gmail.com>
2020-01-30 12:37:28 -0700

    Update game tests

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,5 @@
 const data = require('./data');
+const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 const Round = require('../src/Round');
 const Deck = require('../src/Deck');
@@ -6,23 +7,27 @@ const Card = require('../src/Card');
 
 class Game {
   constructor() {
-    this.currentRound = null;
   }
-
   start() {
-      let deckOfCards = new Deck(this.createCards());
-      let round = new Round(deckOfCards);
-      this.printMessage(deckOfCards, round);
-      this.printQuestion(round);
-    }
-
-  printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
------------------------------------------------------------------------`)
+    const deck = new Deck(this.createCards());
+    const round = new Round(deck);
+    round.timer();
+    this.printMessage(deck, round);
+    this.printQuestion(round);
   }
-
+  createCards() {
+    let randomCards = data.prototypeData.sort(function() { return 0.5 - Math.random() } ).slice(0,10);
+    let deckCards = []
+    randomCards.forEach(function(card) {
+      deckCards.push(new Card(card.id, card.question, card.answers, card.correctAnswer));
+    });
+    return deckCards
+  }
+  printMessage(deck, round) {
+    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.\n-----------------------------------------------------------------------`)
+  }
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
   }
 }
 
