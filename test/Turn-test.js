0f73a623fd587169255acc39f25f76bdcc716f95
Melissa Steele <melissa.steele09@gmail.com>
2020-01-30 13:05:53 -0700

    Refactor turn tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,3 +1,4 @@
+
 const chai = require('chai');
 const expect = chai.expect;
 
@@ -6,38 +7,52 @@ const Card = require('../src/Card');
 
 describe('Turn', function() {
 
-  it('should be a function', function() {
-    const turn = new Turn();
-    expect(Turn).to.be.a('function');
-  });
+it('should be a function', function() {
+  const turn = new Turn();
+  expect(Turn).to.be.a('function');
+});
 
-  it('should be an instance of Turn', function() {
-    const card = new Card(1, "What is Robbie\'s favorite animal", ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const turn = new Turn("pug", card);
-    expect(turn).to.be.an.instanceof(Turn);
-  });
+it('should be an instance of Turn', function() {
+  const turn = new Turn();
+  expect(turn).to.be.an.instanceof(Turn);
+});
 
-  it('should return guess', function() {
-    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const turn = new Turn("pug", card);
-    expect(turn.returnGuess()).to.equal("pug");
-  });
+it('should store the user guess as a string', function() {
+  const turn = new Turn('guess');
+  expect(turn.guess).to.equal('guess');
+});
 
-  it('should return card', function() {
-    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const turn = new Turn("pug", card);
-    expect(turn.returnCard()).to.equal(card);
-  });
+it('should store a card object for the current card in play', function() {
+  const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  const turn = new Turn('guess', card);
+  expect(turn.card).to.deep.equal(card);
+});
 
-  it('should return true or false depending on users guess', function() {
-    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const turn = new Turn("sea otter", card);
-    expect(turn.evaluateGuess()).to.equal(true);
-  });
+it('should have a method that returns the guess', function() {
+  const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  const turn = new Turn('guess', card);
+  expect(turn.returnGuess()).to.equal('guess');
+});
 
-  it('should return feedback based on users guess', function() {
-    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const turn = new Turn("pug", card);
-    expect(turn.giveFeedback()).to.equal("Incorrect!");
-  });
+it('should have a method that returns the card', function() {
+  const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  const turn = new Turn('guess', card);
+  expect(turn.returnCard()).to.equal(card);
+});
+
+it('should have a method that returns a boolean if the guess matches the answer', function() {
+  const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  const turnOne = new Turn('guess', card);
+  const turnTwo = new Turn('object', card);
+  expect(turnOne.evaluateGuess()).to.equal(false);
+  expect(turnTwo.evaluateGuess()).to.equal(true);
+});
+
+it('should have a method that returns a correct/incorrect if the guess matches the answer', function() {
+  const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  const turnOne = new Turn('guess', card);
+  const turnTwo = new Turn('object', card);
+  expect(turnOne.giveFeedback()).to.equal('incorrect!');
+  expect(turnTwo.giveFeedback()).to.equal('correct!');
+});
 });
