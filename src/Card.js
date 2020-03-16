e951f94ff2daa96016a1abdd80a3bcd688d2e986
Joe Haefling <josephhaefling@gmail.com>
2020-03-16 17:42:34 -0600

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+class Card {
+  constructor(cardNum, question, possibleAnswers, correctAnswer) {
+    this.id = cardNum;
+    this.question = 'What allows you to define a set of related information using key-value pairs?';
+    this.answers = possibleAnswers;
+    this.correctAnswer = correctAnswer
+  }
+}
+
+
+
+
+
+
+
+
+
+module.exports = Card
