dc7bc2830631d7acf2b3ece046db7825afde479a
December G <dgarnetsmith@gmail.com>
2020-03-16 21:43:21 -0600

    Build out functionality for Turn class

---
+++ src/Turn.js
@@ -0,0 +1,25 @@
+class Turn {
+  constructor(guess, currentCard) {
+    this.guess = guess;
+    this.currentCard = currentCard;
+
+  }
+
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.currentCard;
+  }
+
+  evaluateGuess() {
+    return this.guess === this.currentCard.correctAnswer ? true : false;
+  }
+
+  giveFeedback() {
+    return this.guess === this.currentCard.correctAnswer ? 'correct!' : 'incorrect!';
+  }
+}
+
+module.exports = Turn;
