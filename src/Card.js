de045575e33f46f103dca0ef93eb91403727228c
“Lauren <lucerolaurenm@gmail.com>
2020-03-16 19:10:36 -0600

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+// Card represents a single flashcard
+import prototypeData from './data.js'
+
+class Card {
+  constructor() {
+    this.id = id
+    this.question = question
+    this.answers = possibleAnswers
+    this.correctAnswer = correctAnswer
+  }
+}
+
+module.exports = card
---
+++ src/Turn.js
@@ -0,0 +1,18 @@
+class Turn {
+  constructor() {
+    this.guess = guess
+    this.card = card
+  }
+  returnGuess() {
+    // returns the guess
+  }
+  returnCard() {
+    // returns the Card
+  }
+  evaluateGuess() {
+    // returns a boolean indicating if the user's guess matches the correct answer on the card
+  }
+  giveFeedback() {
+    // returns incorrect or correct based on guess
+  }
+}
