3be4dd5728854626b6fe81cf340b95bb14fcff44
Dave Pernitz <pernitz@uwalumni.com>
2020-03-18 18:43:31 -0600

    Fix git issues

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,6 +1,5 @@
 const chai = require('chai');
 const expect = chai.expect;
-
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 
@@ -19,26 +18,39 @@ describe('Deck', function() {
   })
 
   it('should take an array of cards', function() {
-    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
-    const card6 = new Card(6, 'What is an example of a mutator method?', ['sort()', 'map()', 'join()'], 'sort()')
-    const card13 = new Card(13, 'The callback function for map() returns a modified version of the current element.', ['true', 'false'], 'true' )
+    const card1 = new Card(1,
+      'What allows you to define a set of related information using key-value pairs?',
+      ['object', 'array', 'function'], 'object')
+    const card6 = new Card(6, 'What is an example of a mutator method?',
+      ['sort()', 'map()', 'join()'], 'sort()')
+    const card13 = new Card(13,
+      'The callback function for map() returns a modified version of the current element.',
+      ['true', 'false'], 'true' )
     const deck = new Deck([card1, card6, card13])
 
     expect(deck.cards.length).to.equal(3)
   })
 
   it('should be able to count how many cards it has', function() {
-    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
-    const card6 = new Card(6, 'What is an example of a mutator method?', ['sort()', 'map()', 'join()'], 'sort()')
+    const card1 = new Card(1,
+      'What allows you to define a set of related information using key-value pairs?',
+      ['object', 'array', 'function'], 'object')
+    const card6 = new Card(6, 'What is an example of a mutator method?',
+      ['sort()', 'map()', 'join()'], 'sort()')
     const deck = new Deck([card1, card6])
 
     expect(deck.countCards()).to.equal(2)
   })
 
   it('should know the details of the cards it has', function() {
-    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
-    const card6 = new Card(6, 'What is an example of a mutator method?', ['sort()', 'map()', 'join()'], 'sort()')
-    const card13 = new Card(13, 'The callback function for map() returns a modified version of the current element.', ['true', 'false'], 'true' )
+    const card1 = new Card(1,
+      'What allows you to define a set of related information using key-value pairs?',
+      ['object', 'array', 'function'], 'object')
+    const card6 = new Card(6, 'What is an example of a mutator method?',
+      ['sort()', 'map()', 'join()'], 'sort()')
+    const card13 = new Card(13,
+      'The callback function for map() returns a modified version of the current element.',
+      ['true', 'false'], 'true' )
     const deck = new Deck([card1, card6, card13])
 
     expect(deck.cards[0].id).to.equal(1)
--- test/Game-test.js
+++ test/Game-test.js
@@ -4,7 +4,6 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
-const Turn = require('../src/Turn')
 const Game = require('../src/Game')
 
 describe('Game', function() {
@@ -15,38 +14,75 @@ describe('Game', function() {
     expect(Game).to.be.a('function')
   })
 
-  it.skip('should be an instance of Game', function() {
+  it('should be an instance of Game', function() {
     const game = new Game()
 
     expect(game).to.be.an.instanceof(Game)
   })
 
-  it.skip('should keep track of current round', function() {
+  it('should keep track of current round', function() {
+    const game = new Game()
+    const game2 = new Game(2)
+    const game32 = new Game(32)
 
+    expect(game.currentRound).to.equal(1)
+    expect(game2.currentRound).to.equal(2)
+    expect(game32.currentRound).to.equal(32)
   })
 
   it.skip('should create cards', function() {
     const game = new Game()
-
+    const card1 = new Card(1,
+      'What allows you to define a set of related information using key-value pairs?',
+      ['object', 'array', 'function'], 'object')
+    const card6 = new Card(6,
+      'What is an example of a mutator method?',
+      ['sort()', 'map()', 'join()'], 'sort()')
+    const card13 = new Card(13,
+      'The callback function for map() returns a modified version of the current element.',
+      ['true', 'false'], 'true' )
+    const deck = new Deck([card1, card6, card13])
+    const round = new Round(deck)
     game.start()
 
-    // expect(game)
+    expect(deck.cards).to.deep.equal([card1, card6, card13])
   })
 
   it.skip('should put cards in the deck', function() {
+    const game = new Game()
+    const card1 = new Card(1,
+      'What allows you to define a set of related information using key-value pairs?',
+      ['object', 'array', 'function'], 'object')
+    const card6 = new Card(6, 'What is an example of a mutator method?',
+      ['sort()', 'map()', 'join()'], 'sort()')
+    const card13 = new Card(13,
+      'The callback function for map() returns a modified version of the current element.',
+      ['true', 'false'], 'true' )
+    const deck = new Deck([card1, card6, card13])
+    // const round = new Round(deck)
 
+    game.start()
+
+    expect(deck.cards).to.deep.equal([card1, card6, card13])
   })
 
   it.skip('should create a new round using the deck', function() {
+    const game = new Game()
+    const card1 = new Card(1,
+      'What allows you to define a set of related information using key-value pairs?',
+      ['object', 'array', 'function'], 'object')
+    const card6 = new Card(6,
+      'What is an example of a mutator method?',
+      ['sort()', 'map()', 'join()'], 'sort()')
+    const card13 = new Card(13,
+      'The callback function for map() returns a modified version of the current element.',
+      ['true', 'false'], 'true' )
+    const deck = new Deck([card1, card6, card13])
+    const round = new Round(deck)
 
-  })
+    game.start()
 
-  it.skip('should invoke printMessage to dipslay message', function() {
-
-  })
-
-  it.skip('should invoke printQuestion', function() {
-
+    expect(round).to.be.instanceof(Round)
   })
 
 })
