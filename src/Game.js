bf7d221397babc0029758d8029e0ddcad4c8dff0
Ed Ignot <edignot@gmail.com>
2020-03-18 22:09:31 -0600

    Game class created

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,35 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = 0;
+  }
 
   printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
 
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
+  }
+
+  start() {
+    const cards = prototypeQuestions.reduce(function(cards, card) {
+      cards.push(new Card(card.id, card.question, card.answers, card.correctAnswer));
+      return cards;
+    }, []);
+
+    const deck = new Deck(cards);
+    console.log(deck);
+    this.currentRound = new Round(deck);
+    this.printMessage(this.currentRound.deck, this.currentRound);
+    this.printQuestion(this.currentRound);
   }
 }
 
---
+++ test/Game-test.js
@@ -0,0 +1,51 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Game = require('../src/Game');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+
+describe('Game', function() {
+
+  beforeEach('game instance', function() {
+    game = new Game();
+  })
+
+  it('should be a function', function() {
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Game', function() {
+    expect(game).to.be.an.instanceof(Game);
+  });
+
+  it('should keep track of current round', function() {
+    expect(game.currentRound).to.equal(0);
+    console.log(game.currentRound);
+    game.start();
+    // expect(game.currentRound).to.equal(30);
+  });
+
+  it('should start a game', function() {
+
+  });
+
+  it('should put cards in a deck', function() {
+
+  });
+
+  it('should create new Round using Deck', function() {
+
+  });
+
+  it('should display message in the CLI', function() {
+
+  });
+
+  it('should run helper function to interaction via the CLI', function() {
+    // game.start();
+  });
+
+});
