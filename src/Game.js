da3fb98e1883ee6f11793e3c55368d7a11e40da0
December G <dgarnetsmith@gmail.com>
2020-03-18 23:35:24 -0600

    Build out initial funcitonality for start method

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,16 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
-  constructor() {}
+  constructor(round) {
+    this.round = [];
+    this.currentRound = round;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +19,20 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start() {
+    let cards = [];
+    prototypeQuestions.forEach(element => {
+      let singleCard = new Card(
+        element.id, element.question, element.answers, element.correctAnswer);
+        cards.push(singleCard)
+    });
+      let deck = new Deck(cards);
+      let round = new Round(deck);
+      this.round.push(round);
+      this.printMessage(deck, round);
+      this.printQuestion(round);
   }
 }
 
