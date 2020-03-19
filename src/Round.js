15bfac84152be5acbbda4db3ab832bcdd81a863b
December G <dgarnetsmith@gmail.com>
2020-03-18 23:36:09 -0600

    Edit class to reference turn class methods in takeTurn method

--- src/Round.js
+++ src/Round.js
@@ -1,34 +1,42 @@
+const Turn = require('../src/Turn');
+
 class Round {
   constructor(deck) {
     this.deck = deck;
     this.turns = 0;
     this.incorrectGuesses = [];
+    this.isAnswerCorrect = null;
   }
 
   returnCurrentCard() {
-    if(this.turns === 0) {
-      return this.currentCard = this.deck.allCards[0];
-    } else {
-      return this.currentCard = this.deck.allCards[this.turns];
-    }
+    return this.currentCard = this.deck.allCards[this.turns];
   }
 
   takeTurn(guess) {
     this.turns++;
+    let turn = new Turn(guess, this.currentCard);
     let rightAnswer = this.currentCard['correctAnswer']
-    if (guess === rightAnswer) {
-      return 'correct!'
-    } else {
+  //   if (guess === rightAnswer) {
+  //     return 'correct!'
+  //   } else {
+  //     this.incorrectGuesses.push(this.currentCard.id);
+  //     return 'incorrect!'
+  // }
+    turn.evaluateGuess();
+    this.isAnswerCorrect = turn.evaluateGuess();
+    if (turn.evaluateGuess() === false) {
       this.incorrectGuesses.push(this.currentCard.id);
-      return 'incorrect!'
+      // this.isAnswerCorrect = false;
     }
-    this.currentCard = this.deck.allCards[this.turns];
+    // else {
+    //   this.isAnswerCorrect = true;
+    // }
+    return turn.giveFeedback();
   }
 
   calculatePercentCorrect() {
     let correctGuesses = this.deck.allCards.length -this.incorrectGuesses.length;
     let percentageCorrect = (correctGuesses/this.deck.allCards.length) * 100;
-
     return Math.floor(percentageCorrect);
   }
 
