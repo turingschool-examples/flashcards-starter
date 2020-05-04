26afa1c0832636fc96d579fc26378ae341f413ac
Bill Wilke <billwilke2@gmail.com>
2020-05-04 17:41:50 -0600

    Adds tests for returnCard and returnGuess

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
+const assert = require('chai').assert;
 
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
@@ -14,10 +15,26 @@ describe('Turn', function() {
   it('should be an instance of Turn', function() {
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
-  })
+  });
 
   it('should have a user guess', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ["object", "array", "function"], "object")
-    const turn = new Turn('array', card)
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ["object", "array", "function"], "object");
+    const turn = new Turn('array', card);
  });
+
+  it('should return guess', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ["object", "array", "function"], "object");
+    const turn = new Turn('array', card);
+
+    assert.equal(turn.userGuess, 'array');
+    assert.equal(turn.returnGuess(), 'array');
+ });
+
+ it('should return Card', function() {
+   const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ["object", "array", "function"], "object");
+   const turn = new Turn('array', card);
+
+   assert.equal(turn.card, card);
+   assert.equal(turn.returnCard(), card);
+})
 });
