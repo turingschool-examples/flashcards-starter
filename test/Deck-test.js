6e1efac93c26a6281278a541d4b8fde56374c944
Andy Tom <andrew.tom@colorado.edu>
2020-05-04 16:39:18 -0600

    Add tests and functionality to initialize Deck with an array of Card objects

---
+++ test/Deck-test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const expect = chai.expect;
+const assert = chai.assert;
+
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Deck', function() {
+  it('should be a function', function() {
+    assert.deepEqual(typeof Deck, 'function')
+  });
+
+  it('should store given cards to a deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    assert.deepEqual(deck.cards, [card1, card2, card3]);
+  });
+  it('should be able to count the number of cards in the deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    assert.deepEqual(deck.countCards(), 3);
+  });
+});
