f7647b9e072d174cdc52b7417d1905fa09148c70
Andy Tom <andrew.tom@colorado.edu>
2020-05-05 08:55:33 -0600

    Adds tests and functionality to Round to take turns, return current card, and store deck

--- src/Round.js
+++ src/Round.js
@@ -1,14 +1,24 @@
+const Turn = require('../src/Turn');
 class Round {
   constructor(deck) {
     this.deck = deck;
     this.turns = 0;
     this.incorrectGuesses = [];
+    this.turn = null;
   }
   returnCurrentCard() {
-    return this.deck.cards[0];
+    return this.deck.cards[this.turns];
   }
-  takeTurn(){
+  takeTurn(guess) {
+    this.turn = new Turn (guess, this.deck.cards[this.turns]);
     this.turns++;
+
+    if(!this.turn.evaluateGuess()){
+      console.log('yay')
+      this.incorrectGuesses.push(guess)
+      console.log(this.incorrectGuesses)
+    };
+    return this.turn.giveFeedback();
   }
 }
 
