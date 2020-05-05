8fdd29eba443495447f54a648d6b22a048dd879b
Andy Tom <andrew.tom@colorado.edu>
2020-05-04 18:29:38 -0600

    Adds ability to update turns

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,14 @@
 class Round {
   constructor(deck) {
     this.deck = deck;
+    this.turns = 0;
+    this.incorrectGuesses = [];
+  }
+  returnCurrentCard() {
+    return this.deck.cards[0];
+  }
+  takeTurn(){
+    this.turns++;
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -8,6 +8,17 @@ describe('Round', function() {
   it('should be a function' , function(){
     assert.deepEqual(typeof Round, 'function');
   });
+
+  it('should start out with 0 turns and no incorrect guesses', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    assert.deepEqual(round.turns, 0);
+    assert.deepEqual(round.incorrectGuesses, []);
+
+  })
   it('should store a deck', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
@@ -15,5 +26,22 @@ describe('Round', function() {
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
     assert.deepEqual(round.deck, deck);
-});
+  });
+  it('should be able to return current card returnCurrentCard()', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    assert.deepEqual(round.returnCurrentCard(), card1);
+  });
+  it('should update turns count', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    round.takeTurn();
+    assert.deepEqual(round.turns, 1);
+  });
 });
