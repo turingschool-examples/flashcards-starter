0f0bca743d7de5d39cca9091125dc99846f746be
emontealvo <edwin.montealvo@gmail.com>
2020-05-04 20:53:24 -0600

    Add turn class and corresponding test for class constructor

---
+++ test/Turn-test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn')
+
+describe('Turn', function() {
+
+  it('shoulb be a function', function() { 
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+});
