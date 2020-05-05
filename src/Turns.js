ffb982d1c16b0262967d22f724a015212ac8eaed
Charles Wefso <cwefso@gmail.com>
2020-05-04 20:31:53 -0600

    Create turns.js and turns-test.js

---
+++ src/Turns.js
@@ -0,0 +1,10 @@
+class Turns {
+  constructor(id, question, answers, correctAnswer) {
+    this.id = id;
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  }
+}
+
+module.exports = Turns
