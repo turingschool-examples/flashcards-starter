142ba48365d700f6d939e147d686c4e8c78a8d03
Charles Wefso <cwefso@gmail.com>
2020-05-05 20:22:42 -0600

    Gives Deck class countCards method to return amount of cards in deck

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -16,7 +16,7 @@ describe('Deck', function() {
     expect(deck).to.be.an.instanceof(Deck);
   }); 
 
-  it('should be an initialized with an array of Card objects', function() {
+  it('should be initialized with an array of Card objects', function() {
     const card1 = new Card( 1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card( 2, 'What is a comma-separated list of related values?', ["array", "object", "function"], "array");
     const card3 = new Card( 3, 'What type of prototype method directly modifies the existing array?', ["mutator method", "accessor method", "iteration method"], "mutator method");
@@ -26,6 +26,14 @@ describe('Deck', function() {
     expect(deck.cards).to.deep.equal([cards]);
   }); 
 
-
+  it('should know how many Cards are in the Deck', function() {
+    const card1 = new Card( 1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card( 2, 'What is a comma-separated list of related values?', ["array", "object", "function"], "array");
+    const card3 = new Card( 3, 'What type of prototype method directly modifies the existing array?', ["mutator method", "accessor method", "iteration method"], "mutator method");
+    const cards = [card1, card2, card3];
+    const deck = new Deck([card1, card2, card3]);
+    let cardCount = deck.countCards();
+    console.log(cardCount);
+  }); 
 
 });
