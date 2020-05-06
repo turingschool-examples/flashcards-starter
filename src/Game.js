cb3673002bb3ffb2b5f935fa2564df1acff8787e
Andy Tom <andrew.tom@colorado.edu>
2020-05-05 19:27:15 -0600

    Adds functionality to game to allow for take turn and user is able to play

--- src/Game.js
+++ src/Game.js
@@ -2,9 +2,13 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 class Game {
   constructor() {
     this.cards = [];
+    this.round;
+    this.currentRound = this.round;
   }
 
   printMessage(deck, round) {
@@ -15,9 +19,14 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
-  start(){
-    this.cards = prototypeQuestions.map(question => new Card(question.id, question.question, question.answers, question.CorrectAnswer));
-    
+  start() {
+    this.cards = prototypeQuestions.map(element => new Card(element.id, element.question, element.answers, element.correctAnswer));
+    this.deck = new Deck(this.cards);
+    this.round = new Round(this.deck);
+    this.currentRound = this.round;
+    this.printMessage(this.deck, this.currentRound);
+    this.printQuestion(this.round);
+
   }
 }
 
