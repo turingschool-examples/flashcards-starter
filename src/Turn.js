379a1592864811225b58797db8411c9df2038335
Stephanie Norton <steph.jane.norton@gmail.com>
2020-06-29 15:55:00 -0600

    Add file for turn class and create class

---
+++ src/Turn.js
@@ -0,0 +1,19 @@
+class Turn {
+    constructor(guess, card) {
+        this.guess = guess;
+        this.card = card;
+    }
+}
+
+
+module.exports = Turn;
+
+
+
+
+// Your Turn class should meet the following requirements:
+// Instantiated with two arguments - a string(that represents a user’s guess to the question), and a Card object for the current card in play.
+//     returnGuess: method that returns the guess
+// returnCard: method that returns the Card
+// evaluateGuess: method that returns a boolean indicating if the user’s guess matches the correct answer on the card
+// giveFeedback - method that returns either ‘incorrect!’ or ‘correct!’ based on whether the guess is correct or not.
