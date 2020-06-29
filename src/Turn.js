095ee6e51ed9cffe816383f3b9c4335085725ba8
Stephanie Norton <steph.jane.norton@gmail.com>
2020-06-29 17:51:02 -0600

    Create methods for turn class

--- src/Turn.js
+++ src/Turn.js
@@ -3,10 +3,37 @@ class Turn {
         this.guess = guess;
         this.card = card;
     }
+    
+    returnGuess() {
+        console.log(this.guess)
+        return this.guess; 
+        
+    }
+    
+    returnCard() {
+        console.log(this.card);
+        return this.card; // works without the this keyword? Do I need it??
+
+    }
+
+    evaluateGuess() { // refactor?
+      if (this.guess === this.card.correctAnswer) {
+          console.log('true')
+          return true;
+        } else {
+          console.log('false')
+          return false;
+        }
+      }
+            
+    // if guess is equal to correcAnswer, return true;
+    // else return false;
+
 }
 
 
 module.exports = Turn;
+
 
 
 
