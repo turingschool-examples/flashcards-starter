2066507b18a03ae4421cbc18e113a3d8546d3c9b
Hanna Kim <hannakim91@gmail.com>
2020-06-30 16:03:24 -0600

    Set up testing template for Turn

---
+++ test/Turn-test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+  var Turn;
+  beforeEach(function() {
+    Turn = new Turn();
+  })
+});
+
+it.skip
