e639a73a80b474059bd2101b2e9677578c0d5c9f
Greyson Elkins <greysonelkins@gmail.com>
2020-06-29 21:39:04 -0600

    Change test files to use arrow functions

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,18 +4,18 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
-describe('Deck', function() {
+describe('Deck', () => {
   
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Deck).to.be.a('function');
   });
 
-  it('should instantiate a new deck', function() {
+  it('should instantiate a new deck', () => {
     const deck = new Deck();
     expect(deck).to.be.an.instanceOf(Deck);
   });
 
-  it('should be initialized with an array of cards', function() {
+  it('should be initialized with an array of cards', () => {
     const card1 = new Card(1, "How do you fly?", ["you don't", "you flap your arms", "you steal an airplane"], "you don't");
     const card2 = new Card(2, "Can I eat a hotdog?", ["Yes", "No", "You can eat several"], "You can eat several");
     const card3 = new Card(3, "Dogs or cats?", ["dogs", "cats", "robots"], "robots");
@@ -23,7 +23,7 @@ describe('Deck', function() {
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
 
-  it('should know how many cards it has, even if it has none', function() {
+  it('should know how many cards it has, even if it has none', () => {
     const deck = new Deck();
     expect(deck.cardCount).to.equal(0);
     const card1 = new Card(1, "How do you fly?", ["you don't", "you flap your arms", "you steal an airplane"], "you don't");
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,31 +4,31 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
-describe('Turn', function() {
+describe('Turn', () => {
     
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Turn).to.be.a('function');
   });
 
-  it('should be an instance of Turn', function () {
+  it('should be an instance of Turn', () => {
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-  it("should be able to return a user's guess", function() {
+  it("should be able to return a user's guess", () => {
     const turn = new Turn('Is it option A?');
     const guess = turn.returnGuess();
     expect(guess).to.equal('Is it option A?');
   });
     
-  it('it should be able to return the current card', function() {
+  it('it should be able to return the current card', () => {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('Is it option A?', card);
     const turnCard = turn.returnCard(); 
     expect(turnCard).to.deep.equal(card);
   });
 
-  it("should be able to tell if the user's guess is correct", function() {
+  it("should be able to tell if the user's guess is correct", () => {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('Is  it option A?', card);
     const incorrectAnswer = turn.evaluateGuess(); 
@@ -38,7 +38,7 @@ describe('Turn', function() {
     expect(correctAnswer).to.equal(true);
   });
 
-  it('should be able to say "incorrect!" and "correct!" depending on whether the user\'s guess is correct', function() {
+  it('should be able to say "incorrect!" and "correct!" depending on whether the user\'s guess is correct', () => {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('Is it option A?', card);
     const response = turn.giveFeedback();
