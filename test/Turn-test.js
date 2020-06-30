8aa1d4fdfab53b61cf56871eefdce153b3cd12a9
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-06-29 20:06:37 -0600

    Create turn class and constructor

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,12 +6,12 @@ const Turn = require('../src/Turn');
 
 describe('Turn', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
-  it.skip('should be an instance of Turn', function() {
+  it('should be an instance of Turn', function() {
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
