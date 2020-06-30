d4395470c540918d35cad81441bd3a23b0adcd1b
Sean Leas <phoenix_24leas.com>
2020-06-29 21:02:44 -0600

    Create and complete test 2 of Deck.js and removed beforeEach code in Turn.js

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Deck = require('../src/Deck')
-
+const Card = require('../src/Card')
 describe('Deck', function() {
   it('Should be a function', function() {
     const deck = new Deck()
@@ -10,5 +10,14 @@ describe('Deck', function() {
     expect(Deck).to.be.a.a('function')
   })
 
+  it('Will accept an array of cards', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    expect(deck.cards).to.deep.equal([card1, card2, card3])
+  })
   
 })
