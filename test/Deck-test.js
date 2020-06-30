fe1f390feec10493b5593d5afbecc859f662027f
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-06-30 17:10:07 -0600

    Create deck class constructor

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -8,12 +8,12 @@ const prototypeData = require('../src/data');
 
 describe('Deck', function() {
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     const deck = new Deck();
     expect(Deck).to.be.a('function');
   });
 
-  it.skip('should be an instance of Deck', () => {
+  it('should be an instance of Deck', () => {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
