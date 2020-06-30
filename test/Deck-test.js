f7bd5f3960b863e40db539fea4818167f2ca8d5a
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-06-30 17:13:47 -0600

    Create cards parameter and property

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -18,17 +18,17 @@ describe('Deck', function() {
     expect(deck).to.be.an.instanceof(Deck);
   });
   
-  it.skip('should be initialized with an array of card objects', () => {
+  it('should be initialized with an array of card objects', () => {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal?', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
 
     const deck = new Deck([card1, card2, card3]);
 
-    expect(deck.cards).to.equal([card1, card2, card3]);
+    expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
   
-  it.skip('should store said array of card objects', () => {
+  it('should store infromation from card objects', () => {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal?', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
