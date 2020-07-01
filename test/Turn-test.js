6be7e4ecaf346c7964e728ea8e9b00fe963dbb53
Naomi Ware <nware1066@yahoo.com>
2020-06-30 19:20:48 -0600

    create initial tests for Turn class

---
+++ test/Turn-test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+
+
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+  const turn = new Turn();
+  expect(turn).to.be.an.instanceof(Turn);
+});
