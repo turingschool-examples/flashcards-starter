a1a3dd4feda005c4d1ce1e2ba4613ea0787edd24
Hanna Kim <hannakim91@gmail.com>
2020-07-01 07:59:44 -0600

    Set up beforeEach() in describe block of test file

---
+++ test/Deck-test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Deck happy path', function() {
+  let card1;
+  let card2;
+  let card3;
+  let deck;
+  beforeEach(function() {
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = new Card(30, 'What type of methods are functions that allow you to manipulate the value of a particular data type or class?', ['prototype method', 'object', 'callback function'], 'prototype method');
+    card3 = new Card(15, 'The callback function for reduce() takes in an accumulator and a current element.', ['true', 'false'], 'true');
+    deck = new Deck([card1, card2, card3]);
+  });
+
+  it('should be a function', function() {
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should initialize a deck with an array of Card objects')
+});
