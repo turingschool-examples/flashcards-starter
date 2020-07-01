378b28f280011b4518421c876c291a3119dd797d
Naomi Ware <nware1066@yahoo.com>
2020-07-01 16:50:11 -0600

    set up test for Deck class

---
+++ test/Deck-test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+const Deck = require('../src/Deck');
+
+describe('Deck', function() {
+  let card1, card2, card3, deck1, deck2;
+
+  beforeEach(function() {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck1 = new Deck([card1, card2, card3]);
+    deck2 = new Deck([card1, card2]);
+  });
+});
