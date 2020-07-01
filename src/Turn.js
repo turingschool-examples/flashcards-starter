320858ff0f55ecf5fc96a432f0593ccfdd26fee5
Naomi Ware <nware1066@yahoo.com>
2020-06-30 20:44:25 -0600

    removed whitespace from files

--- src/Turn.js
+++ src/Turn.js
@@ -23,8 +23,6 @@ class Turn {
       return 'incorrect!'
     }
   }
-
-
 }
 
 
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,8 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-
-
 const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
