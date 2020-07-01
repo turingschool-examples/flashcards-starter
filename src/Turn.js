b0c7a02130c6cb1435d9ba18352c764e59183e02
Hanna Kim <hannakim91@gmail.com>
2020-07-01 06:55:13 -0600

    Complete Turn happy path

--- src/Turn.js
+++ src/Turn.js
@@ -1,3 +1,21 @@
-class Turn {}
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess,
+    this.card = card,
+    this.evaluation = false;
+  }
+
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.card;
+  }
+
+  evaluateGuess() {
+    return this.evaulation = (this.card.correctAnswer === this.guess) ? true : false;
+  }
+}
 
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,14 +2,51 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
-describe('Turn', function() {
-  var Turn;
+describe('Turn happy path', function() {
+  let turn;
+  let card;
   beforeEach(function() {
-    Turn = new Turn();
-  })
+    turn = new Turn('object', card);
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  });
+
+  it('should be a function', function() {
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it('should store a user\'s guess', function() {
+    expect(turn.guess).to.equal('object');
+  });
+
+  it('should return a user\'s guess', function() {
+    expect(turn.returnGuess()).to.equal('object');
+  });
+
+  it('should store a card as an object', function() {
+    expect(turn.card).to.deep.equal({ id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object' });
+  });
+
+  it('should return a card', function() {
+    expect(turn.returnCard()).to.deep.equal({ id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object' });
+  });
+
+  it('should return a boolean indicating if the user\'s guess matches the correct answer on the card', function() {
+    expect(turn.evaluateGuess()).to.equal(true || false);
+  });
+
+  it('should return true if the user\'s guess matches the correct answer on the card', function() {
+    expect(turn.evaluateGuess()).to.equal(true);
+  });
+
+  it('should give feedback on whether the guess is correct or not', function() {
+    expect(turn.giveFeedback()).to.equal('correct' || 'incorrect');
+  });
+
 });
 
-it.skip('should be a function', function() {
-  expect(Turn).to.be.a('function');
-});
