92774a58359aa0efef5c2e93f5ec87ac18c6f90d
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-07-01 19:19:10 -0600

    Implement class and pass all tests

---
+++ src/Round.js
@@ -0,0 +1,38 @@
+const Turn = require("./Turn");
+
+class Round {
+  constructor(deck) {
+    this.deck = deck;
+    this.currentTurn = {};
+    this.turns = 0;
+    // this.currentCardIndex = 0;
+    this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard = () => this.deck.cards[this.turns];
+
+  takeTurn(guess) {
+    let cardId = this.returnCurrentCard().id;
+    this.currentTurn = new Turn(guess, this.returnCurrentCard());
+    this.turns++;
+    if (!this.currentTurn.evaluateGuess()) {
+      this.incorrectGuesses.push(cardId);
+    }
+    return this.currentTurn.giveFeedback();
+  }
+
+  calculatePercentageCorrect() {
+    let correctAnswers = this.turns - this.incorrectGuesses.length;
+    let percentageCorrect = (correctAnswers / this.turns) * 100;
+    return Math.floor(percentageCorrect);
+  }
+
+  endRound() {
+    let percentage = this.calculatePercentageCorrect();
+    let endMessage = `** Round over! ** You answered ${percentage}% of the questions correctly!`;
+    console.log(endMessage);
+    return process.exit();
+  }
+}
+
+module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -8,42 +8,48 @@ const Round = require('../src/Round');
 
 describe('Round', function() {
 
-  it.skip('should be a function', () => {
+  // beforeEach(function() {
+    const card1 = new Card(37, "Why is the guitar like this?", ["physics", "hippies", "cus this is the guitar"], "cus this is the guitar");
+    const card2 = new Card(42, "What color is cooked spaghetti in space?", ["yellow", "black", "transparent"], "transparent");
+    const card3 = new Card(90, "What time of day is the best time for dreams", ["tomorrow", "all day", "sleeping"], "tomorrow");
+  // });
+
+  it('should be a function', () => {
     const round = new Round();
     expect(Round).to.be.a('function');
   });
 
-  it.skip('should be an instance of Round', () => {
+  it('should be an instance of Round', () => {
     const round = new Round();
     expect(round).to.be.an.instanceof(Round);
   });
 
-  it.skip('should take in a deck of cards', () => {
-    const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
-    const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
-    const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
+  it('should take in a deck of cards', () => {
+    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
+    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
+    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
     
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
 
-    expect(round.deck[1].answer).to.equal('transparent');
+    expect(round.deck.cards[1].id).to.equal(42);
   });
 
-  it.skip('should make the first card in the deck the \'currentCard\'', () => {
-    const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
-    const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
-    const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
+  it('should make the first card in the deck the \'currentCard\'', () => {
+    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
+    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
+    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
 
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
 
-    expect(round.currentCard).to.equal(card1);
+    expect(round.returnCurrentCard()).to.equal(card1);
   });
   
-  it.skip('should be able to return the currentCard', () => {
-    const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
-    const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
-    const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
+  it('should be able to return the currentCard', () => {
+    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
+    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
+    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
 
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
@@ -57,10 +63,10 @@ describe('Round', function() {
     });
   });
 
-  it.skip('should instantiate a new turn when a guess is made', () => {
-    const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
-    const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
-    const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
+  it('should instantiate a new turn when a guess is made', () => {
+    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
+    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
+    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
 
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
@@ -70,10 +76,10 @@ describe('Round', function() {
     expect(round.currentTurn).to.be.an.instanceof(Turn);
   });
 
-  it.skip('should be able to keep track of how many turns have been taken', () => {
-    const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
-    const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
-    const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
+  it('should be able to keep track of how many turns have been taken', () => {
+    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
+    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
+    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
 
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
@@ -81,19 +87,74 @@ describe('Round', function() {
     round.takeTurn('cus this is the guitar');
 
     expect(round.turns).to.equal(1);
+    
+    round.takeTurn('transparent');
+
+    expect(round.turns).to.equal(2);
   });
 
-  it.skip('should be able to store incorrect guesses', () => {
-    const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
-    const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
-    const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
+  it('should make the next card the currentcard when a turn is taken', () => {
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    
+    let takeAGuess = round.takeTurn('cus this is the guitar');
+
+    expect(round.returnCurrentCard()).to.equal(card2);
+  });
+
+  it('should be able to evaluate guesses', () => {
+    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
+    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
+    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
 
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
     
-    round.takeTurn('cus this is the guitar');
+    let takeAGuess = round.takeTurn('cus this is the guitar');
 
-    expect(round.incorrectGuesses).to.deep.equal([]);
+    expect(takeAGuess).to.equal('correct!');
+
+    takeAGuess = round.takeTurn('transparent');
+
+    expect(takeAGuess).to.equal('correct!');
+
+    takeAGuess = round.takeTurn('sleeping');
+
+    expect(takeAGuess).to.equal('incorrect!');
   });
 
+  it('should be able to store incorrect guesses', () => {
+    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
+    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
+    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    
+    round.takeTurn('hippies');
+    
+    expect(round.incorrectGuesses).to.deep.equal([37]);
+  });
+  
+  it('should be able to calculate the percentage of questions answered correctly', () => {
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    round.takeTurn('cus this is the guitar');
+    round.takeTurn('transparent');
+    round.takeTurn('sleeping');
+    
+    expect(round.calculatePercentageCorrect()).to.equal(66);
+  });
+  
+  // it('should be able to print "** Round over! ** You answered <>% of the questions correctly!" to the console', () => {
+  //   const deck = new Deck([card1, card2, card3]);
+  //   const round = new Round(deck);
+  
+  //   round.takeTurn('cus this is the guitar');
+  //   round.takeTurn('transparent');
+  //   round.takeTurn('sleeping');
+    
+  //   expect(round.endRound).to.equal(process.exit());
+  // });
 });
