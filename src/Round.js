488380c1b169a2c84723fe395ba1cec2bf37fab9
Naomi Ware <nware1066@yahoo.com>
2020-07-01 19:03:29 -0600

    add functionality for storing incorrect Guesses in takeTurn method

--- src/Round.js
+++ src/Round.js
@@ -14,6 +14,9 @@ class Round{
   takeTurn(guess) {
     const turn = new Turn(guess, this.returnCurrentCard());
     this.turns++;
+    if (!turn.evaluateGuess()) {
+      this.incorrectGuesses.push(this.returnCurrentCard().id);
+    }
     this.deck.cards.shift();
     return turn.returnFeedback();
   }
