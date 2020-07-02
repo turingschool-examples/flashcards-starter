66675a1f2acb2d9fb338aabfcc8c4c53b597ba33
Naomi Ware <nware1066@yahoo.com>
2020-07-01 19:01:36 -0600

    added test for storing incorrectGuesses in takeTurn method

--- test/Round-test.js
+++ test/Round-test.js
@@ -59,6 +59,12 @@ describe('Round', function() {
        expect(sadFeedback).to.equal('incorrect!');
      });
 
-  });
 
+    it('should store the id number of card numbers related to incorrect guesses in the incorrectGuesses array', function() {
+      round.takeTurn('sea otter');
+      expect(round.incorrectGuesses).to.deep.equal([]);
+      round.takeTurn('spleen');
+      expect(round.incorrectGuesses).to.deep.equal([14]);
+    });
+  });
 });
