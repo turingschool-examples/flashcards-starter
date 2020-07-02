0773f95bce9159f8177d855058f9759127cd2b02
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-07-01 19:22:37 -0600

    Remove commented out code

--- test/Round-test.js
+++ test/Round-test.js
@@ -8,11 +8,9 @@ const Round = require('../src/Round');
 
 describe('Round', function() {
 
-  // beforeEach(function() {
-    const card1 = new Card(37, "Why is the guitar like this?", ["physics", "hippies", "cus this is the guitar"], "cus this is the guitar");
-    const card2 = new Card(42, "What color is cooked spaghetti in space?", ["yellow", "black", "transparent"], "transparent");
-    const card3 = new Card(90, "What time of day is the best time for dreams", ["tomorrow", "all day", "sleeping"], "tomorrow");
-  // });
+  const card1 = new Card(37, "Why is the guitar like this?", ["physics", "hippies", "cus this is the guitar"], "cus this is the guitar");
+  const card2 = new Card(42, "What color is cooked spaghetti in space?", ["yellow", "black", "transparent"], "transparent");
+  const card3 = new Card(90, "What time of day is the best time for dreams", ["tomorrow", "all day", "sleeping"], "tomorrow");
 
   it('should be a function', () => {
     const round = new Round();
@@ -25,10 +23,6 @@ describe('Round', function() {
   });
 
   it('should take in a deck of cards', () => {
-    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
-    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
-    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
-    
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
 
@@ -36,10 +30,6 @@ describe('Round', function() {
   });
 
   it('should make the first card in the deck the \'currentCard\'', () => {
-    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
-    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
-    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
-
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
 
@@ -47,10 +37,6 @@ describe('Round', function() {
   });
   
   it('should be able to return the currentCard', () => {
-    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
-    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
-    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
-
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
     const showMeTheCard = round.returnCurrentCard(); 
@@ -64,10 +50,6 @@ describe('Round', function() {
   });
 
   it('should instantiate a new turn when a guess is made', () => {
-    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
-    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
-    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
-
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
     
@@ -77,10 +59,6 @@ describe('Round', function() {
   });
 
   it('should be able to keep track of how many turns have been taken', () => {
-    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
-    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
-    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
-
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
     
@@ -103,10 +81,6 @@ describe('Round', function() {
   });
 
   it('should be able to evaluate guesses', () => {
-    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
-    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
-    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
-
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
     
@@ -124,10 +98,6 @@ describe('Round', function() {
   });
 
   it('should be able to store incorrect guesses', () => {
-    // const card1 = new Card(37, 'Why is the guitar like this?', ['physics', 'hippies', 'cus this is the guitar'], 'cus this is the guitar');
-    // const card2 = new Card(42, 'What color is cooked spaghetti in space?', ['yellow', 'black', 'transparent'], 'transparent');
-    // const card3 = new Card(90, 'What time of day is the best time for dreams', ['tomorrow', 'all day', 'sleeping'], 'tomorrow');
-
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
     
