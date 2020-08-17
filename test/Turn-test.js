3b2a80f98845a8c9ae53f7971d15bca48834863f
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-17 19:30:04 -0400

    Add test for returnCard function

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -18,14 +18,15 @@ describe('Turn', function() {
 
   it('should return a guess', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('pug', card)
-    expect(turn.returnGuess()).to.equal('pug');
+    const turn = new Turn('object', card)
+    expect(turn.returnGuess()).to.equal('object');
   });  
 
-//   it('should store a list of possible answers', function() {
-//     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-//     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
-//   });  
+  it('should return the card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card)
+    expect(turn.returnCard()).to.deep.equal(card);
+  });  
 
 //   it('should store the correct answer', function() {
 //     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
