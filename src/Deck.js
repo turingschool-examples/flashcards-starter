79a73775969545ae448da813cb980769e86633cf
Chriscastanuela <chriscastanuela@ymail.com>
2020-08-17 18:22:54 -0600

    Add array test completion

---
+++ src/Deck.js
@@ -0,0 +1,10 @@
+class Deck {
+    constructor(arrayOfCards) {
+        this.deck = arrayOfCards;
+    }
+    countCards() {
+        // 
+    }
+}
+
+module.exports = Deck;
---
+++ test/Deck-test.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+
+describe('Deck', function() {
+
+  it('should be a function', function() {
+    const deck = new Deck();
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', function() {
+    
+    const deck = new Deck();
+    
+    expect(deck).to.be.an.instanceof(Deck);
+
+  });
+
+  it('should store an array of cards', function() {
+
+    const card1 = new Card(1, "Guess who?", ["Chris", "Jeff"], "Chris");
+    const card2 = new Card(2, "Guess who?", ["Chris", "Jeff"], "Jeff");
+
+    const deck = new Deck([card1, card2]);
+
+    expect(deck.deck).to.eql([card1, card2]);
+  });  
+
+  it.skip('should be able to return the guess', function() {
+    const card = new Card(1, "Guess who?", ["Chris", "Jeff"], "Chris")
+
+    const turn = new Turn("Chris", 1);
+
+    expect(turn.returnGuess()).to.equal("Chris")
+  });
+});
