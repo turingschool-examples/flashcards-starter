9221342473f10d28371698154cbb4a57aef4906b
Kristi Miller <kristi.ann.miller@gmail.com>
2020-08-18 16:06:23 -0600

    Add functionality to Deck class

---
+++ src/Deck.js
@@ -0,0 +1,6 @@
+class Deck {
+  constructor(cards) {
+    this.cards = cards;
+  }
+}
+module.exports = Deck;
--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,17 +6,17 @@ const Card = require('../src/Card');
 
 describe('Deck', () => {
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     const deck = new Deck();
     expect(Deck).to.be.a('function');
   });
 
-  it.skip('should be an instance of Deck', () => {
+  it('should be an instance of Deck', () => {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
 
-  it.skip('should hold an array of Card objects', () => {
+  it('should hold an array of Card objects', () => {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(2, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card3 = new Card(3, 'What kind of bear is best?', ['panda bear', 'balck bear', 'that\'s a ridiculous question'], 'black bear');
@@ -24,3 +24,4 @@ describe('Deck', () => {
     const deck = new Deck([card1, card2, card3])
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
+});
