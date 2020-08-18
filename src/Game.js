6aac7d2ea7c4b5cce421cb0d10ceb4d3f42a85b1
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-18 14:27:22 -0400

    Add require files, add beginning of start method

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,19 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('../src/Round');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
 
 class Game {
   constructor() {}
-
+  start() {
+    let cards = []
+    prototypeQuestions.forEach((x => {
+      cards.push(new Card(x))
+    }))
+    let deck = new Deck(cards);
+  }
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
