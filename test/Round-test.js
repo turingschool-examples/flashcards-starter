19cf2b1b2236d0fed066f5332267e41ca901b9a6
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-18 18:53:04 -0400

    Adjust test suite to match new methods

--- test/Round-test.js
+++ test/Round-test.js
@@ -4,17 +4,13 @@ const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
-let card;
-let turn;
-let deck;
-let round;
 
 describe('Round', function() {
   beforeEach((done) => {
     card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     card2 = new Card(2, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     card3 = new Card(3, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    turn = new Turn('object', card);
+    turn = new Turn();
     deck = new Deck([card1, card2, card3]);
     round = new Round(deck);
     done();
