c46593c4c75a3cfd3e30e120656debb80908631f
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-17 20:18:52 -0400

    Add test for evaluateGuess

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -33,4 +33,11 @@ describe('Turn', function() {
     const turn = new Turn('object', card)
     expect(turn.evaluateGuess()).to.equal(true);
   });
+
+  it('should give feedback' , function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card)
+    let guess = turn.evaluateGuess();
+    expect(turn.giveFeedback(guess)).to.equal('correct!');
+  });
 });
