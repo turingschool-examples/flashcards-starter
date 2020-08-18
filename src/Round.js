a294b9033b91252475d13682b3f4982fe9894f28
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-18 00:12:57 -0400

    Add calculatePercentCorrect method

--- src/Round.js
+++ src/Round.js
@@ -10,12 +10,15 @@ class Round {
     }
     this.takeTurn = (guess) => {
       this.turns++;
-      let currentCard = this.returnCurrentCard()
-      let newTurn = new Turn(guess, currentCard)
-      let result = newTurn.evaluateGuess()
+      let currentCard = this.returnCurrentCard();
+      let newTurn = new Turn(guess, currentCard);
+      let result = newTurn.evaluateGuess();
       !result ? this.incorrectGuesses.unshift(currentCard.id) : null;
       this.deck.shift();
       return newTurn.giveFeedback(result);
+    }
+    this.calculatePercentCorrect = () => {
+      return  Math.round((this.incorrectGuesses.length / this.turns) * 100) ;
     }
   }
 }
