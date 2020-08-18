039043a4ecdf9274d501382db9a6df18ef3d46be
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-17 21:57:14 -0400

    Add returnCurrentCard method

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,9 @@
 class Round {
   constructor(deck) {
     this.deck = deck.deck;
+    this.returnCurrentCard = () => {
+      return this.deck[0];
+    }
   }
 }
-  module.exports = Round;
+module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -11,9 +11,11 @@ let round;
 
 describe('Round', function() {
   beforeEach((done) => {
-    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = new Card(2, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card3 = new Card(3, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     turn = new Turn('object', card);
-    deck = new Deck([card, card]);
+    deck = new Deck([card1, card2, card3]);
     round = new Round(deck);
     done();
   });
@@ -27,11 +29,11 @@ describe('Round', function() {
   }); 
 
   it('should have a deck', function() {
-    expect(round.deck).to.eql([card, card]);
+    expect(round.deck).to.eql([card1, card2, card3]);
   });   
 
-//   it('should return the card being played', function() {
-//     expect(round.returnCurrentCard()).to.equal();
-//   });  
+  it('should return the card being played', function() {
+    expect(round.returnCurrentCard()).to.equal(card1);
+  });  
   
 });
