bac2462b0c4d1547675097831c375f6bdeca5752
Matthew Lane <themplane@gmail.com>
2020-08-17 17:41:09 -0700

    Add beforeEach and test of instantiation

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,8 +4,36 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
-describe('Deck', function (){
-  it('should be a function', function(){
+describe('Deck', function () {
+  let card1, card2, card3, deck;
+  beforeEach( function() {
+    card1 = new Card(
+      1,
+      'What is Robbie\'s favorite animal',
+      ['sea otter', 'pug', 'capybara'],
+      'sea otter'
+    );
+    card2 = new Card(
+      14,
+      'What organ is Khalid missing?',
+      ['spleen', 'appendix', 'gallbladder'],
+      'gallbladder'
+    );
+    card3 = new Card(
+      12,
+      'What is Travis\'s middle name?',
+      ['Lex', 'William', 'Fitzgerald'],
+      'Fitzgerald'
+    );
+    deck = new Deck([card1, card2, card3]);
+  });
+
+
+  it('should be a function', function() {
     expect(Deck).to.be.a('function');
+  });
+
+  it('should instantiate an object', function() {
+    expect(deck).to.be.an.instanceof(Deck);
   });
 });
