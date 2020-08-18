e4f11921dfb4423bebb0e7cf2d2e3e41a913d9e6
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-17 22:36:43 -0400

    Add turns and incorrectGuesses properties

--- src/Round.js
+++ src/Round.js
@@ -1,8 +1,13 @@
 class Round {
-  constructor(deck) {
+  constructor(deck, turn) {
     this.deck = deck.deck;
+    this.turns = 0;
+    this.incorrectGuesses = [];
     this.returnCurrentCard = () => {
       return this.deck[0];
+    }
+    this.takeTurn = () => {
+      
     }
   }
 }
