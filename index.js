a035943d254dae3f2cd8aaa2a7642b346b042c26
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-19 16:16:52 -0400

    Add stop

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let app = http.createServer();
 const Game = require('./src/Game');
 
 // Start the server on port 3000
-app.listen(3000, '127.0.0.1');  
-console.log('Node server running on port 3000'); 
-let game = new Game;
-game.start();
+app.listen(3000, '127.0.0.1', undefined , () => {
+  console.log('Node server running on port 3000'); 
+  let game = new Game;
+  game.start();
+  app.close();
+  });
