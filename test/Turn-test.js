823b3f4bd20fa3c73c9afa089cd93de74db44327
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-18 19:17:24 -0700

    Write tests for class instantiation

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -3,3 +3,34 @@ const expect = chai.expect;
 
 const Card = require('../src/Card');
 const Turn = require('../src/Turn')
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+
+  it('should be an instance of a Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it.skip('', function(){
+    
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
