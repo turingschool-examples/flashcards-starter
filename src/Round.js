eae81ff3648fc8dbf21d952025fff6f5a9d44fc2
Michael Walker <Michael.e.walker.87@gmail.com>
2020-08-18 22:52:52 -0600

    Add takeTurn to the Round.js

--- src/Round.js
+++ src/Round.js
@@ -1,7 +1,55 @@
+const Turn = require('./Turn');
+
 class Round {
-  constructor() {
+  constructor(deck) {
+    this.incorrectGuesses = [];
+    this.deck = deck;
+    this.turn = 0;
+  }
+
+  returnCurrentCard(){
+    return this.deck.card[this.turn]
+  }
+
+  takeTurn(guess){
+    const card = this.returnCurrentCard()
+    console.log("card", card)
+    const turn = new Turn(guess,card);
+    console.log("turn", turn)
+    this.turn++;
+    console.log("evaluateGuess", !turn.evaluateGuess())
+    if(!turn.evaluateGuess()) this.incorrectGuesses.push(card.id);
+    console.log("incorrectGuesses", this.incorrectGuesses);
+    turn.giveFeedback();
+    console.log("giveFeedback", turn.giveFeedback())
+    turn.currentCard = this.returnCurrentCard();
+    console.log("currentsCard", turn.currentCard)
+  }
+
+  calculatePercentCorrect(){
+
+  }
+
+  endRound(){
 
   }
 }
 
 module.exports = Round;
+
+
+
+// * When a guess is made, a new Turn instance is created.
+//*  The turns count is updated, regardless of whether the guess is correct or incorrect
+//* Guess is evaluated/recorded. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses
+//* Feedback is returned regarding whether the guess is incorrect or correct
+//* The next card becomes current card
+
+
+
+
+
+
+
+
+//stop
--- test/Round-test.js
+++ test/Round-test.js
@@ -21,6 +21,28 @@ describe('Round', () => {
   });
 
   it('round should start at the beging of deck', () => {
-    
+    const card1 = new Card(
+      1,
+      'What is Robbie\'s favorite animal',
+      ['sea otter', 'pug', 'capybara'],
+      'sea otter');
+    const card2 = new Card(
+      14, 'What organ is Khalid missing?',
+      ['spleen', 'appendix', 'gallbladder'],
+      'gallbladder');
+    const card3 = new Card(
+      12,
+      'What is Travis\'s favorite stress reliever?',
+      ['listening to music', 'watching Netflix',
+      'playing with bubble wrap'],
+      'playing with bubble wrap');
+
+      const deck = new Deck([card1, card2, card3]);
+
+      const round = new Round(deck);
+      round.takeTurn('ballons');
+      round.takeTurn('gallbladder')
+      // expect(round.deck.card[round.turn]).to.equal(card1);
+      // expect(round.deck.card[round.turn]).to.equal(card2);
   });
 });
