e121e9a10fe87335efd2a5cdb2ca149c1d2d4f8f
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-19 20:08:34 -0700

    Write test to check for card count

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -17,6 +17,27 @@ describe('Deck', function() {
     expect(turn).to.be.an.instanceof(Deck);
   });
 
+  it('should be able to have cards', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck = new Deck([card1, card2, card3])
+
+    expect(deck.cardsInDeck).to.deep.equal([card1, card2, card3])
+  });
+
+  it('should be able to count the cards in the deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck = new Deck([card1, card2, card3])
+    deck.countCards()
+
+    expect(deck.cardCount).to.equal(3)
+  })
+
 
 
 
