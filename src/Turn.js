97941990d5fecaf93cf2e9a5e708e5bcb2bce40b
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-19 17:55:43 -0700

    Write method to evaluate a guess

--- src/Turn.js
+++ src/Turn.js
@@ -10,8 +10,24 @@ class Turn {
   returnCard(newCard) {
     return newCard
   }
+  evaluateGuess(card) {
+    if (this.guess === card.correctAnswer) {
+      return true
+    } else {
+      false
+    }
+  }
+  giveFeedback() {
+    
+  }
 }
 
+
+
+//i want to compare the guess made to the card.correctAnswer key value pair
+//it should return a true or false to be used for later functionality.
+
+//
 
 
 
