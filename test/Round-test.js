dd60dba24f09e1ca63942a4f5a7d04507bfddc76
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-19 20:30:57 -0700

    Write beginning tests to check is class constructor is a function

---
+++ test/Round-test.js
@@ -0,0 +1,28 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+describe('Round', function() {
+
+  it('should be a function', function() {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  })
+
+  it('should be an instance of a Round', function() {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+
+
+
+
+
+
+
+
+});
