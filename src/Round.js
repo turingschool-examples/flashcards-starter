e5c8d33840da04ef151f4f99846807d0e1257dcb
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-20 02:45:46 -0400

    Add require files, add bonusRound function

--- src/Round.js
+++ src/Round.js
@@ -1,4 +1,9 @@
 const Turn = require('../src/Turn');
+const game = require('../src/Game');
+const data = require('./data');
+const prototypeQuestions = data.prototypeData;
+const util = require('./util');
+const Deck = require('./Deck');
 
 class Round {
   constructor(deck, turn) {
@@ -19,6 +24,16 @@ class Round {
     }
     calculatePercentCorrect = () => {
       return  Math.round((this.turns - this.incorrectGuesses.length) / this.turns* 100 ) ;
+    }
+    bonusRound = () =>{
+      console.log(`**Bonus Round!!!**, redemption time!`)
+      let newBigDeck = []
+      this.incorrectGuesses.forEach(id =>{
+       newBigDeck.push(prototypeQuestions.filter(x => x.id === id))
+      })
+      newBigDeck = new Deck([].concat(...newBigDeck))
+      let newestRound = new Round(newBigDeck)
+      util.main(newestRound)
     }
     endRound = () => {
       console.log(`**Round over!** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`);
