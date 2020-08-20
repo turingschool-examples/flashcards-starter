d435673d3430a3ca28bbdd4f5a2d09ca073edd0d
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-19 20:50:27 -0400

    Add round test, adjust for consistency

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,22 +1,11 @@
 let chai = require('chai');
 const expect = chai.expect;
-const Card = require('../src/Card');
-const Turn = require('../src/Turn');
-const Deck = require('../src/Deck');
-const Round = require('../src/Round');
 const Game = require('../src/Game');
-const data = require('../src/data');
-const prototypeQuestions = data.prototypeData;
 
 describe('Game', function() {
+  let game;
   beforeEach((done) => {
-    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    card2 = new Card(2, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    card3 = new Card(3, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    turn = new Turn();
-    deck = new Deck([card1, card2, card3]);
-    round = new Round(deck);
-    game = new Game(round);
+    game = new Game();
     done();
   });
 
@@ -29,17 +18,6 @@ describe('Game', function() {
   }); 
 
   it('should keep track of the round', function() {
-    game.start();
-    let testDeck = game.generateDeck()
-    let testRound = new Round(testDeck)
-    expect(game.currentRound.deck).to.eql(testRound.deck);
+    expect(game.currentRound).to.equal(undefined);
   }); 
-
-  it('should generate a deck', function() {
-    let testDeck = game.generateDeck().deck.length;
-    let dataLength = prototypeQuestions.length;
-    expect(testDeck).to.eql(dataLength)
-  }); 
-
-
 });
