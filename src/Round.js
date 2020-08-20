c96292d30caae5da953c1461d9ac74832ddb7080
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-19 20:45:33 -0400

    Adjust takeTurn method to include all methods

--- src/Round.js
+++ src/Round.js
@@ -9,26 +9,13 @@ class Round {
     returnCurrentCard = (card) => {
       return this.deck[0];
     }
-    takeTurn = (guess) => {
+    takeTurn = (guess) => {     
       let currentCard = this.returnCurrentCard();
-      return this.getResult(guess, currentCard);
-    }
-    getResult = (guess, currentCard) =>{
-      let newTurn = new Turn();
-      return this.evaluateGuess(guess, newTurn, currentCard);
-    }
-    evaluateGuess = (guess, newTurn, currentCard) =>{
-      let result = newTurn.evaluateGuess(guess, currentCard);
-      return this.handleResult(result, newTurn, currentCard);
-    }
-    handleResult = (result, newTurn, currentCard) =>{
-      this.adjustGameSettings(result, currentCard);
-      return newTurn.giveFeedback(result);
-    }
-    adjustGameSettings = (result, currentCard) =>{
-      !result ? this.incorrectGuesses.unshift(currentCard.id) : null;
+      let newTurn = new Turn(guess, currentCard);
+      !newTurn.evaluateGuess() ? this.incorrectGuesses.unshift(currentCard.id) : null;
       this.deck.shift();
       this.turns++;
+      return newTurn.giveFeedback();
     }
     calculatePercentCorrect = () => {
       return  Math.round((this.turns - this.incorrectGuesses.length) / this.turns* 100 ) ;
