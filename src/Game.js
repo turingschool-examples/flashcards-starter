f29fe91c1ad3e2647ae50e52696fcd4cab123580
Michael Walker <Michael.e.walker.87@gmail.com>
2020-08-19 22:28:30 -0600

    Add the start method

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,21 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Deck = require('./Deck');
+const Turn = require('./Turn');
+const Card = require('./Card');
+const Round = require('./Round');
+
 class Game {
   constructor() {}
+
+  start(){
+    const mappedArrayData = prototypeQuestions.map((cardData) => {
+    return cardData = new Card(cardData.id, cardData.question, cardData.answer, cardData.correctAnswer)
+    });
+    const deck = new Deck(mappedArrayData)
+    
+  };
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -14,5 +27,6 @@ class Game {
       util.main(round);
   }
 }
+
 
 module.exports = Game;
