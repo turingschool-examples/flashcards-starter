76dcb991ade494876033924539a45b33928c586a
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-20 15:04:20 -0400

    Refactor bonusRound methods

--- src/Round.js
+++ src/Round.js
@@ -6,7 +6,7 @@ const util = require('./util');
 const Deck = require('./Deck');
 
 class Round {
-  constructor(deck, turn) {
+  constructor(deck) {
     this.deck = deck.deck;
     this.turns = 0;
     this.incorrectGuesses = [];
@@ -15,7 +15,7 @@ class Round {
     this.guess;
     this.currentCard;
     }
-    returnCurrentCard = (card) => {
+    returnCurrentCard = () => {
       return this.deck[0];
     }
     takeTurn = (guess) => {   
@@ -45,13 +45,19 @@ class Round {
     }
     bonusRound = () =>{
       console.log(`**Bonus Round!!!**, redemption time!`)
-      let newBigDeck = []
-      this.incorrectGuesses.forEach(id =>{
-       newBigDeck.push(prototypeQuestions.filter(x => x.id === id))
-      })
-      newBigDeck = new Deck([].concat(...newBigDeck))
-      let newestRound = new Round(newBigDeck)
+      let newestRound = new Round(this.createBonusDeck())
       util.main(newestRound)
+    }
+    createBonusDeck = () => {
+      let bonusRoundDeck = []
+      this.populateDeck(bonusRoundDeck);
+      return bonusRoundDeck = new Deck([].concat(...bonusRoundDeck))
+    }
+    populateDeck = (bonusRoundDeck) => {
+        this.incorrectGuesses.forEach(id =>{
+        bonusRoundDeck.push(prototypeQuestions.filter(x => x.id === id))
+       })
+       return bonusRoundDeck;
     }
     endRound = () => {
       console.log(`**Round over!** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`);
