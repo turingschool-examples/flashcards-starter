933c2298807ee214cf8492efc46a62a59e68c32e
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-19 17:22:54 -0700

    Return card when method on turn called

--- src/Turn.js
+++ src/Turn.js
@@ -1,1 +1,18 @@
-sdf
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess;
+    this.card = card;
+  }
+
+  returnGuess() {
+    return this.guess
+  }
+  returnCard(newCard) {
+    return newCard
+  }
+}
+
+
+
+
+module.exports = Turn;
