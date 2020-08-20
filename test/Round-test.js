a822f9aae3314ec123adcde5a809537848442cf7
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-08-20 18:34:20 -0400

    Refactor test suite for Class refactor

--- test/Round-test.js
+++ test/Round-test.js
@@ -46,81 +46,43 @@ describe('Round Architecture', function() {
   it('should store incorrect guesses', function() {
     expect(round.incorrectGuesses).to.eql([]);
   });  
-});  
 
-describe('takeTest method', function() {
-  let card1;
-  let card2;
-  let card3;
-  let deck;
-  let round;
-  let currentCard;
-  beforeEach((done) => {
-    card1 = new Card(prototypeQuestions[0])
-    card2 = new Card(prototypeQuestions[1])
-    card3 = new Card(prototypeQuestions[2])
-    deck = new Deck([card1, card2, card3]);
-    round = new Round(deck);
-    currentCard = round.returnCurrentCard();
-    guess = round.takeTurn('testGuess');
-    done();
-  });
-  it('should store a guess', function() {
-    expect(round.guess).to.eql('testGuess');
+  it('should evaluate guesses are correct', function() {
+    expect(round.takeTurn('object')).to.eql('correct!');
   });  
 
-  it('should store current turn', function() {
-    expect(round.turn).to.be.an.instanceof(Turn);
+  it('should evaluate guesses are wrong', function() {
+    expect(round.takeTurn('array')).to.eql('incorrect!');
   });  
 
-  it('should return the card being played', function() {
-    expect(round.returnCurrentCard()).to.equal(card2);
-  });  
-
-  it('should store the current card', function() {
-    expect(currentCard).to.eql(prototypeQuestions[0]);
-  });  
-
-  it('should store current turn', function() {
-    round.createTurn();
-    expect(round.turn).to.be.an.instanceof(Turn);
-  });  
-
-  it('should evaluate guesses are true', function() {
-    round.takeTurn('wrong')
-    expect(round.evaluateGuess()).to.eql('incorrect!');
-  });  
-
-  it('should evaluate guesses are false', function() {
-    round.takeTurn('array');
-    expect(round.evaluateGuess()).to.eql('incorrect!');
+  it('should keep track of the turn', function() {
+    round.takeTurn();
+    round.takeTurn();
+    expect(round.turns).to.equal(2);
   });  
 
   it('should add incorrect ID to array', function() {
-    round.handleResult()
-    round.handleResult()
-    expect(round.incorrectGuesses.length).to.eql(3);
+    round.takeTurn('array')
+    expect(round.incorrectGuesses).to.eql([1]);
   });  
 
   it('should not add correct ID to array', function() {
-    round.takeTurn('object');
-    round.takeTurn('object');
-    expect(round.incorrectGuesses.length).to.eql(1);
+    round.takeTurn('object')
+    expect(round.incorrectGuesses).to.eql([]);
   });  
 
-  it('should increase turn counter after every turn', function() {
-    round.incrementTurn();
-    round.incrementTurn();
-    expect(round.turns).to.eql(3);
+  it('should return the card being played', function() {
+    expect(round.returnCurrentCard()).to.equal(card1);
   });  
 
   it('next card should become current card after every turn', function() {
-    round.shiftDeck();
-    expect(round.deck[0].id).to.eql(3);
+    round.takeTurn();
+    expect(round.returnCurrentCard().id).to.eql(2);
   });  
 
   it('should calculate correct percent', function() {
     round.takeTurn('object');
+    round.takeTurn('wrong');
     round.takeTurn('wrong');
     expect(round.calculatePercentCorrect()).to.eql(33);
   });  
