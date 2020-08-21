0792ce2ac0f03ac4352213c05d170ca83fbef1da
Chriscastanuela <chriscastanuela@ymail.com>
2020-08-20 20:59:56 -0600

    Add Readme

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,21 +1,3 @@
-/*
-start: method that starts everything
-Creates Cards
-Puts Cards in a Deck
-Creates a new Round using the Deck
-invokes printMessage to display the message in the CLI
-invokes printQuestion to kick off our helper functions that allow interaction via the CLI
-Note: The helper functions are all fleshed out and fit to work with classes/methods that meet the requirements in the past iterations.
-
-For example:
-
-game.currentRound; // => Round {...} (The new Round object that has been instatiated)
-HINT
-
-Look at the file being run when we want to start the game. Think about where you need to invoke your Game.start method.
-
-*/
-
 const chai = require('chai');
 const expect = chai.expect;
 
@@ -23,78 +5,18 @@ const Round = require('../src/Round');
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
+const Game = require('../src/Game');
 
 describe('Game', function() {
 
   it('Should keep track of the currentRound', function() {
 
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    let game = new Game();
+    game.start();
 
-    const deck = new Deck([card1, card2, card3]);
+    game.currentRound.takeTurn('sea otter', 1);
+    game.currentRound.endRound(game);
 
-    const round = new Round(deck);
-
-    expect(round.returnCurrentCard()).to.equal(round.deck[0]);
-
-  });
-
-  it('The turns count is updated, regardless of whether the guess is correct or incorrect', function() {
-
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-
-    const deck = new Deck([card1, card2, card3]);
-
-    const round = new Round(deck);
-
-    round.takeTurn('sea otter', card1)
-
-    expect(round.turns).to.equal(1);
-  });
-
-  it('The next card becomes current card', function() {
-
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-
-    const deck = new Deck([card1, card2, card3]);
-
-    const round = new Round(deck);
-
-    round.takeTurn('sea otter', card1)
-
-    expect(round.currentCard).to.equal(card2);
-  });
-
-  it('Guess is evaluated/recorded. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses', function() {
-
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-
-    const deck = new Deck([card1, card2, card3]);
-
-    const round = new Round(deck);
-
-    round.takeTurn('asdfasdff', card1)
-
-    expect(round.incorrectGuesses).to.eql([1]);
-  });
-
-  it('Feedback is returned regarding whether the guess is incorrect or correct', function() {
-
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-
-    const deck = new Deck([card1, card2, card3]);
-
-    const round = new Round(deck);
-
-    expect(round.takeTurn('asdfasdff', card1)).to.equal("incorrect!");
+    expect(game.currentRound.turns).to.equal(0);
   });
 });
