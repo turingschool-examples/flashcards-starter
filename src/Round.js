c11d984c690d90170feb295e5ac7512359daf13f
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-20 17:27:56 -0700

    Add functionality to give feedback on guess

--- src/Round.js
+++ src/Round.js
@@ -5,27 +5,36 @@ class Round {
     this.deck = deck;
     this.turnCount = 0;
     this.currentCard = this.deck.cardsInDeck[0];
-  }
+    this.incorrectGuesses = [];
+    this.correct = true;
+    }
   returnCurrentCard() {
     return this.currentCard
   }
   takeTurn(guess) {
-    let newTurn = new Turn()
-    this.turnCount++
-    this.deck.cardsInDeck.shift()
-    this.currentCard = this.deck.cardsInDeck[0];
+    let newTurn = new Turn(guess, this.currentCard);
+    this.turnCount++;
+    if (newTurn.evaluateGuess()) {
+      this.deck.cardsInDeck.shift();
+      this.currentCard = this.deck.cardsInDeck[0];
+      return newTurn.giveFeedback()
+    } else {
+      this.correct = false
+      this.incorrectGuesses.push(this.deck.cardsInDeck[0].id)
+      return newTurn.giveFeedback()
+    }
   }
+  calculatePercentCorrect() {
+  }
+  endRound() {
+    if(this.correct) {
+      console.log('** Round over! ** You answered of the questions correctly!')
+    }
+  }
+
 }
 
-
-
-
-
-
-//shift the card out of the deck!!
-
-
-
+//currentCARD
 
 
 
