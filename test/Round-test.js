fabe240d7370c537f09529cae92887a4e92df029
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-20 17:28:16 -0700

    Write test to log id of incorrect guess on card

--- test/Round-test.js
+++ test/Round-test.js
@@ -64,9 +64,45 @@ describe('Round', function() {
     expect(round.currentCard).to.deep.equal(card2)
   })
 
+  it('should give a message for guessing correctly', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const newDeck = new Deck([card1, card2, card3])
+
+    const round = new Round(newDeck)
+
+    expect(round.takeTurn('sea otter')).to.equal('correct!')
+  })
+
+  it('should give a message for guessing incorrectly', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const newDeck = new Deck([card1, card2, card3])
+
+    const round = new Round(newDeck)
 
 
-  // expect(round.turnCount).to.equal(1)
+    expect(round.takeTurn('pug')).to.equal('incorrect!')
+  })
+
+  it('should log incorrect ID', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const newDeck = new Deck([card1, card2, card3])
+
+    const round = new Round(newDeck)
+
+    round.takeTurn('pug')
+
+    expect(round.incorrectGuesses).to.deep.equal([1])
+  })
+
 
 
 
