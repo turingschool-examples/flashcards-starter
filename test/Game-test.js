2ef2bdb4b326cad21c136d50c30a3b7dfdfd070d
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-22 14:40:08 -0700

    Adjust test

---
+++ test/Game-test.js
@@ -0,0 +1,19 @@
+// const chai = require('chai');
+// const expect = chai.expect;
+//
+// const Card = require('../src/Card');
+// const Turn = require('../src/Turn');
+// const Deck = require('../src/Deck');
+// const Round = require('../src/Round');
+// const Game = require('../src/Game');
+// describe('Game', function() {
+//
+//   it('should be a function', function() {
+//     let game = new Game()
+//
+//       expect(Game).to.be.a('function');
+//   })
+// 
+//
+//
+// })
