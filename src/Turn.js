2553fe2ba91121993af8865723c4f13da441f142
JP Carey <jpcarey4@gmail.com>
2020-10-05 17:58:41 -0600

    Create guess and card properties for the Turn Class

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,13 @@
-class Turn {
-  constructor() {
+const Card = require('../src/Card');
 
+class Turn {
+  constructor(guess, card) {
+    this.card = card
+    if (typeof guess !== 'string') {
+      this.guess = `${guess}`;
+    } else {
+      this.guess = guess;
+    }
   }
 }
 
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 describe('Turn', function() {
   it('should be a function', function() {
     const turn = new Turn();
@@ -9,7 +10,28 @@ describe('Turn', function() {
   });
 
   it('should be an instance of Turn', function() {
-    const card = new Turn();
-    expect(card).to.be.an.instanceof(Turn);
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
   });
+
+  it('should be able to store a guess', function() {
+    const turn1 = new Turn('Harry Potter');
+    const turn2 = new Turn('Ron Weasley');
+    expect(turn1.guess).to.equal('Harry Potter');
+    expect(turn2.guess).to.equal('Ron Weasley');
+  });
+
+  it('should store a guess as a string', function() {
+    const turn = new Turn(7);
+    expect(turn.guess).to.equal('7');
+  });
+
+  it('should store cards', function() {
+    const card = new Card(1, 'Who\'s the Hogwarts Headmaster?', ['Voldemort', 'Harry Potter', 'Dumbledore'], 'Dumbledore');
+    const turn = new Turn('Dumbledore', card);
+    expect(turn.card).to.be.an.instanceOf(Card);
+    expect(card.answers[1]).to.equal('Harry Potter');
+    expect(card.correctAnswer).to.equal('Dumbledore');
+  });
+
 });
