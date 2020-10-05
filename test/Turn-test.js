40ccc7d5cc91db0f9496d5514b18a28256991269
JP Carey <jpcarey4@gmail.com>
2020-10-05 16:20:37 -0600

    set up test and implementation files for Turn class

---
+++ test/Turn-test.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+describe('Turn', function() {
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    const card = new Turn();
+    expect(card).to.be.an.instanceof(Turn);
+  });
+});
