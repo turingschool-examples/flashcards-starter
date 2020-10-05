509e14e9876f81e037dd494c7de27b4b7d6fbf8c
Lola Dolinsky <dolinsky.lola@gmail.com>
2020-10-05 16:32:20 -0600

    Create sample test data file js

---
+++ src/test-data.js
@@ -0,0 +1,16 @@
+const testData = [{
+  "id": 1,
+  "question": "You live in a one story house made entirely of redwood. What color would the stairs be?",
+  "answers": ["wood", "red", "What stairs? You live in a one-story house"],
+  "correctAnswer": "What stairs? You live in a one-story house"
+}, {
+  "id": 2,
+  "question": "I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?",
+  "answers": ["ice", "fire", "pants"],
+  "correctAnswer": "fire"
+}, {
+  "id": 3,
+  "question": "What belongs to you, but other people use it more than you?",
+  "answers": ["your name", "your money", "your internet"],
+  "correctAnswer": "your name"
+}]
---
+++ test/Turn-test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it.skip('should store a user\'s guess to a question', function() {
+    const turn = new Turn('red');
+  })
+});
