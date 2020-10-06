a79e7827cf1125d01ef4c67384bea045cc0599d3
JP Carey <jpcarey4@gmail.com>
2020-10-06 16:18:13 -0600

    Add more variables for cards, a deck, and a round

--- test/Round-test.js
+++ test/Round-test.js
@@ -9,7 +9,10 @@ describe('Round', function() {
   let card1;
   let card2;
   let card3;
+  let card4;
+  let card5;
   let deck1;
+  let deck2;
   let round1;
 
   beforeEach(function () {
@@ -19,9 +22,17 @@ describe('Round', function() {
 
     card3 = new Card(3, 'Who\'s the Hogwarts Headmaster?', ['Voldemort', 'Dumbledore', 'Fudge'], 'Dumbledore');
 
+    card5 = new Card(5, 'Who told Harry he was a wizard?', ['Hagrid', 'Dudley', 'Snape'], 'Hagrid');
+
+    card4 = new Card(4, 'What\'s the town outside of Hogwarts?', ['Diagon Alley', 'Privet Drive', 'Hogsmeade'], 'Hogsmeade');
+
     deck1 = new Deck([card1, card2, card3]);
 
+    deck2 = new Deck([card4, card5]);
+
     round1 = new Round(deck1);
+
+    round2 = new Round(deck2);
   });
 
   it('should be a function', function() {
@@ -32,8 +43,9 @@ describe('Round', function() {
     expect(round1).to.be.an.instanceof(Round);
   });
 
-  it('should have a deck', function() {
+  it('should be able to have a deck', function() {
     expect(round1.deck).to.equal(deck1);
+    expect(round2.deck).to.equal(deck2);
   });
 
 });
