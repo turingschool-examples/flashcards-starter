ce0c3d6983b98e62179784f3c98fa19e57c43d45
JP Carey <jpcarey4@gmail.com>
2020-10-06 17:50:20 -0600

    Use takeTurn method to up turn count, remove top card and return if the answer is correct

--- src/Round.js
+++ src/Round.js
@@ -2,10 +2,17 @@ class Round {
   constructor(deck) {
     this.deck = deck;
     this.turns = 0;
+    this.incorrectGuesses = [];
   }
 
   returnCurrentCard() {
     return this.deck.cards[0];
+  }
+
+  takeTurn(turn) {
+    this.turns++;
+    this.deck.cards.shift();
+    return turn.giveFeedback();
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -4,6 +4,7 @@ const expect = chai.expect;
 const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
+const Turn = require('../src/Turn');
 
 describe('Round', function() {
   let card1;
@@ -13,6 +14,9 @@ describe('Round', function() {
   let card5;
   let deck1;
   let deck2;
+  let turn1;
+  let turn2;
+  let turn3;
   let round1;
 
   beforeEach(function () {
@@ -27,11 +31,11 @@ describe('Round', function() {
     card4 = new Card(4, 'What\'s the town outside of Hogwarts?', ['Diagon Alley', 'Privet Drive', 'Hogsmeade'], 'Hogsmeade');
 
     deck1 = new Deck([card1, card2, card3]);
-
     deck2 = new Deck([card4, card5]);
-
+    turn1 = new Turn('Sirius', card1);
+    turn2 = new Turn('beater', card2);
+    turn3 = new Turn('Volemort', card3);
     round1 = new Round(deck1);
-
     round2 = new Round(deck2);
   });
 
@@ -48,13 +52,38 @@ describe('Round', function() {
     expect(round2.deck).to.equal(deck2);
   });
 
-  it('should start out with no turns taken', function() {
-    expect(round1.turns).to.equal(0);
-  })
-
   it('should be able to return the top card in the deck', function() {
     expect(round1.returnCurrentCard()).to.equal(card1);
     expect(round2.returnCurrentCard()).to.equal(card4);
   });
 
+  it('should start out with no turns taken ', function() {
+    expect(round1.turns).to.equal(0);
+  });
+
+  it('should start out with no incorrect guesses', function() {
+    expect(round1.incorrectGuesses).to.deep.equal([]);
+  });
+
+  it('should be able to take a turn', function() {
+    round1.takeTurn(turn1);
+
+    expect(round1.turns).to.equal(1);
+
+    round1.takeTurn(turn1);
+    round1.takeTurn(turn1);
+
+    expect(round1.turns).to.equal(3);
+  });
+
+  it('should remove the current card when a turn is taken', function() {
+    round1.takeTurn(turn1);
+
+    expect(round1.deck.cards[0]).to.equal(card2);
+  });
+
+  it('should evaluate the guess when a turn is taken', function() {
+    expect(round1.takeTurn(turn1)).to.equal(`correct!`);
+    expect(round1.takeTurn(turn2)).to.equal(`incorrect!`);
+  });
 });
