b4af9014b60d8f49486537bd02c91dc7e7f054a2
JP Carey <jpcarey4@gmail.com>
2020-10-06 08:41:32 -0600

    Initialize and instantiate the Deck class

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -18,6 +18,14 @@ beforeEach(function() {
   card3 = new Card(3, 'Who\'s the Hogwarts Headmaster?', ['Voldemort', 'Dumbledore', 'Fudge'], 'Dumbledore');
 
   deck = new Deck([card1, card2, card3]);
-})
+});
+
+  it('should be a function', function() {
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', function() {
+    expect(deck).to.be.an.instanceof(Deck);
+  });
 
 });
