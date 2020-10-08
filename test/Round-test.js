6a3832c9d047a96250a80ca9a7716e5e811cb16d
Joe Lopez <lopez.4.joseph@gmail.com>
2020-10-08 15:56:48 -0600

    Add functionality to instantiate our Round class

---
+++ test/Round-test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const Card = require('../src/Card.js');
+const Deck = require('../src/Deck.js');
+const Turn = require('../src/Turn.js');
+const Round = require('../src/Round.js');
+const expect = chai.expect;
+
+describe('Round', function() {
+
+  it('should instantiate the Round class', function() {
+    const round = new Round()
+  })
+})
