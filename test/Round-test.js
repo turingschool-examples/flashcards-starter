1e75ef18e09fe68af786e56d827800fb2fdf3ad9
Joe Lopez <lopez.4.joseph@gmail.com>
2020-10-08 20:09:28 -0600

    Add ability to take a turn

--- test/Round-test.js
+++ test/Round-test.js
@@ -35,4 +35,28 @@ describe('Round', function() {
     expect(round.deck.cards[0]).to.deep.equal(card1);
   })
 
+  it('should be able to take a turn', function() {
+    const card1 = new Card(1, 'What is Joe\'s favorite food', ['pizza', 'burgers', 'noodles'], 'noodles');
+    const card2 = new Card(2, 'What is Joe\'s favorite animal', ['dog', 'cat', 'frog'], 'cat');
+    const card3 = new Card(3, 'What is Joe\'s favorite color', ['blue', 'red', 'yellow'], 'blue');
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    round.takeTurn('noodles');
+
+    expect(round.turns).to.equal(1);
+    expect(round.deck.cards[0]).to.deep.equal(card1);
+  })
+
+  it('should be able to calculate the percent of correct guesses', function() {
+    const card1 = new Card(1, 'What is Joe\'s favorite food', ['pizza', 'burgers', 'noodles'], 'noodles');
+    const card2 = new Card(2, 'What is Joe\'s favorite animal', ['dog', 'cat', 'frog'], 'cat');
+    const card3 = new Card(3, 'What is Joe\'s favorite color', ['blue', 'red', 'yellow'], 'blue');
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(round.deck.cards[0]).to.deep.equal(card1);
+  })
 })
