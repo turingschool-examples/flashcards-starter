d3e5a94df0e2d6dc4a67c1ac31a755c88359e40a
Elsa Fluss <elsafluss@gmail.com>
2020-11-30 20:37:11 -0700

    Create initial Round tests.

---
+++ test/Round-test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
+const Turn = require('../src/Turn');
+
+describe('Round', function () {
+
+  it('should be a function', function () {
+    const round = new Round()
+    expect(Round).to.be.a('function')
+  })
+
+  it('should be an instance of Round', function () {
+    const round = new Round()
+    expect(round).to.be.an.instanceof(Round)
+  })
+
+  // it('should instantiate a Turn', function () {
+  // const round = new Round()
+  // const turn = new Turn()
+  // })
+})
