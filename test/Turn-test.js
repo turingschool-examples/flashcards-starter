4cf9f730d3cb008bd9c3728d3949d9dfe2481aad
mattdean <matthewallandean@gmail.com>
2020-11-30 18:19:51 -0700

    Pass Turn tests. Created a Turn class with a constructor and methods to do so.

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -10,52 +10,52 @@ describe('Turn', function() {
     expect(Turn).to.be.a('function');
   });
 
-  it.skip('should be able to instantate a Turn', function() {
+  it('should be able to instantate a Turn', function() {
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   })
 
-  it.skip('should contain a guess', function() {
+  it('should contain a guess', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
-    expect(turn.guess).to.equal('object');
+    expect(turn.guess).to.deep.equal('object');
   });
 
-  it.skip('should contain a Card instance', function() {
+  it('should contain a Card instance', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
     expect(turn.card).to.equal(card);
   });
 
-  it.skip('should return a guess', function() {
+  it('should return a guess', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
     expect(turn.returnGuess).to.be.a('function');
-    expect(turn.returnGuess).to.equal('object');
+    expect(turn.returnGuess()).to.equal('object');
   });
 
-  it.skip('should return a card', function() {
+  it('should return a card', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
     expect(turn.returnCard).to.be.a('function');
-    expect(turn.returnCard).to.equal(card);
+    expect(turn.returnCard()).to.equal(card);
   });
 
-  it.skip('should be able to evaluate a guess', function() {
+  it('should be able to evaluate a guess', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
     expect(turn.evaluateGuess).to.be.a('function');
-    expect(turn.evaluateGuess).to.equal(true);
+    expect(turn.evaluateGuess()).to.equal(true);
   });
 
-  it.skip('should be able to give feedback', function() {
+  it('should be able to give feedback', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn1 = new Turn('object', card);
     expect(turn1.giveFeedback).to.be.a('function');
-    expect(turn1.giveFeedback).to.equal('You got it!');
+    expect(turn1.giveFeedback()).to.equal('You got it!');
 
     const turn2 = new Turn('array', card);
     expect(turn2.giveFeedback).to.be.a('function');
-    expect(turn2.giveFeedback).to.equal('Not even close!');
+    expect(turn2.giveFeedback()).to.equal('Not even close!');
   });
 });
