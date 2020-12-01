c6b834119faf7fe1bae110e7a125779f4bd4090c
Elsa Fluss <elsafluss@gmail.com>
2020-11-30 20:36:47 -0700

    Create Round class.

---
+++ src/Round.js
@@ -0,0 +1,32 @@
+class Round {
+  constructor() {
+
+  }
+
+  // return the current card
+  returnCurrentCard() {
+
+  }
+
+  // create new Turn(guess, currentCard)
+  // increment turn.turns count by one
+  // change current card to be the next one in line
+  // evaluate the guess
+  // record the guess in incorrectGuesses
+  // give feedback
+  takeTurn() {
+
+  }
+
+  // calculate percent correct
+  calculatePercentCorrect() {
+
+  }
+
+  // console.log round over, you answered % of questions correctly
+  endRound() {
+
+  }
+}
+
+module.exports = Round
