f19b65526558296b66db71318a09d48b6af51dda
Cole Fiscus <coletfiscus@gmail.com>
2020-11-30 21:42:56 -0700

    Create Deck.js and test file and add state, behavior, and tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Card = require('../src/Card')
+const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
 describe('Turn', function() {
