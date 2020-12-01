5a3fe3ccac47ebb64f96a818949c997a16ef9107
conconartist <conconart.info@gmail.com>
2020-11-30 19:59:37 -0700

    Add test blocks for instantiation of Turn class

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,14 +4,33 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
-describe('Turn', function() {
+describe('Turn', () => {
+
   //it should be a function
-  it('should be a function', function() {
+  it('should be a function', () => {
     const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
+
   //it should be an instance of Turn
+  it('should be an instance of Turn', () => {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
   //it should be instantiated with 2 arguments (userGuess, Card)
+  it('should take in the user\'s guess', () => {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn).to.have.property('userGuess');
+  });
+
+  it('should take in an instance of Card', () => {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn).to.have.property('card');
+  })
+
   //it should return the user's guess
   //it should return the Card
   //it should evaluate the guess to true if correct or false if incorrect
