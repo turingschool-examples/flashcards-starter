8ede0380256f8c3ce74e18fe6735be9968aa210e
mainlyetcetera <ericcampbell294@gmail.com>
2020-11-30 22:03:37 -0700

    Add constructor skeleton to turn class

--- src/Turn.js
+++ src/Turn.js
@@ -2,3 +2,7 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 const card = require('./Card');
+
+class Turn {
+  constructor() {}
+}
