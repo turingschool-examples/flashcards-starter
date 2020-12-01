69550e7c4f4a817fc4493be454f5bc24803e0e8f
Max Bregman <bregmanmax91@gmail.com>
2020-11-30 19:36:58 -0700

    add and passed tests for the guess and current card parameters

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,7 @@
 class Turn {
-    constructor() {
-
+    constructor(guess, currentCard) {
+        this.guess = guess;
+        this.currentCard = currentCard;
     }
 }
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -9,5 +9,21 @@ describe('Turn', function() {
     it('should be a function', function() {
         const turn = new Turn();
         expect(Turn).to.be.a('function');
+    });
+
+    it('should be an instance of a Turn', function() {
+        const turn = new Turn();
+        expect(turn).to.be.an.instanceof(Turn);
+    });
+
+    it('should be able to store a guess', function() {
+        const turn = new Turn('Up on the mountain');
+        expect(turn.guess).to.equal('Up on the mountain');
+    });
+
+    it('should be able to store a card object', function() {
+        const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+        const turn = new Turn('Up on the mountain', card);
+        expect(turn.currentCard).to.deep.equal(card);
     })
 })
