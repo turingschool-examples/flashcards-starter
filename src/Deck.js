7c802b194696bef1fe3205c057dd16a0662205a6
Elsa Fluss <elsafluss@gmail.com>
2020-11-30 19:05:59 -0700

    Create Deck class.

---
+++ src/Deck.js
@@ -0,0 +1,15 @@
+class Deck {
+  constructor() {
+    // should initialize with an array of card objects
+    this.currentDeck = [{
+      // these objects should each be an instance of Card
+    }];
+  }
+
+  // should know how many cards there are
+  countCards() {
+
+  }
+}
+
+module.exports = Deck
