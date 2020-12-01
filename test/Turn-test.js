241b0a45fd06d88f97b6ad636fba2ff90574a0a4
conconartist <conconart.info@gmail.com>
2020-11-30 20:56:39 -0700

    Add test blocks for evaluateGuess and giveFeedback methods

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -29,15 +29,29 @@ describe('Turn', () => {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
     expect(turn).to.have.property('card');
-  })
+  });
 
   //it should return the user's guess
   it('should return the user\'s guess', () => {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
     expect(turn.returnGuess()).to.equal('object');
-  })
+  });
+
   //it should return the Card
-  //it should evaluate the guess to true if correct or false if incorrect
+  it('should return the Card instance', () => {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.returnCard()).to.equal(card);
+  });
+
+    //it should evaluate the guess to true if correct or false if incorrect
+    //create multiple instances 
+  it('should return a value of true if the user\'s guess is correct', () => {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.evaluateGuess()).to.equal(true);
+  })
+
   //it should state whether the answer is correct or incorrect
 });
