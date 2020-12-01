3d20d8f5247e224756f618087a00269da5ba7835
mainlyetcetera <ericcampbell294@gmail.com>
2020-12-01 15:50:33 -0700

    Add isFunction test to deck-test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,5 +4,10 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 
 describe('Deck', () => {
-
+  
+  it.skip('should be a function', () => {
+    const deck = new Deck();
+    expect(Deck).to.be.a('function');
+  });
+  
 }
