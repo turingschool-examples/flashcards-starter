cb594fd94d535e5e1eab43394d0c951d528be313
Luke Mason <lukjmason@gmail.com>
2020-12-01 12:42:42 -0700

    Create tests to check properties for our Turn class

---
+++ test/Turn-test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+
+describe('Turn', function() {
+
+  beforeEach(function() {
+    return card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  });
+
+  beforeEach(function() {
+    return turn = new Turn('incorrect guess', card)
+  });
+
+  it.skip('should be a function', function() {
+    expect(Turn).to.be.a('function');
+  });
+
+  it.skip('should have a guess property', function() {
+    expect(turn.userGuess).to.equal('incorrect guess')
+  });
+
+  it.skip('should have a card property', function() {
+    expect(turn.currentCard).to.equal(card);
+  });
+
+
+});
