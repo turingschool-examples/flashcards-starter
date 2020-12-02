72d865b347083394d798d53a44e969dbb585c6e4
mainlyetcetera <ericcampbell294@gmail.com>
2020-12-01 22:13:12 -0700

    Add basic beforeEach to turn-test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,3 +4,11 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
+describe('Turn', () => {
+  let turn;
+
+  beforeEach(() => {
+    turn = new Turn();
+  });
+
+});
