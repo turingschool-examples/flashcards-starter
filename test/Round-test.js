e7f99eed4c5281eaf314b85bb142e18d01eff2d6
mainlyetcetera <ericcampbell294@gmail.com>
2020-12-01 19:49:49 -0700

    Require classes for round-test

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,2 +1,8 @@
 const chai = require('chai');
 const expect = chai.expect;
+
+const Round = require('../src/Round');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
+const Game = require('../src/Game');
