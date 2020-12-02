5fa7c77c41a9c383fa746d520fe54b5aa70c58cc
mainlyetcetera <ericcampbell294@gmail.com>
2020-12-02 15:45:12 -0700

    Unskip function/instance tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -11,11 +11,11 @@ describe('Turn', () => {
     turn = new Turn();
   });
 
-  it.skip('should be a function', () => {    
+  it('should be a function', () => {    
     expect(Turn).to.be.a('function');
   });
 
-  it.skip('should be an instance of Turn', () => {    
+  it('should be an instance of Turn', () => {    
     expect(turn).to.be.an.instanceof(Turn);
   });
 
