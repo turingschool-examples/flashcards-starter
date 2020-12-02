1ce6850f7ab85a2488312a54a84232bcdedcd54d
mainlyetcetera <ericcampbell294@gmail.com>
2020-12-02 15:39:16 -0700

    Pass should store correct answer test

--- src/Card.js
+++ src/Card.js
@@ -3,10 +3,11 @@ const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Card {
-  constructor(id, question, answers) {
+  constructor(id, question, answers, right) {
     this.id = id;
     this.question = question;
     this.answers = answers;
+    this.correctAnswer = right;
   }
 }
 
