dd2538039b18f18068284c55b1fedb73095e044a
Luke Mason <lukjmason@gmail.com>
2020-12-02 14:10:01 -0700

    Fix error in Desk-test.js. Input require before class paths

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,22 +1,22 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Card = ('../src/Card');
-const Deck = ('../src/Deck');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
 
 describe('Deck', function() {
+  let card1;
+  let card2;
+  let card3;
 
   beforeEach(function() {
-    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-  });
-
-  beforeEach(function() {
-    const deck = new Deck([card1, card2, card3]);
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
   });
 
   it.skip('should be a function', function() {
+    let deck = new Deck();
     expect(Deck).to.be.a('function');
   });
 
