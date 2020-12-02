5b2663aaff37a90ba6faa5ad5ee65b1b699e02c6
Brian Forbes <brianandrewf81@gmail.com>
2020-12-01 23:51:29 -0700

    Fix weird bug with Turn class

--- src/Turn.js
+++ src/Turn.js
@@ -2,10 +2,6 @@ class Turn {
   constructor(guess, currentCard) {
     this.guess = guess;
     this.card = currentCard;
-<<<<<<< HEAD
-=======
-    this.isCorrect = null;
->>>>>>> ef76c2292a58424158cb30941d46b0f03b45a8f3
   }
   returnGuess() {
     return this.guess;
@@ -14,25 +10,11 @@ class Turn {
     return this.card;
   }
   evaluateGuess() {
-<<<<<<< HEAD
     return this.card.correctAnswer === this.guess ? true : false;
   }
   giveFeedback() {
-    return this.evaluateGuess() ? 'correct!' : 'incorrect!';
+    return this.evaluateGuess() ? 'correct!' : 'incorrect!'
   }
 };
 
-module.exports = Turn
-=======
-    if (this.card.correctAnswer === this.guess) {
-      this.isCorrect = true
-    }
-    else {
-      this.isCorrect = false
-    }
-  }
-  giveFeedback() {
-    return this.isCorrect === true ? 'correct!' : 'incorrect!'
-  }
-}
->>>>>>> ef76c2292a58424158cb30941d46b0f03b45a8f3
+module.exports = Turn;
