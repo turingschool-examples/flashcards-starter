e045c4fe696e2b187ea057ca756de806ae737748
conconartist <conconart.info@gmail.com>
2020-12-02 09:15:02 -0700

    Refactor naming conventions for instances of Turn

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,42 +5,44 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
 describe('Turn', () => {
-  let turn; //should i/ how do i add more instances when using beforeEach?
+  let turn;
   let card;
 
   beforeEach(function() {
-    turn = new Turn('object', card);
-    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card1 = new Card(12, 'What are the two ways you can access properties of an object?', ['bracket notation and dot notation', 'bracket notation and index-array notation', 'dot notation and literal notation'], 'bracket notation and dot notation');
+//card2 w wrong answer
+//card3
+    turn1 = new Turn('bracket notation and dot notation', card1);
   })
   it('should be a function', () => {
     expect(Turn).to.be.a('function');
   });
 
   it('should be an instance of Turn', () => {
-    expect(turn).to.be.an.instanceOf(Turn);
+    expect(turn1).to.be.an.instanceOf(Turn);
   });
 
   it('should be able take the user\'s guess as an argument', () => {
-    expect(turn).to.have.property('userGuess');
+    expect(turn1).to.have.property('userGuess');
   });
 
   it('should be able to take in an instance of Card', () => {
-    expect(turn).to.have.property('card');
+    expect(turn1).to.have.property('card');
   });
 
   it('should be able to return the user\'s guess', () => {
-    expect(turn.returnGuess()).to.equal('object');
+    expect(turn1.returnGuess()).to.equal('bracket notation and dot notation');
   });
 
   it('should be able to return the Card instance', () => {
-    expect(turn.returnCard()).to.equal(turn.card);
+    expect(turn1.returnCard()).to.equal(turn1.card);
   });
 
   it('should return a value of true if the user\'s guess is correct', () => {
-    expect(turn.evaluateGuess()).to.equal(true);
+    expect(turn1.evaluateGuess()).to.equal(true);
   });
 
   it('should return a value of true if the user\'s guess is correct', () => {
-    expect(turn.giveFeedback()).to.equal("Correct!");
+    expect(turn1.giveFeedback()).to.equal("Correct!");
   });
 });
