642f366832fc0547868ddae9180765d2b3756838
conconartist <conconart.info@gmail.com>
2020-12-01 21:48:56 -0700

    Add Card instances to test blocks

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -9,11 +9,12 @@ describe('Deck', function() {
   let deck;
 
   beforeEach(function() {
-    card1 = new Card();
-    card2 = new Card();
-    card3 = new Card();
+    card1 = new Card(11, "Which iteration method returns a new array with all elements that match a given condition?", ["filter()", "find()", "map()"], "filter()");
+    card2 = new Card(17, "What does the reduce() method take in?", ["callback function and initializer", "callback function and current element", "callback function and accumulator"], "callback function and initializer");
+    card3 = new Card(29, "map() takes in two optional arguments: the index of the current element, and the array that map was called upon", ["true", "false"], "true");
     deck = new Deck([card1, card2, card3]);
   })
+  
   it('should be a function', function() {
     expect(Deck).to.be.a('function');
   });
