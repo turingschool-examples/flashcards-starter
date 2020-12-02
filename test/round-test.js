72819c45f2794015a443fd02947419999e9a9060
Luke Mason <lukjmason@gmail.com>
2020-12-02 14:37:43 -0700

    Create data models for round-test.js

---
+++ test/round-test.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
+const Round = require('../src/Round');
+
+describe('Round', function() {
+  let card1;
+  let card2;
+  let card3;
+  let deck;
+  let turn;
+
+  beforeEach(function() {
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck = new Deck([card1, card2, card3]);
+  });
