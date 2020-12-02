da40c6dd7e8101d3be9a302fc9aa645b8edf730a
conconartist <conconart.info@gmail.com>
2020-12-01 22:05:11 -0700

    Initialize Round class test file

---
+++ test/Round-test.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
+
+describe('Round', function() {
+  let round;
+
+  beforeEach(function() {
+    //instances
+    round = new Round();
+  })
+
+  //it should 
+})
