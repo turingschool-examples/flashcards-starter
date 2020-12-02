46a202b74d779ff5da598403f0dc04a9f073d98f
conconartist <conconart.info@gmail.com>
2020-12-01 21:19:19 -0700

    Initialize testing framework for Deck class

---
+++ test/Deck-test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Deck', function() {
+
+});
+//initialize with an array of Card objects
+//should know how many cards are in the Deck (deck.countCards(); to find length of array)
