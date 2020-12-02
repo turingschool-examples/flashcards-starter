82640cddcc39cb5af0799dc776430bb94bc78f21
Elsa Fluss <elsafluss@gmail.com>
2020-12-01 21:53:33 -0700

    Create initial Game tests.

---
+++ test/Game-test.js
@@ -0,0 +1,16 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const Round = require('../src/Round')
+const Turn = require('../src/Turn')
+const Card = require('../src/Card')
+const Deck = require('../src/Deck')
+const Game = require('../src/Game')
+
+describe('Game', function () {
+
+  it('should be a function', function () {
+    expect(Game).to.be.a('function')
+  })
+
+})
