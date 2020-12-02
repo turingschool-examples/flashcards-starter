c148be56fee9971a58f9bea0ef789654d767dec1
mainlyetcetera <ericcampbell294@gmail.com>
2020-12-02 15:29:18 -0700

    Update card-tests to es6 functions

--- test/Card-test.js
+++ test/Card-test.js
@@ -3,37 +3,37 @@ const expect = chai.expect;
 
 const Card = require('../src/Card');
 
-describe('Card', function() {
+describe('Card', () => {
   let card;
 
-  beforeEach(function() {
+  beforeEach(() => {
     card = new Card();
   });
 
-  it.skip('should be a function', function() {    
+  it.skip('should be a function', () => {    
     expect(Card).to.be.a('function');
   });
 
-  it.skip('should be an instance of Card', function() {
+  it.skip('should be an instance of Card', () => {
     expect(card).to.be.an.instanceof(Card);
   });
 
-  it.skip('should have an id', function() {
+  it.skip('should have an id', () => {
     card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.id).to.equal(1);
   });
 
-  it.skip('should store a question', function() {
+  it.skip('should store a question', () => {
     card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
   });  
 
-  it.skip('should store a list of possible answers', function() {
+  it.skip('should store a list of possible answers', () => {
     card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
   });  
 
-  it.skip('should store the correct answer', function() {
+  it.skip('should store the correct answer', () => {
     card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.correctAnswer).to.equal('object');
   });
