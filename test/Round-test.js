3b9860a10a52f2986a6f781751549a9263d8c867
Elsa Fluss <elsafluss@gmail.com>
2020-12-01 17:26:53 -0700

    Add beforeEach and tests for takeTurn.

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,32 +1,44 @@
-const chai = require('chai');
-const expect = chai.expect;
+const chai = require('chai')
+const expect = chai.expect
 
-const Round = require('../src/Round');
-const Turn = require('../src/Turn');
+const Round = require('../src/Round')
+const Turn = require('../src/Turn')
+const Card = require('../src/Card')
 
 describe('Round', function () {
+  let card, turn, round
+
+  beforeEach(function () {
+    card = new Card(1, 'This is the question?', ['object', 'array', 'function'], 'object')
+    turn = new Turn('object', card)
+    round = new Round(card, turn, turn.guess)
+  })
 
   it('should be a function', function () {
-    const round = new Round()
     expect(Round).to.be.a('function')
   })
 
   it('should be an instance of Round', function () {
-    const round = new Round()
     expect(round).to.be.an.instanceof(Round)
   })
 
-  // it('should return the current card', function (){
-  //
-  // })
+  it('should return the current card', function () {
+    const card = new Card(1, 'What allows you to ... key-value pairs?', ['object', 'array', 'function'], 'object')
+    const round = new Round(card)
+    round.returnCurrentCard()
+    expect(round.currentCard).to.deep.equal(card)
+  })
 
-  // it('should instantiate a Turn', function () {
-  // const round = new Round()
-  // const turn = new Turn()
-  // })
+  it('should instantiate a Turn', function () {
+    round.takeTurn(turn.guess, card)
+    expect(round.currentTurn).to.deep.equal(turn)
+  })
 
   it('should add one to the turn count', function () {
-
+    round.takeTurn(turn.guess, card)
+    round.takeTurn(turn.guess, card)
+    round.takeTurn(turn.guess, card)
+    expect(round.turnCount).to.equal(3)
   })
 
   it('should change the current card to be the next card', function () {
@@ -34,6 +46,11 @@ describe('Round', function () {
   })
 
   it('should evaluate the current guess', function () {
-
+    let isRight = turn.evaluateGuess(turn.guess)
+    expect(isRight).to.equal(true)
+    const thisGuess = 'array'
+    isRight = turn.evaluateGuess(thisGuess)
+    expect(isRight).to.equal(false)
   })
+
 })
