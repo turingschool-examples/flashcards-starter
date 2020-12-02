719a564644eda6fbb2f13d2aa464d5f650b53ae9
conconartist <conconart.info@gmail.com>
2020-12-02 14:54:27 -0700

    Refactor Deck test blocks

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -5,16 +5,17 @@ const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 
 describe('Deck', function() {
-  let card1, card2, card3;
+  let card1, card2, card3, card4;
   let deck;
 
   beforeEach(function() {
-    card1 = new Card(11, "Which iteration method returns a new array with all elements that match a given condition?", ["filter()", "find()", "map()"], "filter()");
-    card2 = new Card(17, "What does the reduce() method take in?", ["callback function and initializer", "callback function and current element", "callback function and accumulator"], "callback function and initializer");
-    card3 = new Card(29, "map() takes in two optional arguments: the index of the current element, and the array that map was called upon", ["true", "false"], "true");
-    deck = new Deck([card1, card2, card3]);
+    card1 = new Card(3, 'An object allows you to define a set of related information using what kind of pairs?', ['object-subject', 'key-value', 'index-array'], 'key-value');
+    card2 = new Card(5,'Which dog breed most resembles the data type that indicates a true or false value?', ['Golden Retriver', 'Cocker Spaniel', 'Sheepdog', 'Bolognese'], 'Bolognese');
+    card3 = new Card(12, 'What are the two ways you can access properties of an object?', ['bracket notation and dot notation', 'bracket notation and index-array notation', 'dot notation and literal notation'], 'bracket notation and dot notation');
+    card4 = new Card(30, 'What is the acronym for primitive data types?', ['BUNNS', 'STUBS', 'BUNY', 'YOLO'], 'BUNNS')
+    deck = new Deck([card1, card2, card3, card4]);
   })
-  
+
   it('should be a function', function() {
     expect(Deck).to.be.a('function');
   });
@@ -22,9 +23,9 @@ describe('Deck', function() {
     expect(deck).to.be.an.instanceOf(Deck);
   })
   it('should include an array of Card objects', function() {
-    expect(deck.cards).to.deep.equal([card1, card2, card3]);
+    expect(deck.cards).to.deep.equal([card1, card2, card3, card4]);
   })
   it('should know how many cards are in the Deck', function() {
-    expect(deck.countCards()).to.equal(3);
+    expect(deck.countCards()).to.equal(4);
   })
 });
