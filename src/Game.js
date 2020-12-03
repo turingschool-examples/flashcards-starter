a0157ceb37333a64907df5c40b4887741a07448f
conconartist <conconart.info@gmail.com>
2020-12-03 14:20:39 -0700

    Build Game class

--- src/Game.js
+++ src/Game.js
@@ -2,9 +2,24 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
-class Game {
-  constructor() {}
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
+class Game {
+  constructor() {
+    this.currentRound;
+  }
+  start() {
+    const gameCards = prototypeQuestions.map(gameCard => {
+      return new Card(gameCard.id, gameCard.question, gameCard.answers, gameCard.correctAnswer);
+    })
+    const gameDeck = new Deck(gameCards);
+    let gameRound = new Round(gameDeck);
+    this.currentRound = gameRound;
+    this.printMessage(gameDeck, gameRound);
+    this.printQuestion(gameRound);
+  }
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
