3fe2fbb8f1f264ca65f5e1f458c12752fa9def42
mattdean <matthewallandean@gmail.com>
2020-12-02 17:33:14 -0700

    Refactor tests to include beforeEach functions

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,8 +4,12 @@ const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
 describe('Turn', function() {
-  let card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-  let turn = new Turn('object', card);
+  var card;
+  var turn;
+  beforeEach(function() {
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    turn = new Turn('object', card);
+  });
 
   it('should be a function', function() {
     expect(Turn).to.be.a('function');
