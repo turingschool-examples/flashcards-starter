5658c8249722f9b56fdd17a0a3fa5afcf1a4e9b4
mainlyetcetera <ericcampbell294@gmail.com>
2020-12-02 21:35:47 -0700

    Unskip and pass fn and instance tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,12 +6,12 @@ const Deck = require('../src/Deck');
 
 describe('Deck', () => {
   
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     const deck = new Deck();
     expect(Deck).to.be.a('function');
   });
 
-  it.skip('should be an instance of Deck', () => {
+  it('should be an instance of Deck', () => {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
