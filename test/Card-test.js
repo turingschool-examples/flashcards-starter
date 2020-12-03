8f1c9ca77fd871382280c8ec8c07d6f47dbcfcc2
conconartist <conconart.info@gmail.com>
2020-12-03 15:14:46 -0700

    Refactor Card test

--- test/Card-test.js
+++ test/Card-test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 
 const Card = require('../src/Card');
 
-describe('Card', function() {
+describe('Card', () => {
   let card;
 
   beforeEach(function() {
@@ -11,17 +11,17 @@ describe('Card', function() {
     card2 = new Card(5, 'Which dog breed most resembles the data type that indicates a true or false value?', ['Golden Retriver', 'Cocker Spaniel', 'Sheepdog', 'Bolognese'], 'Bolognese');
   });
 
-  it('should be able to store a question', function() {
+  it('should be able to store a question', () => {
     expect(card1.question).to.equal('An object allows you to define a set of related information using what kind of pairs?');
     expect(card2.question).to.equal('Which dog breed most resembles the data type that indicates a true or false value?')
   });
 
-  it('should be able to store a list of possible answers', function() {
+  it('should be able to store a list of possible answers', () => {
     expect(card1.answers).to.deep.equal(['object-subject', 'key-value', 'index-array']);
     expect(card2.answers).to.deep.equal(['Golden Retriver', 'Cocker Spaniel', 'Sheepdog', 'Bolognese']);
   });
 
-  it('should be able to store the correct answer', function() {
+  it('should be able to store the correct answer', () => {
     expect(card1.correctAnswer).to.equal('key-value');
     expect(card2.correctAnswer).to.equal('Bolognese');
   });
