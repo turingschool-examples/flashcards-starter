c5eb44cba2e7fe8a1fd8c826ad3373f389deb902
Cole Fiscus <coletfiscus@gmail.com>
2020-12-02 23:08:00 -0700

    Add functionality to Game and call start inside of index

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,9 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('./Card');
+const Deck = require('./Deck');
+const Round = require('./Round');
 
 class Game {
   constructor() {
@@ -8,12 +11,11 @@ class Game {
   }
 
   start() {
-    const card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
-    const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
-    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
-    const deck = new Deck([card1, card2, card3]);
+    const cards = prototypeQuestions.map(question => {
+      return new Card(question['id'], question['question'], question['answers'], question['correctAnswer']);
+    });
+    const deck = new Deck(cards);
     const round = new Round(deck);
-
     this.printMessage(deck, round);
     this.printQuestion(round);
   }
