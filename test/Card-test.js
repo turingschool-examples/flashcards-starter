14485a91bdeac640189a9268e3274c4d277e883e
conconartist <conconart.info@gmail.com>
2020-12-02 17:37:26 -0700

    Refactor Card instances in test

--- test/Card-test.js
+++ test/Card-test.js
@@ -9,7 +9,6 @@ describe('Card', function() {
   beforeEach(function() {
     card1 = new Card(3, 'An object allows you to define a set of related information using what kind of pairs?', ['object-subject', 'key-value', 'index-array'], 'key-value');
     card2 = new Card(5, 'Which dog breed most resembles the data type that indicates a true or false value?', ['Golden Retriver', 'Cocker Spaniel', 'Sheepdog', 'Bolognese'], 'Bolognese');
-    card3 = new Card(12, 'What are the two ways you can access properties of an object?', ['bracket notation and dot notation', 'bracket notation and index-array notation', 'dot notation and literal notation'], 'bracket notation and dot notation');
   });
 
   it('should be able to store a question', function() {
