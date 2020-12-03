6b660240a8c28de339fbc43d49913080f07e9b0b
conconartist <conconart.info@gmail.com>
2020-12-02 20:25:01 -0700

    Add test blocks for Round class test

--- test/Round-test.js
+++ test/Round-test.js
@@ -2,13 +2,13 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Card = require('../src/Card');
-const Turn = require('../src/Turn');
 const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
 const Round = require('../src/Round');
 
 describe('Round', function() {
   let card1, card2, card3, card4;
-  let turn;
+  let turn1, turn2;
   let deck;
   let round;
 
@@ -16,16 +16,63 @@ describe('Round', function() {
     card1 = new Card(3, 'An object allows you to define a set of related information using what kind of pairs?', ['object-subject', 'key-value', 'index-array'], 'key-value');
     card2 = new Card(5,'Which dog breed most resembles the data type that indicates a true or false value?', ['Golden Retriver', 'Cocker Spaniel', 'Sheepdog', 'Bolognese'], 'Bolognese');
     card3 = new Card(12, 'What are the two ways you can access properties of an object?', ['bracket notation and dot notation', 'bracket notation and index-array notation', 'dot notation and literal notation'], 'bracket notation and dot notation');
-    card4 = new Card(30, 'What is the acronym for primitive data types?', ['BUNNS', 'STUBS', 'BUNY', 'YOLO'], 'BUNNS')
+    card4 = new Card(30, 'What is the acronym for primitive data types?', ['BUNNS', 'STUBS', 'BUNY', 'YOLO'], 'BUNNS');
     deck = new Deck([card1, card2, card3, card4]);
-    round = new Round();
+    turn1 = new Turn('index-array', card1);//incorrect answer
+    turn2 = new Turn('key-value', card1)//correct answer
+    round1 = new Round(deck);
   })
-  
+
+  it('should include an instance of Deck as a property', () => {
+    expect(round1.deck).to.be.an.instanceOf(Deck);
+  })
+
   //it should have a property that keeps track of number of turns
+  it('should include a property of number of turns taken', () => {
+    expect(round1).to.have.property('turnsCount');
+  })
+
+  it('should start the turns count at the number zero', () => {
+    expect(round1.turnsCount).to.equal(0);
+  })
+
   //it should have a property that keeps track of incorrect guesses
+  it('should have a property that keeps track of incorrect guesses made', () => {
+    expect(round1).to.have.property('incorrectGuesses');
+  })
+
+  it('should start the value of incorrect guesses made with an empty array', () => {
+    expect(round1.incorrectGuesses).to.deep.equal([]);
+  })
   //it should have a method to return the current card being played (returnCurrentCard())
+  it('should have a method that returns the current card being played', () => {
+    expect(round1.returnCurrentCard()).to.equal(round1.deck.cards[0]);
+  })
   //it should have a method that updates turns count, evaluates, guesses, gives feedback, and stores ids of incorrect guesses (takeTurn())
-  //it should instantiate a new instance of Turn when a guess is made
+  it('should have a method that updates the turns count', () => {
+    round1.returnCurrentCard();
+    round1.takeTurn(turn1);
+    expect(round1.turnsCount).to.equal(1);
+  })
+
+  it('should evaluates guesses and stores ids of incorrect guesses in the same method', () => {
+    round1.returnCurrentCard();
+    round1.takeTurn(turn1);
+    expect(round1.incorrectGuesses).to.deep.equal([3]);
+  })
+
+  it('should return feedback in the same method if the guess is incorrect', () => {
+    round1.returnCurrentCard();
+    round1.takeTurn(turn1);
+    expect(round1.takeTurn(turn1)).to.equal('Incorrect!');
+  })
+
+  it('should return feedback in the same method if the guess is correct', () => {
+    round1.returnCurrentCard();
+    round1.takeTurn(turn2);
+    expect(round1.takeTurn(turn2)).to.equal('Correct!');
+  })
+
   //it should update the turn count, regardless of whether the guess is correct or Incorrect
   //it should reassign the next card in the array to the currentCard variable
   //it should evaluate and record the guess
