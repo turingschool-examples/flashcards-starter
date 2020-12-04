bf1a0a9d0780e85f20a4d1e8f9795bd267a6b9fb
Ben Prat <bvprat@gmail.com>
2020-12-03 19:18:24 -0700

    Add take turn function

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,22 @@
+const Turn = require('../src/Turn');
+
 class Round {
     constructor(deck) {
         this.deck = deck.cards;
+        this.turns = 0;
+        this.incorrectGuesses = [];
+    }
+    currentCard() {
+        return this.deck.shift();
+    }
+    takeTurn(guess) {
+        this.turns++;
+        var turn = new Turn(guess, this.deck[0])
+        turn.evaluateGuess(this.guess);
+        turn.giveFeedback(this.guess);
+        if (turn.guess !== this.deck[0].correctAnswer) {
+            this.incorrectGuesses.push(this.deck[0].id);
+        }
     }
 }
 
