fe16f020b362c272fcf1e8e1bb8fff33b6eaeb54
Cole Fiscus <coletfiscus@gmail.com>
2020-12-03 20:58:59 -0700

    Add gif file and tiny fixes

--- src/Game.js
+++ src/Game.js
@@ -16,7 +16,7 @@ class Game {
     });
     const deck = new Deck(cards);
     this.currentRound = new Round(deck);
-    this.printMessage(deck, this.currentRound);
+    this.printMessage(deck);
     this.printQuestion(this.currentRound);
   }
 
