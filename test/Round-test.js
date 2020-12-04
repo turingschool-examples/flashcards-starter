940e4e01d6ed481a520eadfacaa4fbb934d428f8
Ben Prat <bvprat@gmail.com>
2020-12-03 19:31:08 -0700

    Add test for percentage of correct

--- test/Round-test.js
+++ test/Round-test.js
@@ -18,8 +18,7 @@ describe('Round', function() {
         card1 = new Card(1, 'What color is the sky', ['orange', 'green', 'blue'], 'blue');
         card2 = new Card(2, 'What month is Halloween in', ['October', 'December', 'June'], 'October');
         card3 = new Card(3, 'What animal has stripes', ['lion', 'zebra', 'cow'], 'zebra');
-        card4 = new Card(4, 'What color is a stop sign' ['blue', 'red', 'purple'], 'red')
-        deck = new Deck([card1, card2, card3, card4]);
+        deck = new Deck([card1, card2, card3]);
         round = new Round(deck);
     });
 
@@ -32,12 +31,17 @@ describe('Round', function() {
     });
 
     it('should have a deck of cards', function() {
-        expect(round.deck).to.deep.equal([card1, card2, card3, card4]);
+        expect(round.deck).to.deep.equal([card1, card2, card3]);
     })
 
     it('should return the current card', function() {
-        expect(round.currentCard()).to.equal(card1);
+        expect(round.returnCurrentCard()).to.equal(card1);
     })
+
+    it('should create a new instance of the Turn class', function() {
+        expect(turn).to.be.an.instanceof(Turn);
+    })
+
     it('should update turns count', function() {
         round.takeTurn()
         round.takeTurn()
@@ -45,9 +49,28 @@ describe('Round', function() {
         expect(round.turns).to.equal(2);
     })
 
+    it('should return feedback', function() {
+        round.takeTurn('orange')
+
+        expect(round.takeTurn('orange')).to.equal('incorrect!')
+    })
+
     it('should track incorrect guesses', function() {
         round.takeTurn('orange')
 
         expect(round.incorrectGuesses.length).to.equal(1);
+    })
+    it('should update the current card', function() {
+        round.takeTurn('orange')
+
+        expect(round.returnCurrentCard()).to.equal(card2)
+    })
+
+    it('should calculate percentage of correct answers', function() {
+        round.takeTurn('blue')
+        round.takeTurn('December')
+
+        expect(round.calculatePercentCorrect()).to.equal(50);
+
     })
 })
