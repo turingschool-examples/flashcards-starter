da612dabfc2e4e97655ab33f2d0c79939a91342b
Ben Prat <bvprat@gmail.com>
2020-12-03 19:27:09 -0700

    Add calculate percent function

--- src/Round.js
+++ src/Round.js
@@ -6,17 +6,25 @@ class Round {
         this.turns = 0;
         this.incorrectGuesses = [];
     }
-    currentCard() {
-        return this.deck.shift();
+    returnCurrentCard() {
+        let currentCard = this.deck[0];
+        return currentCard;
     }
+
     takeTurn(guess) {
+        var turn = new Turn(guess, this.returnCurrentCard());
         this.turns++;
-        var turn = new Turn(guess, this.deck[0])
-        turn.evaluateGuess(this.guess);
-        turn.giveFeedback(this.guess);
         if (turn.guess !== this.deck[0].correctAnswer) {
-            this.incorrectGuesses.push(this.deck[0].id);
+            this.incorrectGuesses.push(this.returnCurrentCard().id);
         }
+        this.deck.splice(0, 1);
+        return turn.giveFeedback(guess);
+
+    }
+    calculatePercentCorrect() {
+        let correct = this.turns - this.incorrectGuesses.length;
+        let percent = (correct / this.turns) * 100
+        return percent;
     }
 }
 
