0d24b47d06426413f48f39e82bdb001317a294a9
Ben Prat <bvprat@gmail.com>
2020-12-06 11:55:57 -0700

    Resolve usability issue.

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ app.listen(3000, '127.0.0.1');
 console.log('Node server running on port 3000');
 
 const Game = require('../flashcards/src/Game.js');
+
+const currentGame = new Game();
+currentGame.start();
