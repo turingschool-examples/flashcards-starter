5111a4b81a7aa904305669b34960a0f47bf7857a
Jackson McGuire <jaxmcguire@gmail.com>
2021-02-02 10:27:06 -0500

    Pass turn tests

---
+++ src/Turn.js
@@ -0,0 +1,32 @@
+class Turn {
+  constructor(guess, cardInst) {
+    this.userGuess = guess;
+    this.card = cardInst;
+  }
+  
+  returnGuess() {
+    return this.userGuess;
+  }
+
+  returnCard() {
+    return `${this.card}`;
+  }
+
+  evaluateGuess() {
+    if (this.userGuess === this.card.correctAnswer) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  giveFeedback() {
+    if (!this.evaluateGuess()) {
+      return 'incorrect!';
+    } else {
+      return 'correct!';
+    }
+  }
+}
+
+module.exports = Turn;
