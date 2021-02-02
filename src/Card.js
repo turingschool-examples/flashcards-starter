bba22fb784b1bd4678b16d75347690504e5d3cac
Paige Vannelli <paigevannelli@gmail.com>
2021-02-01 18:44:58 -0700

    Redo card class setup

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,10 @@
 class Card {
-    constructor() {
-        this.id = id; 
-        this.question = question; 
-        this.possibleAnswers = possibleAnswers;
-        this.correctAnswer = correctAnswer 
+    constructor(index) {
+        this.id = prototypeData[index].id; 
+        this.question = prototypeData[index].question; 
+        this.possibleAnswers = prototypeData[index].answers;
+        this.correctAnswer = prototypeData[index].correctAnswer 
+        //We want to use our protype data which is an array of objects
+        //for each element in the array the card should be the 
     }
 }
