b8afcebc14cf7e2c511364fe64cda05bb2b52712
Charles Morris <percworld@gmail.com>
2021-02-02 17:21:35 -0700

    Add Deck and Turn files

---
+++ src/Deck.js
@@ -0,0 +1,15 @@
+// being called from Game
+
+
+class Deck {
+  constructor(array) {
+    //forEach(element) instantiate Card?
+    //where will I create a card?
+  }
+  countCards() {
+    //return deck.length
+  }
+
+}
+
+module.exports = Deck;
---
+++ src/Turn.js
@@ -0,0 +1,18 @@
+// being called from Round
+
+class Turn {
+  constructor() {}
+  returnGuess() {
+    //array index?
+  }
+  returnCard() {
+    // returns the object I assume
+  }
+  evaluateGuess() {
+    // returns boolean
+  }
+  giveFeedback() {
+    //returns 'correct' or 'incorrect'
+  }
+}
+module.exports = Turn;
