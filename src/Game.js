11beae30afcfdeb4cbaf62eba5cf94ddedc72d62
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-02-02 20:33:07 -0700

    Add Game class with corresponding test suite

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,16 @@
 const data = require('./data');
+const Deck = require('./Deck');
+const Round = require('./Round');
+const Card = require('../src/Card');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.cards = null;
+    this.deck = null;
+    this.currentRound = null;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +19,14 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+  
+  startGame() {
+    this.cards = prototypeQuestions.map(cardInfo => new Card(cardInfo.id, cardInfo.question, cardInfo.answers, cardInfo.correctAnswer));
+    this.deck = new Deck(this.cards);
+    this.currentRound = new Round(this.deck);
+    this.printMessage(this.deck, this.round);
+    this.printQuestion(this.round);
   }
 }
 
