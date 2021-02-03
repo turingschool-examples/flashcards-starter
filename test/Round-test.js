054d0539928a22cc87109fe3d93b2988befb5774
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-02-02 19:13:04 -0700

    Build Round class to pass all unit tests

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,5 +1,10 @@
-const { expect } = require("chai");
+const chai = require('chai');
+const expect = chai.expect;
+
 const Turn = require("../src/Turn");
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 describe('Round', function() {
 
