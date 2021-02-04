4f3e097c5951a30164d6ae16ef3358e57c5595c6
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2021-02-04 12:29:19 -0700

    Fix errors in Round test suite

--- test/Round-test.js
+++ test/Round-test.js
@@ -49,14 +49,13 @@ describe('Round', function() {
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
 
-    round.takeTurn();
-    round.takeTurn();
-    round.takeTurn();
+    round.takeTurn('object');
+    round.takeTurn('array');
+    round.takeTurn('accessor method');
 
     expect(round.turnCount).to.equal(3);
-    expect(round.totals).to.equal([{card: 1, guess: 'object', correct: true}, {card: 2, guess: 'array', correct: true}, {card: 3, guess: 'object', correct: false} ])
     expect(round.currentCard).to.equal(card4);
-    expect(round.incorrectGuesses).to.equal([{card: 3, guess: 'object', correct: false}]);
+    expect(round.incorrectGuesses).to.equal(1);
   });
 
   it('should calculate percent correct', function() {
@@ -64,14 +63,15 @@ describe('Round', function() {
     const card2 = new Card(testData[1].id, testData[1].question, testData[1].answers, testData[1].correctAnswer);
     const card3 = new Card(testData[2].id, testData[2].question, testData[2].answers, testData[2].correctAnswer);
     const card4 = new Card(testData[3].id, testData[3].question, testData[3].answers, testData[3].correctAnswer);
-    const deck = new Deck([card1, card2, card3]);
+    const deck = new Deck([card1, card2, card3, card4]);
     const round = new Round(deck);
 
-    round.takeTurn();
-    round.takeTurn();
-    round.takeTurn();
+    round.takeTurn('object');
+    round.takeTurn('array');
+    round.takeTurn('accessor method');
+    round.takeTurn('iteration method');
 
-    expect(round.calculatePercentCorrect()).to.equal(`66%`);
+    expect(round.calculatePercentCorrect()).to.equal(`50%`);
   });
 
   it('should be able to end the round', function() {
@@ -79,14 +79,14 @@ describe('Round', function() {
     const card2 = new Card(testData[1].id, testData[1].question, testData[1].answers, testData[1].correctAnswer);
     const card3 = new Card(testData[2].id, testData[2].question, testData[2].answers, testData[2].correctAnswer);
     const card4 = new Card(testData[3].id, testData[3].question, testData[3].answers, testData[3].correctAnswer);
-    const deck = new Deck([card1, card2, card3]);
+    const deck = new Deck([card1, card2, card3, card4]);
     const round = new Round(deck);
 
-    round.takeTurn();
-    round.takeTurn();
-    round.takeTurn();
+    round.takeTurn('object');
+    round.takeTurn('array');
+    round.takeTurn('accessor method');
+    round.takeTurn('iteration method');
 
-    expect(round.endRound()).to.equal(`** Round over! ** You answered ${round.calculatePercentCorrect} of the questions correctly!`);
+    expect(round.endRound()).to.equal(`** Round over! ** You answered 50% of the questions correctly!`);
   });
-
 });
