7e800ec49049697e6a7e63b89098ad1c81a08e27
Matt McVey <matt.mcvey49@gmail.com>
2021-02-03 17:13:13 -0700

    Wrote code to pass all tests in Turns-test file

---
+++ src/Round.js
@@ -0,0 +1,35 @@
+const Turn = require('../src/Turn');
+
+class Round {
+  constructor(deck, guess) {
+    this.currentCard = deck.deck[0];
+    this.guess = guess;
+    this.turns = 0;
+    this.incorrectGuesses = []
+  }
+  returnCurrentCard() {
+    return this.currentCard
+  }
+  takeTurn() {
+    const turn = new Turn(this.guess, this.currentCard)
+    this.turns++;
+    if (turn.evaluateGuess()) {
+      return turn.giveFeedback()
+    } else {
+      this.incorrectGuesses.push(this.currentCard.id)
+      return turn.giveFeedback()
+    }
+  }
+  calculatePercentCorrect() {
+    const correctAnswers =  30 - this.incorrectGuesses.length;
+    const correctAnswersPercent = (correctAnswers / 30) * 100;
+    return correctAnswersPercent.toFixed(2)
+  }
+  endRound() {
+    const percent = this.calculatePercentCorrect()
+    console.log(`** Round over! ** You answered ${percent}% of the questions correctly!`)
+    return `** Round over! ** You answered ${percent}% of the questions correctly!`
+  }
+}
+
+module.exports = Round;
--- test/Turns-test.js
+++ test/Turns-test.js
@@ -1,6 +1,5 @@
 const chai = require('chai');
 const expect = chai.expect;
-const assert = chai.assert;
 
 const Card = require('../src/Card')
 const Turn = require('../src/Turn');
