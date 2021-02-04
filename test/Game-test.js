5291783f49e6cae87ef46c607a4c0fedefc3a111
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2021-02-04 15:56:26 -0700

    Build out Game class test suite

---
+++ test/Game-test.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const data = require('../src/data');
+const testData = data.prototypeData;
+
+describe('Game', function() {
+
+  it('should be a function', function() {
+    const game = new Game;
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of a Game', function() {
+    const game = new Game;
+    expect(Game).to.be.an.instanceOf(Game);
+  });
+
+  it('should be able to start game', function() {
+    const game = new Game();
+
+    game.start();
+
+    expect(game.deck[0]).to.be.an.instanceOf(Card);
+    expect(game.deck).to.be.an.instanceOf(Deck);
+    expect(game.deck.length).to.equal(30);
+    expect(game.currentRound).to.be.an.instanceOf(Round);
+  });
+
+  it('should keep track of current round', function() {
+    const game = new Game();
+    expect(game.currentRound).to.be.an.instanceOf(Round);
+  });
+
+
+});
