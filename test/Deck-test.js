d581e435133419a4c4379e81ec63a007849972eb
Aaron Foucheaux <afoucheaux@gmail.com>
2021-02-03 19:45:17 -0600

    Refactor of testing code

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,33 +4,39 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
-describe('Deck', function() {
+describe('Deck', () => {
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Deck).to.be.a('function');
   });
 
-  it('should be an instance of Deck', function() {
+  it('should be an instance of Deck', () => {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
 
-  it('should be able to store an array of card objects', function() {
-    const cardOne = new Card(1, 'QuestionOne', ['Not', 'It', 'AnswerOne'], 'AnswerOne');
-    const cardTwo = new Card(2, 'QuestionTwo', ['Not', 'It', 'AnswerTwo'], 'AnswerTwo');
-    const cardThree = new Card(3, 'QuestionThree', ['Not', 'It', 'AnswerThree'], 'AnswerThree');
-    const deck = new Deck([cardOne, cardTwo, cardThree]);
-    expect(deck.cards).to.be.an.instanceof(Array);
-    expect(deck.cards[0]).to.be.an.instanceof(Card);
-    expect(deck.cards[2]).to.equal(cardThree);
-  });
+  describe('countCards', () => {
+    let cardOne;
+    let cardTwo;
+    let cardThree;
+    let deck;
 
-  it('should be able to know how many cards are in the deck', function() {
-    const cardOne = new Card(1, 'QuestionOne', ['Not', 'It', 'AnswerOne'], 'AnswerOne');
-    const cardTwo = new Card(2, 'QuestionTwo', ['Not', 'It', 'AnswerTwo'], 'AnswerTwo');
-    const cardThree = new Card(3, 'QuestionThree', ['Not', 'It', 'AnswerThree'], 'AnswerThree');
-    const deck = new Deck([cardOne, cardTwo, cardThree]);
-    expect(deck.countCards()).to.equal(3);
+    beforeEach(function() {
+      cardOne = new Card(1, 'QuestionOne', ['Not', 'It', 'AnswerOne'], 'AnswerOne');
+      cardTwo = new Card(2, 'QuestionTwo', ['Not', 'It', 'AnswerTwo'], 'AnswerTwo');
+      cardThree = new Card(3, 'QuestionThree', ['Not', 'It', 'AnswerThree'], 'AnswerThree');
+      deck = new Deck([cardOne, cardTwo, cardThree]);
+    });
+
+    it('should be able to store an array of card objects', () => {
+      expect(deck.cards).to.be.an.instanceof(Array);
+      expect(deck.cards[0]).to.be.an.instanceof(Card);
+      expect(deck.cards[2]).to.equal(cardThree);
+    });
+
+    it('should be able to know how many cards are in the deck', () => {
+      expect(deck.countCards()).to.equal(3);
+    });
 
   });
 
