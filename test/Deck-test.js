b851ec788fc1baf602eef9c2a31f2a447f339e41
Sarah Fitzsimons <sarahfitz2713@gmail.com>
2021-03-16 17:23:33 -0600

    Refactor card, turn and deck tests and classes

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,12 +3,10 @@ const expect = chai.expect;
 
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
-const { assert } = require('chai');
 
 describe('Deck', function() {
 
   it('should be a function', function() {
-    const deck = new Deck();
     expect(Deck).to.be.a('function');
   });
 
@@ -18,19 +16,33 @@ describe('Deck', function() {
   }); 
 
   it('should have an array of Card objects', function() {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', 
+      ['sea otter', 'pug', 'capybara'], 
+      'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', 
+      ['spleen', 'appendix', 'gallbladder'], 
+      'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', 
+      ['Lex', 'William', 'Fitzgerald'], 
+      'Fitzgerald');
   
     const deck = new Deck([card1, card2, card3]);
     expect(deck.deckCards).to.deep.equal([card1, card2, card3]);
   });
 
   it('should know how many cards in the deck', function() {
-    const card1 = new Card(3, 'What is Sarah\'s favorite animal', ['dog', 'cat', 'hamster'], 'dog');
-    const card2 = new Card(21, 'What organ is Fitz missing?', ['spleen', 'appendix', 'gallbladder'], 'appendix');
-    const card3 = new Card(32, 'What is Devin\'s middle name?', ['William', 'Patrick', 'Fitzgerald'], 'Patrick');
-    const card4 = new Card(15, 'What bone is commonly called the funny bone', ['Femur', 'humerus', 'ulna'], 'humerus');
+    const card1 = new Card(3, 'What is Sarah\'s favorite animal', 
+      ['dog', 'cat', 'hamster'], 
+      'dog');
+    const card2 = new Card(21, 'What organ is Fitz missing?', 
+      ['spleen', 'appendix', 'gallbladder'], 
+      'appendix');
+    const card3 = new Card(32, 'What is Devin\'s middle name?', 
+      ['William', 'Patrick', 'Fitzgerald'], 
+      'Patrick');
+    const card4 = new Card(15, 'What bone is commonly called the funny bone', 
+      ['Femur', 'humerus', 'ulna'], 
+      'humerus');
     
     const deck = new Deck([card1, card2, card3, card4]); 
     expect(deck.countCards()).to.deep.equal(4);
