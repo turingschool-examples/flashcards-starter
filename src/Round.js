64547e516803488eb67c47ad511a8e6a25a683b2
Sarah Fitzsimons <sarahfitz2713@gmail.com>
2021-03-17 09:40:41 -0600

    Set up methods in Round class

--- src/Round.js
+++ src/Round.js
@@ -1,1 +1,28 @@
+class Round {
+  constructor(deck) {
+    this.deck = deck;
+  }
+
+  returnCurrentCard() {
+    //returns current card being played
+  }
+
+  takeTurn() {
+    //updates turns count
+    //evaluates guesses
+    //gives feedback
+    //stores ids of incorrect guesses
+  }
+
+  caluculatePercentCorrect() {
+    //calculates and returns the % of correct guesses
+  }
+
+  endRound() {
+    //prints "**Round over!**You answered <>% of the questions
+    //correctly!"
+  }
+}
+
+
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -5,3 +5,17 @@ const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 const Turn = require('../src/Turn');
+
+
+describe('Round', function() {
+
+  it('should be a function', function() {
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', function() {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  }); 
+
+  }); 
