2f89bf5a5a3a12ad708805b489f53027287726dd
mcraig2342 <matthewcookecraig10@gmail.com>
2021-03-22 15:33:22 -0600

    Add tests to check for card id

--- test/Card-test.js
+++ test/Card-test.js
@@ -13,17 +13,27 @@ describe('Card', function() {
   it.skip('should be an instance of Card', function() {
     const card = new Card();
     expect(card).to.be.an.instanceof(Card);
-  }); 
+  });
+
+  it.skip('should have an id', function() {
+    const card = new Card();
+    expect(card).to.have.property('id');
+  });
+
+  it.skip('should be able to take an id', function() {
+    const card = new Card(1);
+    expect(card.id).to.equal(1);
+  });
 
   it.skip('should store a question', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
-  });  
+  });
 
   it.skip('should store a list of possible answers', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
-  });  
+  });
 
   it.skip('should store the correct answer', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
