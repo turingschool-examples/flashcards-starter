240464f49b05d0a2365e04ea6625653fed0a9ca8
mcraig2342 <matthewcookecraig10@gmail.com>
2021-03-22 15:55:24 -0600

    Add test to check if turn is a function/class

---
+++ test/Turn-test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+  it.skip('should be a function', function() {
+    const card = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+});
