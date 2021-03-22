e0ae0e61bb239700580b15c1e5013ffa9ddc3edf
robertjosephderouin <robertjosephderouin@gmail.com>
2021-03-22 15:50:21 -0700

    Add turn test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,32 +1,68 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
 describe('Card', function() {
 
-  it('should be a function', function() {
-    const card = new Card();
-    expect(Card).to.be.a('function');
+  it.skip('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
   });
 
-  it('should be an instance of Card', function() {
-    const card = new Card();
-    expect(card).to.be.an.instanceof(Card);
+  it.skip('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
   });
 
-  it('should store a question', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
+  it.skip('should store a user guess', function() {
+    const exampleGuess = "peanut";
+    const exampleCard = new Card(1, "What is a legume", ["apple", "pear", "peanut"], "peanut");
+    const turn = new Turn(exampleGuess, exampleCard);
+    expect(turn.guess).to.equal('peanut');
   });
 
-  it('should store a list of possible answers', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    expect(card.answers).to.deep.equal(['object', 'array', 'function']);
+  it.skip('should store a card', function() {
+    const exampleGuess = "peanut";
+    const exampleCard = new Card(1, "What is a legume", ["apple", "pear", "peanut"], "peanut");
+    const turn = new Turn(exampleGuess, exampleCard);
+    expect(turn.card).to.equal(exampleCard);
   });
 
-  it('should store the correct answer', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    expect(card.correctAnswer).to.equal('object');
+  it.skip('should have a method that returns the guess', function() {
+    const exampleGuess = "peanut";
+    const exampleCard = new Card(1, "What is a legume", ["apple", "pear", "peanut"], "peanut");
+    const turn = new Turn(exampleGuess, exampleCard);
+    expect(turn.returnGuess).to.equal(exampleGuess);
   });
+
+  it.skip('should have a method that returns the card', function() {
+    const exampleGuess = "peanut";
+    const exampleCard = new Card(1, "What is a legume", ["apple", "pear", "peanut"], "peanut");
+    const turn = new Turn(exampleGuess, exampleCard);
+    expect(turn.returnCard).to.equal(exampleCard);
+  });
+
+  it.skip('should have a method that returns true if the guess matches the correct answer', function() {
+    const exampleGuess = "peanut";
+    const exampleCard = new Card(1, "What is a legume", ["apple", "pear", "peanut"], "peanut");
+    const turn = new Turn(exampleGuess, exampleCard);
+    expect(turn.evaluateGuess).to.equal(true);
+  });
+
+  it.skip('should give feedback about answer', function() {
+    const exampleGuess = "peanut";
+    const exampleCard = new Card(1, "What is a legume", ["apple", "pear", "peanut"], "peanut");
+    const turn = new Turn(exampleGuess, exampleCard);
+    expect(turn.giveFeedback).to.equal("correct!");
+  });
+
+  it.skip('should give negative feedback about answer if wrong', function() {
+    const exampleGuess = "apple";
+    const exampleCard = new Card(1, "What is a legume", ["apple", "pear", "peanut"], "peanut");
+    const turn = new Turn(exampleGuess, exampleCard);
+    expect(turn.giveFeedback).to.equal("incorrect!");
+  });
+
 });
