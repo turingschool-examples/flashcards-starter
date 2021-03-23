cfd12b414778c2e70c86a61cfa367d3ea56ca4e5
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-03-22 22:12:30 -0600

    Add Turn initialization tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,1 +1,26 @@
+const chai = require("chai");
+const expect = chai.expect;
 
+const Card = require("../src/Card");
+const Turn = require("../src/Turn");
+
+describe("Turn", function() {
+
+  it.skip("Should be a function", function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a("function");
+  });
+
+  it.skip("Should keep track of current card in play", function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn("function", card);
+    expect(turn.currentCard).to.equal(card);
+    expect.(turn.currentCard).to.be.a("object");
+  });
+
+  it.skip("Should store the user\s guess to the question", function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn("function", card);
+    expect(turn.userGuess).to.equal("function");
+  })
+})
