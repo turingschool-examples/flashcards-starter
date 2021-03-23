de11ecba66d24274d9b049e7da3a31570ee58e9e
mcraig2342 <matthewcookecraig10@gmail.com>
2021-03-23 12:41:34 -0600

    Finish deck class

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,26 +4,25 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 describe('Deck', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const deck = new Deck();
-    expect(deck).to.be.a('function');
+    expect(Deck).to.be.a('function');
   });
 
-  it.skip('should be an instance of Deck', function() {
+  it('should be an instance of Deck', function() {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
 
-  it.skip('should be able to take an array of cards', function() {
+  it('should be able to take an array of cards', function() {
     const card1 = new Card(1, '1st planet from the sun', ['earth', 'mercury', 'mars'], 'mercury');
     const card2 = new Card(2, 'capital of NY', ['albany', 'syracuse', 'buffalo'], 'albany');
     const card3 = new Card(3, 'north star name', ['polaris', 'sirius', 'rarius'], 'polaris');
     const deck = new Deck([card1, card2, card3]);
-    expect(deck.cards).to.have.property('cards').with.lengthOf(3);
-    expect(deck.cards).to.equal([card1, card2, card3]);
+    expect(deck).to.have.property('cards').with.lengthOf(3);
   });
 
-  it.skip('should be able to know the amount of cards in the deck', function() {
+  it('should be able to know the amount of cards in the deck', function() {
     const card1 = new Card(1, '1st planet from the sun', ['earth', 'mercury', 'mars'], 'mercury');
     const card2 = new Card(2, 'capital of NY', ['albany', 'syracuse', 'buffalo'], 'albany');
     const card3 = new Card(3, 'north star name', ['polaris', 'sirius', 'rarius'], 'polaris');
