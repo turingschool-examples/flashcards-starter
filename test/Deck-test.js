fcb76cbc020497afc489ca265ffbfcfe7d6385e8
Riley Martin <riley.willow@gmail.com>
2021-03-23 12:25:23 -0600

    Add to test suite to test for more possible outcomes

---
+++ test/Deck-test.js
@@ -0,0 +1,18 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const Deck = require("../src/Deck");
+const Turn = require("../src/Turn");
+const Card = require("../src/Card");
+
+// describe("Deck", () => {
+//   it("should be initialized with an array of Card objects" () => {
+//
+//   });
+//   it("should have an accompanying test file", () => {
+//
+//   });
+//   it("should know how many Cards are in the Deck", () => {
+//
+//   });
+// });
