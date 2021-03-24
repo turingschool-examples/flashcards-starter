6844f0b6f51150ed89e2f69504d278448747b3f1
bhohnco <bryanhohn13@gmail.com>
2021-03-24 07:35:22 -0600

    Taketurn, percent, and endround methods add

--- src/Round.js
+++ src/Round.js
@@ -9,6 +9,25 @@ class Round {
   returnCurrentCard() {
     return this.deck[this.turns];
   }
+
+  takeTurn(guess) {
+    const turn = new Turn(guess, this.returnCurrentCard());
+    this.turns++;
+    if (turn.evaluateGuess() === false) {
+      this.incorrectGuesses.push(turn.card.id)
+    }
+    return turn.giveFeedback();
+  }
+
+  calculatePercentCorrect() {
+    let percentage = (this.turns - this.incorrectGuesses.length) / (this.turns) * 100;
+    return Math.floor(percentage);
+  }
+
+  endRound() {
+    if (this.calculatePercentCorrect() = )
+      console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()} % of the questions correctly!`)
+  }
 }
 
 module.exports = Round;
