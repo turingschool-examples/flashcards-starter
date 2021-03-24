2f8b61a539883099824d29d10a69f89b5f1ae370
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-03-24 16:35:29 -0600

    Add Round tests and applicable methods

--- src/Round.js
+++ src/Round.js
@@ -14,20 +14,17 @@ class Round {
     const currentTurn = new Turn(userGuess, this.currentCard);
     this.turns++;
     if (!currentTurn.evaluateGuess()) {
-      this.incorrectGuesses.push({id:this.turns, question: this.currentCard.question, guess: userGuess});
+      this.incorrectGuesses.push(this.currentCard.id);
     }
     this.currentCard = this.deck.cards[this.turns];
     return `Your value of ${currentTurn.userGuess} is ${currentTurn.giveFeedback()}`
-
-// Guess is evaluated/recorded. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses
-// Feedback is returned regarding whether the guess is incorrect or correct
   }
   calculatePercentCorrect() {
-// calculates and returns the percentage of correct guesses
+    const score = Math.round((1-(this.incorrectGuesses.length/this.turns)) * 100);
+    return score;
   }
   endRound() {
-// prints the following to the console:
-// ‘** Round over! ** You answered <>% of the questions correctly!’
+    return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -8,7 +8,7 @@ const Round = require("../src/Round");
 
 
 describe("Round", () => {
-  let card1, card2, card3, deck, round, turn1, turn2, turn3;
+  let card1, card2, card3, deck, round;
   beforeEach(() => {
     card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
     card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
@@ -17,43 +17,58 @@ describe("Round", () => {
     round = new Round(deck, card1);
   });
 
-  it("Should be a function", () => {
-    expect(Round).to.be.a("function");
-  });
+  it("should be a function", () => expect(Round).to.be.a("function"));
 
-  it("Should store a deck object", () => {
-    expect(round.deck).to.be.a("object");
-  });
+  it("should store a deck object", () => expect(round.deck).to.be.a("object"));
 
-  it("Should store the current card in play", () => {
-    expect(round.returnCurrentCard()).to.equal(card1);
-  });
+  it("should store the current card in play by default", () => expect(round.returnCurrentCard()).to.equal(card1));
 
-  it("Should default to 0 turns", () => expect(round.turns).to.equal(0));
-
-  it("Should take turns", () => {
-    const turn1 = round.takeTurn("object");
-    expect(round.turns).to.equal(1);
-  });
-
-  it("Should update the current card when a turn is made", () => {
+  it("should update the current card when a turn is made", () => {
     const turn1 = round.takeTurn("object");
     expect(round.returnCurrentCard()).to.equal(card2);
   });
 
-  it("Should evaluate the guess", () => {
+  it("should default to 0 turns", () => expect(round.turns).to.equal(0));
+
+  it("should increase turn count when turns are taken", () => {
     const turn1 = round.takeTurn("object");
-    const turn2 = round.takeTurn("function");
+    expect(round.turns).to.equal(1);
+  });
+
+  it("should evaluate the guess", () => {
+    const turn1 = round.takeTurn("object"); //correct
+    const turn2 = round.takeTurn("function"); //incorrect
     expect(turn1).to.equal("Your value of object is correct!");
     expect(turn2).to.equal("Your value of function is incorrect!");
   });
 
-  it("Should store incorrect guesses", () => {
-    const turn1 = round.takeTurn("function");
-    const turn2 = round.takeTurn("function");
-    const turn3 = round.takeTurn("false");
+  it("should store incorrect guesses", () => {
+    const turn1 = round.takeTurn("object"); //correct
+    const turn2 = round.takeTurn("function"); //incorrect
+    const turn3 = round.takeTurn("true"); //incorrect
     expect(round.incorrectGuesses).to.have.lengthOf(2);
-    expect(round.incorrectGuesses[0]).to.be.a("object");
-    expect(round.incorrectGuesses[1].id).to.equal(2);
+    expect(round.incorrectGuesses[0]).to.be.a("number");
+    expect(round.incorrectGuesses[1]).to.equal(3);
+  });
+
+  it("should calculate and return the percentage of correct guesses", () => {
+    const turn1 = round.takeTurn("object"); //correct
+    const turn2 = round.takeTurn("function"); //incorrect
+    const turn3 = round.takeTurn("true"); //incorrect
+    expect(round.calculatePercentCorrect()).to.equal(33);
+  });
+
+  it("should calculate and return the percentage of correct guesses", () => {
+    const turn1 = round.takeTurn("object"); //correct
+    const turn2 = round.takeTurn("function"); //incorrect
+    const turn3 = round.takeTurn("true"); //incorrect
+    expect(round.calculatePercentCorrect()).to.equal(33);
+  });
+
+  it("should end the round and display the score", () => {
+    const turn1 = round.takeTurn("object"); //correct
+    const turn2 = round.takeTurn("function"); //incorrect
+    const turn3 = round.takeTurn("false"); //correct
+    expect(round.endRound()).to.equal("** Round over! ** You answered 67% of the questions correctly!");
   });
 })
