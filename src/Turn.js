b4068fde165a1b288b4c6d429a971698d2fdeb33
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-03-25 18:56:24 -0400

    Create turn tests and class, pass tests

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,31 @@
 class Turn {
-  constructor() {
+  constructor(userGuess, card) {
+    this.guess = userGuess;
+    this.card = card;
+  }
 
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.card;
+  }
+
+  evaluateGuess() {
+    if(this.guess === this.card.correctAnswer) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  giveFeedback() {
+    if(this.evaluateGuess() === true) {
+      return "Correct!"
+    } else {
+      return "Incorrect!"
+    }
   }
 }
 
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,43 +1,43 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('../src/Turn');
 const Card = require('../src/Card');
+const Turn = require('../src/Turn');
 
 describe('Turn', function() {
   let turn, card;
 
-  beforeEach( function() {
-    turn = new Turn('sea otter', card);
+  beforeEach(function() {
     card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    turn = new Turn('sea otter', card);
   });
 
-  it.skip('should be a function', function() {
-    expect(turn).to.be.a('function');
+  it('should be a function', function() {
+    expect(Turn).to.be.a('function');
   });
 
-  it.skip('should instantiate a turn', function() {
-    expect(turn).to.be.an / instanceOf(Turn);
+  it('should instantiate a turn', function() {
+    expect(turn).to.be.an.instanceOf(Turn);
   });
 
-  it.skip('should return user\'s guess', function() {
-    expect(turn.returnGuess()).to.be.a('function');
+  it('should return user\'s guess', function() {
+    expect(turn.returnGuess).to.be.a('function');
     expect(turn.guess).to.equal('sea otter');
   });
 
-  it.skip('should return card', function() {
-    expect(turn.returnCard()).to.be.a('function');
+  it('should return card', function() {
+    expect(turn.returnCard).to.be.a('function');
     expect(turn.card).to.equal(card);
   });
 
-  it.skip('should evaluate user\'s guess', function() {
+  it('should evaluate user\'s guess', function() {
     expect(turn.evaluateGuess).to.be.a('function');
-    expect(turn.evaluateGuess).to.equal(true);
+    expect(turn.evaluateGuess()).to.equal(true);
   });
 
-  it.skip('should give feedback, either correct or incorrect', function() {
+  it('should give feedback, either correct or incorrect', function() {
     expect(turn.giveFeedback).to.be.a('function');
-    expect(turn.giveFeedback).to.equal('Correct!');
+    expect(turn.giveFeedback()).to.equal('Correct!');
   });
-  
+
 });
