fa9ce292a5015dd7df6e5a0617b01c0b43ac9ba1
EllieAzaveda <elliemiller66@icloud.com>
2021-03-24 21:34:28 -0600

    Add most methods for Round class

--- src/Round.js
+++ src/Round.js
@@ -1,8 +1,39 @@
+const Turn = require('../src/Turn');
+
 class Round {
   constructor(deck, currentCard) {
     this.deck = deck;
     this.currentCard = currentCard;
+    this.turns = 0;
+    this.incorrectGuesses = [];
   }
+
+  returnCurrentCard() {
+    this.currentCard = this.deck.cards[this.turns];
+    return this.currentCard;
+  }
+
+  takeTurn(guess) {
+    let turn = new Turn(this.guess, this.currentCard);
+    this.turns++;
+
+    if(!turn.evaluateGuess()) {
+      this.incorrectGuesses.push(this.currentCard.id);
+      return turn.giveFeedback();
+    } else {
+      return turn.giveFeedback();
+    }
+  }
+
+  calculatePercentCorrect() {
+    let percentCorrect = 100 - (this.incorrectGuesses.length/this.turns * 100)
+    return percentCorrect;
+  }
+
+  // endRound() {
+  //   return `** Round over! ** You answered <>% of the questions correctly!`
+  // }
+
 }
 
 module.exports = Round;
--- test/round-test.js
+++ test/round-test.js
@@ -15,12 +15,8 @@ describe('Round', function() {
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
 
-    deck = new Deck();
+    deck = new Deck([card1, card2, card3]);
     round = new Round(deck, card1);
-
-    deck.addCard(card1);
-    deck.addCard(card2);
-    deck.addCard(card3);
   });
 
   it('should be a function', () => {
@@ -33,6 +29,56 @@ describe('Round', function() {
 
   it('should have a current card', () => {
     expect(round.currentCard).to.be.a('object');
+  });
+
+  it('should return current card', () => {
+    expect(round.returnCurrentCard()).to.equal(round.currentCard);
+  });
+
+// Pretty sure this is wrong
+  it('should instantiate a new turn', () => {
+    expect(new Turn()).to.be.an.instanceof(Turn);
+  });
+
+  it('should increase turn count each time it\'s invoked', () => {
+    expect(round.turns).to.equal(0);
+
+    round.takeTurn('bears');
+    round.takeTurn('beets');
+    round.takeTurn('Battlestar Galactica');
+
+    expect(round.turns).to.equal(3);
+  });
+
+// Fix this when you can focus!
+  it('should push card id into incorrectGuesses array when guess is incorrect', () => {
+    card = new Card(1, 'What is Ellie\'s favorite food?', ['sushi', 'beans', 'tomato pie'], 'sushi');
+    turn = new Turn('sushi', card);
+    deck = new Deck(card);
+    round = new Round(deck, card);
+
+    round.takeTurn('sushi');
+
+    console.log(round.incorrectGuesses);
+    expect(round.incorrectGuesses).to.deep.equal([1]);
+  });
+
+  it.skip('should tell user if guess is incorrect', () => {
+
+  });
+
+  it.skip('should tell user if guess is correct', () => {
+
+  });
+
+  it.skip('should be able to calculate percentage of correct answers', () => {
+    round.turns = 4;
+    round.incorrectGuesses = 1;
+
+    round.calculatePercentCorrect();
+    console.log(round.calculatePercentCorrect());
+
+    expect(round.calculatePercentCorrect()).to.equal(75);
   });
 
 })
