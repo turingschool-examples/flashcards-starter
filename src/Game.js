4785f4915104b08167ef943197963e12f170c374
mcraig2342 <matthewcookecraig10@gmail.com>
2021-03-25 14:17:58 -0600

    Create start game method in the game class

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,15 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('../src/Round');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = this.start();
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +18,20 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start() {
+    let cards = [];
+    for (let i = 0; i < prototypeQuestions.length; i++) {
+      let { id, question, answers, correctAnswer } = prototypeQuestions[i];
+      const card = new Card(id, question, answers, correctAnswer);
+      cards.push(card);
+    }
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    this.printMessage(deck, round);
+    this.printQuestion(round);
+    return round;
   }
 }
 
---
+++ test/Game-test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Round = require('../src/Round');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Game = require('../src/Game');
+
+describe('Game', function() {
+
+   it('should create cards from the data', function() {
+
+
+   });
+});
