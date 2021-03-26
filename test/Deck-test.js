30dfddda337bfaa763949ce4ad0d162b543a07b2
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-03-25 23:02:50 -0400

    Add most tests for Round class

---
+++ test/Deck-test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+
+describe('Deck', () => {
+  let deck, card1, card2, card3;
+
+  beforeEach(function() {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck = new Deck([card1, card2, card3]);
+  });
+
+  it('should be a function', function() {
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', function() {
+    expect(deck).to.be.an.instanceOf(Deck);
+  });
+
+  it('should be able to count the amount of cards in a deck', function() {
+    expect(deck.countCards).to.be.a('function');
+    expect(deck.countCards()).to.equal(3);
+  });
+});
