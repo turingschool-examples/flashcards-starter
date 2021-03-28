7715b7dd7895da15135ef537ab6aa9e61b9c5e19
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-03-27 20:01:20 -0400

    Remove comments

--- src/Game.js
+++ src/Game.js
@@ -11,24 +11,15 @@ class Game {
   }
 
   start() {
-    // Creates Cards
     const cards = prototypeQuestions.map(card => new Card(
       card.id,
       card.question,
       card.answers,
       card.correctAnswer
     ));
-
-    // Puts Cards in a Deck
     const deck = new Deck(cards);
-
-      // Creates a new Round using the Deck
     this.currentRound = new Round(deck);
-
-      // invokes printMessage to display the message in the CLI
     this.printMessage(deck, this.currentRound);
-
-    // invokes printQuestion to kick off our helper functions that allow interaction via the CLI
     this.printQuestion(this.currentRound);
   }
 
