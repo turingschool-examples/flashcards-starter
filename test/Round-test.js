df6de2e10c5050e829a77a51c8c28755dfd6c228
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-10 17:56:39 -0600

    Fix 1st round test

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,9 +1,10 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('../src/Turn');
+// const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 const Round = require('../src/Round');
+const Deck = require('../src/Deck');
 
 // Your Round class will be the object that takes in responses and records these guesses (as well as if they are correct or incorrect). The currentCard should be the first Card in the Deck (the array of Cards) at the start of the Round
 
@@ -13,15 +14,25 @@ const Round = require('../src/Round');
 
 describe('Round', function() {
 
-  it('should return current card being played', function() {
+  it.skip('should return current card being played', function() {
     
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
 
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
 
     const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+
+    expect(round.returnCurrentCard()).to.equal({
+      id: 1,
+      question: 'What is Robbie\'s favorite animal',
+      answers: ['sea otter', 'pug', 'capybara'],
+      correctAnswer: 'sea otter'
+    });
   });
-})
 
 //////
 
@@ -31,7 +42,7 @@ describe('Round', function() {
 
   // The turns count is updated, regardless of whether the guess is correct or incorrect
 
-    // The next card becomes current card
+  // The next card becomes current card
 
   // Guess is evaluated/recorded. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses
 
@@ -42,3 +53,5 @@ describe('Round', function() {
 // calculatePercentCorrect: method that calculates and returns the percentage of correct guesses
 
 // endRound: method that prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!’
+
+})
