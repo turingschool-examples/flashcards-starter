de181f0f0965383a7cf93dc96b543f5d7c86efac
Alex Kio <alexandermartelkio@gmail.com>
2021-05-10 19:04:46 -0400

    Create instanceOf tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,9 +1,18 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('../src/Card');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', () => {
+  it('should be a function', () => {
+    const turn = new Turn();
+    expect(Card).to.be.a('function');
+  });
 
+  it('should be an instance of Card', () => {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  }); 
 
 });
