cb7f27f9b914f51f6b0ac8bd2c70505ad945ab9e
Dean Cook <deancook371@gmail.com>
2021-05-10 15:41:12 -0600

    Add tests checking if function and if instance of Turn

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -3,3 +3,16 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+  it.skip('should be a function', function() {
+    const turn = new Turn();
+    expect(Card).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+});
