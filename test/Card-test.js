ac4202d17d262a44b173c30ad97f4e384beb7308
Alex Kio <alexandermartelkio@gmail.com>
2021-05-10 17:56:27 -0400

    Unskip tests and add sad passes

--- test/Card-test.js
+++ test/Card-test.js
@@ -10,23 +10,38 @@ describe('Card', () => {
     expect(Card).to.be.a('function');
   });
 
-  it.skip('should be an instance of Card', () => {
+  it('should be an instance of Card', () => {
     const card = new Card();
     expect(card).to.be.an.instanceof(Card);
   }); 
 
-  it.skip('should store a question', () => {
+  it('should store an id', () => {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    expect(card.id).to.equal(1);
+  });  
+
+  it('should store a question', () => {
     const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     expect(card.question).to.equal('What is Robbie\'s favorite animal');
   });  
 
-  it.skip('should store a list of possible answers', () => {
+  it('should store the correct question', () => {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    expect(card.question).to.not.equal('What is Robbie\'s favorite color');
+  }); 
+
+  it('should store a list of possible answers', () => {
     const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     expect(card.answers).to.deep.equal(['sea otter', 'pug', 'capybara']);
   });  
 
-  it.skip('should store the correct answer', () => {
+  it('should store the correct answer', () => {
     const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     expect(card.correctAnswer).to.equal('sea otter');
+  });
+
+  it('should not store an incorrect answer', () => {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    expect(card.correctAnswer).to.not.equal('Jupiter');
   });
 });
