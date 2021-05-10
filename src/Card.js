48c99fab8138ac2bd06aee5f33f35d5cbe79e131
Alex Kio <alexandermartelkio@gmail.com>
2021-05-10 17:54:00 -0400

    Make Card have an id, a question, possible answers, and a correct answer

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,12 @@ const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Card {
-  constructor() {}
-
+  constructor(id, question, possibleAnswers, answer) {
+  this.id = id;
+  this.question = question;
+  this.answers = possibleAnswers;
+  this.correctAnswer = answer;
+  }
 }
 
 module.exports = Card;
