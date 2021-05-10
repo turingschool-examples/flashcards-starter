c211dfa6d5c5f0c066ff9152671112ec143cf010
Sarah Fitzsimons <sarahfitz2713@gmail.com>
2021-05-10 16:34:06 -0600

    Add code to pass tests in Card class, fix test variable name

--- test/Card-test.js
+++ test/Card-test.js
@@ -6,28 +6,33 @@ const Card = require('../src/Card');
 let defaultCard;
 
 describe('Card', function() {
+
   beforeEach(() => {
     defaultCard = new Card(1, 'What is Robbie\'s favorite animal', 
       ['sea otter', 'pug', 'capybara'], 'sea otter');
   });
 
-  it.only('should be a function', function() {
+  it('should be a function', function() {
     expect(Card).to.be.a('function');
   });
 
   it('should be an instance of Card', function() {
-    expect(card).to.be.an.instanceof(Card);
+    expect(defaultCard).to.be.an.instanceof(Card);
   }); 
 
+  it('should have an id', function() {
+    expect(defaultCard.id).to.equal(1);
+  });
+
   it('should store a question', function() {
-    expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
+    expect(defaultCard.question).to.equal('What is Robbie\'s favorite animal');
   });  
 
   it('should store a list of possible answers', function() {
-    expect(card.answers).to.deep.equal(['object', 'array', 'function']);
+    expect(defaultCard.answers).to.deep.equal(['sea otter', 'pug', 'capybara']);
   });  
 
   it('should store the correct answer', function() {
-    expect(card.correctAnswer).to.equal('object');
+    expect(defaultCard.correctAnswer).to.equal('sea otter');
   });
 });
