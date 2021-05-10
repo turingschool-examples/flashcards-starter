a6ed891c737906bdd771ffbd94f1a03e72e3d504
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-10 15:55:04 -0600

    Make turn singular

---
+++ test/Turn-test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Turn');
+
+describe('Turns', function() {
+
+it('should be a function', function() {
+    const turns = new Turns();
+    expect(Card).to.be.a('function');
+  });
+
+
+
+
+
+
+
+}
