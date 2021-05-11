ec027e948e91c5d54fabb8b910036a5b84eef1f6
Lauren Kessell <lkessell1@gmail.com>
2021-05-11 10:20:35 -0400

    Add tests for Turn arguments

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -14,4 +14,14 @@ describe('Turn', function() {
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
+
+  it('should take the user\'s guess as a string as an argument', function() {
+    const turn = new Turn('object');
+    expect(turn.guess).to.be.a('string');
+  });
+
+  it('should take the current card object as an argument', function() {
+    const turn = new Turn('object', card);
+    expect(card).to.be.an.instanceof(Card);
+  });
 });
