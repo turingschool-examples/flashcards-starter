28d78f6871bd471b0ad54adf64e3f3a92ba8b319
Alex Kio <alexandermartelkio@gmail.com>
2021-05-10 20:41:31 -0400

    Create countCards test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -23,9 +23,11 @@ describe('Deck', () => {
     expect(deck.stack).to.deep.equal([card1, card2, card3]);
   }); 
 
-  
-  // deck.countCards(); // => 3
-
-  // Your Deck class should be initialized with an array of Card objects and should have an accompanying test file.
-  // It should know how many Cards are in the Deck.
+  it('countCards should return the number of cards in the deck', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.countCards()).to.equal(3);
+  }); 
 });
