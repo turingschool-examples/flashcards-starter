15d6897a1a74c580ecefbe5c7d4827d194cb2554
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-11 12:58:19 -0600

    Add skeleton for all tests

--- src/Round.js
+++ src/Round.js
@@ -2,26 +2,37 @@ class Round {
   constructor(deck) {
     this.deck = deck;
     this.currentCard = deck.cards[0];
-    // this.turns = 0;
+    this.turns = 0;
+    this.incorrectGuesses = [];
   }
 
   returnCurrentCard() {
     return this.currentCard;
   }
 
-  takeTurn() {
+  takeTurn(guess) {
   // When a guess is made, a new Turn instance is created.
-  
-    
+
+      // let turn = new Turn(guess, card);
 
   // The turns count is updated, regardless of whether the guess is correct or incorrect
 
+      this.turns++;
+
   // The next card becomes current card
+
+      // this.currentCard = deck.cards[++];
 
   // Guess is evaluated/recorded. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses
 
   // Feedback is returned regarding whether the guess is incorrect or correct
-  }
+
+    //if (guess === this.deck.cards.correctAnswer) {
+    //   return `correct!`;
+    // } else {
+    //   return `incorrect!`;
+    // }
+}
 
   calculatePercentCorrect() {
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -5,6 +5,7 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Round = require('../src/Round');
 const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
 
 // Your Round class will be the object that takes in responses and records these guesses (as well as if they are correct or incorrect). The currentCard should be the first Card in the Deck (the array of Cards) at the start of the Round
 
@@ -54,20 +55,107 @@ describe('Round', function() {
 
 // takeTurn: method that updates turns count, evaluates guesses, gives feedback, and stores ids of incorrect guesses
 
+  it('should start at 0 turns', function() {
+    
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+
+    expect(round.turns).to.equal(0);
+  });
+
+  ///
+
+  it('should start with no incorrect guesses', function() {
+    
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+
+    expect(round.incorrectGuesses).to.deep.equal([]);
+  });
+
   // When a guess is made, a new Turn instance is created.
+
+  it.skip('should create Turn instance if guess made', function() {
+
+  });
 
   // The turns count is updated, regardless of whether the guess is correct or incorrect
 
+  it('should start with no incorrect guesses', function() {
+    
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+
+    const turn = new Turn("guess", "card")
+
+    round.takeTurn('sea otter');
+    round.takeTurn('spleen');
+
+    expect(round.turns).to.deep.equal(2);
+  });
+
   // The next card becomes current card
+
+  it.skip('should make next card become current card', function() {
+
+  });
 
   // Guess is evaluated/recorded. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses
 
+  it.skip('should store incorrect guesses', function() {
+
+  });
+
   // Feedback is returned regarding whether the guess is incorrect or correct
+
+  it.skip('should provide feedback whether correct/incorrect', function() {
+    
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+
+    expect(round.takeTurn('sea otter')).to.deep.equal(false);
+  });
 
 ////////
 
 // calculatePercentCorrect: method that calculates and returns the percentage of correct guesses
 
+  it.skip('should provide percent of correct guesses', function() {
+
+  });
+
 // endRound: method that prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!’
+
+  it.skip('should notify when round over', function() {
+
+  });
 
 })
