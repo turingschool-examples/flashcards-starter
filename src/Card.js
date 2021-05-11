aaa053dd6969af7d7b9cdc30c265c2f2a066b047
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-10 18:52:54 -0600

    Add id property to Card class

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,6 @@
 class Card {
   constructor(cardId, inquiry, possibleAnswers, answer) {
+    this.id = cardId;
     this.question = inquiry;
     this.answers = possibleAnswers;
     this.correctAnswer = answer;
