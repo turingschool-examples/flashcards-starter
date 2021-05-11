94b273995ebdf184939fecdc3471705f37fea55f
Alex Kio <alexandermartelkio@gmail.com>
2021-05-11 10:28:11 -0400

    Add parameter to constructor that takes in Round object and assigns it to key this.currentRound

--- src/Game.js
+++ src/Game.js
@@ -3,13 +3,13 @@ const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
-
+  constructor(thisRound) {
+    this.currentRound = thisRound;
+  }
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
-
   printQuestion(round) {
       util.main(round);
   }
