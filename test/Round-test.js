7c992337fe5bae4884f48ebfcd9d290eae883ba7
Alex Kio <alexandermartelkio@gmail.com>
2021-05-10 21:24:07 -0400

    Create test to check storage of instance of Deck

--- test/Round-test.js
+++ test/Round-test.js
@@ -16,22 +16,19 @@ describe('Round', () => {
     expect(round).to.be.an.instanceof(Round);
   }); 
 
+  it('should store current deck', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.deck).to.deep.equal([card1, card2, card3]);
+  }); 
 
 
 
 
 
-
-
-//   const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-// const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-// const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-
-// const deck = new Deck([card1, card2, card3]);
-
-// const round = new Round(deck);
-
-// round.deck;   // => [card1, card2, card3];
 
 // round.returnCurrentCard(); // => { id: 1,
 //                            //      question: 'What is Robbie\'s favorite animal',
