098470c146026aed3f7dea4b2aa52fc5a40ac39b
Alex Kio <alexandermartelkio@gmail.com>
2021-05-10 20:43:33 -0400

    Create countCards sad test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -30,4 +30,13 @@ describe('Deck', () => {
     const deck = new Deck([card1, card2, card3]);
     expect(deck.countCards()).to.equal(3);
   }); 
+
+  it('countCards should return the correct number of cards in the deck', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.countCards()).to.deep.equal(3);
+    expect(deck.countCards()).to.not.equal(4);
+  }); 
 });
