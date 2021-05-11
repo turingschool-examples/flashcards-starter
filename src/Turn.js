06b2523875aa7d768bf6193e5a225c7196603ed6
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-11 13:51:04 -0600

    Update skeleton methods that pass test

--- src/Turn.js
+++ src/Turn.js
@@ -3,6 +3,19 @@ class Turn {
     this.guess = guess
     this.currentCard = currentCard
   }
+  returnGuess() {
+    return this.guess
+  }
+  returnCard() {
+    return this.currentCard
+  }
+  evaluateGuess() {
+    return this.guess === this.currentCard.correctAnswer ? true : false
+    
+  }
+  giveFeedBack() {
+    return this.evaluateGuess() === true ? 'correct!' : 'incorrect!'
+  }
 }
 
 module.exports = Turn
