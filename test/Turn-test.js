2ea79d1ccaeea8394784404dad2ef1f76e429752
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-10 19:22:13 -0600

    Add test to check if current card is stored and an instance of the Card class

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
 
@@ -18,5 +19,13 @@ describe('Turn', function() {
   it('should store a user\'s guess', function() {
     const turn = new Turn('bracket notation');
     expect(turn.userGuess).to.equal('bracket notation');
+  })
+
+  it('should store the current card in play', function() {
+    const card = new Card((5, 'Who teaches mod 2?', ['Heather', 'Kayla', 'Hannah', 'Nik'], 'Nik'));
+    const turn = new Turn('Heather', card);
+    expect(card).to.be.an.instanceof(Card);
+    //Does this need to deep equal or regular equal?
+    expect(turn.currentCard).to.deep.equal(card);
   })
 })
