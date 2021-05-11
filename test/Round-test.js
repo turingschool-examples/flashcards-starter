9a501aaa3818e53eb0e8662eafe98e41b5ddd5f4
Alex Kio <alexandermartelkio@gmail.com>
2021-05-11 08:13:05 -0400

    Create calculatePercentCorrect test

--- test/Round-test.js
+++ test/Round-test.js
@@ -63,8 +63,21 @@ describe('Round', () => {
     expect(round.incorrectGuesses).to.deep.equal([card2.id]);
   }); 
 
+  it('calculatePercentCorrect calculates and returns the percentage of correct guesses', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
 
+    expect(round.calculatePercentCorrect()).to.deep.equal(`You haven't made a guess yet!`);
+    
+    expect(round.takeTurn('sea otter')).to.equal('correct!');
+    expect(round.calculatePercentCorrect()).to.deep.equal(100);
 
+    expect(round.takeTurn('spleen')).to.equal('incorrect!');
+    expect(round.calculatePercentCorrect()).to.deep.equal(50);
+  }); 
 
 
 // round.calculatePercentCorrect(); // => 50
