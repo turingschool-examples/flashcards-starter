bc9f18a92c720169a509e8fd90e369d9089a66ab
Alex Kio <alexandermartelkio@gmail.com>
2021-05-10 20:29:54 -0400

    Create test to check that class can store an array of objects

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Deck = require('../src/Deck');
+const Card = require('../src/Card');
 
 describe('Deck', () => {
   it('should be a function', () => {
@@ -14,17 +15,14 @@ describe('Deck', () => {
     expect(deck).to.be.an.instanceof(Deck);
   }); 
 
+  it('should be initialized with an array of Card objects', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.stack).to.equal([card1, card2, card3]);
+  }); 
 
-
-
-
-
-
-  // const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-  // const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-  // const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-  
-  // const deck = new Deck([card1, card2, card3]);
   
   // deck.countCards(); // => 3
 
