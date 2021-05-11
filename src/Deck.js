de92e2a6601ae8f2df10e9595799a52e79d67b17
Alex Kio <alexandermartelkio@gmail.com>
2021-05-10 20:38:20 -0400

    Make class store an array of objects

--- src/Deck.js
+++ src/Deck.js
@@ -3,8 +3,9 @@ const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Deck {
-  constructor() {}
-
+  constructor(cards) {
+    this.stack = cards;
+  }
 }
 
 module.exports = Deck;
