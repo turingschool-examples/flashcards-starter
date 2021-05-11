c59f189f1ad8dd741e3553da2fc988a757dceb11
Lauren Kessell <lkessell1@gmail.com>
2021-05-11 14:55:48 -0400

    Adjust card argument test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,25 +6,25 @@ const Card = require('../src/Card');
 
 describe('Turn', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const turn = new Turn();
-    expect(turn).to.be.a('function');
+    expect(Turn).to.be.a('function');
   });
 
-  it.skip('should be an instance of Turn', function() {
+  it('should be an instance of Turn', function() {
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-  it.skip('should take the user\'s guess as a string as an argument', function() {
+  it('should take the user\'s guess as a string as an argument', function() {
     const turn = new Turn('object');
     expect(turn.guess).to.be.a('string');
   });
 
-  it.skip('should take the current card object as an argument', function() {
+  it('should take the current card object as an argument', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
-    expect(card).to.be.an.instanceof(Card);
+    expect(turn.card).to.be.an.instanceof(Card);
   });
 
   it.skip('should return the guess', function() {
