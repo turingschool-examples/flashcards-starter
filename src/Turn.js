afc4eb8f141619ccf9eb9b45279eeca5995f36ef
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-11 08:34:45 -0600

    Add giveFeedback to Turn class

--- src/Turn.js
+++ src/Turn.js
@@ -25,7 +25,13 @@ class Turn {
     }
   }
 
-
+  giveFeedback() {
+    if (this.evaluateGuess()) {
+      return 'correct!'
+    } else {
+      return 'incorrect!'
+    }
+  }
 }
 
 module.exports = Turn
