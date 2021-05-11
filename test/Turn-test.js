ebe7193bf942aa74d2f17ec72e1c089076195378
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-10 19:08:47 -0600

    Add function and instantiation tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,5 +5,13 @@ const Turn = require('../src/Turn');
 
 describe('Turn', function() {
 
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  })
 
+  it('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  })
 })
