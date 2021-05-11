e15d7ca9691550b59621762b14cde2cb5ee42742
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-10 18:36:59 -0600

    Add test setup

---
+++ test/Turn-test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn')
+
+describe('Turn', function() {
+  it('should be a function', function() {
+    const turn = new Turn()
+    expect(Turn).to.be.a('function');
+  })
+})
