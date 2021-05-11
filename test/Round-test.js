aabb5c2354f5aaae96a1e7dde3d31c14f88cb418
Alex Kio <alexandermartelkio@gmail.com>
2021-05-10 21:40:14 -0400

    Create test to check that the first card in the deck is the one that's expected

--- test/Round-test.js
+++ test/Round-test.js
@@ -25,7 +25,32 @@ describe('Round', () => {
     expect(round.deck).to.deep.equal(deck);
   }); 
 
+  it('currentCard should be the first Card in the Deck (the array of Cards) at the start of the Round', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.deck.stack[0]).to.deep.equal(card1);
+  });   
 
+  // it.skip('returnCurrentCard returns the current card being played', () => {
+  //   const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+  //   const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+  //   const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+  //   const deck = new Deck([card1, card2, card3]);
+  //   const round = new Round(deck);
+
+  // }); 
+
+  // it.skip('takeTurn: method that updates turns count, evaluates guesses, gives feedback, and stores ids of incorrect guesses', () => {
+  //   const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+  //   const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+  //   const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+  //   const deck = new Deck([card1, card2, card3]);
+  //   const round = new Round(deck);
+
+  // }); 
 
 
 
