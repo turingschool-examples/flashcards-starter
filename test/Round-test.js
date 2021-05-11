52f23a17f609af66913d875544785ac4843297cb
Alex Kio <alexandermartelkio@gmail.com>
2021-05-11 07:09:46 -0400

    Create returnCurrentCard test

--- test/Round-test.js
+++ test/Round-test.js
@@ -33,14 +33,14 @@ describe('Round', () => {
     expect(round.currentCard).to.deep.equal(card1);
   });
 
-  // it('returnCurrentCard returns the current card being played', () => {
-  //   const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-  //   const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-  //   const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-  //   const deck = new Deck([card1, card2, card3]);
-  //   const round = new Round(deck);
-
-  // }); 
+  it('returnCurrentCard returns the current card being played', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.returnCurrentCard).to.deep.equal(card1);
+  }); 
 
   // it.skip('takeTurn: method that updates turns count, evaluates guesses, gives feedback, and stores ids of incorrect guesses', () => {
   //   const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
