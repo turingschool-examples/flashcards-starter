b8e0a852c030c29977458d0d93fc8fdbd59e6354
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-10 19:30:52 -0600

    Add test to check returnGuess method on Turn class

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -22,10 +22,19 @@ describe('Turn', function() {
   })
 
   it('should store the current card in play', function() {
-    const card = new Card((5, 'Who teaches mod 2?', ['Heather', 'Kayla', 'Hannah', 'Nik'], 'Nik'));
+    const card = new Card((5, 'Who teaches mod 2?', ['Heather', 'Jeff', 'Nik'], 'Nik'));
     const turn = new Turn('Heather', card);
     expect(card).to.be.an.instanceof(Card);
     //Does this need to deep equal or regular equal?
     expect(turn.currentCard).to.deep.equal(card);
+  })
+
+  it('should return the user\'s guess', function() {
+    const card = new Card((7, 'Which datatype is wrapped in quotes?', ['string', 'number', 'boolean'], 'string'));
+    const turn = new Turn('boolean', card);
+
+    turn.returnGuess();
+
+    expect(turn.returnGuess()).to.equal('boolean');
   })
 })
