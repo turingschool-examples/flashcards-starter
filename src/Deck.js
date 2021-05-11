8b17e409659564619368ee4cd28579795d905039
Alex Kio <alexandermartelkio@gmail.com>
2021-05-10 20:13:45 -0400

    Initialize deck and deck-test files

---
+++ src/Deck.js
@@ -0,0 +1,10 @@
+const data = require('./data');
+const prototypeQuestions = data.prototypeData;
+const util = require('./util');
+
+class Deck {
+  constructor() {}
+
+}
+
+module.exports = Deck;
---
+++ test/Deck-test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+
+describe('Deck', () => {
+
+
+});
