9378c39732d870921edd7fd6c4bcf96e5588d8a0
Alex Kio <alexandermartelkio@gmail.com>
2021-05-10 20:37:52 -0400

    Update test to deeply equal

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -15,12 +15,12 @@ describe('Deck', () => {
     expect(deck).to.be.an.instanceof(Deck);
   }); 
 
-  it('should be initialized with an array of Card objects', () => {
+  it('should store an array of Card objects', () => {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     const deck = new Deck([card1, card2, card3]);
-    expect(deck.stack).to.equal([card1, card2, card3]);
+    expect(deck.stack).to.deep.equal([card1, card2, card3]);
   }); 
 
   
