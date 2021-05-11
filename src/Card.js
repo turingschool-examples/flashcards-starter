15433cc82ac3f4b23a2f1c6dc1525e03f88cbb4d
Shayan Golafshani <shayangolafsh@gmail.com>
2021-05-11 08:54:28 -0600

    Implement Card and unskip  accompanying test-suite

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+class Card {
+    constructor(id, question, answers, correctAnswer){
+        //properties
+        this.id = id;
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    }
+    //methods
+
+}
+
+module.exports = Card;
