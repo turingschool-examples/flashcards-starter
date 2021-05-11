55719b758e2eab226d45d066768d975899a9640f
Alex Kio <alexandermartelkio@gmail.com>
2021-05-11 07:04:28 -0400

    Create test for this.currentCard

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,11 +7,10 @@ const Card = require('../src/Card');
 
 describe('Round', () => {
   it('should be a function', () => {
-    const round = new Round();
     expect(Round).to.be.a('function');
   });
 
-  it('should be an instance of Round', () => {
+  it.skip('should be an instance of Round', () => {
     const round = new Round();
     expect(round).to.be.an.instanceof(Round);
   }); 
@@ -22,7 +21,7 @@ describe('Round', () => {
     const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
-    expect(round.deck).to.deep.equal(deck);
+    expect(round.deck).to.deep.equal([card1, card2, card3]);
   }); 
 
   it('currentCard should be the first Card in the Deck (the array of Cards) at the start of the Round', () => {
@@ -31,10 +30,10 @@ describe('Round', () => {
     const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
-    expect(round.deck.stack[0]).to.deep.equal(card1);
-  });   
+    expect(round.currentCard).to.deep.equal(card1);
+  });
 
-  // it.skip('returnCurrentCard returns the current card being played', () => {
+  // it('returnCurrentCard returns the current card being played', () => {
   //   const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
   //   const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
   //   const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
