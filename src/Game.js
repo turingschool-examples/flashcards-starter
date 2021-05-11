4a8236f3e757f9c6c2576cce9e80c1ff8b661728
Alex Kio <alexandermartelkio@gmail.com>
2021-05-11 11:00:03 -0400

    Add start method
    Still work to do. I don't like that I can't seem to access deck.countCards() through this(.currentRound).
    
    start method creates cards, puts cards in deck, creates a new Round using the Deck, and invokes printMessage & printQuestion

--- src/Game.js
+++ src/Game.js
@@ -2,9 +2,26 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
 class Game {
   constructor(thisRound) {
     this.currentRound = thisRound;
+  }
+  start() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    const game = new Game(round);
+    // console.log(round)
+    // this.printMessage(deck, this.currentRound)
+    // this.printQuestion(this.currentRound)
+    this.printMessage(deck, round)
+    this.printQuestion(round)
   }
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
