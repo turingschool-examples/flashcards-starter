1a1f8814286e4b542f5c4b0e411dc0de805da7c0
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-11 18:30:29 -0600

    Update propeties and methods up to the 7th test

--- src/Round.js
+++ src/Round.js
@@ -1,8 +1,13 @@
 class Round {
   constructor(deck) {
-    this.deck = deck
+    this.deck = deck.deck
+    this.currentCard; 
+    this.turns = 0
+    this.incorrectGuesses = []
   }
-  
+  returnCurrentCard() {
+    this.currentCard = this.deck[0]    
+  }
 }
 
 module.exports = Round
