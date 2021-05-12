ace9b6f04d5e14bb169afda77ebd09cb52ee1bfb
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-05-12 16:09:18 -0400

    Write start func

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,14 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -13,6 +19,13 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
+
+  start() {
+
+
+  }
 }
 
 module.exports = Game;
+
+//prototypeQuestions = data.prototypeData which is an array..... 
