9c245aff8b08b38a20a5101314b476cb71cb3cb8
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-05-11 22:16:51 -0400

    Fix endRound test

--- test/Round-test.js
+++ test/Round-test.js
@@ -78,30 +78,20 @@ describe('Round', function() {
   });
 
   it('should calculate percentage of correct guesses', function() {
-    round.takeTurn(`sea otter`);
-    expect(round.calculatePercentCorrect()).to.equal(100);
-    round.takeTurn('spleen');
-    expect(round.calculatePercentCorrect()).to.equal(50);
+    round.takeTurn('sea otter');
+    round.takeTurn('gallbladder');
+    round.takeTurn('listening to music')
+    round.calculatePercentCorrect();
+    expect(round.calculatePercentCorrect()).to.equal(67);
   });
 
   it('should end the round and give user percent of correct answers', function() {
-    expect(round.endRound()).to.equal('** Round over! ** You answered % of the questions correctly!')
+    round.takeTurn('sea otter');
+    round.takeTurn('gallbladder');
+    round.takeTurn('listening to music')
+    round.calculatePercentCorrect();
+    expect(round.calculatePercentCorrect()).to.equal(67);
+    expect(round.endRound()).to.equal('** Round over! ** You answered 67% of the questions correctly!')
   });
 
 });
-
-
-
-// Your Round class will be the object that takes in responses and records these guesses (as well as if they are correct or incorrect). The currentCard should be the first Card in the Deck (the array of Cards) at the start of the Round
-// Your Round class should meet the following requirements:
-// returnCurrentCard: method that returns the current card being played
-// takeTurn: method that updates turns count, evaluates guesses, gives feedback, and stores ids of incorrect guesses
-//
-// When a guess is made, a new Turn instance is created.
-// The turns count is updated, regardless of whether the guess is correct or incorrect
-// The next card becomes current card
-// Guess is evaluated/recorded. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses
-// Feedback is returned regarding whether the guess is incorrect or correct
-
-// calculatePercentCorrect: method that calculates and returns the percentage of correct guesses
-// endRound: method that prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!’
