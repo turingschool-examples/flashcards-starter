08e23cdca2cc615fbd26cce5db3249597e90ccd4
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-05-11 20:20:58 -0400

    Write tests for takeTurn method

--- test/Round-test.js
+++ test/Round-test.js
@@ -17,6 +17,75 @@ describe('Round', function() {
     round = new Round(deck);
   });
 
+  it('should be a function', function() {
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', function() {
+    expect(round).to.be.an.instanceOf(Round);
+  });
+
+  it('should take in an argument of deck', function() {
+    expect(round.deck).to.deep.equal([card1, card2, card3]);
+  });
+
+  it('should store a current card', function() {
+    expect(round.currentCard).to.deep.equal(card1);
+    expect(round.currentCard.id).to.equal(1);
+  });
+
+  it('should have turns set to 0 by default', function() {
+    expect(round.turns).to.equal(0);
+  });
+
+  it('should store incorrect guesses', function() {
+    expect(round.incorrectGuesses).to.deep.equal([]);
+  });
+
+  it('should be able to return the current card', function() {
+    expect(round.returnCurrentCard()).to.equal(card1);
+  });
+
+  it('should update turns count', function() {
+    round.takeTurn('sea otter');
+    expect(round.turns).to.equal(1);
+    round.takeTurn('gallbladder');
+    expect(round.turns).to.equal(2);
+  });
+
+  it('should update current card with next card', function() {
+    round.takeTurn('pug');
+    expect(round.returnCurrentCard()).to.deep.equal(card2);
+
+    round.takeTurn('gallbladder');
+    expect(round.returnCurrentCard()).to.deep.equal(card3);
+  });
+
+  it('should store incorrect guesses', function() {
+   round.takeTurn('pug');
+   expect(round.incorrectGuesses).to.deep.equal([round.deck[0].id]);
+
+   round.takeTurn('appendix');
+   expect(round.incorrectGuesses).to.deep.equal([1, 14]);
+  });
+
+  it('should return if the guess is correct', function() {
+    expect(round.takeTurn('capybara')).to.equal('Your answer of capybara is incorrect!');
+  });
+
+  it('should return if the guess is incorrect', function() {
+    expect(round.takeTurn('sea otter')).to.equal('Your answer of sea otter is correct!');
+  });
+
+  it('should calculate percentage of correct guesses', function() {
+
+
+  });
+
+  it('should end the round', function() {
+
+    ('** Round over! ** You answered % of the questions correctly!')
+  });
 
 });
 
@@ -32,5 +101,6 @@ describe('Round', function() {
 // The next card becomes current card
 // Guess is evaluated/recorded. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses
 // Feedback is returned regarding whether the guess is incorrect or correct
+
 // calculatePercentCorrect: method that calculates and returns the percentage of correct guesses
 // endRound: method that prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!’
