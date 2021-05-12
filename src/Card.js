dd8cd5dbbac6f8263a107abe0a3e84813ad62495
Shayan Golafshani <shayangolafsh@gmail.com>
2021-05-11 19:45:56 -0600

    Add beforeEach hooks and refactor

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,10 @@
 class Card {
     constructor(id, question, answers, correctAnswer){
-        //properties
         this.id = id;
         this.question = question;
         this.answers = answers;
         this.correctAnswer = correctAnswer;
     }
-    //methods
-
 }
 
 module.exports = Card;
