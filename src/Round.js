7a7bbf554294fa67a50407951356de00302545e7
Sydney Bear <sbear3915@gmail.com>
2021-05-11 21:05:01 -0600

    Update round test to evaluate guesses adn give feedback

---
+++ src/Round.js
@@ -0,0 +1,25 @@
+const Turn = require('../src/Turn')
+
+class Round {
+  constructor(deck){
+    this.deck = deck
+    this.currentCard = deck.cards[0]
+    this.turns= 0;
+    this.incorrectGuesses = [];
+  }
+  returnCurrentCard() {
+    return this.currentCard
+  }
+  takeTurn(guess) {
+    const currentTurn = new Turn(guess, this.currentCard)
+    this.turns ++
+     if(!currentTurn.evaluateGuess()) {
+       this.incorrectGuesses.push(this.currentCard.id)
+     }
+     this.currentCard = this.deck.cards[this.turns];
+     this.returnCurrentCard()
+     return currentTurn.giveFeedback()
+  }
+}
+
+module.exports = Round;
