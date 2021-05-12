f390cc44485ed0d83efc4929234d529cd767facf
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-11 20:08:12 -0600

    Add turnturn method

--- src/Round.js
+++ src/Round.js
@@ -1,3 +1,5 @@
+const Turn = require("./Turn");
+
 class Round {
   constructor(deck) {
     this.deck = deck.deck
@@ -7,6 +9,11 @@ class Round {
   }
   returnCurrentCard() {
     this.currentCard = this.deck[0]    
+  }
+  takeTurn(guess) {
+    this.turns++
+    let currentTurn = new Turn(guess, this.currentCard)
+    return currentTurn.giveFeedBack()
   }
 }
 
