204df924cc9d4646d8ad3614a7352ae0f1b7057d
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-05-12 17:10:37 -0400

    Invoke game.start to initialize game

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const http = require('http');
 let app = http.createServer();
 
 // Start the server on port 3000
-app.listen(3000, '127.0.0.1');  
-console.log('Node server running on port 3000'); 
+app.listen(3000, '127.0.0.1');
+const Game = require('./src/Game');
+const game = new Game();
+game.start();
