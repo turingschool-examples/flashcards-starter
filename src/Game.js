78294ee0ce0b9dec907f64d4b8e94e813aa517ca
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-11 18:38:09 -0600

    Progress on game.start() tests

--- src/Game.js
+++ src/Game.js
@@ -8,21 +8,51 @@ const Deck = require('../src/Deck');
 const Turn = require('../src/Turn');
 
 class Game {
-  constructor(round) {
-    this.currentRound = round;
+  constructor() {
+    this.currentRound = 0;
+    this.cards = [];
   }
 
   printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
 
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
+  }
+
+  createCard(id, question, answers, correctAnswer) {
+    // console.log({id, question, answers, correctAnswer})
+    const card = new Card(id, question, answers, correctAnswer);
+    // console.log(card);
+    return card;
+
   }
 
   start() {
+    const newCardArray = [];
 
+    const randomIndex = Math.floor(Math.random()*4);
+
+    for (let i = randomIndex; i <= 4; i++) {
+      // console.log(prototypeQuestions[i])
+      const {id, question, answers, correctAnswer} = prototypeQuestions[i];
+      // console.log(id, question, answers, correctAnswer)
+
+      const newCard = this.createCard(prototypeQuestions[i].id, prototypeQuestions[i].question, prototypeQuestions[i].answers, prototypeQuestions[i].correctAnswer);
+      console.log(newCard)
+
+      newCardArray.push(newCard);
+    }
+
+
+    let deck = new Deck(newCardArray);
+
+    let round = new Round(deck);
+
+    this.currentRound++;
+    this.cards = round.deck.cards;
   }
 }
 
--- test/Game-test.js
+++ test/Game-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Card = require('../src/Game');
+const Card = require('../src/Card');
 const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 const Turn = require('../src/Turn');
@@ -25,11 +25,27 @@ describe('Game', function() {
     expect(game.currentRound).to.equal(round);
   });
 
-// create cards
+// creates Cards
 
-  it.skip('should create Cards', function() {
+  it('should create Cards', function() {
 
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const game = new Game();
+
+    game.start();
+
+
+
+    // console.log(game.cards);
+
+    // expect(round.deck).to.equal(card1, card2, card3)
   });
+
 
 // put cards in a Deck
 
