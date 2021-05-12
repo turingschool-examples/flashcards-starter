4365751968cdaa21d6d92acbcdc4cb8a07f9bb3f
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-05-12 16:08:34 -0400

    Fix interpolation on endRound test

--- test/Round-test.js
+++ test/Round-test.js
@@ -90,8 +90,7 @@ describe('Round', function() {
     round.takeTurn('gallbladder');
     round.takeTurn('listening to music')
     round.calculatePercentCorrect();
-    expect(round.calculatePercentCorrect()).to.equal(67);
-    expect(round.endRound()).to.equal('** Round over! ** You answered 67% of the questions correctly!')
+    expect(round.endRound()).to.equal(`** Round over! ** You answered ${round.calculatePercentCorrect()}% of the questions correctly!`)
   });
 
 });
