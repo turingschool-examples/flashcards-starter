2dc719816a602b8839f70754df15433559512007
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-11 18:24:07 -0600

    Add before each for test suite variables

--- test/Round-test.js
+++ test/Round-test.js
@@ -8,16 +8,15 @@ const Card = require('../src/Card')
 
 
 
-describe('Round', function() {
-  it('should be a function', function() {
-    const round = new Round()
-    expect(Round).to.be.a('function')
-  }) 
-  
-  it('should be an instance of Round', function () {
-    const round = new Round()
-    expect(round).to.be.an.instanceOf(Round)
-  }) 
-  
-  
+describe('Round', () => {
+  let card1, card2, card3, deck, round
+  beforeEach(() => {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    
+    deck = new Deck([card1, card2, card3]);
+    
+    round = new Round(deck);
+  })
 })
