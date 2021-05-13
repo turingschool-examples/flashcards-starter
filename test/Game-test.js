218cec93b80c0d31bde08bcc660d9f3cec622fcd
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-12 20:41:53 -0600

    Add instance of round test

--- test/Game-test.js
+++ test/Game-test.js
@@ -11,7 +11,6 @@ const data = require('../src/data');
 describe('Game', function () {
   let card1, card2, card3, deck, round, game
   beforeEach(() => {
-
     card1 = new Card(2, '?', [], '!')
     card2 = new Card(2, '?', [], '!')
     card3 = new Card(3, '?', [], '!')
@@ -41,6 +40,10 @@ describe('Game', function () {
       game.start()
       expect(game.deck).to.be.an.instanceOf(Deck)
       expect(game.deck.countCards()).to.be.equal(30)
+    })
+    it('should be able to create a a new round with deck', function () {
+      game.start()
+      expect(game.round).to.be.an.instanceOf(Round)
     })
   })
 })
