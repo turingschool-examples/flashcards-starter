52a5062e4cf2f7921d54f06133ec7d2cf3e97642
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-13 12:59:18 -0600

    Remove comments

--- src/Game.js
+++ src/Game.js
@@ -7,8 +7,8 @@ const Deck = require('./Deck')
 const Card = require('./Card')
 
 class Game {
-  constructor(round) {
-    this.currentRound = round   
+  constructor() {
+    this.currentRound;
   }
 
   start() {
@@ -16,9 +16,11 @@ class Game {
       return new Card(card.id, card.question, card.answers, card.correctAnswer)
     })
     this.deck = new Deck(this.cards)  
-    this.round = new Round(this.deck)
-    this.printMessage(this.deck, this.round)
-    this.printQuestion(this.round)
+    this.currentRound = new Round(this.deck)
+
+    // this.round = new Round(this.deck)
+    // this.printMessage(this.deck, this.round)
+    // this.printQuestion(this.round)
   }
 
   printMessage(deck, round) {
