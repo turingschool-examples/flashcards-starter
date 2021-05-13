f03e95b3184f5f0ae158a23357fd0bccd7d26f07
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-13 17:45:08 -0600

    Update round have check score method

--- src/Round.js
+++ src/Round.js
@@ -5,7 +5,7 @@ class Round {
     this.deck = deck.cards
     this.turns = 0
     this.incorrectGuesses = []
-    this.currentCard = this.deck[0]
+    this.currentCard = this.deck[this.turns]
   }
   returnCurrentCard() {
     return this.currentCard
@@ -14,8 +14,8 @@ class Round {
   takeTurn(guess) {
     const turn = new Turn(guess, this.currentCard)
     this.turns++
+
     this.currentCard = this.deck[this.turns]
-  
     if (!turn.evaluateGuess()) {
       this.incorrectGuesses.push(this.currentCard.id)
       return turn.giveFeedBack()
@@ -27,8 +27,24 @@ class Round {
     return Math.round((this.turns - this.incorrectGuesses.length) / this.turns * 100)
   }
   endRound() {
+    this.checkScore()
     console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`)
     return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
+  }
+
+  checkScore() {
+    if (this.calculatePercentCorrect() < 90 && this.turn === this.deck.length) {  
+      console.log(`** Start over! ** You only answered ${this.calculatePercentCorrect()}% correctly`)
+      this.turns = 0
+      this.incorrectGuesses = []
+      console.log(this.incorrectGuesses)
+      this.currentCard = this.deck[0]
+    } else {
+      console.log(this.incorrectGuesses.length)
+
+      console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`)
+      return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
+    }
   }
 }
 
