735d12576bdc5fbfcf082c4990a135d6d91970d7
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-13 20:51:59 -0600

    Clean round and spacing

--- src/Round.js
+++ src/Round.js
@@ -1,5 +1,4 @@
 const Turn = require("./Turn");
-
 class Round {
   constructor(deck) {
     this.deck = deck.cards
@@ -7,22 +6,20 @@ class Round {
     this.incorrectGuesses = []
     this.currentCard = this.deck[this.turns]
   }
+
   returnCurrentCard() {
-    if (!this.currentCard) {
-      console.log(`** Start over! ** You only answered ${this.calculatePercentCorrect()}% correctly`)
-      this.turns = 0
-      this.incorrectGuesses = []
-      this.currentCard = this.deck[0]
-      return (`** Start over! ** You only answered ${this.calculatePercentCorrect()}% correctly`)
+    if (!this.currentCard && this.calculatePercentCorrect() < 90) {
+      this.sayStartOver()
+      this.restart()
+      return (`** Start over! ** You only answered ${this.calculatePercentCorrect()}% correctly!`)
     } else {
       return this.currentCard
     }
   }
-
+  
   takeTurn(guess) {
     const turn = new Turn(guess, this.currentCard)
     this.turns++
-
     this.currentCard = this.deck[this.turns]
     if (!turn.evaluateGuess()) {
       this.incorrectGuesses.push(this.currentCard.id)
@@ -31,15 +28,27 @@ class Round {
       return turn.giveFeedBack()
     }
   }
+
   calculatePercentCorrect() {
     return Math.round((this.turns - this.incorrectGuesses.length) / this.turns * 100)
   }
+
   endRound() {
     console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`)
     return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
   }
 
- 
+  sayStartOver() {
+    console.log(`-----------------------------------------------------------------------`)
+    console.log(`** Start over! ** You only answered ${this.calculatePercentCorrect()}% correctly!`)
+    console.log(`-----------------------------------------------------------------------`)
+  }
+  
+  restart() {
+    this.turns = 0
+    this.incorrectGuesses = []
+    this.currentCard = this.deck[0]
+  }
 }
 
 module.exports = Round
