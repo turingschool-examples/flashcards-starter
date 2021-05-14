0a5d22975ca2175e6178bcf7ec29bbd70329ab9f
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-13 20:56:51 -0600

    Update functions to fat arrows

--- test/Card-test.js
+++ test/Card-test.js
@@ -6,45 +6,45 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
-describe('Card', function() {
-  it('should be a function', function() {
+describe('Card', () => {
+  it('should be a function', () => {
     expect(Card).to.be.a('function');
   });
 
-  it('should be an instance of Card', function() {
+  it('should be an instance of Card', () => {
     const card = new Card();
     expect(card).to.be.an.instanceof(Card);
   }); 
 
-  it('should store a question', function() {
+  it('should store a question', () => {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
   });  
 
 
-  it('should store a list of possible answers', function() {
+  it('should store a list of possible answers', () => {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
   });  
 
-  it('should store the correct answer', function() {
+  it('should store the correct answer', () => {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.correctAnswer).to.equal('object');
   });
 
-  it('should store the correct answer', function() {
+  it('should store the correct answer', () => {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.correctAnswer).to.equal('object');
   });
 });
 
-it('should return a card', function () {
+it('should return a card', () => {
   const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
   const turn = new Turn('pug', card);
   expect(turn.returnCard()).to.deep.equal(card)
 })
 
-it('should evaluate the guess to true or ', function () {
+it('should evaluate the guess to true or ', () =>{
   const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
   let turn = new Turn('pug', card);   
 
