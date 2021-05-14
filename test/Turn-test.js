3ff44a04de9b6c5716adc604541bfda0d62d5225
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-13 20:56:39 -0600

    Update functions to fat arrows

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,37 +5,37 @@ const expect = chai.expect;
 const Turn = require('../src/Turn')
 const Card = require('../src/Card')
 
-describe('Turn', function() {
-  it('should be a function', function() {
+describe('Turn', () => {
+  it('should be a function', () => {
     expect(Turn).to.be.a('function');
   })
 
-  it('should be an instance of Turn', function () {
+  it('should be an instance of Turn', () => {
     const turn = new Turn() 
     expect(turn).to.be.an.instanceOf(Turn)
   })
 
-  it('should instantiate users\'s guess and card', function () {
+  it('should instantiate users\'s guess and card', () => {
     const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const turn = new Turn('pug', card);
     expect(turn.guess).to.be.a('string')
     expect(turn.currentCard).to.equal(card)
   })
 
-  it('should return a guess', function () {
+  it('should return a guess', () => {
     const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const turn = new Turn('pug', card);
     expect(turn.returnGuess()).to.equal('pug')
   })
 
 
-  it('should return a card', function () {
+  it('should return a card', () => {
     const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const turn = new Turn('pug', card);
     expect(turn.returnCard()).to.deep.equal(card)
   })
   
-  it('should evaluate the guess to true or false', function () {
+  it('should evaluate the guess to true or false', () => {
     const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     let turn = new Turn('pug', card);   
 
@@ -46,7 +46,7 @@ describe('Turn', function() {
     expect(turn.evaluateGuess()).to.be.true
   })
 
-  it('should return a verification based on guess', function () {
+  it('should return a verification based on guess', () => {
     const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     let turn = new Turn('pug', card);   
 
