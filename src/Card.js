7a43db7a838f3279869a983ad1f095b979c07a2a
Rachel Jensen <rachelangela@gmail.com>
2021-07-05 16:39:57 -0600

    Add Card class

---
+++ src/Card.js
@@ -0,0 +1,9 @@
+class Card {
+  constructor(id, question, answers, correct) {
+    this.id = id;
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correct;
+  }
+}
+module.exports = Card;
