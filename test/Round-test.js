bc7a4f86e2c17f43d588c09713047d40a3908c23
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-07-05 17:00:44 -0600

    Require chai in test files

---
+++ test/Round-test.js
@@ -0,0 +1,4 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
---
+++ test/Turn-test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+    it('should return true', function() {
+      expect(true).to.equal(true);
+    });
+  });
