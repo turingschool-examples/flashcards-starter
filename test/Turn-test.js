2cdb2afe12472e9ee1c4c865738b50c2974715ba
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-06 13:03:38 -0600

    Create a before each function inside of the describe block

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,25 +4,26 @@ const expect = chai.expect;
 const Turn = require('../src/Turn.js');
 
 describe('Turn', function() {
+  let turn;
+
+  beforeEach(() => {
+    turn = new Turn('dachshund', card1)
+  });
 
   it('should be a function', function() {
-    const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
   it('should be an instance of Turn', function() {
-    const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
 
   it('should take in a string of the user\'s guess', function() {
-    const turn = new Turn('dachshund');
     expect(turn.guess).to.equal('dachshund');
   });
 
   it('should take in a card object for the current card in play', function() {
     const card1 = new Card(1, "What is Mark's dog", ['dachshund', 'beagle', 'husky'], 'dachshund');
-    const turn = new Turn('dachshund', card1);
     expect(turn.card).to.equal(card1);
   });
 
