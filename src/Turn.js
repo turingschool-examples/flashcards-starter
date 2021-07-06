4f52500f48c287f437decf0b4f39fab7ce54e13d
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-07-06 14:41:20 -0500

    Create Trun and Turn-test basic tests

---
+++ src/Turn.js
@@ -0,0 +1,9 @@
+class Turn {
+  constructor (guess, card) {
+    this.guess = guess;
+    this.card = card;
+    }
+
+}
+
+module.exports = Turn;
---
+++ test/Turn-test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it('should be instantiated with two arugments', function() {
+    const card = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    const turn = new Turn('guess', card);
+
+    expect(turn).to.have.property('guess');
+  });
+
+
+});
