2be3115c009d5173ae8036a20a21dfc39b5ca503
Hayley Dixon <hdixon37@gmail.com>
2021-07-06 15:51:40 -0600

    Write test code for Turn class and the Turn class

---
+++ src/Turn.js
@@ -0,0 +1,28 @@
+class Turn{
+  constructor(guess, card){
+    this.guess = guess;
+    this.card = card;
+  }
+  returnGuess(){
+    return this.guess;
+  };
+
+  returnCard(){
+    return this.card;
+  };
+
+  evaluateGuess(){
+    console.log(this.card.correctAnswer)
+    if(this.guess === this.card.correctAnswer){
+      return true
+    } else { return false }
+  };
+
+  giveFeedback(){
+    if (this.evaluateGuess()){
+      return `correct!`
+    } else {return `incorrect!`}
+  }
+
+};
+module.exports= Turn;
--- test/Card-test.js
+++ test/Card-test.js
@@ -3,30 +3,30 @@ const expect = chai.expect;
 
 const Card = require('../src/Card');
 
-describe('Card', function() {
 
-  it.skip('should be a function', function() {
-    const card = new Card();
+describe('Card', function() {
+  let card;
+
+  it('should be a function', function() {
+
     expect(Card).to.be.a('function');
   });
 
-  it.skip('should be an instance of Card', function() {
-    const card = new Card();
+  beforeEach(() => {card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object') })
+
+  it('should be an instance of Card', function() {
     expect(card).to.be.an.instanceof(Card);
-  }); 
+  });
 
-  it.skip('should store a question', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  it('should store a question', function() {
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
-  });  
+  });
 
-  it.skip('should store a list of possible answers', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  it('should store a list of possible answers', function() {
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
-  });  
+  });
 
-  it.skip('should store the correct answer', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  it('should store the correct answer', function() {
     expect(card.correctAnswer).to.equal('object');
   });
 });
