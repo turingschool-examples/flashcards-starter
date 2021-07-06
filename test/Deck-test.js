2008454b443a058ad7d08f99f3173a376fc78783
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-07-05 20:09:45 -0600

    Add Deck class and create Deck tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -2,3 +2,40 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Deck', function() {
+  let card1;
+  let card2;
+  let card3;
+  let cards;
+  let deck;
+
+  before(() => {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    cards = ([card1, card2, card3]);
+    deck = new Deck([card1, card2, card3]);
+  });
+
+  it('should be a function', function() {
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Card', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  }); 
+
+  it('should store a deck', () => {
+    expect(deck.cards).to.deep.equal(cards);
+  });
+
+  it('should count cards', () => {
+    const countCards = deck.countCards();
+  
+    expect(countCards).to.equal(3);
+  });
+  
+});
