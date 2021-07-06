ccec96676714eaa882383fbf201c2a85694db910
David Leach <davidleach724@gmail.com>
2021-07-06 15:38:39 -0600

    Incorportate beforeEach in the Card-test file

--- test/Card-test.js
+++ test/Card-test.js
@@ -6,9 +6,14 @@ const Card = require('../src/Card');
 describe('Card', function () {
   let card;
   beforeEach(() => {
-    card = new Card();
+    card = new Card(
+      1,
+      'What allows you to define a set of related information using key-value pairs?',
+      ['object', 'array', 'function'],
+      'object'
+    );
   });
-  
+
   it('should be a function', function () {
     expect(Card).to.be.a('function');
   });
@@ -18,34 +23,16 @@ describe('Card', function () {
   });
 
   it('should store a question', function () {
-    card = new Card(
-      1,
-      'What allows you to define a set of related information using key-value pairs?',
-      ['object', 'array', 'function'],
-      'object'
-    );
     expect(card.question).to.equal(
       'What allows you to define a set of related information using key-value pairs?'
     );
   });
 
   it('should store a list of possible answers', function () {
-    card = new Card(
-      1,
-      'What allows you to define a set of related information using key-value pairs?',
-      ['object', 'array', 'function'],
-      'object'
-    );
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
   });
 
   it('should store the correct answer', function () {
-    card = new Card(
-      1,
-      'What allows you to define a set of related information using key-value pairs?',
-      ['object', 'array', 'function'],
-      'object'
-    );
     expect(card.correctAnswer).to.equal('object');
   });
 });
