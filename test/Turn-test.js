63fffe0c4ea89aef8399024981cfab346147d289
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-05 18:17:27 -0600

    Create an it block to test if the card parameter is an object

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -16,9 +16,14 @@ describe('Turn', function() {
   });
 
   it('should take in a string of the user\'s guess', function() {
-    const turn = new Turn('dachshund', 1);
-    expect(turn.guess).to.be.a('string');
+    const turn = new Turn('dachshund');
+    expect(turn.guess).to.equal('dachshund');
   });
 
-  
+  it('should take in a card object for the current card in play', function() {
+    const card1 = new Card(1, "What is Mark's dog", ['dachshund', 'beagle', 'husky'], 'dachshund');
+    const turn = new Turn('dachshund', card1);
+    expect(turn.card).to.equal(card1);
+  });
+
 });
