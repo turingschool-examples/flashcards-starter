b187917f4ab55d8920f8711e4c6b1184e453ca71
Mae Duphorne <maelduphorne@gmail.com>
2021-07-06 14:53:43 -0600

    Make turn test pass

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -7,51 +7,50 @@ const Turn = require('../src/Turn');
 describe('Turn', function() {
 
   it('Should be a function', function() {
-      const card = new Card();
+      const card = new Card(1, 'Which is not a name of Jim O\'heir\'s character in Parks & Recreation?', ['Jerry', 'Larry', 'Terry', 'Barry'], 'Barry');
       expect(Turn).to.be.a('function');
     });
 
   it('Should be an instance of a turn', function() {
-      const card = new Card();
-      const turn = new Turn("guess", card);
+      const card = new Card(1, 'Which is not a name of Jim O\'heir\'s character in Parks & Recreation?', ['Jerry', 'Larry', 'Terry', 'Barry'], 'Barry');
+      const turn = new Turn('guess', card);
       expect(turn).to.be.an.instanceof(Turn);
     });
 
   it('Should return the guess of the user', function() {
-      const card = new Card();
-      const turn = new Turn("guess", card);
+      const card = new Card(1, 'Which is not a name of Jim O\'heir\'s character in Parks & Recreation?', ['Jerry', 'Larry', 'Terry', 'Barry'], 'Barry');
+      const turn = new Turn('guess', card);
 
       expect(turn.returnGuess()).to.equal(turn.guess);
     });
 
   it('Should return the Card', function() {
-      const card = new Card();
-      const turn = new Turn("guess", card);
+      const card = new Card(1, 'Which is not a name of Jim O\'heir\'s character in Parks & Recreation?', ['Jerry', 'Larry', 'Terry', 'Barry'], 'Barry');
+      const turn = new Turn('guess', card);
 
       expect(turn.returnCard()).to.equal(card);
     });
-    // evaluateGuess: method that returns a boolean indicating if the user’s guess matches the correct answer on the card
 
   it('Should evaluate if the user guess is correct', function() {
-      const card1 = new Card();
-      const turn1 = new Turn("guess", card);
+      const card1 = new Card(1, 'Which is not a name of Jim O\'heir\'s character in Parks & Recreation?', ['Jerry', 'Larry', 'Terry', 'Barry'], 'Barry');
+      const turn1 = new Turn('Barry', card1);
 
-      expect(turn.evaluateGuess()).to.equal(true);
+      expect(turn1.evaluateGuess()).to.equal(true);
 
-      const card2 = new Card();
-      const turn2 = new Turn("incorrect guess", card);
-      expect(turn.evaluateGuess()).to.equal(false);
+      const card2 = new Card(1, 'Which is not a name of Jim O\'heir\'s character in Parks & Recreation?', ['Jerry', 'Larry', 'Terry', 'Barry'], 'Barry');
+      const turn2 = new Turn('Terry', card2);
+      expect(turn2.evaluateGuess()).to.equal(false);
     });
-    // giveFeedback - method that returns either ‘incorrect!’ or ‘correct!’ based on whether the guess is correct or not.
+
   it('Should give feedback on if the user guess is correct or not', function() {
-      const card1 = new Card();
-      const turn1 = new Turn("guess", card);
+      const card1 = new Card(1, 'Which is not a name of Jim O\'heir\'s character in Parks & Recreation?', ['Jerry', 'Larry', 'Terry', 'Barry'], 'Barry');
+      const turn1 = new Turn('Barry', card1);
 
-      expect(turn.giveFeedback()).to.equal('correct!');
+      expect(turn1.giveFeedback()).to.equal('Correct!');
 
-      const card2 = new Card();
-      const turn2 = new Turn("incorrect guess", card);
+      const card2 = new Card(1, 'Which is not a name of Jim O\'heir\'s character in Parks & Recreation?', ['Jerry', 'Larry', 'Terry', 'Barry'], 'Barry');
+      const turn2 = new Turn('Terry', card2);
 
-      expect(turn.giveFeedback()).to.equal('incorrect!');
+      expect(turn2.giveFeedback()).to.equal('Incorrect!');
     });
   });
