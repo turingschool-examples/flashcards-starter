ace5a2e6dd558cb7260caf6de7e8a3feae7d9a47
Maria DelSignore <maria.ad27@gmail.com>
2021-07-06 14:18:18 -0600

    Add first 2 it blocks

---
+++ test/Round-test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
+
+describe('Round', () => {
+
+    beforeEach();
+
+    it.skip('should be a function', () => {
+        expect(Round).to.be.a('function');
+    });
+
+    it.skip('should be an instance of Round', () => {
+        expect(round).to.be.an.instanceOf(Round);
+    });
+
+    it.skip('should', () => {});
+});
