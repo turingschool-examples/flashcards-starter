989a2e0b21401866b7646b2fb7ed82069660e628
David Leach <davidleach724@gmail.com>
2021-07-06 20:49:43 -0600

    Return first card in round file

--- src/Round.js
+++ src/Round.js
@@ -1,7 +1,21 @@
 class Round {
-  constructor() {
+  constructor(deck) {
+    this.deck = deck;
+    this.cardCount = 0;
+    this.currentCard = this.deck.cards[this.cardCount];
+  }
+
+  returnCurrentCard() {
+    let current = this.currentCard;
+    this.cardCount++;
+    return current;
+  }
+
+  takeTurn() {
 
   }
+
+
 }
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -3,7 +3,21 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
+const Turn = require('../src/Turn');
 
 describe('Round', () => {
-  
+  let card1, card2, card3, deck, round;
+  beforeEach(() => {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
+  })
+
+  it('it should return the first card', () => {
+    expect(round.currentCard).to.equal(card1)
+  })
+
+
 })
