e3ef421d862a1ff0356ea093dddaf234258c3c09
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-07-07 15:19:00 -0500

    Add takeTurn() test

--- src/Round.js
+++ src/Round.js
@@ -1,5 +1,28 @@
+const Turn = require('../src/Turn');
 class Round {
   constructor(deck) {
-    this.currentCard = deck[0];
+    this.currentCard = deck.allCards[0];
+    this.currentCardTest = deck.allCards
+    this.turns = 0;
+    this.incorrectGuesses = [];
+  }
+  returnCurrentCard() {
+    return this.currentCard;
+  }
+  takeTurn(userGuess) {
+    this.turns ++;
+    // this.currentCard.pop();
+    const turn = new Turn(userGuess, this.currentCard);
+    // const evaluateGuess = turn.evaluateGuess();
+    this.addIncorrectAnswersId(turn.evaluateGuess());
+    return turn.giveFeedback();
+
+  }
+  addIncorrectAnswersId(badGuess) {
+    if (!badGuess) {
+      this.incorrectGuesses.push(this.currentCard.id)
+    }
   }
 };
+
+module.exports = Round;
--- test/round-test.js
+++ test/round-test.js
@@ -6,8 +6,9 @@ const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 const Round = require('../src/Round');
 
-describe('Round', () = {
+describe('Round', () => {
   let round;
+
   beforeEach(() => {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?',['object', 'array', 'function'],'object');
     const card2 = new Card(2, 'What is a comma-separated list of related values?',['array', 'object', 'function'],'array');
@@ -16,16 +17,58 @@ describe('Round', () = {
     round = new Round(deck);
   })
 
-  it('should be a function', function() {
+  it('should be a function', () => {
 
     expect(Round).to.be.a('function');
   });
 
-  it('should be an instance of Turn', function() {
+  it('should be an instance of Turn', () => {
 
     expect(round).to.be.an.instanceof(Round);
   });
 
+  it('should return the current card', () => {
+    // console.log(typeof round.currentCard);
+    // console.log('deck object', round.currentCard);
+    // console.log('array in deck object', round.currentCard.allCards[0]);
 
+    expect(round.returnCurrentCard()).to.deep.equal({
+    "id": 1,
+    "question": "What allows you to define a set of related information using key-value pairs?",
+    "answers": ["object", "array", "function"],
+    "correctAnswer": "object"
+    })
+  });
 
+  it('should increase by one every time that the takeTurn() is called', () => {
+
+    round.takeTurn('array');
+
+    expect(round.turns).to.equal(1);
+  });
+
+  it.skip('should make the next card becomes the currentCard', () => {
+
+    console.log(typeof round.currentCardTest);
+    round.takeTurn('array');
+
+    expect(round.currentCard).to.deep.equal({
+      'id': 2,
+      'question': 'What is a comma-separated list of related values?',
+      'answers': ['array', 'object', 'function'],
+      'correctAnswer': 'array'
+    })
+  });
+
+  it('should add bad answerts ids, into the incorrect answers container', () => {
+
+    round.takeTurn('array');
+
+    expect(round.incorrectGuesses[0]).to.equal(1);
+  });
+
+  it('should return a feedback based on the player guess', () => {
+
+    expect(round.takeTurn('array')).to.deep.equal('incorrect');
+  });
 });
