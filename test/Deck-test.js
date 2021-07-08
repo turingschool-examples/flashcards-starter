5421628d80e05c1ddf1b0502513de844653adce5
William McGuire <wmcguire18@gmail.com>
2021-07-08 17:11:26 -0400

    Refactored Deck

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,23 +3,30 @@ const expect = chai.expect;
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 
-describe('Deck', function() {
+describe('Deck', () => {
 
-  beforeEach(function() {
-    const card1 = new Card(1, 'What author invented the detective story?', ['Dickens', 'Doyle', 'Gaboriau'], 'Poe');
-    const card2 = new Card(2, 'Where did Sherlock Holmes confront Professor Moriarty?',['Victoria Falls', 'Niagra Falls', 'Bushkill Falls'],'Reichenbach Falls');
-    const card3 = new Card(3, 'What was the profession of G.K. Chestertons famous detective?',['Police Officer', 'Socialite', 'University Professor'],'Catholic Priest');
-    deck = new Deck([card1, card2, card3]);
+let card1, card2, card3, deck;
+
+  beforeEach( () => {
+     card1 = new Card(1, 'What author invented the detective story?', ['Dickens', 'Doyle', 'Gaboriau'],'Poe');
+
+     card2 = new Card(2, 'Where did Sherlock Holmes confront Professor Moriarty?',['Victoria Falls', 'Niagra Falls', 'Bushkill Falls'],'Reichenbach Falls');
+
+     card3 = new Card(3, 'What was the profession of G.K. Chestertons famous detective?',['Police Officer', 'Socialite', 'University Professor'],'Catholic Priest');
+
+     deck = new Deck([card1, card2, card3]);
   });
 
   it('should be a function', function() {
     expect(Deck).to.be.a('function');
   });
+
   it('should be an instance of Deck', function() {
     expect(deck).to.be.an.instanceof(Deck);
   });
+
   it('should store all cards required in the game', function() {
-    expect(deck.allCards).to.deep.equal([
+    expect(deck.cards).to.deep.equal([
       {
       'id': 1,
       'question': 'What author invented the detective story?',
