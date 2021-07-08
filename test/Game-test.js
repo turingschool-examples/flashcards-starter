d3de439fe17a7fd2b3c25fdee3c6eb1980b582ad
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-07 21:22:47 -0600

    Create before each function in describe block to instantiate cards deck and round

--- test/Game-test.js
+++ test/Game-test.js
@@ -5,3 +5,20 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 const Game = require('../src/Game');
+
+describe('Game', () => {
+
+  let card1, card2, card3, deck, round;
+  beforeEach(() => {
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+
+    card2 = new Card(2,'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+
+    card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'],'mutator method');
+
+    deck = new Deck([card1, card2, card3]);
+
+    round = new Round(deck);
+
+  })
+})
