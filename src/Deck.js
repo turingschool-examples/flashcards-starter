bd38c4e80b4d9f83f970b741894e06d1053b41f9
Sarah <sarahrudy@gmail.com>
2021-07-07 20:37:42 -0600

    Change countCards() method name to cardCount()

--- src/Deck.js
+++ src/Deck.js
@@ -3,7 +3,7 @@ class Deck {
     this.cards = cards
   }
 
-  countCards() {
+  cardCount() {
     return this.cards.length
   }
 }
@@ -11,8 +11,5 @@ class Deck {
 
 module.exports = Deck 
 
-
-// it should know how many cards are in the deck
-// this.cards.length with a countCards() method
 
 
