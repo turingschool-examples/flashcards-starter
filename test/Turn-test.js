3ca51705f109421f99396466baa8f10737863833
Samantha Brown <bro7445@gmail.com>
2021-07-08 18:45:48 -0400

    Include the describe in ES6

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,5 +1,18 @@
 const chai = require('chai');
 const expect = chai.expect;
-
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
+
+describe('Turn', () => {
+  let card;
+  let turn;
+
+  beforeEach(() => { //do I need a parameter???
+    card = new Card(
+   1,
+   "What is Robbie's favorite animal",
+   ['sea otter', 'pug', 'capybara'],
+   'sea otter'
+ );
+ turn = new Turn('pug', card);
+});
