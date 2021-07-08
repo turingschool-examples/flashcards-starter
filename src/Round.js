e2441c9b5db6970a82dcca99b9392eab890cc8a9
Sarah <sarahrudy@gmail.com>
2021-07-07 21:04:36 -0600

    Add turnCounter and incorrectCards array to constructor

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,8 @@
 class Round {
   constructor(deck) {
     this.deck = deck
+    this.turnCounter = 0
+    this.incorrectCards = []
   }
 }
 
@@ -26,7 +28,7 @@ module.exports = Round
 //calculatePercentCorrect() 
 // calculates and returns the percentage of correct guesses
 // var correctCards = this.turns - this.incorrectCards.length
-// var correctCardsPercentage = correctCards / this.turns * 100
+// var correctCardsPercentage = correctCards / this.turnCounter * 100
 
 //endRound() 
 // return `Round over! You answered ${correctCardsPercentage}% of the questions correctly!`
