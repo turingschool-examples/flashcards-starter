cbc726fcd122bfb1663ce9326fa212fd3d93b1dc
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-08 15:52:04 -0600

    Make changes presented by the linter on the round test

--- test/Round-test.js
+++ test/Round-test.js
@@ -11,11 +11,11 @@ describe('Round', function() {
   let card1, card2, card3, deck, round;
 
   beforeEach(() => {
-    card1 = new Card(1,'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
 
-    card2 = new Card(2,'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
 
-    card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'],'mutator method');
+    card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
 
     deck = new Deck([card1, card2, card3]);
 
@@ -79,7 +79,7 @@ describe('Round', function() {
     });
 
     it('should evaluate a guess as incorrect', function() {
-      turn = new Turn('function', card1);
+      let turn = new Turn('function', card1);
       expect(round.takeTurn(turn)).to.equal('incorrect!');
     });
 
@@ -119,7 +119,7 @@ describe('Round', function() {
 
   });
 
-  describe('endRound()',function() {
+  describe('endRound()', function() {
 
     it('should be a function', function() {
       expect(round.endRound).to.be.a('function');
