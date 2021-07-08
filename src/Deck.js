120558712713e703f41d162c62d67e3085b68bd8
Samantha Brown <bro7445@gmail.com>
2021-07-08 08:39:10 -0400

    Add the countCards method to Deck class

--- src/Deck.js
+++ src/Deck.js
@@ -2,6 +2,10 @@ class Deck {
   constructor(cards) {
     this.cards = cards;
   }
+  
+  countCards() {
+    return this.cards.length;
+  }
 }
-
+// should know how many cards are in the Deck...
 module.exports = Deck;
