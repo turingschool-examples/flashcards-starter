2619ef4308805e1eee84595a6b4f392223694699
Sarah <sarahrudy@gmail.com>
2021-07-07 18:36:49 -0600

    Add beforeEach() block and test that Deck is a function

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,10 +1,23 @@
 const chai = require('chai')
 const expect = chai.expect
 
-const Turn = require('../src/Turn')
 const Card = require('../src/Card')
 const Deck = require('../src/Deck')
 
-const card1 = new Card(1, 'What do iterator methods take in as their first argument?', ['callback function', 'current element', 'an array'], 'callback function')
-const card2 = new Card(2, 'What does the callback function for the map() method take in?', ["current element", "initializer", "boolean"], 'current element')
-const card3 = new Card(3, 'What does the callback function for reduce() return?', ["the accumulator", "the current element", "the initializer"], 'the accumulator')
+describe('Deck', function() {
+  let card1, card2, card3, deck
+
+  beforeEach(function() {
+    card1 = new Card(1, 'What do iterator methods take in as their first argument?', ['callback function', 'current element', 'an array'], 'callback function')
+    card2 = new Card(2, 'What does the callback function for the map() method take in?', ["current element", "initializer", "boolean"], 'current element')
+    card3 = new Card(3, 'What does the callback function for reduce() return?', ["the accumulator", "the current element", "the initializer"], 'the accumulator')
+    deck = new Deck([card1, card2, card3])
+  })
+
+  it('should be a function', function() {
+    expect(Deck).to.be.a('function')
+  })
+
+
+})
+  
