35eb002342cee59610d864a4633e416662217d58
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-08 14:21:15 -0600

    Create a before each function to instantiate 2 different cards

--- test/Card-test.js
+++ test/Card-test.js
@@ -5,29 +5,37 @@ const Card = require('../src/Card');
 
 describe('Card', () => {
 
+  let card1, card2;
+
+  beforeEach('instantiate a card from the Card class', () => {
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+
+    card2 = new Card(2, 'What is Mark\'s dog\'s name?', ['Rover', 'Mr. Wiggles', 'Rudder'], 'Rudder');
+
+  });
+
   it('should be a function', () => {
-    const card = new Card();
     expect(Card).to.be.a('function');
   });
 
   it('should be an instance of Card', () => {
-    const card = new Card();
-    expect(card).to.be.an.instanceof(Card);
+    expect(card1).to.be.an.instanceof(Card);
+    expect(card2).to.be.an.instanceof(Card);
   });
 
   it('should store a question', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
+    expect(card1.question).to.equal('What allows you to define a set of related information using key-value pairs?');
+    expect(card2.question).to.equal('What is Mark\'s dog\'s name?');
   });
 
   it('should store a list of possible answers', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    expect(card.answers).to.deep.equal(['object', 'array', 'function']);
+    expect(card1.answers).to.deep.equal(['object', 'array', 'function']);
+    expect(card2.answers).to.deep.equal(['Rover', 'Mr. Wiggles', 'Rudder']);
   });
 
   it('should store the correct answer', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    expect(card.correctAnswer).to.equal('object');
+    expect(card1.correctAnswer).to.equal('object');
+    expect(card2.correctAnswer).to.equal('Rudder');
   });
-  
+
 });
