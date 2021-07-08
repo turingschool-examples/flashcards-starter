131d6ae80a784295cb218bb6c6711e1deafcf0e2
Eric Li <ericli@knights.ucf.edu>
2021-07-07 22:56:25 -0600

    Unskip tests for Deck class

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,13 +6,13 @@ const Card = require('../src/Card');
 
 describe('Deck', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const deck = new Deck();
 
     expect(Deck).to.be.a('function');
   });
 
-  it.skip('should take in an array of Card objects', function() {
+  it('should take in an array of Card objects', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
@@ -24,7 +24,7 @@ describe('Deck', function() {
 
   describe('countCards()', function() {
 
-    it.skip('should be able to count the number of cards in the deck', function() {
+    it('should be able to count the number of cards in the deck', function() {
       const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
       const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
       const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
