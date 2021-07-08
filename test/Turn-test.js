1a93ab9d7e302ddba6be3704d17cb1f943f1ec24
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-08 15:45:11 -0600

    Change callback functions to fat arrow syntax in the turn test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,7 +4,7 @@ const expect = chai.expect;
 const Turn = require('../src/Turn.js');
 const Card = require('../src/Card.js');
 
-describe('Turn', function() {
+describe('Turn', () => {
 
   let turn, card;
 
@@ -13,23 +13,23 @@ describe('Turn', function() {
     turn = new Turn('dachshund', card);
   });
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Turn).to.be.a('function');
   });
 
-  it('should be an instance of Turn', function() {
+  it('should be an instance of Turn', () => {
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-  it('should take in a string of the user\'s guess', function() {
+  it('should take in a string of the user\'s guess', () => {
     expect(turn.guess).to.equal('dachshund');
   });
 
-  it('should take in a card object for the current card in play', function() {
+  it('should take in a card object for the current card in play', () => {
     expect(turn.card).to.equal(card);
   });
 
-  describe('returnGuess', function() {
+  describe('returnGuess', () => {
 
     let turn, card;
 
@@ -38,17 +38,17 @@ describe('Turn', function() {
       turn = new Turn('dachshund', card);
     });
 
-    it('should be a function', function() {
+    it('should be a function', () => {
       expect(turn.returnGuess).to.be.a('function');
     });
 
-    it('should return the user\'s guess', function() {
+    it('should return the user\'s guess', () => {
       expect(turn.returnGuess()).to.equal('dachshund');
     });
 
   });
 
-  describe('returnCard', function() {
+  describe('returnCard', () => {
 
     let turn, card;
 
@@ -57,15 +57,15 @@ describe('Turn', function() {
       turn = new Turn('dachshund', card);
     });
 
-    it('should be a funtion', function() {
+    it('should be a funtion', () => {
       expect(turn.returnCard).to.be.a('function');
     });
 
-    it('should return an object', function() {
+    it('should return an object', () => {
       expect(turn.returnCard()).to.be.an('object');
     });
 
-    it('should have specific properties', function() {
+    it('should have specific properties', () => {
       expect(turn.returnCard().answers).to.deep.equal(['dachshund', 'beagle', 'husky'])
       expect(turn.returnCard().question).to.equal('What is Mark\'s dog?');
       expect(turn.returnCard().correctAnswer).to.equal('dachshund');
@@ -73,7 +73,7 @@ describe('Turn', function() {
 
   });
 
-  describe('evaluateGuess', function() {
+  describe('evaluateGuess', () => {
 
     let turn, card;
 
@@ -82,24 +82,26 @@ describe('Turn', function() {
       turn = new Turn('dachshund', card);
     });
 
-    it('should be a function', function() {
+    it('should be a function', () => {
       expect(turn.evaluateGuess).to.be.a('function');
     });
 
-    it('should return a boolean', function() {
+    it('should return a boolean', () => {
       expect(turn.evaluateGuess()).to.be.a('boolean');
     });
 
-    it('should return true if guess matches the correct answer', function() {
+    it('should return true if guess matches the correct answer', () => {
       expect(turn.evaluateGuess()).to.equal(true);
     });
 
-    it('should return false if guess does not match the correct answer', function() {
+    it('should return false if guess does not match the correct answer', () => {
       turn = new Turn('husky', card);
       expect(turn.evaluateGuess()).to.equal(false);
     });
 
-    describe('giveFeedback', function() {
+    });
+
+    describe('giveFeedback', () => {
 
       let turn, card;
 
@@ -108,25 +110,23 @@ describe('Turn', function() {
         turn = new Turn('dachshund', card);
       });
 
-      it('should be a function', function() {
+      it('should be a function', () => {
         expect(turn.giveFeedback).to.be.a('function');
       });
 
-      it('should return a string', function() {
+      it('should return a string', () => {
         expect(turn.giveFeedback()).to.be.a('string');
       });
 
-      it('should return "correct!" if the guess is the right answer', function() {
+      it('should return "correct!" if the guess is the right answer', () => {
         expect(turn.giveFeedback()).to.equal('correct!');
       });
 
-      it('should return "incorrect!" if the guess is not the right answer', function() {
+      it('should return "incorrect!" if the guess is not the right answer', () => {
         turn = new Turn('husky', card);
         expect(turn.giveFeedback()).to.equal('incorrect!');
       });
 
     });
-
-  });
 
 });
