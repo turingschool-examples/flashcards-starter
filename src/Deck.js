f4e60d08b87cb61e862fd3b51e6302a9c5907d68
Sarah <sarahrudy@gmail.com>
2021-07-07 18:37:39 -0600

    Add constructor to Deck class with cards as a property

---
+++ src/Deck.js
@@ -0,0 +1,13 @@
+class Deck {
+  constructor(cards) {
+    this.cards = cards
+  }
+}
+
+module.exports = Deck 
+
+
+// it should know how many cards are in the deck
+// this.cards.length with a countCards() method
+
+
