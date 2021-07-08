b9d02a3b76f05feb01abec71b6679deb3f5f22c9
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-08 12:39:13 -0600

    Write describe block tests to create the start method

--- src/Game.js
+++ src/Game.js
@@ -6,7 +6,20 @@ const Deck = require('./Deck');
 const Round = require('./Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null;
+  }
+
+  start() {
+    const questions = prototypeQuestions;
+    const cards = questions.map(question => {
+      return new Card(question.id, question.question, question.answers, question.correctAnswer);
+    });
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
--- test/Game-test.js
+++ test/Game-test.js
@@ -9,6 +9,7 @@ const Game = require('../src/Game');
 describe('Game', () => {
 
   let card1, card2, card3, deck, round;
+  
   beforeEach(() => {
     card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
 
@@ -20,5 +21,46 @@ describe('Game', () => {
 
     round = new Round(deck);
 
-  })
-})
+    game = new Game();
+
+  });
+
+  it('should be a function', () => {
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of the Game class', () => {
+    expect(game).to.be.an.instanceOf(Game);
+  });
+
+  it('should keep track of the current round', () => {
+    expect(game.currentRound).to.equal(null);
+  });
+
+  describe('start()', () => {
+
+    it('should be a function', () => {
+      expect(game.start).to.be.a('function');
+    });
+
+    it('should create an instance of the Card class', () => {
+      // game.start();
+      expect(game.currentRound.currentCard).to.be.an.instanceOf(Card);
+    });
+
+    it('should create an instance of the Deck class', () => {
+      // game.start();
+      expect(game.currentRound.deck).to.be.an.instanceOf(Deck);
+    });
+
+    it('should create a new round that is an instance of the Round class', () => {
+      expect(game.currentRound).to.equal(null);
+      // game.start();
+      console.log(game.currentRound);
+      expect(game.currentRound).to.be.an('object');
+      expect(game.currentRound).to.be.an.instanceOf(Round);
+    });
+
+  });
+
+});
