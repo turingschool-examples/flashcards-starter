a9d649aa026f2142f800c3152d12d9a1240571f4
Sarah <sarahrudy@gmail.com>
2021-07-07 21:00:51 -0600

    Create Round-test.js file setup

--- test/Round-test.js
+++ test/Round-test.js
@@ -5,10 +5,22 @@ const Card = require('../src/Card')
 const Deck = require('../src/Deck')
 const Round = require('../src/Round')
 
-// What is this app actually supposed to do? Then I would walk through doing all of those things and test them. Like, see the home page with all the stuff on it, click on button, see a new thing
-// What are the ways this could get screwed up? Like user clicks at wrong time, or user enters bad data, or the server is dead, etc.
+describe('Round', function() {
+  let card1, card2, card3, card4, deck, round 
 
-// we will need card1, card2, card3, deck(cards), round
+  beforeEach(function() {
+    card1 = new Card(1, 'What do iterator methods take in as their first argument?', ['callback function', 'current element', 'an array'], 'callback function')
+    card2 = new Card(2, 'What does the callback function for the map() method take in?', ["current element", "initializer", "boolean"], 'current element')
+    card3 = new Card(3, 'What does the callback function for reduce() return?', ["the accumulator", "the current element", "the initializer"], 'the accumulator')
+    card4 = new Card(4, 'Which prototype method is used to iterate over each property of an object?', ["Object.keys()", "Object.values()", "reduce()"], 'Object.keys()')
+    deck = new Deck([card1, card2, card3, card4])
+    round = new Round(deck);
+  })
+
+  it('should be a function', function() {
+    expect(Round).to.be.a('function')
+  })
+})
 
 // tests
 // should be a function
