c6d08076da2ded3c92cc5c0ac8d77ca68ff46c0b
Samantha Brown <bro7445@gmail.com>
2021-07-08 20:16:01 -0400

    Add the describe of Round

--- test/Round-test.js
+++ test/Round-test.js
@@ -5,3 +5,29 @@ const sinon = require('sinon');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
+
+describe('Round', () => {
+  let card1, card2, card3, deck, round;
+
+  beforeEach(() => {
+    card1 = new Card(
+      1,
+      "What is Robbie's favorite animal",
+      ['sea otter', 'pug', 'capybara'],
+      'sea otter'
+    );
+    card2 = new Card(
+      14,
+      'What organ is Khalid missing?',
+      ['spleen', 'appendix', 'gallbladder'],
+      'gallbladder'
+    );
+    card3 = new Card(
+      12,
+      "What is Travis's favorite stress reliever?",
+      ['listening to music', 'watching Netflix', 'playing with bubble wrap'],
+      'playing with bubble wrap'
+    );
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
+  });
