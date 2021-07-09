a491595fbdfc8f2b4595032304315c39c770edec
Samantha Brown <bro7445@gmail.com>
2021-07-08 20:49:33 -0400

    Add the chai and expect and require links

---
+++ test/Game-test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Game = require('../src/Game');
+const data = require('../src/data');
+const prototypeQuestions = data.prototypeData;
