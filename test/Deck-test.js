78bc6b538c96acac1ffa073d43b546e0f87d6d73
Nolan Caine <nolancaine@gmail.com>
2021-08-23 19:41:55 -0600

    Complete deck and deck test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,6 +1,30 @@
 const assert = require('chai').assert;
 const Deck = require('../src/Deck.js');
+const Card = require('../src/Card.js');
 
 describe('Deck', function() {
-  
+  let card1;
+  let card2;
+  let card3;
+  let deck;
+  beforeEach(function() {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck = new Deck([card1, card2, card3]);
+  })
+
+  it('should be a function', function() {
+    assert.isFunction(Deck);
+  });
+
+  it('should be an instance of Deck', function() {
+    assert.instanceOf(deck, Deck);
+  });
+
+  it('should know how many Cards are in the Deck', function() {
+    let howManyCards = deck.countCards();
+
+    assert.equal(howManyCards, 3);
+  })
 })
