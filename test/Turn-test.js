63120e1982b4cf9cedad9a66ba4f47acb0660c35
Nolan Caine <nolancaine@gmail.com>
2021-08-23 19:00:44 -0600

    Rewrite using variables and beforeEach in describe block

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -3,31 +3,28 @@ const Turn = require('../src/Turn.js');
 const Card = require('../src/Card.js');
 
 describe('Turn', function() {
+  let card;
+  let turn;
+  beforeEach(function() {
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    turn = new Turn('object', card);
+  });
 
   it('should take a guess', function() {
-    const turn = new Turn('object')
-
     assert.equal(turn.guess, 'object')
   });
 
   it('should take a Card', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card);
-
     assert.equal(turn.card, card);
   });
 
   it('should return the guess', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card);
     let myGuess = turn.returnGuess();
 
     assert.equal(myGuess, 'object');
   });
 
   it('should return the Card', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card);
     let myCard = turn.returnCard();
 
     assert.equal(myCard, card);
@@ -35,16 +32,13 @@ describe('Turn', function() {
   });
 
   it('should evaluate the guess', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card);
     let myGuess = turn.evaluateGuess();
 
     assert.equal(myGuess, true);
   });
 
   it('should tell whether the guess is correct', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('array', card);
+    turn.guess = "array";
     let myFeedback = turn.giveFeedback();
 
     assert.equal(myFeedback, 'incorrect!');
