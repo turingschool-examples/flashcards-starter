0d348f5e9661abc900645aaf5cb7ae21d8e02f83
Nolan Caine <nolancaine@gmail.com>
2021-08-24 12:54:06 -0600

    Create test skeleton

---
+++ test/Game-test.js
@@ -0,0 +1,29 @@
+const assert = require('chai').assert;
+const Game = require('../src/Game.js');
+const Round = require('../src/Round.js');
+const Card = require('../src/Card.js');
+const Turn = require('../src/Turn.js');
+const Deck = require('../src/Deck.js');
+
+describe('Game', function() {
+  it.skip('should keep track of the current round', function() {
+
+  })
+
+  it.skip('should greet the user', function() {
+
+  })
+
+  it.skip('should prompt the user with a game question', function() {
+    
+  })
+})
+
+
+// Should keep track of the currentRound
+// start: method that starts everything
+  // Creates Cards
+  // Puts Cards in a Deck
+  // Creates a new Round using the Deck
+  // invokes printMessage to display the message in the CLI
+  // invokes printQuestion to kick off our helper functions that allow interaction via the CLI
