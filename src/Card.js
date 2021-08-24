2189bf4358fc93c90ecfae7651d49aba08ad828a
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-08-24 09:28:14 -0600

    Add base layout to Card class

---
+++ src/Card.js
@@ -0,0 +1,8 @@
+class Card{
+    constructor(id, question, ansArr, correctAns){
+    this.id = id;
+    this.question = question;
+    this.ansArr = ansArr;
+    this.correctAns = correctAns;
+    }
+}
