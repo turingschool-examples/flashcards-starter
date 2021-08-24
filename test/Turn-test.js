8c7422d3f3abfcc46d88bf5d48019caf9472bb96
Nolan Caine <nolancaine@gmail.com>
2021-08-23 18:28:36 -0600

    Outline tests

---
+++ test/Turn-test.js
@@ -0,0 +1,21 @@
+const assert = require('chai').assert;
+const Turn = require('../src/Turn.js');
+
+describe('Turn', function() {
+  it.skip('should take a guess', function(){});
+  it.skip('should take a Card', function(){});
+  it.skip('should return the guess', function(){});
+  it.skip('should return the Card', function(){});
+  it.skip('should evaluate the guess', function(){});
+  it.skip('should tell whether the guess is correct', function(){});
+})
+
+// - Instantiated with two arguments - a string (that represents a user’s guess to the question), and a Card object for the current card in play.
+//
+// - returnGuess: method that returns the guess
+//
+// - returnCard: method that returns the Card
+//
+// - evaluateGuess: method that returns a boolean indicating if the user’s guess matches the correct answer on the card
+//
+// - giveFeedback - method that returns either ‘incorrect!’ or ‘correct!’ based on whether the guess is correct or not.
