ea827e8868af2fc068af06ba9335b88cbb3093e3
Nolan Caine <nolancaine@gmail.com>
2021-08-23 21:47:01 -0600

    Complete round and round test

--- src/Round.js
+++ src/Round.js
@@ -1,18 +1,27 @@
-class Round {
-  constructor() {
+const Turn = require('../src/Turn.js');
 
+class Round {
+  constructor(deck) {
+    this.deck = deck;
+    this.currentCard = this.deck.cards[0];
+    this.turns = 0;
+    this.incorrectGuesses = [];
   }
   returnCurrentCard() {
-
+    return this.currentCard;
   }
-  takeTurn() {
-
+  takeTurn(guess) {
+    let turn = new Turn(guess, this.currentCard);
+    this.turns ++;
+    this.currentCard = this.deck.cards[this.turns];
+    turn.evaluateGuess() ? console.log() : this.incorrectGuesses.push(guess.id);
+    return turn.giveFeedback();
   }
   calculatePercentCorrect() {
-
+    return Math.floor((1 - (this.incorrectGuesses.length/this.turns)) * 100);
   }
   endRound() {
-    
+    return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,20 +1,22 @@
 const assert = require('chai').assert;
 const Round = require('../src/Round.js');
 const Card = require('../src/Card.js');
+const Turn = require('../src/Turn.js');
 const Deck = require('../src/Deck.js');
 
 describe('Round', function() {
-  // let card1;
-  // let card2;
-  // let card3;
-  // let deck;
-  // let round;
+  let card1;
+  let card2;
+  let card3;
+  let deck;
+  let round;
+  let turn;
   beforeEach(function() {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
   })
 
   it('should be a function', function() {
@@ -25,19 +27,48 @@ describe('Round', function() {
     assert.instanceOf(round, Round);
   })
 
+  it('should hold a deck of cards', function() {
+    assert.equal(round.deck.cards, deck.cards);
+  })
 
+  it('should show the card being played', function() {
+    let cardPlayed = round.returnCurrentCard();
+
+    assert.equal(cardPlayed, card1);
+  })
+
+  it('should start at zero turns', function() {
+    assert.equal(round.turns, 0);
+  })
+
+  it('should be able to take a turn', function() {
+    let guess = "pug";
+
+    let myTurn = round.takeTurn(guess);
+
+    assert.equal(round.turns, 1);
+    assert.equal(round.currentCard, deck.cards[1]);
+    assert.equal(round.incorrectGuesses[0], guess.id);
+    assert.equal(myTurn, "incorrect!");
+  })
+
+  it('should calculate the percentage of correct guesses', function() {
+    round.takeTurn("capybara");
+    round.takeTurn("appendix");
+    round.takeTurn("playing with bubble wrap");
+
+    let percentCorrect = round.calculatePercentCorrect();
+
+    assert.equal(percentCorrect, 33);
+  })
+
+  it('should announce the end of a round', function() {
+    round.takeTurn("capybara");
+    round.takeTurn("appendix");
+    round.takeTurn("playing with bubble wrap");
+
+    let completedRound = round.endRound();
+
+    assert.equal(completedRound, "** Round over! ** You answered 33% of the questions correctly!")
+  })
 })
-
-// Your Round class will be the object that takes in responses and records these guesses (as well as if they are correct or incorrect). The currentCard should be the first Card in the Deck (the array of Cards) at the start of the Round
-//
-// Your Round class should meet the following requirements:
-// returnCurrentCard: method that returns the current card being played
-// takeTurn: method that updates turns count, evaluates guesses, gives feedback, and stores ids of incorrect guesses
-//
-// When a guess is made, a new Turn instance is created.
-// The turns count is updated, regardless of whether the guess is correct or incorrect
-// The next card becomes current card
-// Guess is evaluated/recorded. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses
-// Feedback is returned regarding whether the guess is incorrect or correct
-// calculatePercentCorrect: method that calculates and returns the percentage of correct guesses
-// endRound: method that prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!’
