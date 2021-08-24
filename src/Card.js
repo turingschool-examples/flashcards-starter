3b66f18d2f83fce86a5edd569be3fb0b0ddd166a
Anna Johnson <ae.johnson2931@gmail.com>
2021-08-23 19:24:02 -0700

    Add card file and card-test file

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+const Game = require('./Game');
+
+class Card {
+  constructor(id, question, answers, correctAnswer) {
+    this.id = id;
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  }
+}
+
+module.exports = Card;
