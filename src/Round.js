0c8edce7c06df964370aa8c427c3fda4b9cf75e9
Renee Swick-Ziller <reneeswick@gmail.com>
2021-08-24 21:17:54 -0600

    Add functionality to the the takeTurn method

--- src/Round.js
+++ src/Round.js
@@ -1,14 +1,32 @@
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
+const Turn = require('../src/Turn');
 
 class Round {
-  constructor(deck, turns){
+  constructor(deck){
     this.deck = deck;
     this.turns = 0;
     this.incorrectGuesses = [];
+    this.correctGuesses = [];
   }
   returnCurrentCard(){
     return this.deck.cardsInDeck[0];
+  }
+
+  takeTurn(guess){
+    this.turns++;
+    let cardInPlay = this.returnCurrentCard();
+    let turn1 = new Turn(guess, cardInPlay);
+    turn1.evaluateGuess();
+
+    if(turn1.evaluateGuess() === false){
+      this.incorrectGuesses.push(cardInPlay.id);
+      this.deck.cardsInDeck.splice(0, 1);
+      return turn1.giveFeedback();
+    }else{
+      this.deck.cardsInDeck.splice(0, 1);
+      return turn1.giveFeedback();
+    }
   }
 };
 
