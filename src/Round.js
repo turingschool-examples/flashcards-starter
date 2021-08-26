1638a5f696df5ee96d84bbc53c6c25c7b8a3749f
Renee Swick-Ziller <reneeswick@gmail.com>
2021-08-26 12:20:17 -0600

    Add calculatePercentCorrect and endRound methods

--- src/Round.js
+++ src/Round.js
@@ -8,8 +8,9 @@ class Round {
     this.turns = 0;
     this.incorrectGuesses = [];
     this.currentCard = this.deck.cardsInDeck.find(card => !card.hasBeenPlayed);
-    this.cardInPlay = this.returnCurrentCard();
+    this.cardInPlay = {};
     this.feedback = '';
+    this.endRoundMsg = '';
   }
   returnCurrentCard(){
     this.currentCard.hasBeenPlayed = true;
@@ -17,17 +18,32 @@ class Round {
   }
 
   takeTurn(guess){
+    this.cardInPlay = this.returnCurrentCard();
     this.turns++;
     let turn1 = new Turn(guess, this.cardInPlay);
-    this.currentCard = this.deck.cardsInDeck.find(card => !card.hasBeenPlayed);
     this.feedback = turn1.giveFeedback();
 
     if(this.feedback === 'Incorrect!'){
       this.incorrectGuesses.push(this.cardInPlay.id)
+      this.currentCard = this.deck.cardsInDeck.find(card => !card.hasBeenPlayed);
       return this.feedback;
     }else if(this.feedback === 'Correct!'){
+      this.currentCard = this.deck.cardsInDeck.find(card => !card.hasBeenPlayed);
       return this.feedback;
     }
+  }
+
+  calculatePercentCorrect(){
+    let numCorrect = (this.deck.cardsInDeck.length) - (this.incorrectGuesses.length);
+    let decimalCorrect = numCorrect / this.deck.cardsInDeck.length;
+    let roundedPercent = Math.round((decimalCorrect) * 100);
+    return roundedPercent;
+  }
+
+  endRound(){
+    this.endRoundMsg = '';
+    let percentCorrect = this.calculatePercentCorrect();
+    this.endRoundMsg = console.log(`**Round over!** You answered ${percentCorrect}% of the questions correctly!`);
   }
 };
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -88,6 +88,61 @@ describe('Round', function(){
 
     round1.takeTurn('sea otter');
     expect(round1.currentCard).to.equal(card2);
+  });
 
+  it('should calculate the percentage of correct guesses', function(){
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck1 = new Deck([card1, card2, card3]);
+    const round1 = new Round(deck1);
+
+    round1.takeTurn('pug');
+    round1.takeTurn('gallbladder');
+    round1.takeTurn('Fitzgerald');
+    round1.calculatePercentCorrect();
+    expect(round1.calculatePercentCorrect()).to.equal(67);
+
+    const card4 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card5 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card6 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck2 = new Deck([card4, card5, card6]);
+    const round2 = new Round(deck2);
+
+    round2.takeTurn('sea otter');
+    round2.takeTurn('gallbladder');
+    round2.takeTurn('Fitzgerald');
+    round2.calculatePercentCorrect();
+    expect(round2.calculatePercentCorrect()).to.equal(100);
+  });
+
+  it('should print feedback to the console when the round is over', function(){
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck1 = new Deck([card1, card2, card3]);
+    const round1 = new Round(deck1);
+
+    round1.takeTurn('pug');
+    round1.takeTurn('gallbladder');
+    round1.takeTurn('Fitzgerald');
+    round1.endRound();
+    expect(round1.endRound()).to.equal(round1.endRoundMsg);
+
+    const card4 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card5 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card6 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck2 = new Deck([card4, card5, card6]);
+    const round2 = new Round(deck2);
+
+    round2.takeTurn('sea otter');
+    round2.takeTurn('gallbladder');
+    round2.takeTurn('Fitzgerald');
+    round2.endRound();
+    expect(round2.endRound()).to.equal(round2.endRoundMsg);
   });
 });
