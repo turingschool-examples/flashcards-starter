f4d3da51f277f8cdcc0a5644f8ba56a7bb4dbf34
Anna Johnson <ae.johnson2931@gmail.com>
2021-08-26 11:27:18 -0700

    Add started Game files

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,10 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('./Card');
+const Deck = require('./Deck');
+const Turn = require('./Turn');
+const Round = require('./Round');
 
 class Game {
   constructor() {}
@@ -13,6 +17,29 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
+
+  start() {
+// Creates Cards
+// Puts Cards in a Deck
+// Creates a new Round using the Deck
+// invokes printMessage to display the message in the CLI
+// invokes printQuestion to kick off our helper functions
+// that allow interaction via the CLI
+  }
+
+  currentRound() {
+
+  }
 }
 
 module.exports = Game;
+
+// Your Game class should meet these other requirements:
+// Should keep track of the currentRound
+// start: method that starts everything
+// Creates Cards
+// Puts Cards in a Deck
+// Creates a new Round using the Deck
+// invokes printMessage to display the message in the CLI
+// invokes printQuestion to kick off our helper functions that allow interaction via the CLI
+// Note: The helper functions are all fleshed out and fit to work with classes/methods that meet the requirements in the past iterations.
