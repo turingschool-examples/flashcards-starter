386168bb53bf785217158f5330a22bf47fe78049
Anna Johnson <ae.johnson2931@gmail.com>
2021-08-26 11:24:35 -0700

    Add Card files with refactored code and passing tests

--- src/Card.js
+++ src/Card.js
@@ -8,5 +8,14 @@ class Card {
     this.correctAnswer = correctAnswer;
   }
 }
+//
+// class Card {
+//   constructor(currentCard) {
+//     this.id = currentCard.id;
+//     this.question = currentCard.question;
+//     this.answers = currentCard.answers;
+//     this.correctAnswer = currentCard.correctAnswer;
+//   }
+// }
 
 module.exports = Card;
