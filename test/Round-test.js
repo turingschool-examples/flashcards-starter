0429e6c4c0035f2d36aad5b5fb31d3942d72d2a3
William McGuire <wmcguire18@gmail.com>
2021-08-26 17:54:04 -0400

    Test refactor

---
+++ test/Round-test.js
@@ -0,0 +1,39 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
+describe('Round', function() {
+
+  let card1, card2, card3, cards, deck, round;
+
+  beforeEach(function() {
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = new Card(2, 'what array method executes a provided function once for each array element?', ['map', 'forEach', 'filter'], 'forEach');
+    card3 = new Card(3, 'what array method creates a new array with all elements that pass the test implemented by the provided function?', ['for loop', 'filter', 'if statement'], 'filter');
+    cards = [card1, card2, card3]
+    deck = new Deck(cards);
+    round = new Round(deck);
+  });
+
+  it('should return current card', function() {
+
+    expect(round.returnCurrentCard()).to.equal(card1)
+  })
+
+  it('should update turns count', function() {
+
+  expect(round.turns).to.equal(0);
+  round.takeTurn("1");
+  expect(round.turns).to.equal(1);
+})
+
+it('should update correct or incorrect guesses', function() {
+
+  expect(round.takeTurn('function')).to.equal('function is incorrect!');
+  })
+
+
+});
