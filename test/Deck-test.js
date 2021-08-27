af1c310d0eed86c43050a43a3261546101a10060
Anna Johnson <ae.johnson2931@gmail.com>
2021-08-26 18:22:44 -0700

    Add refactored tests with before each hooks

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -5,26 +5,23 @@ const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 
 describe('Deck', function() {
+  beforeEach(function() {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    currentDeck = [card1, card2, card3];
+    deck = new Deck(currentDeck);
+});
 
   it('should be a function', () => {
     expect(Deck).to.be.a('function');
   });
 
   it('should have a deck of cards', () => {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    let currentDeck = [card1, card2, card3];
-    let deck = new Deck(currentDeck);
     expect(deck.currentDeck).to.equal(currentDeck);
   });
 
   it('should know how many cards are in the deck', () => {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    let currentDeck = [card1, card2, card3];
-    let deck = new Deck(currentDeck);
     expect(deck.countCards()).to.equal(3);
   });
 });
