67af0b6dc25c0af9dd19894ec78a3aa87b527373
jose Tovar <jose.tovar102397@gmail.com>
2021-10-12 15:58:33 -0600

    Create test to make a guess

---
+++ test/Turn-test.js
@@ -0,0 +1,19 @@
+const expect = require('chai').expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should make a guess from current card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+    
+    expect(turn.guess).to.equal('array');
+    expect(turn.currentCard).to.be.an.instanceOf(Card);
+  });
+});
