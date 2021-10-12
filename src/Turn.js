437f44a0ffe14dfb0869e77fd972f16a4e25c707
John Kiernan <jkiernan12@yahoo.com>
2021-10-12 09:07:59 -0400

    Add Turn class

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,27 @@
 class Turn {
-  constructor() {
+  constructor(guess, card) {
+    this.guess = guess;
+    this.currentCard = card;
+  }
 
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.currentCard;
+  }
+
+  evaluateGuess() {
+    return (this.guess === this.currentCard.correctAnswer);
+  }
+
+  giveFeedback() {
+    if (this.evaluateGuess()) {
+      return "correct!";
+    } else {
+      return "incorrect!";
+    }
   }
 }
 
