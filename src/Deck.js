3a6443ea7e51f6e822d1198405286ac24f86461f
Colgan Meanor <colganmeanor7@gmail.com>
2021-10-12 00:04:30 -0500

    Make progress on deck.js

---
+++ src/Deck.js
@@ -0,0 +1,10 @@
+const Card = require('../src/Card');
+
+class Deck{
+  constructor(cards){
+    this.cards = cards;
+  }
+}
+
+
+module.exports = Deck
---
+++ test/Deck-test.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck')
+const Card = require('../src/Card');
+
+describe('Deck', function() {
+
+  it('should be a function', function() {
+    expect(Deck).to.be.a('function')
+  })
+
+  it('should be able to have cards', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const cards = [card1, card2, card3]
+
+    const deck = new Deck(cards)
+    expect(deck.cards).to.equal(cards)
+  })
+
+  it('should be able to count cards', function(){
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const cards = [card1, card2, card3]
+
+    const deck = new Deck(cards)
+    // need to write test and implementation code that returns
+    // the length of the deck.cards array  
+})
+
+})
