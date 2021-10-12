4430a7b190633962a37200cfa257d4474c605cab
John Kiernan <jkiernan12@yahoo.com>
2021-10-12 18:51:45 -0400

    Add Round test suite

---
+++ test/Round-test.js
@@ -0,0 +1,78 @@
+const chai = require("chai");
+const Card = require("../src/Card.js");
+const Turn = require("../src/Turn.js");
+const Deck = require("../src/Deck.js");
+const Round = require("../src/Round.js");
+const expect = chai.expect;
+
+describe("Round", () => {
+
+  const card1 = new Card(1, "What is my fav food?", ["rice", "reeses"], "rice");
+  const card2 = new Card(2, "What is my name?", ["Blohn", "Grohn", "John"], "John");
+  const card3 = new Card(3, "What is my fav animal?", ["koala", "panda", "sloth"], "sloth");
+  const deck = new Deck([card1, card2, card3]);
+  const round = new Round(deck);
+
+  beforeEach(function() {
+    const card1 = new Card(1, "What is my fav food?", ["rice", "reeses"], "rice");
+    const card2 = new Card(2, "What is my name?", ["Blohn", "Grohn", "John"], "John");
+    const card3 = new Card(3, "What is my fav animal?", ["koala", "panda", "sloth"], "sloth");
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+  })
+
+  it("should be a function", () => {
+    expect(Round).to.be.a("function");
+  });
+
+  it("should instantiate an instance of Round", () => {
+    expect(round).to.be.an.instanceOf(Round);
+  });
+
+  it("should contain a deck", () => {
+    expect(round.deck).to.be.an.instanceOf(Deck);
+  });
+
+  it("should return the current card being played and update after a turn", () => {
+    let currentCard = round.returnCurrentCard();
+    expect(currentCard).to.equal(card1);
+
+    round.takeTurn();
+    currentCard = round.returnCurrentCard();
+    expect(currentCard).to.equal(card2);
+  });
+  
+  it("should create a new Turn instance when a guess is made", () => {
+    expect(round.currentTurn).to.be.an.instanceOf(Turn);
+  });
+
+  it("should update turns", () => {
+    expect(round.turns).to.equal(0);
+    round.takeTurn("rice");
+    expect(round.turns).to.equal(1);
+  });
+
+  it("should update the current card to the next card", () => {
+    expect(round.incorrectGuesses).to.equal(0);
+    round.takeTurn("plogl");
+    expect(round.incorrectGuesses.length).to.equal(1);
+    expect(round.incorrectGuesses[0]).to.equal(1);
+  });
+
+  it("should provide feedback depending on if you're right or wrong", () => {
+    expect(round.takeTurn("pie")).to.equal("incorrect!");
+    expect(round.takeTurn("John")).to.equal("correct!");
+  });
+
+  it("should calculate the percent correct", () => {
+    round.takeTurn("pie");
+    round.takeTurn("John");
+    expect(round.calculatePercentCorrect()).to.equal(50);
+  });
+
+  it("should be able to end round", () => {
+    round.takeTurn("pie");
+    round.takeTurn("John");
+    expect(round.endRound()).to.equal("** Round over! ** You answered 50% of the questions correctly!");
+  });
+});
