ad8936096d651b453a2fff937c7e979711e3205c
Colgan Meanor <colganmeanor7@gmail.com>
2021-10-11 20:18:31 -0500

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+class Card{
+  constructor(id, question, answers, correct){
+    this.id = Date.now();
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correct;
+  }
+}
+
+
+
+
+
+
+module.exports = Card
