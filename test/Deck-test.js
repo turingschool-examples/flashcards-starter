8419a2af570a970b3b131c8938a5c94c021891ec
John Kiernan <jkiernan12@yahoo.com>
2021-10-12 09:49:19 -0400

    Create Deck test suite

---
+++ test/Deck-test.js
@@ -0,0 +1,28 @@
+const chai = require("chai");
+const Card = require("../src/Card.js");
+const Deck= require("../src/Deck.js");
+const expect = chai.expect;
+
+describe("Deck", () => {
+  const card1 = new Card(1, "What is my fav food?", ["rice", "reeses"], "rice");
+  const card2 = new Card(2, "What is my name?", ["Blohn", "Grohn", "John"], "John");
+  const card3 = new Card(3, "What is my fav animal?", ["koala", "panda", "sloth"], "sloth");
+  const deck = new Deck(card1, card2, card3);
+
+  it("should be a function", () => {
+    expect(deck).to.be.a("function");
+  });
+
+  it("should contain cards", () => {
+    expect(deck.cards).to.be.a("array");
+  });
+
+  it("should have instances of Card in its cards", () => {
+    expect(deck.cards[0].to.be.an.instanceOf(Card));
+  });
+
+  it("should be able to return the deck length", () => {
+    const cardTotal = deck.countCards();
+    expect(cardTotal).to.equal(3);
+  });
+})
