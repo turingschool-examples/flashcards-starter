83e9aea202cad4ce99b111425ec0766082dbd32a
August Reid <c.august.reid@gmail.com>
2021-10-13 17:55:18 -0600

    Add tests for currentCard and turn counter

--- test/round-test.js
+++ test/round-test.js
@@ -28,6 +28,41 @@ describe("Round", function() {
     let round = new Round(deck);
     expect(round.deck).to.equal(deck);
   });
+
+  it("should have a current card", function() {
+    let card1 = new Card(1, "What's up?", ["the sky", "not much", "chicken butt"], "chicken butt");
+    let card2 = new Card(2, "Who's the cutest?", ["Percy", "Rufus", "Desilu"], "Desilu");
+    let card3 = new Card(3, "Worst food ever?", ["olives", "pretzels", "pickles"], "olives");
+    let card4 = new Card(4, "Best food ever?", ["cheese", "pickles", "cheese and pickles"], "cheese and pickles");
+    let cards = [card1, card2, card3, card4];
+    let deck = new Deck(cards);
+    let round = new Round(deck);
+    expect(round.currentCard).to.equal(card1);
+  });
+
+  it.skip("should have a turn counter with default value 0", function() {
+    let round = new Round();
+    expect(round.turns).to.equal(0);
+  });
+
+  it.skip("should store incorrect guesses", function() {
+    let round = new Round();
+    expect(round.incorrectGuesses).to.equal([]);
+  });
+
+  it.skip("should return the current card", function() {
+    let card1 = new Card(1, "What's up?", ["the sky", "not much", "chicken butt"], "chicken butt");
+    let card2 = new Card(2, "Who's the cutest?", ["Percy", "Rufus", "Desilu"], "Desilu");
+    let card3 = new Card(3, "Worst food ever?", ["olives", "pretzels", "pickles"], "olives");
+    let card4 = new Card(4, "Best food ever?", ["cheese", "pickles", "cheese and pickles"], "cheese and pickles");
+    let cards = [card1, card2, card3, card4];
+    let deck = new Deck(cards);
+    let round = new Round(deck);
+    expect(round.currentCard).to.equal(card1);
+    expect(round.returnCurrentCard).to.equal(card1);
+  })
+
+
 })
 
 // Your Round class will be the object that takes in responses and records these guesses (as well as if they are correct or incorrect). The currentCard should be the first Card in the Deck (the array of Cards) at the start of the Round
