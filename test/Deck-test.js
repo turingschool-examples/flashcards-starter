c3fcb16eac9a7dd542c281bb6dce74a418eb7161
jose Tovar <jose.tovar102397@gmail.com>
2021-10-12 21:41:29 -0600

    Create test to check for number of cards in deck

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -17,4 +17,16 @@ describe('Deck', function() {
 
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
+
+  it('should know the number of cards in the deck', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const numberOfCards = deck.countCards();
+
+    expect(numberOfCards).to.equal(3);
+  });
 });
