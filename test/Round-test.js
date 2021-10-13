42a54334120d6c63d275aca30cc97505f7f298f7
jose Tovar <jose.tovar102397@gmail.com>
2021-10-13 14:24:10 -0600

    Add test to have turns in a round

--- test/Round-test.js
+++ test/Round-test.js
@@ -17,7 +17,7 @@ describe('Round', function() {
 
     const deck = new Deck([card1, card2, card3]);
 
-    const round = new Round(deck.cards);
+    const round = new Round(deck);
 
     expect(round.deck).to.deep.equal([card1, card2, card3]);
   });
@@ -29,10 +29,22 @@ describe('Round', function() {
 
     const deck = new Deck([card1, card2, card3]);
 
-    const round = new Round(deck.cards);
+    const round = new Round(deck);
 
     const currentCard = round.returnCurrentCard();
 
     expect(currentCard).to.equal(card1)
+  });
+  
+  it('should start with 0 turns', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+
+    expect(round.turns).to.equal(0);
   });
 });
