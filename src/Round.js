aac2b73ce00f222371c791f301ec82a9a84d7017
Kyra Bergsund <k.bergsund@gmail.com>
2021-10-12 19:02:33 -0700

    Complete Round class and tests

---
+++ src/Round.js
@@ -0,0 +1,37 @@
+const Turn = require('./Turn');
+
+class Round {
+  constructor(deck) {
+    this.deck = deck;
+    this.turns = 0;
+    this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard() {
+    return this.deck.cards[this.turns];
+  }
+
+  takeTurn(userGuess) {
+    const turn = new Turn(userGuess, this.returnCurrentCard());
+    this.turns += 1;
+    this.returnCurrentCard();
+    const message = turn.giveFeedback();
+    if(message === 'incorrect!') {
+      this.incorrectGuesses.push(turn.currentCard.id);
+    }
+    return message;
+  }
+
+  calculatePercentCorrect() {
+    const percent = (this.incorrectGuesses.length / this.turns) * 100;
+    return percent;
+  }
+
+  endRound() {
+    const endMessage = `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`;
+    console.log(endMessage);
+    return endMessage;
+  }
+}
+
+module.exports = Round;
