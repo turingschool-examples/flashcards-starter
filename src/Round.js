4eb9cb441e97285364eb3a41fc9ffcbec976db38
jose Tovar <jose.tovar102397@gmail.com>
2021-10-13 14:29:59 -0600

    Add properties and a methods to round class

---
+++ src/Round.js
@@ -0,0 +1,13 @@
+class Round {
+  constructor(deck) {
+    this.deck = deck.cards;
+    this.turns = 0;
+    this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard() {
+    return this.deck[0];
+  }
+}
+
+module.exports = Round;
