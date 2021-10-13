82a05f3f811e3fd87ed0356fb948d3309c2cb0b2
August Reid <c.august.reid@gmail.com>
2021-10-12 21:16:57 -0600

    Create Turn and pass tests through returnGuess

--- test/turn-test.js
+++ test/turn-test.js
@@ -1,42 +1,44 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const Turn = require("../src/Turn");
-const Card = require("../src/Card");
+const Turn = require("../src/Turn.js");
+const Card = require("../src/Card.js");
 
 describe("Turn", function() {
 
     it("should be a function", function() {
       let turn = new Turn();
-      expect(turn).to.be.a("function");
+      expect(Turn).to.be.a("function");
     });
 
-    it.skip("should be an instance of Turn", function() {
+    it("should be an instance of Turn", function() {
       let turn = new Turn();
       expect(turn).to.be.an.instanceof(Turn);
     });
 
-    it.skip("should have a player guess", function() {
+    it("should have a player guess", function() {
       let card = new Card(1, "What's up?", ["the sky", "not much", "chicken butt"], "chicken butt");
       let turn = new Turn("not much", card);
       expect(turn.guess).to.equal("not much");
     });
 
-    it.skip("should have a card", function() {
+    it("should have a card", function() {
       let card = new Card(1, "What's up?", ["the sky", "not much", "chicken butt"], "chicken butt");
       let turn = new Turn("not much", card);
       expect(turn.card).to.equal(card);
     });
 
-    it.skip("should be able to return the guess", function() {
+    it("should be able to return the guess", function() {
       let card = new Card(1, "What's up?", ["the sky", "not much", "chicken butt"], "chicken butt");
       let turn = new Turn("not much", card);
-      expect(turn.returnGuess).to.equal("not much");
+      turn.returnGuess();
+      expect(turn.returnGuess()).to.equal("not much");
     });
 
     it.skip("should be able to return the card", function() {
       let card = new Card(1, "What's up?", ["the sky", "not much", "chicken butt"], "chicken butt");
       let turn = new Turn("not much", card);
+      turn.returnCard();
       expect(turn.returnCard).to.equal({id: 1, question: "What's up?", answers: ["the sky", "not much", "chicken butt"], correctAnswer: "chicken butt"})
     });
 
