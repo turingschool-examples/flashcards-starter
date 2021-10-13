8480c52231e0fdd1c6d14979798a800dd9c46a01
jose Tovar <jose.tovar102397@gmail.com>
2021-10-12 19:19:46 -0600

    Refactor expect statement to return card

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -30,7 +30,9 @@ describe('Turn', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('array', card);
  
-    expect(turn.returnCard()).to.equal(card);
+    const returnedCard = turn.returnCard();
+    
+    expect(returnedCard).to.equal(card);
   });
 
   it.skip('should evaluate if guess is correct', function() {
