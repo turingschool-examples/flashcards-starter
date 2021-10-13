94eac09a6e044448f96c506685af2f3ad78e49b4
jose Tovar <jose.tovar102397@gmail.com>
2021-10-13 14:30:51 -0600

    Add test to store incorrect guesses

--- test/Round-test.js
+++ test/Round-test.js
@@ -47,4 +47,16 @@ describe('Round', function() {
 
     expect(round.turns).to.equal(0);
   });
+
+  it('should be able to store incorrect guesses', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+
+    expect(round.incorrectGuesses).to.deep.equal([]);
+  });
 });
