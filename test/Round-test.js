8818f86ecd23201e8bbf75c1a368f4d946eadfe3
Joan Rasmussen <raz.joan@gmail.com>
2021-10-12 19:45:19 -0600

    Create files and initialization tests for Round

---
+++ test/Round-test.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Round', function() {
+
+  it.skip('should be a function', function() {
+    expect(Round).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Round', function() {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+  it.skip('should store the deck of cards in play', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.deck).to.deep.equal(deck);
+  });
+
+  it.skip('should store the first card in the deck as the current card', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.currentCard).to.deep.equal(card1);
+  });
+});
