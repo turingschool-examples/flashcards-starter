d974f8999540922833ff096be5c13485b9979caf
August Reid <c.august.reid@gmail.com>
2021-10-12 22:40:44 -0600

    Pass deck tests

--- test/deck-test.js
+++ test/deck-test.js
@@ -11,12 +11,12 @@ describe("Deck", function() {
     expect(Deck).to.be.a("function");
   });
 
-  it.skip("should be an instance of Deck", function() {
+  it("should be an instance of Deck", function() {
     let deck = new Deck();
     expect(deck).to.be.a.instanceof(Deck);
   });
 
-  it.skip("should contain cards", function() {
+  it("should contain cards", function() {
     let card1 = new Card(1, "What's up?", ["the sky", "not much", "chicken butt"], "chicken butt");
     let card2 = new Card(2, "Who's the cutest?", ["Percy", "Rufus", "Desilu"], "Desilu");
     let card3 = new Card(3, "Worst food ever?", ["olives", "pretzels", "pickles"], "olives");
@@ -26,7 +26,7 @@ describe("Deck", function() {
     expect(deck.cards).to.equal(cards);
   });
 
-  it.skip("should know how many cards are in the deck", function() {
+  it("should know how many cards are in the deck", function() {
     let card1 = new Card(1, "What's up?", ["the sky", "not much", "chicken butt"], "chicken butt");
     let card2 = new Card(2, "Who's the cutest?", ["Percy", "Rufus", "Desilu"], "Desilu");
     let card3 = new Card(3, "Worst food ever?", ["olives", "pretzels", "pickles"], "olives");
