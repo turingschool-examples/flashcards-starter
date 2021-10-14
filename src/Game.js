c1a418f48f2023ac14deba63152b80a3a3625304
Joan Rasmussen <raz.joan@gmail.com>
2021-10-13 20:24:02 -0600

    Test Game-tests and add start method and currentRound prop to Game class

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,14 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +18,17 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start() {
+    let cards = prototypeQuestions.map(function() {
+      const card = new Card(element["id"], element["question"], element["answers"], element["correctAnswer"]);
+    });
+    let deck = new Deck(cards);
+    let round = new Round(deck);
+    this.currentRound = round;
+    this.printMessage();
+    this.printQuestion();
   }
 }
 
