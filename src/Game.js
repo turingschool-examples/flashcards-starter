e9f140b6c7daa76a430ac846ef8998a00027271c
jose Tovar <jose.tovar102397@gmail.com>
2021-10-14 16:19:16 -0600

    Add start method for game class

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,36 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('./Card');
+const Deck = require('./Deck');
+const Round = require('./Round');
 
 class Game {
   constructor() {}
 
-  printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+  start() {
+    const cards = prototypeQuestions.map((question) => {
+      const card = new Card(
+        question.id, 
+        question.question, 
+        question.answers, 
+        question.correctAnswer
+      );
+      return card;
+    });
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    this.printMessage(deck);
+    this.printQuestion(round);
+  }
+
+  printMessage(deck) {
+    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
 
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
   }
 }
 
