e9917b1c560090095454a1941bb9d15695efbd8b
jose Tovar <jose.tovar102397@gmail.com>
2021-10-13 18:18:30 -0600

    Add test to update turns in round class

--- test/Round-test.js
+++ test/Round-test.js
@@ -71,7 +71,6 @@ describe('Round', function() {
     const turn = round.takeTurn('object');
 
     expect(turn).to.equal('correct!');
-
   });
 
   it('should be able to give feedback if guess is incorrect', function() {
@@ -85,6 +84,18 @@ describe('Round', function() {
     const turn = round.takeTurn('array');
 
     expect(turn).to.equal('incorrect!');
+  });
 
+  it('should update turns everytime it makes a guess', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+    round.takeTurn('object');
+
+    expect(round.turns).to.equal(1);
   });
 });
