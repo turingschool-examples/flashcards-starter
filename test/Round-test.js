3078ed17053141e9904ce5c5cd9f7ad472db5366
jose Tovar <jose.tovar102397@gmail.com>
2021-10-13 18:40:51 -0600

    Add test to update incorrect guesses

--- test/Round-test.js
+++ test/Round-test.js
@@ -98,4 +98,19 @@ describe('Round', function() {
 
     expect(round.turns).to.equal(1);
   });
+
+  it('should update incorrect guesses', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+    round.takeTurn('array');
+    round.takeTurn('object');
+
+    expect(round.incorrectGuesses.length).to.equal(2)
+    expect(round.incorrectGuesses).to.deep.equal([1, 2])
+  });
 });
