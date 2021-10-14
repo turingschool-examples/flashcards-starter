957873242804cbc856afdb8e5a8d90c9199c2faf
Joan Rasmussen <raz.joan@gmail.com>
2021-10-13 20:06:08 -0600

    Add tests for Game class

---
+++ test/Game-test.js
@@ -0,0 +1,32 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Game = require('../src/Game');
+
+describe('Game', function() {
+
+  it.skip('should be an instance of Game', function() {
+    const game = new Game();
+    expect(game).to.be.an.instanceof(Game);
+  });
+
+  it.skip('printMessage should be a method on Game', function() {
+    const game = new Game();
+    expect(game.printMessage).to.be.a('function');
+  });
+
+  it.skip('printQuestion should be a method on Game', function() {
+    const game = new Game();
+    expect(game.printQuestion).to.be.a('function');
+  });
+
+  it.skip('should keep track of the currentRound, which is empty initially', function() {
+    const game = new Game();
+    expect(game.currentRound).to.deep.equal(undefined);
+  });
+
+  it.skip('start should be a method on Game', function() {
+    const game = new Game();
+    expect(game.start).to.be.a('function');
+  });
+});
