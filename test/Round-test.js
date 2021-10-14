1dbd2c845d3349397169ca6f579dc51f65ca1659
jose Tovar <jose.tovar102397@gmail.com>
2021-10-13 18:57:14 -0600

    Test to return card after taking a turn

--- test/Round-test.js
+++ test/Round-test.js
@@ -113,4 +113,18 @@ describe('Round', function() {
     expect(round.incorrectGuesses.length).to.equal(2)
     expect(round.incorrectGuesses).to.deep.equal([1, 2])
   });
+
+  it('should return the current card after taking a turn', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+
+    const deck = new Deck([card1, card2, card3]);
+    
+    const round = new Round(deck);
+    round.takeTurn('object');
+    const cardInPlay = round.returnCurrentCard();
+
+    expect(cardInPlay).to.deep.equal(card2);
+  });
 });
