42d1903d840c6f4bdf31498e3f35f9a0371ecfc8
Kai Kuller <kavakai@gmail.com>
2021-11-29 15:28:26 -0700

    Pass card tests

--- test/Card-test.js
+++ test/Card-test.js
@@ -10,22 +10,22 @@ describe('Card', function() {
     expect(Card).to.be.a('function');
   });
 
-  it.skip('should be an instance of Card', function() {
+  it('should be an instance of Card', function() {
     const card = new Card();
     expect(card).to.be.an.instanceof(Card);
   }); 
 
-  it.skip('should store a question', function() {
+  it('should store a question', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
   });  
 
-  it.skip('should store a list of possible answers', function() {
+  it('should store a list of possible answers', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
   });  
 
-  it.skip('should store the correct answer', function() {
+  it('should store the correct answer', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.correctAnswer).to.equal('object');
   });
