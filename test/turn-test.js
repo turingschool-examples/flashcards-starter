9bea3fd5d9977ad8d45895d80972b38415defefd
Cesare Gallo <cesaregallo0@gmail.com>
2021-11-29 21:51:39 -0700

    Add guess argument

--- test/turn-test.js
+++ test/turn-test.js
@@ -1,12 +1,25 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('../src/Turn');
+const Turn = require('../src/Turn.js');
+const Card = require('../src/Card.js');
 
 describe( 'Turn', () => {
-    
+  const nextCard = new Card(1, 'What is Teddy\'s favorite color?', ['green', 'purple', 'blue'], 'green');
+  const turn = new Turn('green', nextCard);
+  const wrongTurn = new Turn('purple', nextCard);
+
+
    it('should be a function', () => {
         expect(Turn).to.be.a('function');
     })
+
+   it('should be an instance of Turn', () => {
+       expect(turn).to.be.an.instanceOf(Turn);
+   })
+
+   it('should store a guess', () => {
+       expect(turn.guess).to.equal('green');
+   })
 
 })
