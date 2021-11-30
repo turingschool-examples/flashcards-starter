305472fe2edd2121bc55b3998028d6acb1b47378
Kai Kuller <kavakai@gmail.com>
2021-11-29 21:17:29 -0700

    Add methods to Turn class

--- src/Turn.js
+++ src/Turn.js
@@ -1,5 +1,24 @@
 class Turn {
-    constructor() {
+    constructor(guess, card) {
+        this.guess = guess;
+        this.currentCard = card;
+    };
+
+    returnGuess() {
+        return this.guess;
+    };
+
+    returnCard() {
+        return this.currentCard;
+    };
+
+    evaluateGuess() {
+        switch (this.guess) {
+            case this.currentCard.correctAnswer:
+                return true;
+            default:
+                return false;
+        }
     }
 }
 
