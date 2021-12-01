f933f92000c31726ddf28585a3236eef3aae8599
Kai Kuller <kavakai@gmail.com>
2021-11-30 20:40:29 -0700

    Pass first three tests

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,10 +1,10 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
+const Round = require('../src/Round');
 
 describe("Round", function () {
 
@@ -24,10 +24,10 @@ describe("Round", function () {
         const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
         const deck = new Deck([card1, card2, card3]);
         const round = new Round(deck);
-        expect(round.deck).to.equal([card1, card2, card3]);
+        expect(round.deck).to.deep.equal([card1, card2, card3]);
     });
 
-    it.skip("should be able to show the current card being played", function () {
+    it("should be able to show the current card being played", function () {
         const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
         const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
         const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
