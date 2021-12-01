09a1567b7d1c813e67d2c6841d444fbe38f5aed0
Kai Kuller <kavakai@gmail.com>
2021-11-30 17:31:37 -0700

    Pass all deck tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
-const prototypeData = require('../src/data');
+const cards = require('../src/data');
 
 
 describe("Deck", function () {
@@ -19,7 +19,37 @@ describe("Deck", function () {
     });
 
     it("should have a method to keep track of the cards in the deck", function () {
-        const deck = new Deck([prototypeData]);
-        expect(deck.countCards).to.equal(30);
+        const deck = new Deck([{
+            "id": 1,
+            "question": "What allows you to define a set of related information using key-value pairs?",
+            "answers": ["object", "array", "function"],
+            "correctAnswer": "object"
+        }, {
+            "id": 2,
+            "question": "What is a comma-separated list of related values?",
+            "answers": ["array", "object", "function"],
+            "correctAnswer": "array"
+        }, {
+            "id": 3,
+            "question": "What type of prototype method directly modifies the existing array?",
+            "answers": ["mutator method", "accessor method", "iteration method"],
+            "correctAnswer": "mutator method"
+        }, {
+            "id": 4,
+            "question": "What type of prototype method does not modify the existing array but returns a particular representation of the array?",
+            "answers": ["mutator method", "accessor method", "iteration method"],
+            "correctAnswer": "accessor method"
+        }, {
+            "id": 5,
+            "question": "What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?",
+            "answers": ["mutator method", "accessor method", "iteration method"],
+            "correctAnswer": "iteration method"
+        }, {
+            "id": 6,
+            "question": "What is an example of a mutator method?",
+            "answers": ["sort()", "map()", "join()"],
+            "correctAnswer": "sort()"
+        }]);
+        expect(deck.countCards()).to.equal(6);
     });
 });
