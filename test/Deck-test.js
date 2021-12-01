e02e7094fe2ff609a40d969b420f73d1d9c11c0c
Kai Kuller <kavakai@gmail.com>
2021-11-30 20:10:16 -0700

    Refactor deck test file countcards method

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -19,37 +19,10 @@ describe("Deck", function () {
     });
 
     it("should have a method to keep track of the cards in the deck", function () {
-        const deck = new Deck([{
-            "id": 1,
-            "question": "What allows you to define a set of related information using key-value pairs?",
-            "answers": ["object", "array", "function"],
-            "correctAnswer": "object"
-        }, {
-            "id": 2,
-            "question": "What is a comma-separated list of related values?",
-            "answers": ["array", "object", "function"],
-            "correctAnswer": "array"
-        }, {
-            "id": 3,
-            "question": "What type of prototype method directly modifies the existing array?",
-            "answers": ["mutator method", "accessor method", "iteration method"],
-            "correctAnswer": "mutator method"
-        }, {
-            "id": 4,
-            "question": "What type of prototype method does not modify the existing array but returns a particular representation of the array?",
-            "answers": ["mutator method", "accessor method", "iteration method"],
-            "correctAnswer": "accessor method"
-        }, {
-            "id": 5,
-            "question": "What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?",
-            "answers": ["mutator method", "accessor method", "iteration method"],
-            "correctAnswer": "iteration method"
-        }, {
-            "id": 6,
-            "question": "What is an example of a mutator method?",
-            "answers": ["sort()", "map()", "join()"],
-            "correctAnswer": "sort()"
-        }]);
-        expect(deck.countCards()).to.equal(6);
+        const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+        const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+        const deck = new Deck([card1, card2, card3]);
+        expect(deck.countCards()).to.equal(3);
     });
 });
