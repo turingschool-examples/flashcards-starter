3eb3217bfbbac6426ea89849ed1db4d837da40e5
Kai Kuller <kavakai@gmail.com>
2021-12-01 15:34:33 -0700

    Add endRound test

--- test/Round-test.js
+++ test/Round-test.js
@@ -58,8 +58,8 @@ describe("Round", function () {
         const deck = new Deck([card1, card2, card3]);
         const round = new Round(deck);
 
-        round.takeTurn();
-        round.takeTurn();
+        round.takeTurn("array");
+        round.takeTurn("object");
 
         expect(round.turns).to.equal(2);
     });
@@ -92,8 +92,8 @@ describe("Round", function () {
         round.takeTurn("array");
 
         expect(round.turns).to.equal(2);
-        expect(round.incorrectGuesses).to.equal([1]);
-        expect(round.returnCurrentCard()).to.equal({
+        expect(round.incorrectGuesses).to.deep.equal([1]);
+        expect(round.returnCurrentCard()).to.deep.equal({
             "id": 3,
             "question": "What type of prototype method directly modifies the existing array?",
             "answers": ["mutator method", "accessor method", "iteration method"],
@@ -101,4 +101,27 @@ describe("Round", function () {
         });
         expect(round.calculatePercentCorrect()).to.equal(50);
     });
+
+    it("should end the round and tell you the percentage of correct answers", function () {
+        const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+        const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+        const deck = new Deck([card1, card2, card3]);
+        const round = new Round(deck);
+        
+        round.takeTurn("function");
+        round.takeTurn("array");
+
+        expect(round.turns).to.equal(2);
+        expect(round.incorrectGuesses).to.deep.equal([1]);
+        expect(round.returnCurrentCard()).to.deep.equal({
+            "id": 3,
+            "question": "What type of prototype method directly modifies the existing array?",
+            "answers": ["mutator method", "accessor method", "iteration method"],
+            "correctAnswer": "mutator method"
+        });
+        expect(round.calculatePercentCorrect()).to.equal(50);
+        expect(round.endRound()).to.be.a("function");
+        expect(round.endRound()).to.equal("** Round over! ** You answered 50 % of the questions correctly!");
+    })
 });
