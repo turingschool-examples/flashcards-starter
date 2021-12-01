7f75bb0d1ab831e60babeefcf8f7c640b18f7ed0
Kai Kuller <kavakai@gmail.com>
2021-12-01 16:45:21 -0700

    Build out game functionality

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,22 @@
+const Card = require('./Card');
 const data = require('./data');
+const Deck = require('./Deck');
+const Round = require('./Round');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+  }
+
+  start() {
+    const cards = prototypeQuestions.map(card => {return card = new Card(card.id, card.question, card.answers, card.correctAnswer) });
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -11,8 +24,9 @@ class Game {
   }
 
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
   }
+
 }
 
 module.exports = Game;
