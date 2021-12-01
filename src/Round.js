79611cc9a984643e179e3d25134f99dd7f3bda1c
Kai Kuller <kavakai@gmail.com>
2021-11-30 21:07:47 -0700

    Add methods to round class

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,15 @@
 class Round {
     constructor(deck = {}) {
         this.deck = deck.cards;
+        this.turns = 0;
+        this.incorrectGuesses = [];
+    };
+
+    returnCurrentCard() {
+    };
+
+    takeTurn() {
+        
     }
 }
 
