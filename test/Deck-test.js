d46b20c2f17910e1a84ca1beb338844d57553a1b
Emili Kaiman <emilikaiman@gmail.com>
2022-01-31 18:28:17 -0700

    Create test to count cards

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -19,5 +19,13 @@ describe('Deck', function () {
     expect(deck.thirdCard).to.equal(card3)
   })
 
-  
+  it('should have a method that counts and returns the number of cards in the deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    expect(deck.countCards()).to.equal(3)
+  })
 })
