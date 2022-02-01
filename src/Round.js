cbdc384c5cf534623647ca0d594c3ce286a3b7c8
Jordan Skomal <jskomal@gmail.com>
2022-02-01 15:22:21 -0700

    Implement Round.js functionality

---
+++ src/Round.js
@@ -0,0 +1,32 @@
+const Card = require('../src/Card')
+const Turn = require('../src/Turn')
+const Deck = require('../src/Deck')
+
+class Round {
+  constructor(deck) {
+    this.deck = deck
+    this.turns = 0
+    this.incorrectGuesses = []
+  }
+  returnCurrentCard() {
+    return this.deck.cards[this.turns]
+  }
+  takeTurn(guess) {
+    const currentTurn = new Turn(guess, this.deck.cards[this.turns])
+    currentTurn.evaluateGuess()
+    this.turns++
+    if (currentTurn.isCorrect) {
+      currentTurn.currentCard = this.deck.cards[this.turns]
+      return currentTurn.giveFeedback()
+    } else {
+      this.incorrectGuesses.push(currentTurn.currentCard.id)
+      currentTurn.currentCard = this.deck.cards[this.turns]
+      return currentTurn.giveFeedback()
+    }
+  }
+  calculatePercentCorrect() {
+    return Math.floor(100 * (1 - this.incorrectGuesses.length / this.turns))
+  }
+}
+
+module.exports = Round
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,77 +1,82 @@
 // file dependencies
-const chai = require('chai');
-const expect = chai.expect;
+const chai = require('chai')
+const expect = chai.expect
 
-const Card = require('../src/Card');
-const Turn = require('../src/Turn');
-const Deck = require('../src/Deck');
-const Round = require('../src/Round');
+const Card = require('../src/Card')
+const Turn = require('../src/Turn')
+const Deck = require('../src/Deck')
+const Round = require('../src/Round')
 
 describe('Round Tests', () => {
-  let card1;
-  let card2;
-  let card3;
-  let deck;
-  let round;
+  let card1
+  let card2
+  let card3
+  let deck
+  let round
   beforeEach(() => {
     card1 = new Card(
       1,
       "What is Robbie's favorite animal",
       ['sea otter', 'pug', 'capybara'],
       'sea otter'
-    );
+    )
     card2 = new Card(
       14,
       'What organ is Khalid missing?',
       ['spleen', 'appendix', 'gallbladder'],
       'gallbladder'
-    );
+    )
     card3 = new Card(
       12,
       "What is Travis's favorite stress reliever?",
       ['listening to music', 'watching Netflix', 'playing with bubble wrap'],
       'playing with bubble wrap'
-    );
+    )
 
-    deck = new Deck([card1, card2, card3]);
-    round = new Round(deck);
-  });
+    deck = new Deck([card1, card2, card3])
+    round = new Round(deck)
+  })
 
   // tests
 
   it('should be a function', () => {
-    expect(Round).to.be.a('function');
-  });
+    expect(Round).to.be.a('function')
+  })
 
   it('should instantiate the Round class', () => {
-    expect(round).to.be.an.instanceof(Round);
-  });
+    expect(round).to.be.an.instanceof(Round)
+  })
 
   it("should have a method called 'returnCurrentCard'", () => {
-    expect(round.returnCurrentCard).to.be.a('function');
-  });
+    expect(round.returnCurrentCard).to.be.a('function')
+  })
 
   it('returnCurrentCard should return the current card object', () => {
-    expect(round.returnCurrentCard()).to.equal(deck.cards[round.turns]);
-  });
+    expect(round.returnCurrentCard()).to.equal(deck.cards[round.turns])
+  })
 
   it("should have a method called 'takeTurn'", () => {
-    expect(round.takeTurn).to.be.a('function');
-  });
+    expect(round.takeTurn).to.be.a('function')
+  })
 
   it('takeTurn should end by returning positive feedback if the guess was correct', () => {
-    expect(round.takeTurn('sea otter')).to.equal('correct!');
-  });
+    expect(round.takeTurn('sea otter')).to.equal('correct!')
+  })
 
   it('takeTurn should end by returning negative feedback if the guess was incorrect', () => {
-    expect(round.takeTurn('wrong answer')).to.equal('incorrect!');
-  });
+    expect(round.takeTurn('wrong answer')).to.equal('incorrect!')
+  })
 
   it("should have a method named 'calculatePercentCorrect'", () => {
-    expect(round.calculatePercentCorrect).to.be.a('function');
-  });
+    expect(round.calculatePercentCorrect).to.be.a('function')
+  })
 
   it('calculatePercentCorrect should return the percentage of correct answers', () => {
-    //set u
-  });
-});
+    round.takeTurn('sea otter')
+    expect(round.calculatePercentCorrect()).to.equal(100)
+    round.takeTurn('wrong answer')
+    expect(round.calculatePercentCorrect()).to.equal(50)
+    round.takeTurn('playing with bubble wrap')
+    expect(round.calculatePercentCorrect()).to.equal(66)
+  })
+})
