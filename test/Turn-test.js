459c270f417f08091f2f2edc87ef47dbf11a9580
Jake Dunafon <jlad795@gmail.com>
2022-01-31 18:11:35 -0800

    Add instantiation of Turn

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,9 +1,9 @@
 const chai = require('chai');
 const expect = chai.expect;
-
+const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
-describe('Card', function() {
+describe('Turn', function() {
 
   it('should be a function', function() {
     const turn = new Turn();
@@ -14,4 +14,18 @@ describe('Card', function() {
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
+  it('should instantiate with a users guess to the question', function() {
+    const turn = new Turn('pug');
+    expect(turn.guess).to.equal('pug');
+  });
+
+  it('should also instantiate with a Card object for the current card in play', function() {
+
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+
+    const turn = new Turn('pug', card);
+
+    expect(turn.cardInPlay).to.equal(card);
+  });
+
 });
