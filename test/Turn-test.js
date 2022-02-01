e84869e96a3fc663d8f69d772ac12093efb8a758
Emili Kaiman <emilikaiman@gmail.com>
2022-01-31 17:14:41 -0700

    Create second test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,11 +2,18 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card')
 
 describe('Turn', function() {
 
   it('should take in a guess', function() {
     let firstTurn = new Turn('pig')
     expect(firstTurn.guess).to.equal('pig')
+  })
+
+  it('should take in a new instantiation of Card Class', function() {
+    const card = new Card()
+    let firstTurn = new Turn('pig', card)
+    expect(firstTurn.card).to.equal(card)
   })
 })
