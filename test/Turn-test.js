64a8533c20d537d6b759bfb9bffdd5386786bcd9
Jake Dunafon <jlad795@gmail.com>
2022-01-31 18:25:10 -0800

    Add Turn-test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -28,4 +28,45 @@ describe('Turn', function() {
     expect(turn.cardInPlay).to.equal(card);
   });
 
+  it.skip('should have a method that returns the guess', function() {
+
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+
+    const turn = new Turn('pug', card);
+
+    expect(turn.returnGuess()).to.equal('pug');
+  });
+
+  it.skip('should have a method that returns the Card', function() {
+
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+
+    const turn = new Turn('pug', card);
+
+    expect(turn.returnCard()).to.equal(card);
+  });
+
+  it.skip('should have a method that returns a boolean indicating if the user’s guess matches the correct answer on the card', function() {
+
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+
+    const turn = new Turn('pug', card);
+    const turn2 = new Turn('sea otter', card);
+
+    expect(turn.evaluateGuess()).to.equal(false);
+    expect(turn2.evaluateGuess()).to.equal(true);
+  });
+
+  it.skip('should have a method that returns either ‘incorrect!’ or ‘correct!’ based on whether the guess is correct or not', function() {
+
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+
+    const turn = new Turn('pug', card);
+    const turn2 = new Turn('sea otter', card);
+
+    expect(turn.giveFeedback()).to.equal('incorrect!');
+    expect(turn2.evaluateGuess()).to.equal('correct!');
+  });
+
+
 });
