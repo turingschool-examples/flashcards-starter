678ebf7bdd4981f5b84ee43847386dad58c584ed
Rory Magee <roryemagee@gmail.com>
2022-02-01 14:23:44 -0700

    Complete card class and associated tests

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,9 @@
 class Card {
-  constructor() {
-
+  constructor(id, question, answers, correctAnswer) {
+    this.id = id;
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
   }
 }
 
