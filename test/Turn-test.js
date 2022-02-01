2d09b6b856e8e65219a00460c2e312c3bc98aaa3
Jordan Skomal <jskomal@gmail.com>
2022-01-31 19:07:49 -0700

    Add instantiation of class test

---
+++ test/Turn-test.js
@@ -0,0 +1,19 @@
+// file dependencies 
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+// global testing variables
+const card = new Card(
+  1,
+  'What allows you to define a set of related information using key-value pairs?',
+  ['object', 'array', 'function'],
+  'object'
+);
+const turn = new Turn('object', card);
+
+//  tests
+it('should instantiate Turn as a class', () => {
+  expect(turn).to.be.an.instanceof(Turn)
+});
