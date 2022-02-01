52ebf15f4621dd51b453264d3f439c836c67c7ea
Jake Dunafon <jlad795@gmail.com>
2022-01-31 17:40:51 -0800

    Add Card and Turn Functionality

---
+++ test/Turn-test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+describe('Card', function() {
+
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+});
