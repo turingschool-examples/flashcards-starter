dfdf9a4cecca7fdc17e6e77b5d7a8b086a67439a
Francesca McConnell <fmcconnell59@gmail.com>
2022-01-31 21:13:38 -0700

    Finish turn class

--- src/Turn.js
+++ src/Turn.js
@@ -11,6 +11,22 @@ class Turn {
     returnCard() {
         return this.currentCard;
     }
+
+    evaluateGuess() {
+        if (this.guess === this.currentCard.correctAnswer) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    giveFeedback() {
+        if (this.evaluateGuess()) {
+            return 'correct!';
+        } else {
+            return 'incorrect!';
+        }
+    }
 }
 
 
