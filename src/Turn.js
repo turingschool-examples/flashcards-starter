1b988a80c0f0929fdd56efd93ed893b01a5fd1dc
Emili Kaiman <emilikaiman@gmail.com>
2022-01-31 17:26:17 -0700

    Pass test for evaluating guess

--- src/Turn.js
+++ src/Turn.js
@@ -11,6 +11,14 @@ class Turn {
   returnCard() {
     return this.card
   }
+
+  evaluateGuess() {
+    if (this.guess === this.card.correctAnswer) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
 
 module.exports = Turn
