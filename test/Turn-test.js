dc6df3a346488f4bda8a63ba3f89027049aac55e
Emili Kaiman <emilikaiman@gmail.com>
2022-01-31 17:02:36 -0700

    Create first test

---
+++ test/Turn-test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+  it('should take in a guess', function() {
+    let firstTurn = Turn('pig')
+    expect(firstTurn.guess).to.equal('pig')
+  })
+})
