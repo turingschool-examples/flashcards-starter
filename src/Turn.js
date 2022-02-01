e7db45350bc5ec0f9381fc0c225efc6e9cfbb343
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-01-31 21:18:52 -0600

    Instantiate turn and take in a guess

---
+++ src/Turn.js
@@ -0,0 +1,15 @@
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess
+    this.card = card
+  }
+  returnGuess() {
+    return this.guess
+    }
+    returnCard() {
+      console.log(this.card)
+      return this.card
+    }
+}
+
+module.exports = Turn;
