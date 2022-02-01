3c336d1b087a2f620890476683be6b7778c0f758
Francesca McConnell <fmcconnell59@gmail.com>
2022-02-01 15:09:06 -0700

    Add test for instantiated turn in method

--- test/Round-test.js
+++ test/Round-test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Round = require('../src/Round');
-// const Turn = require('../src/Turn');
+const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
@@ -42,13 +42,47 @@ describe('Round', () => {
         expect(round.deck.length).to.equal(3);
     })
 
-    it('should return current card', () => {
+    it('should store and return current card', () => {
         const card1 = new Card(1, 'What type of object is closest to a list?', ['array', 'string', 'boolean'], 'array');
         const card2 = new Card(2, 'What data type is 40?', ['boolean', 'integer', 'string'], 'integer');
         const card3 = new Card(3, 'How do you check the length of a string or array?', ['.length', '.includes', '.sum'], '.length');
         const deck = new Deck([card1, card2, card3]);
         const round = new Round(deck);
 
+        expect(round.currentCard).to.deep.equal({ id: 1, question: 'What type of object is closest to a list?', answers: ['array', 'string', 'boolean'], correctAnswer: 'array' });
         expect(round.returnCurrentCard()).to.deep.equal({ id: 1, question: 'What type of object is closest to a list?', answers: ['array', 'string', 'boolean'], correctAnswer: 'array' });
+    })
+
+    it('should be able to take turns', () => {
+        const card1 = new Card(1, 'What type of object is closest to a list?', ['array', 'string', 'boolean'], 'array');
+        const card2 = new Card(2, 'What data type is 40?', ['boolean', 'integer', 'string'], 'integer');
+        const card3 = new Card(3, 'How do you check the length of a string or array?', ['.length', '.includes', '.sum'], '.length');
+        const deck = new Deck([card1, card2, card3]);
+        const round = new Round(deck);
+
+        expect(round.takeTurn).to.be.a('function');
+    })
+
+    it('should update turns count when each new turn', () => {
+        const card1 = new Card(1, 'What type of object is closest to a list?', ['array', 'string', 'boolean'], 'array');
+        const card2 = new Card(2, 'What data type is 40?', ['boolean', 'integer', 'string'], 'integer');
+        const card3 = new Card(3, 'How do you check the length of a string or array?', ['.length', '.includes', '.sum'], '.length');
+        const deck = new Deck([card1, card2, card3]);
+        const round = new Round(deck);
+        round.takeTurn();
+        round.takeTurn();
+
+        expect(round.turns).to.equal(2);
+    })
+
+    it('should instantiate turn class when entering a guess', () => {
+        const card1 = new Card(1, 'What type of object is closest to a list?', ['array', 'string', 'boolean'], 'array');
+        const card2 = new Card(2, 'What data type is 40?', ['boolean', 'integer', 'string'], 'integer');
+        const card3 = new Card(3, 'How do you check the length of a string or array?', ['.length', '.includes', '.sum'], '.length');
+        const deck = new Deck([card1, card2, card3]);
+        const round = new Round(deck);
+        round.takeTurn('array');
+
+        expect(round.takeTurn('array')).to.deep.equal({ guess: 'array', currentCard: { id: 1, question: 'What type of object is closest to a list?', answers: ['array', 'string', 'boolean'], correctAnswer: 'array' }})
     })
 })
