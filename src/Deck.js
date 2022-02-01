4dae66572b2798010ed26df4895d217ed7f0fc07
Emili Kaiman <emilikaiman@gmail.com>
2022-01-31 18:28:36 -0700

    Pass counting cards test

--- src/Deck.js
+++ src/Deck.js
@@ -3,6 +3,11 @@ class Deck {
     this.firstCard = card1
     this.secondCard = card2
     this.thirdCard = card3
+    this.deck = [card1, card2, card3]
+  }
+
+  countCards() {
+    return this.deck.length
   }
 }
 
