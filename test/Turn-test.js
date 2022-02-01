2d57ffa66b0c79dbc8b251cea4f5c31b5f61e248
Nicole Valentini <ncvalentini@gmail.com>
2022-02-01 16:26:22 -0700

    Add test skeleton to construct Turn class

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,2 +1,35 @@
 const chai = require('chai');
 const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+  it.skip('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it.skip('should keep track of the user\'s guess', function() {
+  })
+
+  it.skip('should keep track of the current card at play', () => {
+  })
+
+  it.skip('should return the user\'s guess', () => {
+    //this is a method
+  })
+
+  it.skip('should evaluate if the guess is correct', () => {
+    //this is a method, evaluate to a boolean of true or false
+  })
+
+  it.skip('should return correct or incorrect based on user guess', () => {
+    //this is a method
+  })
+})
