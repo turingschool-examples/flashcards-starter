23e49eb5fc719b36563790cf7594b41d4834aa97
Francesca McConnell <fmcconnell59@gmail.com>
2022-01-31 20:50:16 -0700

    Create Turn class, returnCard method

---
+++ src/Turn.js
@@ -0,0 +1,18 @@
+class Turn {
+    constructor(guess, card) {
+        this.guess = guess;
+        this.currentCard = card;
+    }
+
+    returnGuess() {
+        return this.guess;
+    }
+
+    returnCard() {
+        return this.currentCard;
+    }
+}
+
+
+
+module.exports = Turn;
