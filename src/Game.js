0bfff88290093e2bdf00595ce2d8b08d88e97e59
Jordan Skomal <jskomal@gmail.com>
2022-02-01 20:42:49 -0700

    Add shuffleDeck function

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,18 @@
-const data = require('./data');
-const prototypeQuestions = data.prototypeData;
-const util = require('./util');
+const data = require('./data')
+const prototypeQuestions = data.prototypeData
+const util = require('./util')
+
+const Card = require('../src/Card')
+const Turn = require('../src/Turn')
+const Deck = require('../src/Deck')
+const Round = require('../src/Round')
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentIndexes = this.getThreeIndexes()
+    this.currentDeck = null
+    this.currentRound = null
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -11,8 +20,23 @@ class Game {
   }
 
   printQuestion(round) {
-      util.main(round);
+    util.main(round)
+  }
+
+  getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length)
+  }
+
+  getThreeIndexes() {
+    let indexes = []
+    do {
+      let indexToAdd = this.getRandomIndex(prototypeQuestions)
+      if (!indexes.includes(indexToAdd)) {
+        indexes.push(indexToAdd)
+      }
+    } while (indexes.length < 3)
+    return indexes
   }
 }
 
-module.exports = Game;
+module.exports = Game
