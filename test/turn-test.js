53765aae43d27e155c25b1ff74caac7a8abeb129
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-02-01 20:51:33 -0700

    Add turn-test refactor

--- test/turn-test.js
+++ test/turn-test.js
@@ -46,6 +46,3 @@ describe('Turn', function() {
   })
 
 });
-
-// evaluateGuess: method that returns a boolean indicating if the user’s guess matches the correct answer on the card
-// giveFeedback - method that returns either ‘incorrect!’ or ‘correct!’ based on whether the guess is correct or not.
