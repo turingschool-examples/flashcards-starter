ceb3fa3f3db1d37ea9be40c2c46cba00b08488d0
Emili Kaiman <emilikaiman@gmail.com>
2022-02-02 16:08:54 -0700

    Pass test for storing incorect guess

--- src/Round.js
+++ src/Round.js
@@ -4,18 +4,22 @@ class Round {
   constructor(deck) {
     this.deck = deck
     this.turnCount = 0
+    this.currentCard
+    this.incorrectGuesses = []
   }
 
   returnCurrentCard() {
+    this.currentCard = this.deck.cards[this.turnCount]
     return this.deck.cards[this.turnCount]
   }
 
   takeTurn(guess) {
     var newTurn = new Turn(guess, this.returnCurrentCard())
+    if (!newTurn.evaluateGuess()){
+      this.incorrectGuesses.push(this.returnCurrentCard().id)
+    }
     this.turnCount ++
     return newTurn.giveFeedback()
-
-
   }
 }
 
