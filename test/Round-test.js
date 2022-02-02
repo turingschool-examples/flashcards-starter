b194036b8ebd75d3c8b7ff31626303777a2cd26b
Francesca McConnell <fmcconnell59@gmail.com>
2022-02-02 14:51:33 -0700

    Finalize takeTurn tests

--- test/Round-test.js
+++ test/Round-test.js
@@ -81,9 +81,11 @@ describe('Round', () => {
         const card3 = new Card(3, 'How do you check the length of a string or array?', ['.length', '.includes', '.sum'], '.length');
         const deck = new Deck([card1, card2, card3]);
         const round = new Round(deck);
-        // round.takeTurn('array');
+        round.takeTurn('array');
 
-        expect(round.takeTurn('array')).to.deep.equal({ guess: 'array', currentCard: { id: 1, question: 'What type of object is closest to a list?', answers: ['array', 'string', 'boolean'], correctAnswer: 'array' }})
+        // expect(round.turn).to.be.an.instanceof(Turn);
+
+        ////need to come back to this, how to test for instantiated class without returning it
     })
 
     it('should change to next card after each turn', () => {
@@ -99,5 +101,36 @@ describe('Round', () => {
         round.takeTurn('integer');
 
         expect(round.currentCard).to.deep.equal({ id: 3, question: 'How do you check the length of a string or array?', answers: ['.length', '.includes', '.sum'], correctAnswer: '.length' })
+    })
+
+    it('should evaluate if guess is true or false', () => {
+        const card1 = new Card(1, 'What type of object is closest to a list?', ['array', 'string', 'boolean'], 'array');
+        const card2 = new Card(2, 'What data type is 40?', ['boolean', 'integer', 'string'], 'integer');
+        const card3 = new Card(3, 'How do you check the length of a string or array?', ['.length', '.includes', '.sum'], '.length');
+        const deck = new Deck([card1, card2, card3]);
+        const round = new Round(deck);
+
+        expect(round.takeTurn('array')).to.equal('correct!');
+        expect(round.takeTurn('string')).to.equal('incorrect!');
+    })
+
+    it('should add incorrect answers to incorrectGuesses array by id', () => {
+        const card1 = new Card(1, 'What type of object is closest to a list?', ['array', 'string', 'boolean'], 'array');
+        const deck = new Deck([card1]);
+        const round = new Round(deck);
+        round.takeTurn('string');
+
+        expect(round.incorrectGuesses.length).to.equal(1);
+        expect(round.incorrectGuesses).to.deep.equal([1]);
+    })
+
+    it('should calculate percentage of correct answers', () => {
+        const card1 = new Card(1, 'What type of object is closest to a list?', ['array', 'string', 'boolean'], 'array');
+        const card2 = new Card(2, 'What data type is 40?', ['boolean', 'integer', 'string'], 'integer');
+        const card3 = new Card(3, 'How do you check the length of a string or array?', ['.length', '.includes', '.sum'], '.length');
+        const deck = new Deck([card1, card2, card3]);
+        const round = new Round(deck);
+
+        
     })
 })
