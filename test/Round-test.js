a9ac83172d4e0615d274d0469300b348508d5821
Emili Kaiman <emilikaiman@gmail.com>
2022-02-02 16:10:23 -0700

    Refactor code using beforeEach

--- test/Round-test.js
+++ test/Round-test.js
@@ -39,77 +39,33 @@ var round = null;
   })
 
   it("should be a function", function () {
-    // const round = new Round();
+    
     expect(Round).to.be.a("function");
   });
 
   it("should be an instance of Round", function () {
-    // const round = new Round();
+
     expect(round).to.be.an.instanceof(Round);
   });
 
   it("should store a deck", function () {
-    // const card1 = new Card(
-    //   1,
-    //   "What is Robbie's favorite animal",
-    //   ["sea otter", "pug", "capybara"],
-    //   "sea otter"
-    // );
-    // const card2 = new Card(
-    //   14,
-    //   "What organ is Khalid missing?",
-    //   ["spleen", "appendix", "gallbladder"],
-    //   "gallbladder"
-    // );
-    // const card3 = new Card(
-    //   12,
-    //   "What is Travis's favorite stress reliever?",
-    //   ["listening to music", "watching Netflix", "playing with bubble wrap"],
-    //   "playing with bubble wrap"
-    // );
-    //
-    // const deck = new Deck([card1, card2, card3]);
-    //
-    // const round = new Round(deck);
+
     expect(round.deck).to.equal(deck);
   });
 
   it("should return the current card", function () {
-    // const card1 = new Card(
-    //   1,
-    //   "What is Robbie's favorite animal",
-    //   ["sea otter", "pug", "capybara"],
-    //   "sea otter"
-    // );
-    // const card2 = new Card(
-    //   14,
-    //   "What organ is Khalid missing?",
-    //   ["spleen", "appendix", "gallbladder"],
-    //   "gallbladder"
-    // );
-    // const card3 = new Card(
-    //   12,
-    //   "What is Travis's favorite stress reliever?",
-    //   ["listening to music", "watching Netflix", "playing with bubble wrap"],
-    //   "playing with bubble wrap"
-    // );
-    //
-    // const deck = new Deck([card1, card2, card3]);
-    //
-    // const round = new Round(deck);
+
     expect(round.returnCurrentCard()).to.equal(card1);
   });
 
   it("should have a method the updates the turns in the turn class", function () {
-    // const round = new Round();
-    // const turn = new Turn()
 
     round.takeTurn('pig');
 
     expect(round.turnCount).to.equal(1);
 
     round.takeTurn('hello');
-    // round.takeTurn('hi');
+
 
     expect(round.turnCount).to.equal(2);
   });
@@ -144,26 +100,7 @@ var round = null;
   });
 
   it("should update turns count regardles of wether the guess is correct", function () {
-    // const card1 = new Card(
-    //   1,
-    //   "What is Robbie's favorite animal",
-    //   ["sea otter", "pug", "capybara"],
-    //   "sea otter"
-    // );
-    // const card2 = new Card(
-    //   14,
-    //   "What organ is Khalid missing?",
-    //   ["spleen", "appendix", "gallbladder"],
-    //   "gallbladder"
-    // );
-    // const card3 = new Card(
-    //   12,
-    //   "What is Travis's favorite stress reliever?",
-    //   ["listening to music", "watching Netflix", "playing with bubble wrap"],
-    //   "playing with bubble wrap"
-    // );
-    //
-    // const deck = new Deck([card1, card2, card3]);
+
     const turn = new Turn("pig");
 
 
