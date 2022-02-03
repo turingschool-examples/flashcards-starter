6a08daa8743c69f5af9296ee23d3e5198ee4788e
Ali Nooner <alinooner@gmail.com>
2022-02-03 13:30:02 -0600

    Add ability to test Round is a function

---
+++ src/Round.js
@@ -0,0 +1,10 @@
+const Turn = require('../src/Turn');
+
+class Round {
+  constructor() {
+
+  }
+}
+
+
+module.exports = Round;
---
+++ test/Round-test.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+
+
+
+describe('Round', function() {
+  //create variables that will store information
+  let card1, card2, card3;
+  let cards;
+  let deck;
+  let round;
+
+  beforeEach(function() {
+    //instantiate new cards with values
+    card1 = new Card (1, 'What is a group of hedgehogs called?', ['Gaggle', 'Parade', 'Pickle'], 'Pickle');
+    card2 = new Card (2, 'Where in it\s body is a shrimp\s heart located?', ['Head', 'Chest', 'Legs'], 'Head');
+    card3 = new Card (3, 'Which animal never sleeps?', ['Bullfrog', 'Hummingbird', 'Shark'], 'Bullfrog');
+
+    //let cards will be assigned to the 3 new cards
+    cards = [card1, card2, card3];
+    //let deck will be assigned to a new deck instantiation taking in the cards
+    deck = new Deck(cards);
+    //let round will be assigned to a new round instantiation taking in the deck
+    round = new Round(deck);
+  });
+
+  //Round should be a function
+  it('should be a function', function() {
+    expect(Round).to.be.a('function');
+  });
+
+});
