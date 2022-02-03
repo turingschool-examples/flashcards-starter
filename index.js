e4920bfb0bb0f72e52d0546dbb01c49a2ce23fde
Jordan Skomal <jskomal@gmail.com>
2022-02-03 16:06:52 -0700

    Revert "Revert "Add ASCII art to 90% or more correct""
    This reverts commit adeff16174b220e786262f9df281445f79fb6f68.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-// This is where your project starts.
+const Game = require('./src/Game')
 
-console.log('Your project is running...'); 
+const game = new Game()
+
+const playGame = () => {
+  game.start()
+}
+
+playGame()
