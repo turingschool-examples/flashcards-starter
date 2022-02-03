09a4130abdf754a88e2a7910221e37f90bd10b8a
Jordan Skomal <jskomal@gmail.com>
2022-02-03 15:57:58 -0700

    Revert "Add ASCII art to 90% or more correct"
    This reverts commit 6810657b5e590db57a4cd79b3ced7ae8989ac745.

--- src/util.js
+++ src/util.js
@@ -1,44 +1,45 @@
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
 
 const genList = (round) => {
-  let card = round.returnCurrentCard()
-
+  let card = round.returnCurrentCard();
+  
   let choices = card.answers.map((answer, index) => {
     return {
       key: index,
-      value: answer,
+      value: answer
     }
-  })
+  });
   return {
     type: 'rawlist',
     message: card.question,
     name: 'answers',
-    choices: choices,
-  }
+    choices: choices
+  };
 }
 
 const getRound = (round) => {
-  return Promise.resolve(round)
+  return Promise.resolve(round);
 }
 
 const confirmUpdate = (id, round) => {
-  const feedback = round.takeTurn(id)
+  const feedback = round.takeTurn(id);
   return {
     name: 'feedback',
-    message: `Your answer of ${id} is ${feedback}`,
+    message: `Your answer of ${id} is ${feedback}`
   }
 }
 
 async function main(round) {
-  const currentRound = await getRound(round)
-  const getAnswer = await inquirer.prompt(genList(currentRound))
-  const getConfirm = await inquirer.prompt(confirmUpdate(getAnswer.answers, round))
 
-  if (!round.returnCurrentCard()) {
-    round.endRound()
-  } else {
-    main(round)
-  }
+  const currentRound = await getRound(round);
+  const getAnswer = await inquirer.prompt(genList(currentRound));
+  const getConfirm = await inquirer.prompt(confirmUpdate(getAnswer.answers, round));
+
+    if(!round.returnCurrentCard()) {
+      round.endRound();
+    } else {
+      main(round);
+    }
 }
 
-module.exports.main = main
+module.exports.main = main;
