97fbf857cdb656ab04a625c66b2867120134b017
Rio Foster <friotious@gmail.com>
2022-02-04 11:31:51 -0800

    Add giveFeedback method

--- src/Turns.js
+++ src/Turns.js
@@ -2,6 +2,7 @@ class Turn {
   constructor(guess, card) {
     this.guess = guess;
     this.currentCard = card;
+    this.guessIsCorrect = "";
   }
 
   returnGuess() {
@@ -14,9 +15,19 @@ class Turn {
 
   evaluateGuess() {
     if (this.guess === this.currentCard) {
+      this.guessIsCorrect = true
       return true
     } else {
+      this.guessIsCorrect = false
       return false
+    }
+  }
+
+  giveFeedback() {
+    if (this.guessIsCorrect === true) {
+      return "correct!"
+    } else {
+      return "incorrect!"
     }
   }
 }
