7e3bea6170a372675a3e101d964a8d9249b86f0c
Rio Foster <friotious@gmail.com>
2022-02-05 14:16:19 -0800

    Pass all Round tests

---
+++ src/Round.js
@@ -0,0 +1,46 @@
+const Turn = require('./Turn')
+
+class Round {
+  constructor(deck) {
+    this.deck = deck.cards
+    this.currentCard = deck.cards[0]
+    this.turnCount = 0
+    this.incorrectGuesses = []
+
+  }
+
+  returnCurrentCard() {
+    this.currentCard = this.deck[this.turnCount]
+    return this.currentCard
+  }
+
+  takeTurn(guess) {
+    this.turnCount++
+    const turn = new Turn(guess, this.currentCard)
+    if (turn.evaluateGuess() === true) {
+      this.returnCurrentCard();
+      this.endRound()
+      return turn.giveFeedback()
+    } if (!turn.evaluateGuess()) {
+      this.incorrectGuesses.push(this.currentCard.id)
+      this.returnCurrentCard();
+      this.endRound()
+      return turn.giveFeedback()
+    }
+
+  }
+
+  calculatePercentCorrect() {
+    var numberCorrect = this.turnCount - this.incorrectGuesses.length
+    var percentCorrect = Math.round(numberCorrect / this.turnCount * 100)
+    return percentCorrect
+  }
+
+  endRound() {
+    if (this.turnCount === this.deck.length) {
+      return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
+    }
+
+  }
+}
+module.exports = Round
--- test/Round-test.js
+++ test/Round-test.js
@@ -3,6 +3,7 @@ const expect = chai.expect;
 
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
+const Turn = require('../src/Turn');
 const Round = require('../src/Round');
 
 const card1 = new Card(
@@ -30,86 +31,75 @@ const round = new Round(deck)
 
 describe('Round', () => {
 
-  it.skip('should be a function', () => {
-  expect(Round).to.be.a.('function')
+  it('should be a function', () => {
+    expect(Round).to.be.a('function')
   })
 
-  it.skip('should be an instance of Round', () => {
+  it('should be an instance of Round', () => {
     expect(round).to.be.a.instanceOf(Round)
   })
 
-  it.skip('should have a deck of cards', () => {
-    expect(round.deck).to.equal(deck)
+  it('should have a deck of cards', () => {
+    expect(round.deck).to.deep.equal([card1, card2, card3])
   })
 
-  it.skip('should have a current card', () => {
+  it('should have a current card', () => {
     expect(round.currentCard).to.equal(card1)
   })
 
-  it.skip('should return the current card', () => {
+  it('should return the current card', () => {
     expect(round.returnCurrentCard()).to.equal(card1)
   })
 
-  it.skip('should create a new Turn instance')
-
-  it.skip('should update turn count when a turn is taken', () => {
+  it('should update turn count when a turn is taken', () => {
     round.takeTurn()
     expect(round.turnCount).to.equal(1)
   })
 
-  it.skip('should return true if guess is true', () => {
-    expect(round.takeTurn("object")).to.return(true)
-  })
-
-  it.skip('should return false if guess is false', () => {
-    expect(round.takeTurn("array")).to.return(false)
-  })
-
-  it.skip('should store an incorrect guess', () => {
+  it('should store an incorrect guess', () => {
     const round = new Round(deck)
     round.takeTurn("array")
-    expect(round.incorrectGuesses).to.equal([1])
+    expect(round.incorrectGuesses).to.deep.equal([1])
   })
 
-  it.skip('should say "correct!" when answer is correct', () => {
+  it('should say "correct!" when answer is correct', () => {
     const round = new Round(deck);
-    expect(round.takeTurn("object")).to.return('correct!')
+    expect(round.takeTurn("object")).to.equal('correct!')
   })
 
-  it.skip('should say "incorrect!" when answer is incorrect', () => {
+  it('should say "incorrect!" when answer is incorrect', () => {
     const round = new Round(deck);
-    expect(round.takeTurn("array")).to.return('incorrect!')
+    expect(round.takeTurn("array")).to.equal('incorrect!')
   })
 
-  it.skip('should create and instance of Turn', () => {
+  // it('should create an instance of Turn', () => {
+  //   const round = new Round(deck);
+  //   round.takeTurn('array')
+  //   expect(turn).to.be.a.instanceOf(Turn)
+  // })
+
+  it('should change currentCard to the next card', () => {
     const round = new Round(deck);
     round.takeTurn('array')
-    expect(turn).to.be.an.instanceOf(Turn)
+    expect(round.currentCard).to.deep.equal(card2)
+    round.takeTurn('array')
+    expect(round.currentCard).to.deep.equal(card3)
   })
 
-  it.skip('should change currentCard to the next card', () => {
-    const round = new Round(deck);
-    round.takeTurn('array')
-    expect(round.currentCard).to.equal(card2)
-    round.takeTurn('array')
-    expect(round.currentCard).to.equal(card3)
-  })
-
-  it.skip('should calculate the percentage of correct guesses', () => {
+  it('should calculate the percentage of correct guesses', () => {
     const round = new Round(deck);
     round.takeTurn('array')
     round.takeTurn('array')
     round.takeTurn('accessor method')
-    expect(round.calculatePercentCorrect).to.equal(33)
+    expect(round.calculatePercentCorrect()).to.equal(33)
   })
 
-  it.skip('should end the round', () => {
+  it('should end the round', () => {
     const round = new Round(deck);
     round.takeTurn('array')
     round.takeTurn('array')
     round.takeTurn('accessor method')
     expect(round.endRound()).to.equal('** Round over! ** You answered 33% of the questions correctly!')
   })
-  )
+
 })
-//expect(card).to.be.an.instanceof(Card);
