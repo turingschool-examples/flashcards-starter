5903fd0fd965b1aa8f2cebdfd6bbed7616148ed5
Nicholas Ao <aominhlong@yahoo.com>
2022-03-21 21:41:23 -0600

    Add methods to return user guess and card and rename card id

--- src/Turn.js
+++ src/Turn.js
@@ -3,6 +3,12 @@ class Turn {
         this.userGuess = userAnswer;
         this.currentCard = card;
     }
+    returnGuess() {
+        return this.userGuess
+    }
+    returnCard() {
+        return this.currentCard
+    }
 }
 
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -9,14 +9,25 @@ describe('Turn', function() {
         const turn = new Turn()
         expect(Turn).to.be.a('function');
     })
-    it('should have a user\'s guess', function() {
-        const turn = new Turn('otter')
-        expect(turn.userGuess).to.equal('otter');
-    })
     it('should have the current card in play', function() {
         const card = new Card(1, 'What is this animal', ['pig', 'otter', 'pub'], 'otter')
         const turn = new Turn('penguin', card)
-        expect(turn.userGuess).to.equal('penguin');
         expect(turn.currentCard).to.equal(card);
+    })
+    it('should return the user\'s guess', function() {
+        const card = new Card()
+        const turn = new Turn('penguin', card)
+
+        turn.returnGuess()
+
+        expect(turn.returnGuess()).to.equal('penguin');
+    })
+    it('should return the card', function() {
+        const card = new Card(1, 'What is between 2 and 4', [1, 3, 5, 2], 3)
+        const turn = new Turn('2', card)
+
+        turn.returnCard();
+
+        expect(turn.returnCard()).to.equal(card);
     })
 })
