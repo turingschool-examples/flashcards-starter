d1364004fb2ef5b9728f68f8a34669ba4d1e7ecc
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-03-21 18:23:26 -0600

    Add tests to instantiate Turn

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,8 +2,9 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
-describe('Turn'), function() {
+describe('Turn', function() {
 
   it.skip('should be a function', function() {
     const turn = new Turn();
@@ -19,4 +20,10 @@ describe('Turn'), function() {
     const turn = new Turn('This is my guess');
     expect(turn.userGuess).to.equal('This is my guess');
   });
-}
+
+  it.skip('should contain a card object', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('This is my guess', card);
+    expect(turn.currentCard).to.equal(card);
+  });
+});
