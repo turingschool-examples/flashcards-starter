7be432212235243289020ed1f83f69550730a805
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-03-21 18:34:25 -0600

    Add Turn code to pass the return guess and card tests

--- src/Turn.js
+++ src/Turn.js
@@ -3,6 +3,14 @@ class Turn {
     this.userGuess = userGuess;
     this.currentCard = currentCard;
   }
+
+  returnGuess() {
+    return this.userGuess;
+  }
+
+  returnCard() {
+    return this.currentCard;
+  }
 }
 
 module.exports = Turn;
