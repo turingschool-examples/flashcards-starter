6684c6a72538b827335bb3053da497acaf9bd395
Kevin King <kevinturingking@gmail.com>
2022-03-21 21:58:48 -0600

    finish building out the turn class and test file

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,34 @@
 class Turn {
-  constructor () {
+  constructor(playerGuess, card) {
+    this.playerGuess = playerGuess;
+    this.card = card
+  }
 
+  returnGuess() {
+    return this.playerGuess;
+
+  }
+
+  returnCard() {
+    return this.card;
+  }
+
+  evaluateGuess() {
+    // console.log('playerGuess', this.playerGuess)
+    // console.log('card', this.card)
+    if (this.playerGuess === this.card.correctAnswer) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  giveFeedBack() {
+    if (this.evaluateGuess()) {
+      return 'correct!'
+    } else {
+      return 'incorrect!'
+    }
   }
 }
 
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
+const Card = require('../src/Card');
 
 
 const Turn = require('../src/Turn');
@@ -10,10 +11,40 @@ describe('Turn', function() {
     expect(Turn).to.be.a('function')
   });
 
-  it('should be an instance os a Turn', () => {
+  it('should be an instance of a Turn', () => {
     const turn = new Turn();
+
     expect(turn).to.be.an.instanceof(Turn)
   });
 
-  
+  it.skip('should be able to return a user\'s guess', () => {
+    const sampleCard = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('sea otter', sampleCard)
+
+    expect(turn.returnGuess()).to.equal('sea otter')
+  });
+
+  it.skip('should be able to return a card', () => {
+    const sampleCard = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', sampleCard)
+
+    expect(turn.returnCard()).to.equal(sampleCard)
+  });
+
+  it.skip('should be able to evaluate guess', () => {
+    const sampleCard = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', sampleCard)
+
+    expect(turn.evaluateGuess()).to.equal(false)
+  });
+
+  it.skip('should be able to give feedback', () => {
+    const sampleCard = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('sea otter', sampleCard)
+
+    expect(turn.giveFeedBack()).to.equal('correct!')
+  });
+
+
+
 })
