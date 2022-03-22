dbfa306add790251f2084879ae0ef7d64d592965
Kevin King <kevinturingking@gmail.com>
2022-03-22 16:14:27 -0600

    Add tests to the file

---
+++ test/Deck-test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck')
+
+describe('Deck', function() {
+
+  it('should be a function', function() {
+    const deck = new Deck();
+    expect(Deck).to.be.a('function')
+  });
+
+  it('should be an instance of Deck', function () {
+    const deck = new Deck;
+    expect(Deck)to.be.an.instanceof(Deck);
+  });
+
+  
+})
