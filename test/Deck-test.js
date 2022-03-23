8f859cc57cdb3d05b9dd1c89b27781af7db2e6b4
Rio Foster <friotious@gmail.com>
2022-03-22 17:56:37 -0700

    Add Deck class and countCards method and pass all tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,12 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Card = require('../src/Card');
+const Deck = require('../src/Deck');
 
 const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
 const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
 const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-
-
 
 describe('Deck', function() {
 
@@ -28,6 +27,5 @@ describe('Deck', function() {
   it('should know how many Cards are in the Deck', function() {
     const deck = new Deck([card1, card2, card3]);
     expect(deck.countCards()).to.equal(3)
-
   })
 })
