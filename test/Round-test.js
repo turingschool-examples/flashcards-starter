eab16ba0a91499efdad6236fa9a2a44eb0915e36
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-03-23 17:43:50 -0600

    Allow deck to be passed in

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,14 +1,27 @@
 const chai = require("chai");
 const expect = chai.expect;
 
+const Card = require("../src/Card");
+const Deck = require("../src/Deck");
 const Round = require("../src/Round");
 
 describe("Round", () => {
+  let card1;
+  let card2;
+  let card3;
+  let deck;
   let round;
   beforeEach(() => {
-    round = new Round();
+    card1 = new Card(1, "What is Robbie's favorite animal?", ["sea otter", "pug", "capybara"], "sea otter");
+    card2 = new Card(14, "What organ is Khalid missing?", ["spleen", "appendix", "gallbladder"], "gallbladder");
+    card3 = new Card(12, "What is Travis's middle name?", ["Lex", "William", "Fitzgerald"], "Fitzgerald");
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
   });
   it("should be an instance of Round", () => {
     expect(round).to.be.an.instanceof(Round);
   });
+  it("should pass in a deck", () => {
+    expect(round.deck).to.be.an.instanceof(Deck);
+  })
 });
