5bf66285a76906bbf138930abb023fee6383920a
Ross Landino <mrlandino@gmail.com>
2022-03-22 18:08:35 -0600

    Complete test and implementation for Turn class

---
+++ src/Turn.js
@@ -0,0 +1,33 @@
+class Turn {
+  constructor(userGuess, card){
+    this.guess = userGuess;
+    this.currentCard = card;
+  }
+
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.currentCard;
+  }
+
+  evaluateGuess() {
+    if(this.guess === this.currentCard.correctAnswer){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  giveFeedback() {
+    const currentGuess = this.evaluateGuess();
+    if(currentGuess === true) {
+      return 'correct!';
+    } else {
+      return 'incorrect!';
+    }
+  }
+}
+
+module.exports = Turn;
