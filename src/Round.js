90b0ee437854264ec1ae820a7c9e6016ef77e9fc
Whitney Perricone <whitney.perricone@gmail.com>
2022-03-22 23:17:49 -0600

    Start Round

--- src/Round.js
+++ src/Round.js
@@ -1,1 +1,11 @@
-//module.exports = Round;
+class Round {
+  constructor(deck) {
+    this.deck = deck
+  }
+  returnCurrentCard() {
+    return this.deck.cards[0];
+  }
+}
+
+
+module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,3 +1,24 @@
 const chai = require('chai');
 const expect = chai.expect;
-const Box = require('../src/Round');
+const Round = require('../src/Round');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+
+
+
+describe('Round', function() {
+  it('should be a function', function() {
+  const round = new Round();
+  expect(Round).to.be.a('function');
+});
+
+it('should return the current card being played', function() {
+  const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+  const card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+  const deck = new Deck([card1, card2, card3]);
+  const round = new Round(deck);
+  expect(round.returnCurrentCard()).to.equal(card1)
+})
+
+})
