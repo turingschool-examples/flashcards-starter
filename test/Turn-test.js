9e0fc2f01afdebaf62c0b978dd6ac36a4133c8ab
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-03-22 19:16:23 -0600

    Add test for first param and refector to use beforeEach

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,15 +1,23 @@
 const chai = require("chai");
 const expect = chai.expect;
 
+const Card = require("../src/Card");
 const Turn = require("../src/Turn");
 
 describe("Turn", () => {
+  let card;
+  let turn;
+  beforeEach(() => {
+    card = new Card("What is Robbie's favorite animal", ["sea otter", "pug", "capybara"], "sea otter");
+    turn = new Turn("pug", card);
+  });
   it("should be a function", () => {
-    const turn = new Turn();
     expect(Turn).to.be.a("function");
   });
   it("should be an instance of Turn", () => {
-    const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
-  })
+  });
+  it("should pass in user's guess to the question", () => {
+    expect(turn.guess).to.equal("pug");
+  });
 });
