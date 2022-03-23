c93677c3fe4a8a85757fdf589e8796ad602021d3
Rio Foster <friotious@gmail.com>
2022-03-22 17:47:04 -0700

    Add tests for Deck class

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,3 +1,33 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Card = require('../src/Card');
+
+const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+
+
+describe('Deck', function() {
+
+  it('should be a function', function() {
+    const deck = new Deck([card1, card2, card3]);
+    expect(Deck).to.be.a('function');
+  })
+
+  it('should be an instance of Deck', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+
+  it('should contain an array of cards', function() {
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.cards).to.deep.equal([card1, card2, card3])
+  })
+
+  it('should know how many Cards are in the Deck', function() {
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.countCards()).to.equal(3)
+
+  })
+})
