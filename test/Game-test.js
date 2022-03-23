d417f149b28703f3f76d5ea63628e59bf9d285f6
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-03-22 19:49:48 -0600

    Add basic Game tests

---
+++ test/Game-test.js
@@ -0,0 +1,44 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const Game = require('../src/Game');
+
+describe('Game', function() {
+
+  let cardOne;
+  let cardTwo;
+  let cardThree;
+  let turnOne;
+  let deck;
+  let round;
+
+  beforeEach(() => {
+    cardOne = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    cardTwo = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    cardThree = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    turnOne = new Turn('object', cardOne);
+    deck = new Deck([cardOne, cardTwo, cardThree]);
+    //'../src/data'
+    round = new Round(deck);
+  });
+
+  it('should be a function', function() {
+    const game = new Game();
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Game', function() {
+    const game = new Game();
+    expect(game).to.be.an.instanceof(Game);
+  });
+
+  // it.skip('the game should keep track of the current round', function() {
+  //   const game = new Game();
+  //   expect(game.currentRound).to.equal()
+  // })
+
+})
