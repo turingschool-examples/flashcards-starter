94b7338c799e03b69d0397db08f322469f9e1983
Ross Landino <mrlandino@gmail.com>
2022-03-22 20:18:35 -0600

    Refactor Turn test file

--- src/Turn.js
+++ src/Turn.js
@@ -13,7 +13,7 @@ class Turn {
   }
 
   evaluateGuess() {
-    if(this.guess === this.currentCard.correctAnswer){
+    if(this.guess === this.currentCard.correctAnswer) {
       return true;
     } else {
       return false;
