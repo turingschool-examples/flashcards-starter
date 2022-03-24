0aef2cc22a67187e211418ca58f22ac38cd044b3
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-03-24 13:43:49 -0600

    Adjust it block spacing

--- test/Round-test.js
+++ test/Round-test.js
@@ -11,6 +11,7 @@ describe("Round", () => {
   let card3;
   let deck;
   let round;
+
   beforeEach(() => {
     card1 = new Card(1, "What is Robbie's favorite animal?", ["sea otter", "pug", "capybara"], "sea otter");
     card2 = new Card(14, "What organ is Khalid missing?", ["spleen", "appendix", "gallbladder"], "gallbladder");
@@ -18,30 +19,38 @@ describe("Round", () => {
     deck = new Deck([card1, card2, card3]);
     round = new Round(deck);
   });
+
   it("should be an instance of Round", () => {
     expect(round).to.be.an.instanceof(Round);
   });
+
   it("should pass in a deck", () => {
     expect(round.deck).to.be.an.instanceof(Deck);
   });
+
   it("should have a method to return current card", () => {
     let currCard = round.returnCurrentCard();
     expect(currCard).to.deep.equal(card1);
   });
+
   it("should have a turn counter", () => {
     expect(round.turns).to.equal(0);
   });
+
   it("should have an array of incorrect guesses", () => {
     expect(round.incorrectGuesses).to.deep.equal([]);
   });
+
   it("should allow user to take turn with a method", () => {
     let guessResult = round.takeTurn("sea otter");
     expect(guessResult).to.equal("correct!");
   });
+
   it("should return incorrect on wrong guess", () => {
     let guessResult = round.takeTurn("pug");
     expect(guessResult).to.equal("incorrect!");
   });
+
   it("should increment turn counter every turn", () => {
     expect(round.turns).to.equal(0);
 
@@ -50,6 +59,7 @@ describe("Round", () => {
 
     expect(round.turns).to.equal(2);
   });
+
   it("should use a new card per turn", () => {
     expect(round.currentCard).to.deep.equal(card1);
     
@@ -58,6 +68,7 @@ describe("Round", () => {
     
     expect(round.currentCard).to.deep.equal(card3);
   });
+
   it("should pass card id into array when guess is incorrect", () => {
     expect(round.incorrectGuesses).to.deep.equal([]);
     
@@ -66,6 +77,7 @@ describe("Round", () => {
 
     expect(round.incorrectGuesses).to.deep.equal([14]);
   });
+
   it("should calculate percent correct with a method", () => {
     round.takeTurn("sea otter");
     round.takeTurn("spleen");
@@ -74,6 +86,7 @@ describe("Round", () => {
 
     expect(prcCorrect).to.equal(33);
   });
+  
   it("should have method that indicates end of round and percentage correct", () => {
     expect(round.endRound).to.be.a("function");
   });
