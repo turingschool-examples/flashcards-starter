7e54069b550f1ed1dc4d438c12e8f483d80f91e6
Ross Landino <mrlandino@gmail.com>
2022-03-24 14:59:50 -0600

    Finish Game to run game

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,40 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('../src/Round');
+const Turn = require('../src/Turn');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+    
+  };
+
+  start() {
+    const cards = [];
+
+    prototypeQuestions.forEach(card => {
+      let newCard = new Card(card.id, card.question, card.answers, card.correctAnswer);
+      cards.push(newCard);
+    });
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    this.currentRound = round;
+
+    this.printMessage(deck, round);
+    this.printQuestion(round);
+  };
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
-  }
+  };
 
   printQuestion(round) {
       util.main(round);
-  }
+  };
 }
 
 module.exports = Game;
---
+++ test/Game-test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const Game = require('../src/Game');
+
+let game = new Game();
+
+describe('Game', function() {
+
+  it('should be an instance of Round', function() {
+    expect(game.currentRound).to.be.an.instanceof(Round);
+  });
+
+})
