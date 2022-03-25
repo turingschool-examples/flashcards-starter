5ddee07bea588c2893398cb23bb20d6577e03589
Kyle Howard <kylehoward1228@yahoo.com>
2022-03-24 19:13:01 -0600

    Minor revisions to complete and make playable

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // This is where your project starts.
+const Game = require('./src/Game');
+const game = new Game()
 
-console.log('Your project is running...'); 
+console.log('Your project is running...');
+
+game.start()
--- test/round-test.js
+++ test/round-test.js
@@ -1,4 +1,3 @@
-
 const chai = require('chai');
 const expect = chai.expect;
 const Card = require('../src/Card');
