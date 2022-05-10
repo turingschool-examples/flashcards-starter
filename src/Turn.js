5b6c1656d8c1586e15e38b2055926d614a2dfef6
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-05-09 20:06:57 -0700

    create card class and start on turn class and test

--- src/Turn.js
+++ src/Turn.js
@@ -1,5 +1,17 @@
 class Turn {
-
+    constructor(guess, card) {
+        this.guess = guess;
+        this.card = card;
+    }
+    returnGuess() {
+        return this.guess;
+    }
+    returnCard() {
+        return this.card;
+    }
+    evaluateGuess() {
+        if 
+    }
 }
 
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,15 +1,29 @@
 const chai = require('chai');
+const Card = require('../src/Card');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
 
 describe('Turn', function() {
-    it('should be a function', () => {
-        const turn = new Turn();
-        expect(Turn).to.be.a('function');
-      });
-    it('should be an instance of Turn', () => {
-        const turn = new Turn();
-        expect(turn).to.be.an.instanceof(Turn);
-      })
+    let turn;
+    let card;
+    beforeEach(() =>{
+        card = new Card((1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object'))
+        turn = new Turn('object', card)
     })
+    it('should be a function', () => {
+        expect(Turn).to.be.a('function');
+    });
+    it('should be an instance of Turn', () => {
+        expect(turn).to.be.an.instanceof(Turn);
+    })
+    it('should have a method that returns the guess', () => {
+        expect(turn.returnGuess()).to.equal('object');
+     })
+    it('should have a method that returns the card', () => {
+        expect(turn.returnCard()).to.equal(card)
+    })
+    it('should be able to evaluate the guess', () => {
+        expect(turn.evaluateGuess()).to.equal(true);
+    })
+ })
