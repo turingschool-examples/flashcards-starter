1f9af6d95de5eecd6f077d24a1ff1072eef40f93
Amanda Sands <amandajean555@gmail.com>
2022-05-10 14:47:35 -0600

    Add turn tests and turn class

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -3,47 +3,58 @@ const expect = chai.expect;
 
 
 const Turn = require('../src/Turn');
-
+let Card = require('../src/Card');
 
 describe ('Turn', function() {
 
   it('should be a function', function(){
-    const turn = new Turn();
-    expect(Turn).to.be.a('function');
+    const testCard = new Card(1, 'What color is the sky?', ['Blue', 'Yellow', 'Banana'], 'Blue');
+    const turn = new Turn('answer', testCard);
 
+    expect(Turn).to.be.a('function');
   });
 
   it('should take in a string as its first argument', function(){
-    const turn1 = new Turn('answer');
-    expect('answer').to.be.a('string');
+    const testCard2 = new Card(2, 'What is the coolest animal?', ['Cat', 'Dog', 'Unicorn'], 'Unicorn');
+    const turn2 = new Turn('answer', testCard2);
 
+    expect('answer').to.be.a('string');
   });
 
   it('should take in an instance of card as the second argument', function(){
-    const turn2 = new Turn('answer', {id: 1, question: 'Huh?', answers: ['Yes', 'No', 'Maybe'], correctAnswer: 'No'});
-    expect({id: 1, question: 'Huh?', answers: ['Yes', 'No', 'Maybe'], correctAnswer: 'No'}).to.be.an.instanceof(Card);
+    const testCard3 = new Card(3, 'What is 2 plus 2?', ['4', '1', '1145'], '4');
+    const turn3 = new Turn('answer', testCard3);
 
+    expect(testCard3).to.be.an.instanceof(Card);
   });
 
   it('should return the player guess', function(){
-    const turn3 = new Turn('Guess', {id: 1, question: 'Huh?', answers: ['Yes', 'No', 'Maybe'], correctAnswer: 'No'});
+    const testCard3 = new Card(3, 'What is 2 plus 2?', ['4', '1', '1145'], '4');
+    const turn3 = new Turn('Guess', testCard3);
+
     expect(turn3.returnGuess()).to.equal('Guess');
   });
 
   it('should return the current card', function() {
-    const turn3 = new Turn('Guess', {id: 1, question: 'Huh?', answers: ['Yes', 'No', 'Maybe'], correctAnswer: 'No'});
-    expect(turn3.returnCard()).to.equal({id: 1, question: 'Huh?', answers: ['Yes', 'No', 'Maybe'], correctAnswer: 'No'});
+    const testCard3 = new Card(3, 'What is 2 plus 2?', ['4', '1', '1145'], '4');
+    const turn3 = new Turn('2', testCard3);
+
+
+    expect(turn3.returnCard()).to.equal(testCard3);
   });
 
   it('should check if the guess is the correct answer', function() {
-    const turn3 = new Turn('Guess', {id: 1, question: 'Huh?', answers: ['Yes', 'No', 'Maybe'], correctAnswer: 'No'});
+    const testCard3 = new Card(3, 'What is 2 plus 2?', ['4', '1', '1145'], '4');
+    const turn3 = new Turn('4', testCard3);
+
     expect(turn3.evaluateGuess()).to.be.a('boolean');
   });
 
   it('should give feedback to the player', function() {
-    const turn3 = new Turn('Guess', {id: 1, question: 'Huh?', answers: ['Yes', 'No', 'Maybe'], correctAnswer: 'No'});
-    expect(turn3.giveFeedback()).to.be.a('string');
+    const testCard3 = new Card(3, 'What is 2 plus 2?', ['4', '1', '1145'], '4');
+    const turn3 = new Turn('1145', testCard3);
 
+    expect(turn3.giveFeedback()).to.be.a('string');
   });
 
 });
