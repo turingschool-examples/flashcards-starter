ee024996cda0c215cb3e42837d7779924f098b1a
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-05-09 21:10:31 -0600

    Added turn class methods and tests"

--- src/Turn.js
+++ src/Turn.js
@@ -2,10 +2,23 @@ class Turn {
   constructor(guess, card) {
     this.guess = guess;
     this.currentCard = card;
-  }
+  };
+
   returnGuess() {
-    console.log(this.guess)
     return this.guess;
-  }
-}
+  };
+
+  returnCard() {
+    return this.currentCard;
+  };
+
+  evaluateGuess() {
+    if (this.guess === this.currentCard.correctAnswer) {
+      return true;
+    } else if (this.guess !== this.currentCard.correctAnswer) {
+      return false;
+    };
+  };
+};
+
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -12,28 +12,46 @@ describe('Turn', function() {
 
   it('should be an instance of Turn', function() {
     const turn = new Turn;
-    expect(turn).to.be.a.instanceof(Turn)
+    expect(turn).to.be.a.instanceof(Turn);
   });
 
   it('should store a guess', function() {
     const turn = new Turn('banana');
-    expect(turn.guess).to.equal('banana')
+    expect(turn.guess).to.equal('banana');
   });
 
-  it('should store any guess', function() {
+  it('should store a different guess', function() {
     const turn = new Turn('tinfoil');
-    expect(turn.guess).to.equal('tinfoil')
+    expect(turn.guess).to.equal('tinfoil');
   });
 
   it('should store the current card in play', function() {
-    const card = new Card(1, 'How many mates will a seahorse have in a lifetime?', ['one', 'as many as possible', 'none'], 'one')
-    const turn = new Turn('one', card)
-    expect(turn.currentCard).to.equal(card)
+    const card = new Card(1, 'How many mates will a seahorse have in a lifetime?', ['one', 'as many as possible', 'none'], 'one');
+    const turn = new Turn('one', card);
+    expect(turn.currentCard).to.equal(card);
   });
 
-  it('should return a user guess', function() {
-    const card = new Card(1, 'How many mates will a seahorse have in a lifetime?', ['one', 'as many as possible', 'none'], 'one')
-    const turn = new Turn('one', card)
-    expect(turn.returnGuess()).to.equal('one')
-  })
+  it('should be able to return the current guess', function() {
+    const card = new Card(1, 'How many mates will a seahorse have in a lifetime?', ['one', 'as many as possible', 'none'], 'one');
+    const turn = new Turn('one', card);
+    expect(turn.returnGuess()).to.equal('one');
+  });
+
+  it('should be able to return the current card', function() {
+    const card = new Card(1, 'How many mates will a seahorse have in a lifetime?', ['one', 'as many as possible', 'none'], 'one');
+    const turn = new Turn('one', card);
+    expect(turn.returnCard()).to.equal(card);
+  });
+
+  it('should return true if the guess is correct', function() {
+    const card = new Card(2, 'How many hours a day can an adult panda spend eating?', ['two', 'twenty-four', 'twelve'], 'twelve');
+    const turn = new Turn('twelve', card);
+    expect(turn.evaluateGuess()).to.equal(true);
+  });
+
+  it('should return false if the guess is incorrect', function() {
+    const card = new Card(2, 'How many hours a day can an adult panda spend eating?', ['two', 'twenty-four', 'twelve'], 'twelve');
+    const turn = new Turn('twenty-four', card);
+    expect(turn.evaluateGuess()).to.equal(false);
+  });
 })
