285e13527573043e397eeed97d3eaad13dbcad3f
Grant Beard <Grantxbeard@gmail.com>
2022-05-09 19:33:11 -0600

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,9 @@
+class Card {
+  constructor(id, question, answers, correctAnswer) {
+    this.id = id;
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  }
+}
+module.exports = Card;
