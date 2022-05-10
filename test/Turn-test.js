46c12dc90c20cf175fd1530405c0af93df5a5011
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-05-09 20:33:26 -0700

    complete Turn.js and Turn-test.js

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,19 +1,24 @@
 const chai = require('chai');
 const Card = require('../src/Card');
 const expect = chai.expect;
-
 const Turn = require('../src/Turn');
 
 describe('Turn', function() {
     let turn;
     let card;
     beforeEach(() =>{
-        card = new Card((1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object'))
+        card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
         turn = new Turn('object', card)
     })
     it('should be a function', () => {
         expect(Turn).to.be.a('function');
     });
+    it('should take in a string as an argument', () => {
+        expect(turn.guess).to.be.a('string');
+    });
+    it('should take in a Card instance as a second argument', () => {
+        expect(turn.card).to.be.an.instanceOf(Card)
+    })
     it('should be an instance of Turn', () => {
         expect(turn).to.be.an.instanceof(Turn);
     })
@@ -21,9 +26,12 @@ describe('Turn', function() {
         expect(turn.returnGuess()).to.equal('object');
      })
     it('should have a method that returns the card', () => {
-        expect(turn.returnCard()).to.equal(card)
+        expect(turn.returnCard()).to.equal(card);
     })
     it('should be able to evaluate the guess', () => {
         expect(turn.evaluateGuess()).to.equal(true);
+    })
+    it('should be able to give feedback based on the test', () => {
+        expect(turn.giveFeedback()).to.equal('correct!');
     })
  })
