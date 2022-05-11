dd2e63b22367d9fc338f4039c55d494604a86a43
Cami Anderson <camidantas@icloud.com>
2022-05-10 20:06:21 -0400

    Add Round.js and Round-test.js

---
+++ src/Round.js
@@ -0,0 +1,31 @@
+const Turn = require('../src/Turn');
+
+class Round{
+    constructor(deck){
+        this.deck  = deck;
+        this.turns = 0;
+        this.incorrectGuesses = [];
+    }
+    returnCurrentCard(){
+        return this.deck.deck[this.turns]
+    }
+    takeTurn(guess){
+        const turn = new Turn(guess, this.returnCurrentCard())
+        this.turns++;
+        if(turn.evaluateGuess() === false){
+            this.incorrectGuesses.push(turn.card.id);
+            return turn.giveFeedback()
+        } else{
+            return turn.giveFeedback()
+        }
+    }
+    calculatePercentCorrect(){
+        return (this.incorrectGuesses.length/this.turns) * 100;
+    }
+    endRound(){
+        const amount = this.calculatePercentCorrect()
+        return `** Round over! ** You answered ${amount}% of the questions correctly!`;
+    }
+}
+
+module.exports = Round;
