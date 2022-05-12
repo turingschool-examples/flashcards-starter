27505a5b277bdbfd8b8c218667604d3a4da9b58e
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-05-12 14:55:19 -0600

    remove unneccesary code

--- test/Game-test.js
+++ test/Game-test.js
@@ -13,22 +13,6 @@ describe('Game', function() {
   let round;
   let game, game2
   beforeEach(() => {
-
-    card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object")
-    card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array")
-    card3 = new Card (3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method")
-
-    deck = new Deck([card1, card2, card3]);
-    deck2 = new Deck([card1, card2, card3]);
-
-    turn1 = new Turn('tinfoil', card1);
-    turn2 = new Turn('array', card2);
-    turn3 = new Turn('mutator method', card3)
-
-    round1 = new Round(deck);
-    round2 = new Round(deck2);
-
-    game = new Game();
     game2 = new Game();
   });
 
@@ -37,7 +21,7 @@ describe('Game', function() {
   });
 
   it('should instantiate a new Game', function() {
-    expect(game).to.be.an.instanceof(Game)
+    expect(game2).to.be.an.instanceof(Game)
   });
 
 });
