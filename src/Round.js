a0486f71fa7b1ee9c38abbab9e85980585f1fedd
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-05-11 19:01:13 -0600

    refactor Round class

--- src/Round.js
+++ src/Round.js
@@ -3,14 +3,36 @@ const Deck = require('../src/Deck');
 
 class Round {
     constructor(deck, currentCard) {
-    this.currentCard = currentCard;
+    this.currentCard;
     this.deck = deck.cards;
+    this.turns = 0;
+    this.incorrectGuesses = [];
     }
 
     returnCurrentCard() {
         return (this.currentCard = this.deck[0]);
       }
+
+      takeTurn(guess) {
+        var currentCard = this.returnCurrentCard();
+        var turn = new Turn(guess, currentCard);
+        this.turns++;
+        if (turn.giveFeedback() === "Incorrect 😩 ") {
+          this.incorrectGuesses.push(this.currentCard.id);
+        }
+        return turn.giveFeedback();
+    }
+    calculatePercentCorrect() {
+        var num = this.turns - this.incorrectGuesses.length;
+        var percentCorrect = (num / this.turns) * 100;
+        return percentCorrect;
+    }
+    endRound() {
+        var percentCorrectMessage = `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`;
+        console.log(percentCorrectMessage);
+        return percentCorrectMessage;
+    }
 }
 
 
-module.exports = Round; 
+module.exports = Round;
