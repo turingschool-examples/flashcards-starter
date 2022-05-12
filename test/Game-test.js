70a78280a2f9de6079272ec06c06c43c3c18a7d4
Megan Schuetz <mschuetz411@gmail.com>
2022-05-12 15:24:00 -0600

    create Game class

---
+++ test/Game-test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const Game = require('../src/Game');
+const Round = require('../src/Round');
+const expect = chai.expect;
+
+
+
+describe('Game', () => {
+
+    let game;
+
+    beforeEach(() => {
+        game = new Game();
+    });
+
+    it('should create cards', () => {
+        game.start();
+
+        expect(game.cards).to.be.a('array');
+    });
+
+    it('should check round', () => {
+        game.start();
+
+        expect(game.currentRound).to.be.an.instanceOf(Round);
+    });
+})
