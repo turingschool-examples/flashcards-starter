40d6c48e6249bbc367714ae80ad4420e71fe3a12
Kyle Howard <kylehoward1228@yahoo.com>
2022-05-12 13:45:08 -0600

    Add deck class and deck test file with tests

---
+++ test/Deck-test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+const Deck = require('../src/Deck');
+
+describe('Deck', () => {
+
+ it('should be a function', function() {
+     const deck = new Deck();
+      expect(Deck).to.be.a('function');
+ });
+ let card1
+ let card2
+ let card3
+ beforeEach(function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder')
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald')
+ });
+ it('should instantiate with an array of cards', function() {
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.cards).to.deep.equal([card1, card2, card3])
+ });
+ it('should have a method to know how many cards are in the deck', function(){
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.countCards()).to.equal(3)
+ });
+})
