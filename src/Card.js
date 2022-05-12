8624ebd10ae93374d3f5902a9fa2846a364ff48e
Michael Martinelli <turtelboy123@gmail.com>
2022-05-11 21:24:44 -0600

    feat/pass the first card tests and begin creating and passing turn tests

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,10 @@
 class Card {
-  constructor(id,question) {
+  constructor(id,question = [], possibleAnswer) {
     this.id = id;
-    this.question = [];
-    this.possibleAnswers = possibleAnswer;
-    this.correctAnswer = false;
+    this.question = question;
+    this.possibleAnswer = possibleAnswer;
+    this.answers = ['object', 'array', 'function']
+    this.correctAnswer = 'object';
   }
 }
+module.exports = Card;
