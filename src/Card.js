786308468d9c0c88b0a641e4cd907a1732fe630f
Maia Cochran <maia.cochran1@gmail.com>
2022-07-05 16:11:52 -0700

    Create card class

---
+++ src/Card.js
@@ -0,0 +1,8 @@
+class Card {
+    constructor(cardId, question, answerOptions, correctAnswer) {
+        this.cardId = cardId;
+        this.question = question;
+        this.answerOptions = answerOptions;
+        this.correctAnswer = correctAnswer;
+    };
+};
