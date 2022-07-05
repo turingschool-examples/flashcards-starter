9f006bd4c95b17f6db808a4173369ad2af6138e9
Chantal Goethals <chantalgoethals@gmail.com>
2022-07-05 17:48:39 -0600

    Feat: create card class

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+class Card{
+    constructor(id, question, possible, answer){
+        this.id = id;
+        this.question = question;
+        this.answers = possible;
+        this.correctAnswer = answer;
+    }
+} ;
+
+
+
+
+module.exports = Card;
---
+++ src/Turn.js
@@ -0,0 +1,17 @@
+
+class Turn{
+    constructor(){
+
+
+
+    }
+};
+
+//instantiate two arguments
+// arg 1 instantiate a string tht is  a user's guess to a question
+// arg 2 card object for current card
+// 4 methods
+// - returnGues
+// - returnCard
+ // - evaluateGuess
+ // - giveFeedback
