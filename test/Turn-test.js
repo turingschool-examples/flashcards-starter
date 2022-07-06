53fabc8ad69f000f30917593d1b43d854603dd80
Maya Kappen <mayakappen@gmail.com>
2022-07-05 20:36:20 -0400

    Ensure is a function and instance of Turn

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,3 +2,16 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+    it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+});
+
+it('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.an.instanceof(Turn)
+})
+})
