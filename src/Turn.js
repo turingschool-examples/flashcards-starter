804e486b844a3976bee5c00e0bd9dce7833064df
Chantal Goethals <chantalgoethals@gmail.com>
2022-07-05 18:16:08 -0600

    Feat: add turn class

---
+++ src/Turn.js
@@ -0,0 +1,37 @@
+
+class Turn{
+    constructor(){
+      this.userGuess = userGuess;
+      this.currentCard = cardInfo.currentCard;
+
+    }
+
+    returnGuess(){
+        //userGuess
+        return this.input
+    }
+
+    returnCard(){
+        return this.card
+    }
+
+    evaluateGuess(){
+
+    }
+
+    giveFeedback(){
+
+    }
+};
+
+//instantiate two arguments
+// arg 1 instantiate a string tht is  a user's guess to a question
+// arg 2 card object for current card
+// 4 methods
+// - returnGues
+// - returnCard
+ // - evaluateGuess
+ // - giveFeedback
+
+
+ module.Exports = Turn;
