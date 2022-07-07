0a863e6c20ad3bc09d531440d22be1a7be5b7633
Anna Spitz <aspitzemail@gmail.com>
2022-07-06 20:40:32 -0500

    Add Test class

---
+++ src/Turn.js
@@ -0,0 +1,26 @@
+class Turn {
+    constructor(guess, card) {
+        this.userGuess = guess;
+        this.currentCard = card;
+    };
+
+    returnGuess() {
+        return this.userGuess;
+    }
+
+    returnCard() {
+        return this.currentCard;
+    }
+
+    evaluateGuess() {
+        return this.userGuess === this.currentCard.correctAnswer;
+    }
+
+    giveFeedback() {
+        const isCorrect = this.evaluateGuess();
+        return (isCorrect ? 'correct!' : 'incorrect!');
+    }
+
+};
+
+module.exports = Turn;
