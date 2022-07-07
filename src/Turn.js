dfdc42cf8d246d62d1819c13eb038797784137f8
Morgan Berry <morganlyn.berry@gmail.com>
2022-07-06 20:03:23 -0600

    Finish Turn.js & Turn-test.js

--- src/Turn.js
+++ src/Turn.js
@@ -4,12 +4,29 @@ class Turn {
         this.card = card
 
     }
+
     returnGuess() {
         return this.userGuess
     }
     
     returnCard() {
         return this.card
+    }
+
+    evaluateGuess() {
+        if (this.userGuess !== this.returnCard.correctAnswer) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    giveFeedback() {
+        if (this.userGuess === this.card.correctAnswer) {
+            return "CORRECT!!!";
+        } else {
+            return "INCORRECT!!!";
+        }
     }
 
 }
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,16 +5,47 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
+
+
 describe('Turn', function() {
 
+    let turn 
+    let turn2
+    let card
+
+    beforeEach(() => {
+        turn = new Turn("Morgan", card)
+        card = new Card(1, "What is my name?", ["Morgan", "Sassy", "Zoey"], "Morgan")
+        turn2 = new Turn("Zoey", card)
+    });
+
+
     it('should be a function', function() {
-        const turn = new Turn();
         expect(Turn).to.be.a('function');
     });
 
     it('should be an instance of Turn', function() {
-        const turn = new Turn();
-        expect(turn).to.be.an.instanceof(Turn);
+       expect(turn).to.be.an.instanceof(Turn);
+    });
+
+    it('should take a user/s guess', () => {
+        expect(turn.userGuess).to.be.a('string');
+        // expect(turn2.userGuess).to.equal('Zoey');
+    });
+
+    it('should return a user/s card', () => {
+        expect(turn.returnCard()).to.equal(turn.card);
+        // expect(turn2.returnCard()).to.equal(turn.card);
+
+    });
+
+    it('should evaluate guess', () => {
+        expect(turn.evaluateGuess()).to.equal(false)
+    })
+
+    it('should give the user/s feedback', () => {
+        expect(turn.giveFeedback()).to.equal('CORRECT!!!');
+        // expect(turn2.returnCard()).to.equal('Zoey)
     });
 
 
