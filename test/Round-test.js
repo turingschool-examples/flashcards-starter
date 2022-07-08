7b0393436959033f68bc4651fe91486510e4330a
Anna Spitz <aspitzemail@gmail.com>
2022-07-07 19:42:58 -0500

    Add Round Test

--- test/Round-test.js
+++ test/Round-test.js
@@ -13,30 +13,127 @@ describe('Round', () => {
     let round;
 
     beforeEach(() => {
-        cards = 
-            [
-                new Card(cardData.prototypeData[1]), 
-                new Card(cardData.prototypeData[10]), 
-                new Card(cardData.prototypeData[28])
-            ]
+        cards = cardData.prototypeData
         
         deck = new Deck(cards);
         round = new Round();
+
     });
 
     it('should be a function', () => {
         expect(Round).to.be.a('function');
+
     });
 
     it('should be an istance of Round', () => {
         expect(round).to.be.an.instanceOf(Round);
+
+    });
+
+    it('should have current round', () => {
+        expect(round.currentTurn).to.be.a.instanceOf(Turn);
+
     });
 
     it('should return current card', () => {
         const currentCard = round.returnCurrentCard();
         expect(currentCard).to.equal(deck.cards[0]);
+
     }); 
 
-    it()
+    it('has turn count that starts at 0', () => {
+        expect(round.turns).to.equal(0);
+
+    }); 
+
+    it('should update turn count when guess is made', () => {
+        const userGuess = deck.cards[0].answers[0];
+        round.takeTurn(userGuess, deck.cards[1]);
+        expect(round.turns).to.equal(1);
+
+    });    
+
+    it('should create new Turn when guess is made', () => {
+        const userGuess = deck.cards[0].answers[0];
+        round.takeTurn(userGuess, deck.cards[1]);
+        expect(round.currentTurn.currentCard).to.equal(deck.cards[1]);
+
+    });
+
+    it('should turn count if guess is wrong', () => {
+        const userGuess = deck.cards[0].answers[1];
+        round.takeTurn(userGuess, deck.cards[1]);
+        expect(round.turns).to.equal(1);
+
+    });
+
+    it('should keep track right and wrong answers', () => {
+        let userGuess = deck.cards[0].answers[1];
+        round.takeTurn(userGuess, deck.cards[1]);
+        
+        userGuess = deck.cards[1].answers[0];
+        round.takeTurn(userGuess, deck.cards[2]);
+
+        userGuess = deck.cards[2].answers[0];
+        round.takeTurn(userGuess, deck.cards[3]);
+
+        userGuess = deck.cards[3].answers[0];
+        round.takeTurn(userGuess, deck.cards[4]);
+        
+        expect(round.guesses.wrong[0]).to.equal(deck.cards[0].id);
+        expect(round.guesses.wrong[1]).to.equal(deck.cards[3].id);
+        expect(round.guesses.right[0]).to.equal(deck.cards[1].id);
+        expect(round.guesses.right[1]).to.equal(deck.cards[2].id);
+
+    });
+
+    it('returns feedback on answers correctness', () => {
+        const userGuess1 = deck.cards[0].answers[1];
+        const answer1 = round.takeTurn(userGuess1, deck.cards[1]);
+        const userGuess2 = deck.cards[1].answers[0];
+        const answer2 = round.takeTurn(userGuess2, deck.cards[2]);
+        
+        expect(answer1).to.equal('incorrect!');
+        expect(answer2).to.equal('correct!');
+
+    });
+
+    it('should calculate percent of correct guesses', () => {
+        let userGuess = deck.cards[0].answers[1];
+        round.takeTurn(userGuess, deck.cards[1]);
+        
+        userGuess = deck.cards[1].answers[0];
+        round.takeTurn(userGuess, deck.cards[2]);
+
+        userGuess = deck.cards[2].answers[0];
+        round.takeTurn(userGuess, deck.cards[3]);
+
+        userGuess = deck.cards[3].answers[0];
+        round.takeTurn(userGuess, deck.cards[4]);
+
+        const percentOfCorrect = round.calculatePercentCorrect(round.turns, round.guesses.right);
+        expect(percentOfCorrect).to.equal('50%');
+
+    });
+
+    it('should end round with messege and percent correct guesses', () => {
+        let userGuess = deck.cards[0].answers[1];
+        round.takeTurn(userGuess, deck.cards[1]);
+        
+        userGuess = deck.cards[1].answers[0];
+        round.takeTurn(userGuess, deck.cards[2]);
+
+        userGuess = deck.cards[2].answers[0];
+        round.takeTurn(userGuess, deck.cards[3]);
+
+        userGuess = deck.cards[3].answers[0];
+        round.takeTurn(userGuess, deck.cards[4]);
+
+        const percentOfCorrect = round.calculatePercentCorrect(round.turns, round.guesses.right);
+        const endRound = round.endRound(percentOfCorrect);
+
+        expect(endRound).to.equal('** Round over! ** You answered 50% of the questions correctly!’');
+
+    });
 
 });
