4b71be8061a52d0589ae2d7bfea500e3831eba4d
Dinne Kopelevich <dinne22@hotmail.com>
2022-07-07 23:39:35 -0600

    Add round funtionality

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,47 @@
+const Turn = require("../src/Turn");
+
 class Round {
-  constructor(round) {
-    this.round = round;
+  constructor(deck) {
+    this.deck = deck.cards;
+    //this.currentCard = this.deck[0];
+    this.turns = 0;
+    this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard() {
+    return this.deck[this.turns];
+  }
+
+  takeTurn(guess) {
+    let turn = new Turn(guess, this.returnCurrentCard());
+    // console.log(turn);
+    // console.log("1", turn.guess);
+    // console.log("2", turn.card);
+    // console.log("3", turn.giveFeedback());
+    // console.log("4", turn.evaluateCard());
+    turn.evaluateCard();
+    this.turns++;
+    // if (turn.evaluateCard()) {
+    //   // console.log("6", this.incorrectGuesses);
+    //   //return true;
+    //   return "Correct";
+    // }
+    if (!turn.evaluateCard()) {
+      this.incorrectGuesses.push(this.returnCurrentCard());
+      //console.log(this.returnCurrentCard().id);
+      //return false;
+      // return "Incorrect";
+    }
+    //console.log(turn.giveFeedback());
+    return turn.giveFeedback();
+  }
+  calculatePercentCorrect() {
+    let wins = this.turns - this.incorrectGuesses.length;
+    let percent = (wins / this.turns) * 100;
+    return percent;
+  }
+  endRound() {
+    return `Round over! You answered %${this.calculatePercentCorrect()}`;
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -5,25 +5,25 @@ const Card = require("../src/Card");
 //const prototypeData = require("../src/data");
 const Deck = require("../src/Deck");
 const Round = require("../src/Round");
+const Turn = require("../src/Turn");
 
 describe("Round", function () {
-  let deck;
-  let round;
+  let deck, round, card1, card2, card3, turn;
 
   beforeEach(function () {
-    let card1 = new Card(
+    card1 = new Card(
       1,
       "What is Robbie's favorite animal",
       ["sea otter", "pug", "capybara"],
       "sea otter"
     );
-    let card2 = new Card(
+    card2 = new Card(
       14,
       "What organ is Khalid missing?",
       ["spleen", "appendix", "gallbladder"],
       "gallbladder"
     );
-    let card3 = new Card(
+    card3 = new Card(
       12,
       "What is Travis's favorite stress reliever?",
       ["listening to music", "watching Netflix", "playing with bubble wrap"],
@@ -32,59 +32,93 @@ describe("Round", function () {
 
     deck = new Deck([card1, card2, card3]);
     round = new Round(deck);
+    turn = new Turn("sea otter", card1);
+    // console.log("1", turn);
+    // console.log("2", card1);
+    // console.log("3", turn.guess);
   });
 
   it("should be a function", function () {
     expect(Round).to.be.a("function");
   });
 
-  it("should be an instance of Deck", function () {
+  it("should be an instance of Round", function () {
     expect(round).to.be.an.instanceof(Round);
   });
 
-  it.skip("should return the current card", function () {
-    expect(round.returnCurrentCard()).to.equal(deck.deck[0]);
+  it("should return the current card", function () {
+    expect(round.returnCurrentCard()).to.equal(deck.cards[0]);
   });
 
-  it.skip("should take turn", function () {
-    expect(round.takeTurn()).to.equal(updatesTurn);
+  it("should take turn and increment the turns", function () {
+    round.takeTurn();
+    round.takeTurn();
+    round.takeTurn();
+
+    expect(round.turns).to.equal(3);
   });
 
-  it.skip("should calculate the correct percentage", function () {
-    expect(round.calculatePercentCorrect()).to.equal("percent");
+  it("should start with no wrong guesses", function () {
+    expect(round.incorrectGuesses).to.deep.equal([]);
   });
 
-  it.skip("should end the round", function () {
-    expect(round.endRound()).to.equal(
-      `Round over! You answered %${percentage}`
-    );
+  it("should evaluate correct guess", function () {
+    round.takeTurn("gallbladder");
+    expect(round.takeTurn("gallbladder")).to.equal("Correct");
+  });
+
+  it("should evaluate incorrect guess", function () {
+    // const correctAnswersForRound = turn.card;
+    // const newAnswers = correctAnswersForRound.map();
+    // console.log(correctAnswersForRound);
+    round.takeTurn("manatee");
+    expect(round.takeTurn()).to.equal("Incorrect");
+  });
+
+  // const strings = ['10', '10', '10'];
+  // const numbers = strings.map(parseInt);
+  // const map1 = array1.map(x => x * 2);
+
+  it("should do nothing if guess is correct", function () {
+    round.takeTurn("sea otter");
+    expect(round.incorrectGuesses).to.deep.equal([]);
+  });
+
+  it("should store incorrect guess", function () {
+    round.takeTurn("guess");
+    expect(round.incorrectGuesses.length).to.deep.equal(1);
+  });
+
+  // it("should give the user feedback on their guess", function () {
+  //   round.takeTurn("sea otter");
+  //   expect(round.takeTurn()).to.equal("Correct");
+
+  //   round.takeTurn("guess");
+  //   expect(round.takeTurn()).to.deep.equal("Incorrect");
+  // });
+
+  it("should calculate the correct percentage", function () {
+    round.takeTurn("sea otter");
+    round.takeTurn("manatee");
+    round.calculatePercentCorrect();
+
+    expect(round.calculatePercentCorrect()).to.equal(50);
+  });
+
+  it("should end the round", function () {
+    round.takeTurn("sea otter");
+    round.takeTurn("manatee");
+    round.calculatePercentCorrect();
+
+    expect(round.endRound()).to.equal(`Round over! You answered %${50}`);
   });
 });
 
-// Your Round class will be the object that takes in responses and
-// records these guesses (as well as if they are correct or incorrect).
-// The currentCard should be the first Card in the Deck
-// (the array of Cards) at the start of the Round
+// it('should be able to set the race distance in miles', function() {
+//   var race1 = new Roadrace({title: 'Rock N Roll Half Marathon', city: "Las Vegas"});
+//   var race2 = new Roadrace({title: 'City Park Marathon', city: "Denver"});
 
-// Your Round class should meet the following requirements:
+//   var race1Distance = race1.setDistance(13.1);
+//   var race2Distance = race2.setDistance(26.2);
 
-// returnCurrentCard: method that returns the current card being played
-
-// takeTurn: method that updates turns count, evaluates guesses,
-// gives feedback, and stores ids of incorrect guesses
-
-// When a guess is made, a new Turn instance is created.
-
-// The turns count is updated, regardless of whether the guess is correct or
-// incorrect
-
-// The next card becomes current card Guess is evaluated/recorded.
-// Incorrect guesses will be stored (via the id) in an array of incorrectGuesses
-// Feedback is returned regarding whether the guess is incorrect or correct
-
-// calculatePercentCorrect: method that calculates and
-// returns the percentage of correct guesses
-
-// endRound: method that prints the following to the
-
-// console: ‘** Round over! ** You answered <>% of the questions correctly!’
+//   assert.equal(race1.distance, 13.1);
