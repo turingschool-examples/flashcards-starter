220a066b1acd4d934e7af78984c0695fb51e2b81
Emily Miles <ms.emily.j.miles@gmail.com>
2022-07-08 20:59:09 -0400

    Add game class and test files

--- src/Game.js
+++ src/Game.js
@@ -4,10 +4,13 @@ const util = require('./util');
 const Card = require('./Card');
 const Deck = require('./Deck');
 const Round = require('./Round');
-const Turn = require('./Turn');
+
 
 class Game {
   constructor() {
+    this.cards = [];
+    this.deck = {};
+    this.currentRound = {};
 
   }
 
@@ -18,6 +21,29 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  createCards() {
+   this.cards = prototypeQuestions.map(card => {
+      return new Card(card.id, card.question, card.answers, card.correctAnswer)
+    })
+  }
+
+  createDeck() {
+    this.deck = new Deck(this.cards)
+  }
+
+  createRound() {
+    this.currentRound = new Round(this.deck.cards);
+  }
+
+  startGame() {
+    this.createCards()
+    this.createDeck()
+    this.createRound()
+    this.printMessage(this.deck, this.currentRound)
+    this.printQuestion(this.currentRound);
+    console.log(this.currentRound);
   }
 }
 
---
+++ test/Game-test.js
@@ -0,0 +1,42 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Game = require('../src/Game');
+
+describe('Game', function(){
+    let sampleCards;
+    let game;
+
+    beforeEach(function(){
+        sampleCards = [{
+            "id": 5,
+            "question": "What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?",
+            "answers": ["mutator method", "accessor method", "iteration method"],
+            "correctAnswer": "iteration method"
+          }, {
+            "id": 15,
+            "question": "The callback function for reduce() takes in an accumulator and a current element.",
+            "answers": ["true", "false"],
+            "correctAnswer": "true"
+          }, {
+            "id": 25,
+            "question": "Arrays are specialized objects where the keys are ordered numbers.",
+            "answers": ["true", "false"],
+            "correctAnswer": "true"
+          }]
+        game = new Game(sampleCards)
+    })
+
+    it('should be a function', function() {
+        expect(Game).to.be.a('function')
+    })
+    
+    it('should be able to create and store cards in a new Deck', function() {
+        expect(game.createDeck(game.cards)).to.equal(game.createCards())
+    })
+
+    it('should be able to start a new round of the Game', function() {
+        expect(game.startGame()).to.equal(game.currentRound)
+        //this test is not passing
+    })
+})
