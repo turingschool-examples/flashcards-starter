d766aa7704bfb9b7795449c82b722ef4f42d81b0
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-08-22 14:36:49 -0600

    Add: turn-test.file

---
+++ test/Turn-test.js
@@ -0,0 +1,21 @@
+describe('Turn', () => {
+    it.skip('should be a function', () => {
+
+    });
+
+    it.skip('should return guessed answer', () => {
+
+    });
+
+    it.skip('should return card', () => {
+
+    });
+
+    it.skip('should returns a boolean indicating if the guess matches the correct answer on the card', () => {
+
+    });
+
+    it.skip('should returns either ‘incorrect!’ or ‘correct!’ based on whether the guess is correct or not.', () => {
+
+    })
+} )
