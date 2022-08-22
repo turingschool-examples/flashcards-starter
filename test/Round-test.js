58ce20b56304fb02795c23aa1b5d9fec4834605e
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-08-22 17:45:11 -0600

    Add: class

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,6 +1,8 @@
+const { expect } = require('chai');
 const Round = require('../src/Round');
 describe('Round', () => {
     it('should be a function', () => {
-        
-    })
+        expect(Round).to.a('function');
+    });
+
 })
