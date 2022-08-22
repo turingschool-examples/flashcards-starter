7b34e7a75e39f80bdfb1a91d82893be61f42d162
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-08-22 16:49:22 -0600

    Initial Commit

---
+++ src/turn.js
@@ -0,0 +1,8 @@
+class Turn{
+  constructor(guess, currentCard){
+
+  }
+}
+
+
+module.exports = Turn
---
+++ test/Turn-test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+})
