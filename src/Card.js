d8254c1ffb0cf513fa0a3eb8a98a9f63c99dc247
Scott Gaines <scottdgaines@gmail.com>
2022-08-22 17:37:32 -0600

    Build Card class according to Card-test, pass all tests

---
+++ src/Card.js
@@ -0,0 +1,10 @@
+class Card {
+   constructor(id, question, answers, answer) {
+    this.id = id,
+    this.question = question,
+    this.answers = answers,
+    this.correctAnswer = answer
+   }
+}
+
+module.exports = Card
