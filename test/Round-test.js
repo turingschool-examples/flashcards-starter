8bd7789ed9f2559331def1c9f3edb6ea2f9e6434
Victoria Fields <victoriaashleyfields@gmail.com>
2022-08-22 20:22:51 -0600

    Add property to store deck of cards

--- test/Round-test.js
+++ test/Round-test.js
@@ -11,7 +11,22 @@ describe('Round', () => {
   });
 
   it ('should be an instance of Round', () => {
-    const round = new Round();
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What can you use to make unit testing code DRYer?', ['hooks', 'assertion libraries', 'Mocha'], 'hooks');
+    const card3 = new Card(3, 'What can you use to store similar data in a list?', ['object', 'array', 'function'], 'array');
+    const deck = new Deck([card, card2, card3]);
+    const round = new Round(deck);
+    
     expect(round).to.be.an.instanceOf(Round);
   });
+
+  it ('should store a deck of cards', () => {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What can you use to make unit testing code DRYer?', ['hooks', 'assertion libraries', 'Mocha'], 'hooks');
+    const card3 = new Card(3, 'What can you use to store similar data in a list?', ['object', 'array', 'function'], 'array');
+    const deck = new Deck([card, card2, card3]);
+    const round = new Round(deck);
+
+    expect(round.deck).to.deep.equal([card, card2, card3]);
+  })
 });
