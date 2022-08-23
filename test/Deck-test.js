066af9897762f9bc6244181d5a8ef51a2d2d59b5
Scott Gaines <scottdgaines@gmail.com>
2022-08-23 17:00:04 -0600

    Remove const Turn = require... on line 4

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,6 +1,5 @@
 const chai = require('chai');
 const expect = chai.expect;
-const Turn = require('../src/Turn.js');
 const Card = require('../src/Card.js');
 const Deck = require('../src/Deck.js');
 
