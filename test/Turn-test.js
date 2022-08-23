bd405d66c14496b68717b55e9a36c1eed7fccc71
Scott Gaines <scottdgaines@gmail.com>
2022-08-22 19:07:41 -0600

    Refactor format, syntax, and DRYness issues

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,87 +1,87 @@
 const chai = require('chai');
+const Turn = require('../src/Turn.js');
 const expect = chai.expect;
-const Card = require('../src/Turn.js')
+const Card = require('../src/Card.js')
 
 describe('Turn', function() {
 
-    it.skip('Should be a function', function() {
-        let turn = new Turn;
+    it('Should be a function', function() {
+        let turn = new Turn();
         
-        expect(turn).to.be.a('function')
+        expect(Turn).to.be.a('function')
     });
 
-    it.skip('Should be an instance of Turn', function() {
-        let turn = new Turn;
+    it('Should be an instance of Turn', function() {
+        let turn = new Turn();
 
         expect(turn).to.be.an.instanceOf(Turn)
     });
 
-    it.skip('Should have a property for the card currently in play', function() {
+    it('Should have a property for the card currently in play', function() {
+        let card = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne');
         let turn = new Turn(card);
-        let card = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne') 
 
-        expect(turn.card).to.deepEqual(card)
+        expect(turn.card).to.equal(card)
     });
 
-    it.skip('Should be able to reference a different card', function() {
+    it('Should be able to reference a different card', function() {
+        let card = new Card(2, 'Who is Batman\'s arch nemesis?', ['Cat Woman', 'Poison Ivy', 'The Joker'], 'The Joker');
         let turn = new Turn(card);
-        let card = new Card(2, 'Who is Batman\'s arch nemesis?', ['Cat Woman', 'Poison Ivy', 'The Joker'], 'The Joker') 
     
-        expect(turn.card).to.deepEqual(card)
+        expect(turn.card).to.equal(card)
     })
 
-    it.skip('Should have a property for player\'s guess', function() {
+    it('Should have a property for player\'s guess', function() {
+        let card = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne');
         let turn = new Turn(card, 'Bruce Wayne');
-        let card = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne') 
 
-        expect(turn.answer).to.equal('Bruce Wayne')
+        expect(turn.guess).to.equal('Bruce Wayne')
     });
 
-    it.skip('Should be able to accept a different guess', function() {
+    it('Should be able to accept a different guess', function() {
+        let card = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne');
         let turn = new Turn(card, 'Clark Kent')
-        let card = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne') 
-    
-        expect(turn.answer).to.equal('Clark Kent')
+
+        expect(turn.guess).to.equal('Clark Kent')
     });
 
-    it.skip('Should return the player\'s guess', function() {
+    it('Should return the player\'s guess', function() {
+        let card = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne');
         let turn = new Turn(card, 'Bruce Wayne')
-        let card = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne') 
         
         let guess = turn.returnGuess();
 
         expect(guess).to.equal('Bruce Wayne')
     });
 
-    it.skip('Should return the current card in play', function() {
+    it('Should return the current card in play', function() {
+        let card = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne');
         let turn = new Turn(card, 'Bruce Wayne')
-        let card = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne') 
 
         let currentCard = turn.returnCard();
 
         expect(currentCard).to.equal(card)
     });
 
-    it.skip('Should return a boolean value indicating if the player\'s guess matches the answer on the card', function() {
-        let turn1 = new Turn(card1, 'Bruce Wayne');
-        let card1 = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne'); 
-        let turn2 = new Turn(card2, 'Poison Ivy');
-        let card2 = new Card (2, 'Who is Batman\'s arch nemesis?', ['Cat Woman', 'Poison Ivy', 'The Joker'], 'The Joker') 
+    it('Should return a boolean value indicating if the player\'s guess matches the answer on the card', function() {
+        let card = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne'); 
+        let turn1 = new Turn(card, 'Bruce Wayne');
+        let turn2 = new Turn(card, 'Clark Kent');
        
-        let guess1 = turn.evaluateGuess();
-        let guess2 = turn.evaluateGuess();
+        let guess1 = turn1.evaluateGuess();
+        let guess2 = turn2.evaluateGuess();
 
         expect(guess1).to.equal(true);
         expect(guess2).to.equal(false)
     });
 
-    it.skip('Should provide the player with feedback based on their answer', function() {
-        let turn1 = new Turn(card1, 'Bruce Wayne');
-        let card1 = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne'); 
-        let turn2 = new Turn(card2, 'Poison Ivy');
-        let card2 = new Card (2, 'Who is Batman\'s arch nemesis?', ['Cat Woman', 'Poison Ivy', 'The Joker'], 'The Joker') 
+    it('Should provide the player with feedback based on their answer', function() {
+        let card = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne'); 
+        let turn1 = new Turn(card, 'Bruce Wayne');
+        let turn2 = new Turn(card, 'Clark Kent');
 
-        let feedback1 = turn.giveFeedback();
+        let feedback1 = turn1.giveFeedback();
+        let feedback2 = turn2.giveFeedback();
 
         expect(feedback1).to.equal('Correct!');
         expect(feedback2).to.equal('Incorrect!')
