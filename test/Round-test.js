c8d8bffaad3502e6d8f56db3a037f7a52d5fdd52
Victoria Fields <victoriaashleyfields@gmail.com>
2022-08-22 20:28:29 -0600

    Add ability to store first card as current card

--- test/Round-test.js
+++ test/Round-test.js
@@ -16,7 +16,7 @@ describe('Round', () => {
     const card3 = new Card(3, 'What can you use to store similar data in a list?', ['object', 'array', 'function'], 'array');
     const deck = new Deck([card, card2, card3]);
     const round = new Round(deck);
-    
+
     expect(round).to.be.an.instanceOf(Round);
   });
 
@@ -28,5 +28,18 @@ describe('Round', () => {
     const round = new Round(deck);
 
     expect(round.deck).to.deep.equal([card, card2, card3]);
-  })
+  });
+
+  it ('should store current card as first card in the deck at the start of the round', () => {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What can you use to make unit testing code DRYer?', ['hooks', 'assertion libraries', 'Mocha'], 'hooks');
+    const card3 = new Card(3, 'What can you use to store similar data in a list?', ['object', 'array', 'function'], 'array');
+    const deck = new Deck([card, card2, card3]);
+    const round = new Round(deck);
+    const deck2 = new Deck([card3, card2, card]);
+    const round2 = new Round(deck2);
+
+    expect(round.currentCard).to.equal(card);
+    expect(round2.currentCard).to.equal(card3);
+  });
 });
