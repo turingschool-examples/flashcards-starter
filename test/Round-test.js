8120ca62b8c314be8a85c0831c236edca8c99e63
Victoria Fields <victoriaashleyfields@gmail.com>
2022-08-22 20:13:46 -0600

    Add function and instance tests to Round

--- test/Round-test.js
+++ test/Round-test.js
@@ -4,3 +4,14 @@ const expect = chai.expect;
 const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
+
+describe('Round', () => {
+  it ('should be a function', () => {
+    expect(Round).to.be.a('function');
+  });
+
+  it ('should be an instance of Round', () => {
+    const round = new Round();
+    expect(round).to.be.an.instanceOf(Round);
+  });
+});
