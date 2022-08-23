5ed87f8ad83d2fbb3aecdb29fcbf2153db72d481
Hannah.Celemen <hcelemen@icloud.com>
2022-08-23 16:03:01 -0600

    Add testing code

---
+++ test/Deck-test.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const flashcards = require('../src/data')
+const Card = require('../src/Card');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+
+describe('Deck', () => {
+  let cards = [];
+  let deck = {};
+
+  beforeEach(() => {
+    cards = 
+    [
+      new Card(flashcards.prototypeData[0]),
+      new Card(flashcards.prototypeData[2]),
+      new Card(flashcards.prototypeData[3]),
+    ]
+
+    deck = new Deck(cards)
+  }) 
+
+  it('should be a function', () => {
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', () => {
+    expect(deck).to.be.an.instanceOf(Deck);
+  });
+
+  it('should have a deck of cards', () => {
+    expect(deck.cards).to.deep.equal(cards); 
+  });
+  
+  it('should count how many cards are in the deck', () => {
+    const cardCount = deck.countCards();
+
+    expect(deck.cards).to.have.lengthOf(3);
+    expect(cardCount).to.equal(3);
+  });
+})
