f334a1d7c4a7d16e7af056f661f47ab21c694dad
Scott Gaines <scottdgaines@gmail.com>
2022-08-23 16:42:15 -0600

    Build out Deck Class, passes all tests

---
+++ src/Deck.js
@@ -0,0 +1,13 @@
+class Deck {
+    constructor(cards) {
+        this.cards = cards,
+        this.cardCount = 
+    }
+
+    countCards = () => {
+        let count = this.cards.length;
+        return count
+    }
+}
+
+module.exports = Deck
