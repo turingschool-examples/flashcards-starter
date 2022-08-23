6aeb65f15f2c8ea872f79ec0af3bdc4ea293025f
Scott Gaines <scottdgaines@gmail.com>
2022-08-23 16:43:51 -0600

    Refactor last test to account for testing countCards method

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,19 +6,19 @@ const Deck = require('../src/Deck.js');
 
 describe('Deck', function() {
 
-    it.skip('Should be a function', function() {
+    it('Should be a function', function() {
         let deck = new Deck();
 
-        expect(deck).to.be.a('function')
+        expect(Deck).to.be.a('function')
     });
 
-    it.skip('Should be an instance of the Deck class', function() {
+    it('Should be an instance of the Deck class', function() {
         let deck = new Deck();
 
         expect(deck).to.be.an.instanceOf(Deck)
     });
 
-    it.skip('Should have an array of cards by default', function() {
+    it('Should have an array of cards by default', function() {
         let card1 = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne');
         let card2 = new Card(2, 'Who is Batman\'s arch nemesis?', ['Cat Woman', 'Poison Ivy', 'The Joker'], 'The Joker');
         let card3 = new Card(3, 'What is the name of Batman\'s Butler?', ['Sir Pennywise', 'Ralph', 'Alfred'], 'Alfred');
@@ -30,7 +30,7 @@ describe('Deck', function() {
         expect(deck.cards).to.equal(cards)
     });
 
-    it.skip('Should keep track of how many cards are in the deck', function() {
+    it('Should keep track of how many cards are in the deck', function() {
         let card1 = new Card(1, 'Who is Batman\'s alter ego?', ['Bruce Wayne', 'Clark Kent', 'Diana Prince'], 'Bruce Wayne');
         let card2 = new Card(2, 'Who is Batman\'s arch nemesis?', ['Cat Woman', 'Poison Ivy', 'The Joker'], 'The Joker');
         let card3 = new Card(3, 'What is the name of Batman\'s Butler?', ['Sir Pennywise', 'Ralph', 'Alfred'], 'Alfred');
@@ -39,14 +39,14 @@ describe('Deck', function() {
 
         let deck = new Deck(cards);
 
-        expect(deck.cardCount).to.equal(3);
+        let count = deck.countCards()
+
+        expect(count).to.equal(3);
 
         let card4 = new Card(4, 'Who is Batman\'s sidekick?', ['Toto', 'John Steed', 'Robin'], 'Robin');
         deck.cards.push(card4);
+        count = deck.countCards();
 
-        expect(deck.cardCount).to.equal(4)
+        expect(count).to.equal(4)
     })
-
-
-
 })
