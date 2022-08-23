05c4cf4b22b0a6a3f57021fd73cde8e01af8b7cc
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2022-08-22 19:02:13 -0500

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,8 @@
+class Card {
+    constructor(id, question, possibleAnswers, correctAnswer) {
+        this.question = question;
+        this.answers = possibleAnswers;
+        this.correctAnswer = correctAnswer;
+    }
+}
+module.exports = Card;
---
+++ test/Turn-test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+    it('should be a function', function() {
+        const turn = new Turn();
+        expect(Turn).to.be.a('function');
+    });
+
+    it('should be an instance of Turn', function() {
+        const turn = new Turn();
+        expect(turn).to.be.an.instanceOf(Turn);
+    });
+
+    
+}
