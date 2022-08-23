63686797bdaa80854a4e87b3ee7269d25e66a2d5
Victoria Fields <victoriaashleyfields@gmail.com>
2022-08-23 13:47:53 -0600

    Refactor to include beforeEach

--- test/Round-test.js
+++ test/Round-test.js
@@ -10,59 +10,43 @@ describe('Round', () => {
     expect(Round).to.be.a('function');
   });
 
-  it ('should be an instance of Round', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const card2 = new Card(2, 'What can you use to make unit testing code DRYer?', ['hooks', 'assertion libraries', 'Mocha'], 'hooks');
-    const card3 = new Card(3, 'What can you use to store similar data in a list?', ['object', 'array', 'function'], 'array');
-    const deck = new Deck([card, card2, card3]);
-    const round = new Round(deck);
+  let card;
+  let card2;
+  let card3;
+  let deck;
+  let deck2;
+  let round;
+  let round2;
 
+  beforeEach(() => {
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = new Card(2, 'What can you use to make unit testing code DRYer?', ['hooks', 'assertion libraries', 'Mocha'], 'hooks');
+    card3 = new Card(3, 'What can you use to store similar data in a list?', ['object', 'array', 'function'], 'array');
+    deck = new Deck([card, card2, card3]);
+    deck2 = new Deck([card3, card2, card]);
+    round = new Round(deck);
+    round2 = new Round(deck2);
+  });
+
+  it ('should be an instance of Round', () => {
     expect(round).to.be.an.instanceOf(Round);
   });
 
   it ('should store a deck of cards', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const card2 = new Card(2, 'What can you use to make unit testing code DRYer?', ['hooks', 'assertion libraries', 'Mocha'], 'hooks');
-    const card3 = new Card(3, 'What can you use to store similar data in a list?', ['object', 'array', 'function'], 'array');
-    const deck = new Deck([card, card2, card3]);
-    const round = new Round(deck);
-
     expect(round.deck).to.deep.equal([card, card2, card3]);
   });
 
   it ('should store current card as first card in the deck at the start of the round', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const card2 = new Card(2, 'What can you use to make unit testing code DRYer?', ['hooks', 'assertion libraries', 'Mocha'], 'hooks');
-    const card3 = new Card(3, 'What can you use to store similar data in a list?', ['object', 'array', 'function'], 'array');
-    const deck = new Deck([card, card2, card3]);
-    const round = new Round(deck);
-    const deck2 = new Deck([card3, card2, card]);
-    const round2 = new Round(deck2);
-
     expect(round.currentCard).to.equal(card);
     expect(round2.currentCard).to.equal(card3);
   });
 
   it ('should return the current card being played', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const card2 = new Card(2, 'What can you use to make unit testing code DRYer?', ['hooks', 'assertion libraries', 'Mocha'], 'hooks');
-    const card3 = new Card(3, 'What can you use to store similar data in a list?', ['object', 'array', 'function'], 'array');
-    const deck = new Deck([card, card2, card3]);
-    const round = new Round(deck);
-    const deck2 = new Deck([card3, card2, card]);
-    const round2 = new Round(deck2);
-
     expect(round.returnCurrentCard()).to.equal(round.currentCard);
     expect(round2.returnCurrentCard()).to.equal(round2.currentCard);
   });
 
   it ('should start with 0 turns recorded', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const card2 = new Card(2, 'What can you use to make unit testing code DRYer?', ['hooks', 'assertion libraries', 'Mocha'], 'hooks');
-    const card3 = new Card(3, 'What can you use to store similar data in a list?', ['object', 'array', 'function'], 'array');
-    const deck = new Deck([card, card2, card3]);
-    const round = new Round(deck);
-
     expect(round.turns).to.equal(0);
   })
 
