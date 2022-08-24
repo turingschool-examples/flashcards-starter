6042866f58c7d1e9dacfbfa210a78f6f11d1e62f
Connor Corcoran <connorcorc@gmail.com>
2022-08-23 19:52:43 -0600

    Add Deck and Deck testing

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,35 +1,61 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('../src/Turn.js');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card')
 
-describe('Turn', function(){
+describe('Turn', () => {
+  let card1, card2, turn1, turn2;
 
-  it('should be a function', function() {
-    const turn = new Turn();
+  beforeEach(() => {
+    card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?",
+    ["object", "array", "function"],
+    "object"
+    )
+    card2 = new Card(2, "What is a comma-separated list of related values?",
+    ["array", "object", "function"],
+    "array"
+    )
+
+    turn1 = new Turn("object", card1)
+    turn2 = new Turn("function", card2)
+
+  })
+
+
+
+
+
+  it('should be a function', () => {
     expect(Turn).to.be.a('function')
   })
 
-  it('should be an instance of Turn', function() {
-      const turn = new Turn();
-      expect(turn).to.be.an.instanceof(Turn);
+  it('should be an instance of Turn', () => {
+    expect(turn1).to.be.an.instanceof(Turn);
   })
 
-  it('should return a guess', function() {
-    const turn = new Turn();
-    turn.returnGuess('User guess')
-    expect(turn.guess).to.equal('User guess')
+  it('should store a guess', () => {
+    expect(turn1.userGuess).to.equal("object")
   })
 
-  it('should evaluate the guess', function() {
-    const turn = new Turn();
-    turn.evaluateGuess();
-    expect(turn.evaluate).to.equal(true);
+  it('should store a card', () => {
+    expect(turn2.currentCard).to.equal(card2)
   })
 
-  it('should give feedback', function() {
-    const turn = new Turn();
-    turn.giveFeedback('Correct!')
-    expect(turn.feedback).to.equal('Correct!');
+  it('should determine if guess is incorrect', () => {
+    expect(turn2.evaluateGuess()).to.equal(false)
   })
+
+  it('should determine if guess is correct', () => {
+    expect(turn1.evaluateGuess()).to.equal(true)
+  })
+
+  it('should give feedback for wrong guess', () => {
+    expect(turn2.giveFeedback()).to.equal('You wrong')
+  })
+
+  it('should give feedback for correct guess', () => {
+    expect(turn1.giveFeedback()).to.equal('You right')
+  })
+
 })
