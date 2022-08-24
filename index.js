ceda81553d2efc8b271388d8c71653ec2e795c48
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-08-23 19:31:30 -0600

    Add: instance of game class

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-// This is where your project starts.
-
+const Game = require("./src/Game");
 console.log('Your project is running...'); 
+// This is where your project starts.
+const newGame = new Game();
+
+newGame.start()
+
