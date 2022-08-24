30ee9f0c4799e3974944fb7b66664c85563b86a8
Victoria Fields <victoriaashleyfields@gmail.com>
2022-08-24 15:19:38 -0600

    Delete code comments in game test file

--- test/Game-test.js
+++ test/Game-test.js
@@ -10,13 +10,4 @@ describe('Game', () => {
   it ('should be a function', () => {
     expect(Game).to.be.a('function');
   });
-
-  // it ('should start a new game with a new round using a new deck of cards', () => {
-  //   let game = new Game();
-  //   expect(new Card()).to.be.an.instanceOf(Card);
-  // });
-  //
-  // it ('should keep track of the current round', () => {
-  //
-  // });
 });
