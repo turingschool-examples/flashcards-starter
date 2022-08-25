82b211f4d9cc4dac9b5c5defdfebae34ddc69ed0
Hannah.Celemen <hcelemen@icloud.com>
2022-08-24 18:57:18 -0600

    Add testing code

---
+++ test/Round-test.js
@@ -0,0 +1,90 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const flashcards = require('../src/data');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
+describe('Round', () => {
+  let cards;
+  let deck;
+  let round;
+
+  beforeEach(() => {
+    cards = 
+    [
+      new Card(flashcards.prototypeData[0]),
+      new Card(flashcards.prototypeData[1]),
+      new Card(flashcards.prototypeData[2]),
+    ];
+    deck = new Deck(cards);
+    round = new Round(deck);
+  });
+
+  it('should be a function', () => {
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', () => {
+    expect(round).to.be.an.instanceOf(Round);
+  });
+
+  it('should return the current card being played', () => {
+    const currentCard = round.getCurrentCard();
+    expect(currentCard).to.equal(deck.cards[0]);
+  });
+
+  it('should have turns start counting from 0', () => {
+    expect(round.currentTurnIteration).to.equal(0);
+  });
+
+  it('should go to the next card after guessing', () => {
+    round.takeTurn('object');
+    expect(round.getCurrentCard()).to.equal(deck.cards[1]);
+  });
+
+  it('should update turns when guess is made', () => {
+    round.takeTurn('object');
+    expect(round.currentTurnIteration).to.equal(1);
+  });
+
+  it('should handle successful guess', () => {
+    round.takeTurn('object');
+    expect(round.answeredCardIds.correct).to.deep.equal([1]);
+  });
+
+  it('should handle unsuccessful guess', () => {
+    round.takeTurn('array');
+    expect(round.answeredCardIds.incorrect).to.deep.equal([1]);
+  });
+
+  it('should tell user if guess is correct', () => {
+    const result = round.takeTurn('object');
+    expect(result).to.equal('Correct!'); 
+  });
+  
+  it('should tell user if guess is incorrect', () => {
+    const result = round.takeTurn('array');
+    expect(result).to.equal('Incorrect!'); 
+  });
+  
+  it('should return percent of correct guesses', () => {
+    round.takeTurn('object'); 
+    round.takeTurn('array'); 
+    round.takeTurn('accessor method'); 
+    
+    const grade = round.getCorrectPercentage();
+    expect(grade).to.equal(66.67);
+  })
+
+  it('should tell user when round is over', () => {
+    round.takeTurn('object'); 
+    round.takeTurn('array'); 
+    round.takeTurn('accessor method'); 
+
+    const finishedMsg = round.endRound();
+    const grade = round.getCorrectPercentage();
+    expect(finishedMsg).to.equal(`**Round over!**You answered ${grade} of the questions correctly!`)
+  });
+})
