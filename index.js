97c52039cfcce1e4a7f660575e061e2e9205a52b
Hannah.Celemen <hcelemen@icloud.com>
2022-08-24 22:09:25 -0600

    Run game on CLI

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 // This is where your project starts.
 
+// need to import game, create a new game with all required data, and call start()
+const flashcards = require('./src/data');
+const Game = require('./src/Game');
+const Round = require('./src/Round');
+const Deck = require('./src/Deck');
+const Card = require('./src/Card');
+
+
+let cards = 
+[
+  new Card(flashcards.prototypeData[0]),
+  new Card(flashcards.prototypeData[1]),
+  new Card(flashcards.prototypeData[2]),
+];
+let deck = new Deck(cards);
+let round = new Round(deck);
+let game = new Game(round);
+
+game.start();
+
 console.log('Your project is running...'); 
