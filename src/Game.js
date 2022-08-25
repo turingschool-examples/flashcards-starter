062875f5118817b1b222497e23d5be85e458d8c5
Hannah.Celemen <hcelemen@icloud.com>
2022-08-24 22:06:02 -0600

    Add Game functionality

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,16 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+
 class Game {
-  constructor() {}
+  constructor(round) {
+    this.currentRound = round;
+  }
+
+  start() {
+    this.printMessage(this.currentRound.deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -11,7 +19,7 @@ class Game {
   }
 
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
   }
 }
 
