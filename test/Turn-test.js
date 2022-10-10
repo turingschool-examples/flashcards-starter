5e3d066668e32307c2dc4af0a2d6139442f2a1de
James Wasmer <jameswasmer@gmail.com>
2022-10-10 19:50:04 -0400

    Add test for giveFeedback() method

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,39 +5,39 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
-describe('Turn', () => {
+describe(`Turn`, () => {
   let turn;
   let card;
   let guess;
 
-  it.skip('should accept a string argument representing a users guess', () => {
+  it.skip(`should accept a string argument representing a users guess`, () => {
     turn = new Turn('guess');
 
     expect(turn.guess).to.equal('guess');
   })
 
-  it.skip('should accept the currently played card as an argument', () => {
+  it.skip(`should accept the currently played card as an argument`, () => {
     card = new Card(1, "This is a test question", ["Yes", "No"], "Yes");
     turn = new Turn('Yes', card);
 
     expect(turn.card).to.equal(card);
   })
 
-  it.skip('should have a returnGuess() method to return the users guess', () => {
+  it.skip(`should have a returnGuess() method to return the users guess`, () => {
     card = new Card(1, "This is a test question", ["Yes", "No"], "Yes");
     turn = new Turn('Yes', card);
 
     expect(turn.returnGuess()).to.equal('Yes');
   })
 
-  it.skip('should have a returnCard() method to return the passed Card instance', () => {
+  it.skip(`should have a returnCard() method to return the passed Card instance`, () => {
     card = new Card(1, "This is a test question", ["Yes", "No"], "Yes");
     turn = new Turn('Yes', card);
 
     expect(turn.returnCard()).to.equal(card);
   })
 
-  it.skip('should have an evaluateGuess() method returning a boolean that indicates if the guess was right or wrong', () => {
+  it.skip(`should have an evaluateGuess() method returning a boolean that indicates if the guess was right or wrong`, () => {
     card = new Card(1, "This is a test question", ["Yes", "No"], "Yes");
     turn = new Turn('Yes', card);
 
@@ -47,5 +47,17 @@ describe('Turn', () => {
     turn = new Turn('No', card);
 
     expect(turn.evaluateGuess()).to.equal(false);
+  })
+
+  it.skip(`should have a giveFeedback() method that returns either 'correct!' or 'incorrect!' if the guess is right or wrong`, () => {
+    card = new Card(1, "This is a test question", ["Yes", "No"], "Yes");
+    turn = new Turn('Yes', card);
+
+    expect(turn.giveFeedback()).to.equal('correct!');
+
+    card = new Card(1, "This is a test question", ["Yes", "No"], "Yes");
+    turn = new Turn('No', card);
+
+    expect(turn.giveFeedback()).to.equal('incorrect!');
   })
 })
