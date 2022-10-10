2545c5171312711ed91705a27009305058fec7f0
James Wasmer <jameswasmer@gmail.com>
2022-10-10 19:21:54 -0400

    Add test for accepting a string argument

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,8 +1,18 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
 describe('Turn', () => {
+  let turn;
+  let card;
+  let guess;
 
+  it.skip('should accept a string argument representing a users guess', () => {
+    guess = 'guess'
+    turn = new Turn(guess)
+
+    expect(turn.guess).to.equal(guess)
+  })
 })
