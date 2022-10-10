c40422bc8e48fbd39775911506f61d42ca371127
James Wasmer <jameswasmer@gmail.com>
2022-10-10 19:43:57 -0400

    Add test for returnCard() method

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -10,22 +10,29 @@ describe('Turn', () => {
   let guess;
 
   it.skip('should accept a string argument representing a users guess', () => {
-    turn = new Turn('guess')
+    turn = new Turn('guess');
 
-    expect(turn.guess).to.equal('guess')
+    expect(turn.guess).to.equal('guess');
   })
 
   it.skip('should accept the currently played card as an argument', () => {
-    card = new Card(1, "This is a test question", ["Yes", "No"], "Yes")
-    turn = new Turn('Yes', card)
+    card = new Card(1, "This is a test question", ["Yes", "No"], "Yes");
+    turn = new Turn('Yes', card);
 
-    expect(turn.card).to.equal(card)
+    expect(turn.card).to.equal(card);
   })
 
-  it.skip('should have a .returnGuess() method to return the users guess', () => {
-    card = new Card(1, "This is a test question", ["Yes", "No"], "Yes")
-    turn = new Turn('Yes', card)
+  it.skip('should have a returnGuess() method to return the users guess', () => {
+    card = new Card(1, "This is a test question", ["Yes", "No"], "Yes");
+    turn = new Turn('Yes', card);
 
-    expect(turn.returnGuess()).to.equal('Yes')
+    expect(turn.returnGuess()).to.equal('Yes');
+  })
+
+  it.skip('should have a returnCard() method to return the passed Card instance', () => {
+    card = new Card(1, "This is a test question", ["Yes", "No"], "Yes");
+    turn = new Turn('Yes', card);
+
+    expect(turn.returnCard()).to.equal(card);
   })
 })
