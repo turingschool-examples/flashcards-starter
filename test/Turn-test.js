b9f1492123c438498488b1d4d8a0eece76003ee4
James Wasmer <jameswasmer@gmail.com>
2022-10-10 19:40:04 -0400

    Refactor Turn-test.js tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -10,17 +10,17 @@ describe('Turn', () => {
   let guess;
 
   it.skip('should accept a string argument representing a users guess', () => {
-    guess = 'guess'
-    turn = new Turn(guess)
+    turn = new Turn('guess')
 
-    expect(turn.guess).to.equal(guess)
+    expect(turn.guess).to.equal('guess')
   })
-  
+
   it.skip('should accept the currently played card as an argument', () => {
-    guess = 'guess'
     card = new Card(1, "This is a test question", ["Yes", "No"], "Yes")
-    turn = new Turn(guess, card)
+    turn = new Turn('Yes', card)
 
     expect(turn.card).to.equal(card)
   })
+
+
 })
