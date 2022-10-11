104d926809ea9dac09d6decd9180fadcba137838
Josh Pierce <joshua.alan.pierce@gmail.com>
2022-10-10 20:08:15 -0600

    Adds static code for the card class

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+class Card {
+    constructor(id, question, answers, correctAnswer) {
+        this.id = id
+        this.question = question
+        this.answers = answers
+        this.correctAnswer = 'object'
+    }
+}
+
+
+
+
+module.exports = Card;
