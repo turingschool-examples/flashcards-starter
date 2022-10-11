6dfbf54218fa4d8515fb82a10c6e748cc6ac8695
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-10-10 19:52:22 -0600

    Update Cards class in Turns Class test suite

--- test/Turns-test.js
+++ test/Turns-test.js
@@ -16,40 +16,40 @@ describe('Turns', function() {
     });
 
     it.skip('should store Card class as argument', function() {
-        const card = new Card((1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter'));
-        const turns = new Turns('guess', {id: 1, question: 'What is Robbie\'s favorite animal?', answers: ['sea otter', 'pug', 'capybara'], correctAnswer:  'sea otter'})
-        expect(turns.card).to.deep.equal({id: 1, question: 'What is Robbie\'s favorite animal?', answers: ['sea otter', 'pug', 'capybara'], correctAnswer:  'sea otter'});
+        const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+        const turns = new Turns('guess', {id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'})
+        expect(turns.card).to.deep.equal({id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
     });
     it.skip('should return the guess', function() {
-        const turns = new Turns( 'sea otter');
-        expect(turns.returnGuess()).to.equal( 'sea otter');
+        const turns = new Turns('object');
+        expect(turns.returnGuess()).to.equal( 'object');
     });
     it.skip('should return the Card', function() {
-        const card = new Card(1, 'What is Robbie\'s favorite animal?', ['sea otter', 'pug', 'capybara'], 'sea otter')
-        const turns = new Turns('guess', {id: 1, question: 'What is Robbie\'s favorite animal?', answers: ['sea otter', 'pug', 'capybara'], correctAnswer:  'sea otter'});
-        expect(turns.returnCard()).to.deep.equal({id: 1, question: 'What is Robbie\'s favorite animal?', answers: ['sea otter', 'pug', 'capybara'], correctAnswer:  'sea otter'});
+        const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'],'object')
+        const turns = new Turns('object', {id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
+        expect(turns.returnCard()).to.deep.equal({id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
     });
     it.skip('should return true if user\'s guess is correct', function() {
-        const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
-        const turns = new Turns('sea otter', {id: 1, question: 'What is Robbie\'s favorite animal?', answers: ['sea otter', 'pug', 'capybara'], correctAnswer:  'sea otter'});
+        const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'],'object')
+        const turns = new Turns('object', {id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
 
         expect(turns.evaluateGuess()).to.deep.equal(true)
     });
     it.skip('should return false if user\'s guess is incorrect', function() {
-        const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
-        const turns = new Turns('pug', {id: 1, question: 'What is Robbie\'s favorite animal?', answers: ['sea otter', 'pug', 'capybara'], correctAnswer:  'sea otter'});
+        const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'],'object')
+        const turns = new Turns('array', {id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
 
         expect(turns.evaluateGuess()).to.deep.equal(false)
     });
     it.skip('should return correct if guess is correct', function() {
-        const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
-        const turns = new Turns('sea otter', {id: 1, question: 'What is Robbie\'s favorite animal?', answers: ['sea otter', 'pug', 'capybara'], correctAnswer:  'sea otter'});
+        const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'],'object')
+        const turns = new Turns('object', {id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
 
         expect(turns.giveFeedback()).to.deep.equal('Correct!')
     });
     it.skip('should return incorrect if guess is incorrect', function() {
-        const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
-        const turns = new Turns('pug', {id: 1, question: 'What is Robbie\'s favorite animal?', answers: ['sea otter', 'pug', 'capybara'], correctAnswer:  'sea otter'});
+        const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'],'object')
+        const turns = new Turns('array', {id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
 
         expect(turns.giveFeedback()).to.deep.equal('Incorrect!')
     })
