2a827c75470a84e43b533855ea9cf00f7172dbfe
James Wasmer <jameswasmer@gmail.com>
2022-10-11 07:45:26 -0400

    Prepare Round-test.js to have tests added

--- test/Round-test.js
+++ test/Round-test.js
@@ -3,3 +3,9 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Round = require('../src/Round');
+
+describe(`Round`, () => {
+  it.skip(`should have a returnCurrentCard() method that returns the current card being played`, () =>  {
+    
+  })
+})
