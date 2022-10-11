82c05808be2ae103fb016916c3ad671aa9410689
Angela Wirth <angiewirth92@gmail.com>
2022-10-11 10:14:05 -0400

    Add to each method

--- src/Turns.js
+++ src/Turns.js
@@ -1,22 +1,29 @@
 class Turns {
   constructor() {
-    this.currentCard = new Card()
-    this.userGuess = ''
+    this.currentCard = new Card(id, question, userAnswer, correctAnswer)
+    this.userGuess = false
   }
 
   returnGuess() {
-
+    return this.userGuess
   }
 
   returnCard() {
+    return this.currentCard
 
   }
   
   evaluateGuess() {
-
+    if (this.userGuess === this.currentCard.correctAnswer) {
+      this.userGuess = true
+    }
   }
 
   giveFeedback() {
-    
+    if (this.userGuess === this.currentCard.correctAnswer) {
+      return 'Correct!'
+    } else {
+      return 'Incorrect!'
+    }
   }
 }
