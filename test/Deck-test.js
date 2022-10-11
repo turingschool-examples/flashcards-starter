23523d4c52e88cbf3a242f2f82bf106ff89de77e
Eleanor Ruth <ellie.ruth@me.com>
2022-10-10 21:00:26 -0600

    Add setup for testing

---
+++ test/Deck-test.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck')
+
+describe('Deck', () => {
+  it('should be a function', () => {
+    const deck = new Deck()
+    expect(Deck).to.be.a('function')
+  })
+  it('should hold an array of Cards', () => {
+    const 
+  })
+})
