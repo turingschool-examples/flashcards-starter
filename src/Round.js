33512f122058a985410682dd1019369451e0d4df
Ryan Nagel <RNagel29@gmail.com>
2022-10-11 17:07:42 -0600

    Add Round class with all testing

--- src/Round.js
+++ src/Round.js
@@ -1,8 +1,35 @@
+const Turn = require("./Turn");
+
 class Round {
-    constructor() {
-        
+    constructor(deck) {
+        this.deck = deck;
+        this.currentCard = deck.cards[0];
+        this.turns = 0;
+        this.incorrectGuesses = [];
+    }
+
+    returnCurrentCard() {
+        return this.currentCard;
+    }
+
+    takeTurn(guess) {
+        const turn = new Turn(guess, this.currentCard);
+        if (!turn.evaluateGuess()) {
+            this.incorrectGuesses.push(this.currentCard.id);
+        }
+        turn.giveFeedback();
+        this.turns++;
+        this.currentCard = this.deck.cards[this.turns];
+    }
+
+    calculatePercentCorrect() {
+        const percent = (1 - this.incorrectGuesses.length / this.turns) * 100;
+        return percent.toFixed(2) + '%';
+    }
+
+    endRound() {
+        return `**Round over!** You answered ${this.calculatePercentCorrect()} of the questions correctly!`
     }
 }
 
-
-odule.exports = Round;
+module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,9 +1,65 @@
 const chai = require('chai');
 const expect = chai.expect;
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 describe('Round', () => {
+    let card1;
+    let card2;
+    let card3;
+    let cards;
+    let deck;
+    let round;
+
+    beforeEach(() => {
+        card1 = new Card(1, 'What is my name?', ['Ryan', 'Michael', 'Bob'], 'Ryan');
+        card2 = new Card(2, 'What is my middle name?', ['Bryan', 'Adam', 'Matthew'], 'Matthew');
+        card3 = new Card(3, 'Who is the best dog?', ['Kya', 'Lou', 'Toby'], 'Lou');
+        cards = [card1, card2, card3];
+        deck = new Deck(cards);
+        round = new Round(deck);
+    })
 
     it('should be able to return the current card', () => {
-        
+        expect(round.returnCurrentCard()).to.equal(card1);
+    })
+
+    it('when taking a turn, should update the turn count', () => {
+        round.takeTurn('Ryan');
+        expect(round.turns).to.equal(1);
+
+        round.takeTurn('Ryan');
+        expect(round.turns).to.equal(2);
+    })
+
+    it('when taking a turn, should change the current card', () => {
+        round.takeTurn('Ryan');
+        expect(round.currentCard).to.equal(card2);
+
+        round.takeTurn('Ryan');
+        expect(round.currentCard).to.equal(card3);
+    })
+
+    it('when taking a turn and guessing incorrectly, should push card id into incorrectGuesses', () => {
+        round.takeTurn('Michael');
+        expect(round.incorrectGuesses).to.deep.equal([1]);
+
+        round.takeTurn('Bryan');
+        expect(round.incorrectGuesses).to.deep.equal([1, 2]);
+    })
+
+    it('should be able to calculate and return the percentage of correct guesses', () => {
+        round.takeTurn('Ryan');
+        round.takeTurn('Bryan');
+        round.takeTurn('Lou');
+        expect(round.calculatePercentCorrect()).to.equal('66.67%')
+    })
+
+    it('should be able to end round and provide results', () => {
+        round.takeTurn('Ryan');
+        round.takeTurn('Bryan');
+        round.takeTurn('Lou');
+        expect(round.endRound()).to.equal('**Round over!** You answered 66.67% of the questions correctly!')
     })
 })
