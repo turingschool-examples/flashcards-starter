e0c1b842d9bf49552c2f283d8c43da5cc34834b8
Adelle Pitsas <apitsas4@gmail.com>
2022-10-11 19:08:27 -0400

    created test and implementation code for currentCard feature

--- test/round-test.js
+++ test/round-test.js
@@ -7,20 +7,25 @@ const Deck = require('../src/deck');
 
 
 describe('Round', function() {
-
-    it('should be an instantiation of Round', () => {
-        const round = new Round();
-
-        expect(round).to.be.an.instanceOf(Round);
-    })
     
-    it('should take in a deck', () => {
+    it('should take in the cards in a deck', () => {
         const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
         const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
         const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
         const deck = new Deck([card1, card2, card3]);
         const round = new Round(deck);
 
-        expect(round.deck).to.deep.equal(deck);
+        expect(round.deck).to.deep.equal(deck.cards);
+    })
+
+    it('should keep track of the current card in the deck', () => {
+        const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+        const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+        const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+        const deck = new Deck([card1, card2, card3]);
+        const round = new Round(deck);
+
+        expect(round.currentCard).to.deep.equal(card1);
+
     })
 })
