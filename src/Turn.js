9c2c96f736a45b1034412efa15b6eb1020ffa944
Ryan Nagel <RNagel29@gmail.com>
2022-10-10 18:32:55 -0600

    Add class of Turn & Card, and add testing for Turn

---
+++ src/Turn.js
@@ -0,0 +1,31 @@
+class Turn {
+    constructor(guess, card) {
+        this.guess = guess;
+        this.card = card;
+        this.isCorrect;
+    }
+
+    returnGuess() {
+        return this.guess;
+    }
+
+    returnCard() {
+        return this.card;
+    }
+
+    evaluateGuess() {
+        this.isCorrect = this.guess === this.card.correctAnswer;
+        return this.isCorrect;
+    }
+
+    giveFeedback() {
+       if (this.isCorrect) {
+        return 'Correct! 😎';
+       } else {
+        return 'Incorrect! 🙁';
+        }
+    }
+
+}
+
+module.exports = Turn;
