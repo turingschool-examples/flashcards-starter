8cfbbbb6bd5129e4abf9c35b8049f34145941eba
Tim T <tim.email.personal@gmail.com>
2022-10-10 17:09:02 -1000

    Complete turn test and class

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,30 @@
+const Card = require('../src/Card')
+
 class Turn {
-  constructor() {
-    
+  constructor(userGuess, cardObject) {
+    this.userGuess = userGuess
+    this.card = cardObject
+
+  }
+  returnGuess() {
+    return this.userGuess
+  }
+  returnCard() {
+    return this.card
+  }
+  evaluateGuess() {
+    if (this.userGuess === this.card.correctAnswer) {
+      return true 
+    } else {
+      return false 
+    }
+  }
+  giveFeedback() {
+    if (this.evaluateGuess() === true) {
+      return 'correct!'
+    } else {
+      return 'incorrect!'
+    }
   }
 }
 
