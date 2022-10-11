4c5ebc4cc37281e35a826fbabe9b910d51726214
Adelle Pitsas <apitsas4@gmail.com>
2022-10-11 18:47:21 -0400

    create first tests to set up round class

---
+++ test/round-test.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/round');
+const Card = require('../src/Card');
+const Deck = require('../src/deck');
+
+
+describe('Round', function() {
+
+    it('should be an instantiation of Round', () => {
+        const round = new Round();
+
+        expect(round).to.be.an.instanceOf(Round);
+    })
+    
+    it('should take in a deck', () => {
+        const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+        const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+        const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+        const deck = new Deck([card1, card2, card3]);
+        const round = new Round(deck);
+
+        expect(round.deck).to.deep.equal(deck);
+    })
+})
