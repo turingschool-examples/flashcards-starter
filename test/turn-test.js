74ef38de4816e4a0a54212ca74b14df9d724f169
Adelle Pitsas <apitsas4@gmail.com>
2022-10-11 09:18:31 -0400

    update turn class with its properties

--- test/turn-test.js
+++ test/turn-test.js
@@ -4,21 +4,25 @@ const expect = chai.expect;
 const Turn = require('../src/turn');
 const Card = require('../src/Card');
 
-describe('Card', function() {
+describe('Turn', function() {
     
     it('should be an instance of Turn', () => {
       const turn = new Turn();
+
       expect(turn).to.be.an.instanceOf(Turn);
     });
 
     it('should take in the user\'s guess', () => {
         const turn = new Turn('pug');
+
         expect(turn.guess).to.equal('pug');
     });
 
     it('should take in the card as an argument', () => {
         const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
         const turn = new Turn('pug', card);
+
+        expect(turn.card).to.equal(card);
     })
 
 });
