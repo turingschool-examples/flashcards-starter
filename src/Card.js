3e534963075abf5f6d591aea7fc49c24b0542ae5
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-10-10 21:54:51 -0400

    started card Class boilerPlate solved

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+class Card {
+	constructor(num, question,possibilities,correct) {
+		this.question = question;
+		this.answers = possibilities;
+		this.correctAnswer = correct;
+  }
+};
+
+
+
+
+
+
+
+module.exports = Card;
