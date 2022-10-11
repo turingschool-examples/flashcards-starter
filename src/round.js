b1a07acd15c9f694c7ffef960b9e9da2a7c8673a
Adelle Pitsas <apitsas4@gmail.com>
2022-10-11 19:18:13 -0400

    adds test and implementation code for the returnCurrentCard method on round class

--- src/round.js
+++ src/round.js
@@ -3,6 +3,10 @@ class Round{
         this.deck = deck.cards;
         this.currentCard = deck.cards[0];
     }
+
+    returnCurrentCard() {
+        return this.currentCard;
+    }
 }
 
 module.exports = Round;
--- test/round-test.js
+++ test/round-test.js
@@ -26,6 +26,17 @@ describe('Round', function() {
         const round = new Round(deck);
 
         expect(round.currentCard).to.deep.equal(card1);
+    })
 
+    it('should have a method that returns the current card being played', () => {
+        const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+        const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+        const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+        const deck = new Deck([card1, card2, card3]);
+        const round = new Round(deck);
+
+        const currentCard1 = round.returnCurrentCard()
+        
+        expect(currentCard1).to.deep.equal(card1);
     })
 })
