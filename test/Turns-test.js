9a1b87b9233373aad19929a4fd65e4a8e26211d6
Jacob Zimmer <jacobzimmer1@outlook.com>
2022-10-11 15:35:36 -0600

    refactor: removed skips and fixed naming

--- test/Turns-test.js
+++ test/Turns-test.js
@@ -6,58 +6,58 @@ const Card = require('../src/Card')
 
 describe('Turns', () => {
     
-    it.skip('should be a function', () => {
+    it('should be a function', () => {
         expect(Turns).to.be.a('function')
     })
 
-    it.skip('should an instance of Turn', () => {
-        const Turns = new Turns()
+    it('should an instance of Turn', () => {
+        const turns = new Turns()
 
-        expect(Turns).to.be.a.instanceOf(Turns)
+        expect(turns).to.be.a.instanceOf(Turns)
     })
 
-    it.skip('should have a guess', () => {
+    it('should have a guess', () => {
         const turns = new Turns('object')
 
         expect(turns.guess).to.equal('object')
     })
 
-    it.skip('should have a card', () => {
+    it('should have a card', () => {
         const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ["object", "array", "function"], 'object')
         const turns = new Turns('object', card)
 
-        expect(turn.card.question).to.deep.equal('What allows you to define a set of related information using key-value pairs?')
-        expect(turn.guess).to.equal('object')
+        expect(turns.card.question).to.deep.equal('What allows you to define a set of related information using key-value pairs?')
+        expect(turns.guess).to.equal('object')
     })
 
-    it.skip('should return the guess', () => {
+    it('should return the guess', () => {
         const turns = new Turns('guess')
 
         expect(turns.returnGuess()).to.equal('guess')
     })
 
-    it.skip('should return the card', () => {
+    it('should return the card', () => {
         const card = new Card(2, 'What is a comma-separated list of related values?', ["array", "object", "function"], 'array')
-        const turn = new Turn('array', card)
+        const turns = new Turns('array', card)
 
-        expect(turn.returnCard()).to.be.an.instanceof(Card)    
+        expect(turns.returnCard()).to.be.an.instanceof(Card)    
     })
 
-    it.skip('should determine the correct answer', () => {
+    it('should determine the correct answer', () => {
         const card = new Card(2, 'What is a comma-separated list of related values?', ["array", "object", "function"], 'array')
-        const turn1 = new Turn('array', card)
-        const turn2 = new Turn('object', card)
+        const turns1 = new Turns('array', card)
+        const turns2 = new Turns('object', card)
 
-        expect(turn1.evaluateGuess()).to.equal(true)
-        expect(turn2.evaluateGuess()).to.equal(false)
+        expect(turns1.evaluateGuess()).to.equal(true)
+        expect(turns2.evaluateGuess()).to.equal(false)
     })
 
-    it.skip('should give feedback', () => {
+    it('should give feedback', () => {
         const card = new Card(2, 'What is a comma-separated list of related values?', ["array", "object", "function"], 'array')
-        const turn1 = new Turn('array', card)
-        const turn2 = new Turn('object', card)
+        const turns1 = new Turns('array', card)
+        const turns2 = new Turns('object', card)
 
-        expect(turn1.giveFeedback()).to.equal('Correct!')
-        expect(turn2.giveFeedback()).to.equal('Incorrect!')
+        expect(turns1.giveFeedback()).to.equal('Correct!')
+        expect(turns2.giveFeedback()).to.equal('Incorrect!')
     })
 })
