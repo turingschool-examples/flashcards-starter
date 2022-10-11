62eea58cf0678628f0b9f48ad649691bfc648bc1
Spencer Haka <spencer.haka@gmail.com>
2022-10-10 20:53:20 -0600

    Write evaluateGuess and giveFeedback methods

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -51,17 +51,42 @@ describe('Turn', function() {
 
   it.skip('should have a method that returns the card', () => {
     var card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    var turn = new Turn('khakis', card);
+    var turn = new Turn('Jake from State Farm', card);
     var testCard = {
       id: 1,
       question: 'What is Robbie\'s favorite animal',
       answers: ['sea otter', 'pug', 'capybara'],
       correctAnswer: 'sea otter'
     }
+    var returnedCard = turn.returnCard();
 
-    expect(turn.card).to.eql(testCard);
+    expect(returnedCard).to.eql(testCard);
   })
 
-  it.skip('')
+  it.skip('should be able to determine if the answer is correct', () => {
+    var card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    var card2 = new Card(2, 'What is the best 2000\'s emo band?', ['Emery', 'My Chemical Romance', 'The Starting Line', 'Mae', 'Copeland', 'Senses Fail'], 'Copeland');
+    var turn = new Turn('sea otter', card);
+    var turn2 = new Turn('My Chemical Romance', card2);
+    
+    var cardAnswer = turn.evaluateGuess();
+    var card2Answer = turn2.evaluateGuess();
+
+    expect(cardAnswer).be.true;
+    expect(card2Answer).be.false;
+  })
+
+  it.skip('should tell the player is their answer was correct or incorrect', () => {
+    var card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    var card2 = new Card(2, 'What is the best 2000\'s emo band?', ['Emery', 'My Chemical Romance', 'The Starting Line', 'Mae', 'Copeland', 'Senses Fail'], 'Copeland');
+    var turn = new Turn('sea otter', card);
+    var turn2 = new Turn('My Chemical Romance', card2);
+    
+    var feedback = turn.giveFeedback();
+    var feedback2 = turn2.giveFeedback();
+
+    expect(feedback).to.equal('correct!');
+    expect(feedback2).to.equal('incorrect!');
+  })
 
 })
