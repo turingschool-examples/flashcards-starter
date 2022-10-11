1cd6e527e6d048b6d79b42f09cd7a74fca307c08
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-10-11 10:42:44 -0400

    test to see if Turn is building correctly

--- src/Turn.js
+++ src/Turn.js
@@ -1,7 +1,33 @@
+class Turn {
+    constructor( guess, cardDetails){
+    this.guess = guess;
+    this.card = cardDetails;
+    this.won = false;
+  }
+ 
+  returnGuess() {
+ return this.guess;
+ }
 
+ returnCard() {
+    return this.card;
+ }
+ evaluateGuess(card) {
+   if(this.guess === card.correctAnswer) {
+     this.won = true;
+    return true
+   } else {
+    this.won = false;
+    return false;
+   }
+ }
+ giveFeedBack(){
+    if(this.won === true){
+      return 'Correct'  
+    } else {
+        return Incorrect
+    }
+ }
+};
 
-
-
-
-
-module.exports = Turn;
+module.exports = Turn; 
