db95bda9d5f4b221309a4b71743f088fecd3326c
Adelle Pitsas <apitsas4@gmail.com>
2022-10-11 10:55:15 -0400

    create tests for countCards method

--- test/deck-test.js
+++ test/deck-test.js
@@ -18,6 +18,17 @@ describe('Deck', function() {
         const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
         const deck = new Deck([card1, card2, card3]);
 
-        expect(deck.cards).to.deeply.equal([card1, card2, card3])
+        expect(deck.cards).to.deep.equal([card1, card2, card3])
+    })
+
+    it('should be able to count the cards in the deck', () => {
+        const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+        const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+        const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+        const deck = new Deck([card1, card2, card3]);
+
+        let cardAmount = deck.countCards();
+
+        expect(cardAmount).to.equal(3);
     })
 })
