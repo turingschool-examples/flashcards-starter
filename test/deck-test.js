171bf78bef2dff5aac2407d18b79d839ac011cb2
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-10-10 20:03:39 -0600

    Begin writing tests in deck test suite

--- test/deck-test.js
+++ test/deck-test.js
@@ -2,3 +2,17 @@ const chai = require('chai');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const expect = chai.expect;
+
+describe('Deck', function() {
+
+    it.skip('should be a function', function() {
+        const deck = new Deck();
+        expect(Deck).to.be.a('function');
+    });
+
+    it.skip('should be an instance of Deck', function() {
+        const deck = new Deck();
+        expect(deck).to.be.an.instanceof(Deck);
+    })
+
+})
