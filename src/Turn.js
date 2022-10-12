cbd53729c6669c2b57a81556fedbe50ed37544a2
Eleanor Ruth <ellie.ruth@me.com>
2022-10-12 07:25:21 -0600

    Refactor evaluateGuess method

--- src/Turn.js
+++ src/Turn.js
@@ -10,16 +10,18 @@ class Turn {
     return this.card
   }
   evaluateGuess() {
-    if(this.guess === this.card.correctAnswer) {
-      return true
-    } else {
-      return false
-    }
+    return this.guess === this.card.correctAnswer
+    // if(this.guess === this.card.correctAnswer) {
+    //   return true
+    // } else if (this.guess != this.card.correctAnswer) {
+    //   return false
+    // }
+    //Return true or false? Ask Sarah if this is the same thing?
   }
   giveFeedback() {
-    if(this.evaluateGuess() === true) {
+    if(this.evaluateGuess()) {
       return "correct!"
-    } else {
+    } else if (!this.evaluateGuess()) {
       return "incorrect!"
     }
   }
