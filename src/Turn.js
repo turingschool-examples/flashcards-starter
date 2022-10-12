1f05816daa39933e6e0cf3086551dd04dd9f4cd9
Dana Truong <tramtramphotos@gmail.com>
2022-10-11 21:13:40 -0500

    Refactor iteration 1

--- src/Turn.js
+++ src/Turn.js
@@ -1,5 +1,3 @@
-const Card = require('../src/Card')
-
 class Turn {
   constructor(guess, currentCard) {
     this.guess = guess
@@ -33,12 +31,5 @@ const turn1 = new Turn('Oklahoma City', card1)
 
 const card2 = new Card(1, 'What is the capital of Texas?', ['Houston', 'Dallas', 'Austin'], 'Austin')
 const turn2 = new Turn('Houston', card2)
-
-console.log(card1)
-console.log(turn1)
-console.log(turn1.evaluateGuess())
-console.log(turn1.giveFeedback())
-console.log(turn2.evaluateGuess())
-console.log(turn2.giveFeedback())
 
 module.exports = Turn
