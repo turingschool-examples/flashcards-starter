7262aa8246487cd101b6fd2ecdc1cd5ad480b7af
Courtney Lippman <courtney.lippman@colorado.edu>
2022-10-11 21:25:35 -0600

    Add passing functionality.

---
+++ src/Turn.js
@@ -0,0 +1,35 @@
+
+class Turn {
+    constructor(guess, card) {
+        this.guess = guess
+        this.card = card
+
+    };
+
+    returnGuess() {
+        return this.guess;
+    };
+
+    returnCard() {
+        return this.card;
+    };
+
+    evaluateGuess() {
+        if (this.guess === this.card.correctAnswer) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    giveFeedback() {
+        if (this.evaluateGuess()) {
+            return 'correct!';
+        } else if (!this.evaluateGuess()) {
+            return 'incorrect!';
+        }
+    };
+};
+
+
+module.exports = Turn;
