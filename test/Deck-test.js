f73008e05e764ea9b3f28a1907e07b22e5ec469f
Dana Truong <tramtramphotos@gmail.com>
2022-10-11 22:15:24 -0500

    Add Deck class and Deck tests

---
+++ test/Deck-test.js
@@ -0,0 +1,42 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const Card = require('../src/Card')
+const Turn = require('../src/Turn')
+const Deck = require('../src/Deck')
+
+describe('Deck', () => {
+
+  it('should be a function', () => {
+    expect(Deck).to.be.a('function')
+  })
+
+  it('should be an instance of Deck', () => {
+    const deck = new Deck()
+    expect(deck).to.be.an.instanceOf(Deck)
+  })
+
+  it('should store an array', () => {
+    const card1 = new Card(1, 'What is the capital of Texas?', ['Austin', 'Houston', 'Dallas'], 'Austin')
+    const card2 = new Card(2, 'What is the capital of Louisiana?', ['New Orleans', 'Lafayette', 'Lake Charles'], 'New Orleans')
+    const card3 = new Card(3, 'What is the capital of Colorado?', ['Boulder', 'Denver', 'Aspen'], 'Denver')
+    const deck = new Deck([card1, card2, card3])
+    expect(deck.cardCollection).to.be.an('array')
+  })
+
+  it('should only contain Card objects in the array', () => {
+    const card1 = new Card(1, 'What is the capital of Texas?', ['Austin', 'Houston', 'Dallas'], 'Austin')
+    const card2 = new Card(2, 'What is the capital of Louisiana?', ['New Orleans', 'Lafayette', 'Lake Charles'], 'New Orleans')
+    const card3 = new Card(3, 'What is the capital of Colorado?', ['Boulder', 'Denver', 'Aspen'], 'Denver')
+    const deck = new Deck([card1, card2, card3])
+    expect(deck.cardCollection[0]).to.be.an('object')
+  })
+
+  it('should return length of card collection array with the method countCards()', () => {
+    const card1 = new Card(1, 'What is the capital of Texas?', ['Austin', 'Houston', 'Dallas'], 'Austin')
+    const card2 = new Card(2, 'What is the capital of Louisiana?', ['New Orleans', 'Lafayette', 'Lake Charles'], 'New Orleans')
+    const card3 = new Card(3, 'What is the capital of Colorado?', ['Boulder', 'Denver', 'Aspen'], 'Denver')
+    const deck = new Deck([card1, card2, card3])
+    expect(deck.countCards()).to.equal(3)
+  })
+})
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -10,16 +10,19 @@ describe('Turn', () => {
     const turn = new Turn()
     expect(Turn).to.be.a('function')
   })
+
   it('should be an instance of Turn', () => {
     const turn = new Turn()
     expect(turn).to.be.an.instanceOf(Turn)
   })
+
   it('should store a guess', () => {
     const card = new Card(1, 'What is the capital of Texas?', ['Austin', 'Houston', 'Dallas'], 'Austin')
     const turn = new Turn('Dallas', card)
     expect(turn.guess).to.be.a('string')
     expect(turn.guess).to.equal('Dallas')
   })
+  
   it('should store a card', () => {
     const card = new Card(1, 'What is the capital of Texas?', ['Austin', 'Houston', 'Dallas'], 'Austin')
     const turn = new Turn('Austin', card)
@@ -31,11 +34,13 @@ describe('Turn', () => {
       correctAnswer: 'Austin'
     })
   })
+
   it('should be able to return the guess with the method returnGuess()', () => {
     const card = new Card(1, 'What is the capital of Louisiana?', ['New Orleans', 'Lafayette', 'Lake Charles'], 'New Orleans')
     const turn = new Turn('New Orleans', card)
     expect(turn.returnGuess()).to.equal('New Orleans')
   })
+  
   it('should be able to return the card object with the method returnCard()', () => {
     const card = new Card(1, 'What is the capital of Colorado?', ['Boulder', 'Denver', 'Aspen'], 'Denver')
     const turn = new Turn('Denver ', card)
@@ -46,6 +51,7 @@ describe('Turn', () => {
       correctAnswer: 'Denver'
     })
   })
+
   it('should return true boolean if guess is correct with the method evaluateGuess()', () => {
     const card1 = new Card(1, 'What is the capital of California?', ['Sacramento', 'Los Angeles', 'San Diego'], 'Sacramento')
     const card2 = new Card(2, 'What is the capital of Washington?', ['Seattle', 'Olympia', 'Bellevue'], 'Olympia')
@@ -56,6 +62,7 @@ describe('Turn', () => {
     expect(turn2.evaluateGuess()).to.be.a('boolean')
     expect(turn2.evaluateGuess()).to.equal(true)
   })
+
   it('should return false boolean if guess is incorrect with the method evaluateGuess()', () => {
     const card1 = new Card(1, 'What is the capital of Florida?', ['Orlando', 'Miami', 'Tallahassee'], 'Tallahassee')
     const card2 = new Card(2, 'What is the capital of Georgia?', ['Atlanta', 'Savannah', 'Augusta'], 'Atlanta')
@@ -66,6 +73,7 @@ describe('Turn', () => {
     expect(turn2.evaluateGuess()).to.be.a('boolean')
     expect(turn2.evaluateGuess()).to.equal(false)
   })
+
   it('should return "Correct!" if the guess is correct with the method giveFeedback()', () => {
     const card1 = new Card(1, 'What is the capital of Oklahoma?', ['Tulsa', 'Oklahoma City', 'Norman'], 'Oklahoma City')
     const card2 = new Card(2, 'What is the capital of Maryland?', ['Annapolis', 'Baltimore', 'Beltsville'], 'Annapolis')
@@ -76,6 +84,7 @@ describe('Turn', () => {
     expect(turn2.giveFeedback()).to.be.a('string')
     expect(turn2.giveFeedback()).to.equal('Correct!')
   })
+
   it('should return "Incorrect!" if the guess is incorrect with the method giveFeedback()', () => {
     const card1 = new Card(1, 'What is the capital of Tennessee?', ['Chattanooga', 'Memphis', 'Nashville'], 'Nashville')
     const card2 = new Card(2, 'What is the capital of Alabama?', ['Montgomery', 'Birmingham', 'Mobile'], 'Montgomery')
