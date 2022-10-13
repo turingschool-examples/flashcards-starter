ff94a6d43d72955b4fcdb50f12a102ee92f9d7f9
James Wasmer <jameswasmer@gmail.com>
2022-10-13 10:23:11 -0400

    Add test for creating new Turn instance and iterating current card

--- test/Round-test.js
+++ test/Round-test.js
@@ -5,6 +5,7 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Round = require('../src/Round');
 const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
 
 describe(`Round`, () => {
   let card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
@@ -33,10 +34,47 @@ describe(`Round`, () => {
     expect(round.takeTurn()).to.be.a('function')
   })
 
+  it.skip(`takeTurn() method should create a new Turn class instance`, () => {
+    round = new Round()
+    deck = new Deck(cards)
+    guess = 'guess'
+
+    round.takeTurn(guess, round.returnCurrentCard())
+
+    expect(round.turn.returnGuess()).to.equal(guess)
+    expect(round.returnCurrentCard()).to.equal(deck.cards[1])
+  })
+
+  it.skip(`takeTurn() method should accept a guess as a parameter`, () => {
+    round = new Round()
+    guess = 'guess'
+    round.takeTurn(guess)
+
+    expect(round.guess).to.equal(guess)
+  })
+
   it.skip(`takeTurn() method should update turns count`, () => {
     round = new Round()
+    const turnCheckBefore = round.turns
+    
     round.takeTurn()
+    const turnCheckAfter = round.turns
 
-    expect(round.takeTurn())
+    expect(turnCheckAfter).to.equal(turnCheckBefore + 1)
   })
+
+  it.skip(`the next card should become the current card`, () => {
+    deck = new Deck(cards)
+    round = new Round()
+    guess = 'guess'
+
+    round.takeTurn(guess)
+
+    expect(round.returnCurrentCard()).to.equal(cards[1])
+  })
+
+  it.skip(`the guess should be evaluated`, () => {
+
+  })
+
 })
