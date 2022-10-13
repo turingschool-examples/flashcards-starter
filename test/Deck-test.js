4c0af878f10ac9740d9660b04b321aa104e327a6
Eleanor Ruth <ellie.ruth@me.com>
2022-10-12 19:19:53 -0600

    Remove space

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -21,7 +21,6 @@ describe('Deck', () => {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-
     const deck = new Deck([card1, card2, card3]);
 
     deck.countCards()
