15293c221b2a3b55fda5e83626db1fbabdcbf982
Jenn Yacoubian <jyacoubian@gmail.com>
2022-10-12 19:45:09 -0600

    Created unit tests for Turn class

---
+++ test/Turn-test.js
@@ -0,0 +1,52 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Turn = require('../src/Test');
+
+describe('Turn', function() {
+  it('should be function', function() {
+    const turn = new Turn ();
+    expect(Turn).to.be.a.('function');
+  });
+
+  it('should be an instance of Turn that takes two arguments', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it('should takes two argument, user guess and Card in play', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+    const turn = new Turn('pug', card1);
+    expect(turn.guess).to.equal('pug');
+    expect(turn.currentCard).to.deep.equal(card1);
+  });
+
+  it('should have a method that returns the guess', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+    const turn = new Turn('pug', card1);
+    expect(turn.returnGuess()).to.equal('pug');
+  });
+
+  it('should have a method that returns the card', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+    const turn = new Turn('pug', card1);
+    expect(turn.returnCard()).to.deep.equal(card1);
+  });
+
+  it('should have a method that evaluates the guess', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+    const turn1 = new Turn('pug', card1);
+    const turn2 = new Turn('sea otter', card1);
+    expect(turn1.evaluateGuess()).to.equal(true);
+    expect(turn2.evaluateGuess()).to.equal(false);
+  });
+
+  it('should have a method that gives feedback', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+    const turn1 = new Turn('pug', card1);
+    const turn2 = new Turn('sea otter', card1);
+    expect(turn1.giveFeedback()).to.equal("Incorrect!");
+    expect(turn2.giveFeedback()).to.equal("Correct!");
+  });
+});
