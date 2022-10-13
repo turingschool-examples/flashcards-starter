4d2b07bf78d42a83d501e5950193b6dce74b677d
Eleanor Ruth <ellie.ruth@me.com>
2022-10-12 18:07:52 -0600

    Complete Game class

--- src/Game.js
+++ src/Game.js
@@ -2,16 +2,38 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Round = require('./Round')
+const Card = require('./Card')
+const Deck = require('./Deck')
+
 class Game {
-  constructor() {}
+  constructor(data) {
+    this.currentRound
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
-
   printQuestion(round) {
       util.main(round);
+  }
+  createCard() {
+   return prototypeQuestions.map((cardInfo) => {
+     return new Card(cardInfo.id, cardInfo.question, cardInfo.answers, cardInfo.correctAnswer)
+    })
+  }
+  makeDeck() {
+    return new Deck(this.createCard())
+  }
+  createRound() {
+    this.currentRound = new Round(this.makeDeck())
+    return this.currentRound
+  }
+  start() {
+    this.printMessage(this.makeDeck(), this.createRound())
+    this.printQuestion(this.createRound())
+   
   }
 }
 
