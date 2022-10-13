56fa31869ba1ea4db4d4c6ca3a2700535cdd68ec
Eleanor Ruth <ellie.ruth@me.com>
2022-10-12 18:08:34 -0600

    Complete turn class

--- src/Turn.js
+++ src/Turn.js
@@ -11,12 +11,7 @@ class Turn {
   }
   evaluateGuess() {
     return this.guess === this.card.correctAnswer
-    // if(this.guess === this.card.correctAnswer) {
-    //   return true
-    // } else if (this.guess != this.card.correctAnswer) {
-    //   return false
-    // }
-    //Return true or false? Ask Sarah if this is the same thing?
+ 
   }
   giveFeedback() {
     if(this.evaluateGuess()) {
