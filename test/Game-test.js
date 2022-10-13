8a257ec229efe3d695eba6fa632cc97025fe971a
Eleanor Ruth <ellie.ruth@me.com>
2022-10-12 18:10:02 -0600

    Complete Game Tests

---
+++ test/Game-test.js
@@ -0,0 +1,26 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const Card = require('../src/Card')
+const Deck = require('../src/Deck')
+const Round = require('../src/Round')
+const Game = require('../src/Game')
+
+describe("Game", () => {
+  it("should be a function", () => {
+    
+   // expect(Game).to.be.a('function')
+  })
+  it("should keep track of the current round", () => {
+    //expect(game.currentRound).to.equal()
+  })
+  it("should create new instances of Card", () => {
+
+  })
+  it("should create a new instance of Deck with the Cards", () => {
+
+  })
+  it("should create a new instance of Round with the Deck", () => {
+
+  })
+})
