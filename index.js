cdfe8b8ce5127eb55a375c3a164b9301dd645a96
Eleanor Ruth <ellie.ruth@me.com>
2022-10-12 18:06:27 -0600

    invoke game class to make game playable in the terminal

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-// This is where your project starts.
+const data = require("./src/data")
+const Game = require("./src/Game")
 
-console.log('Your project is running...'); 
+// This is where your project starts.
+const newGame = new Game(data.prototypeData)
+newGame.start()
+console.log('Your project is running...')
