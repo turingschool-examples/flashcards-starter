108f2039d5a6c4109315f894106aefa431ba81d1
Adelle Pitsas <apitsas4@gmail.com>
2022-10-13 17:59:11 -0400

    adds beforeEach to test files for DRYer code

--- test/turn-test.js
+++ test/turn-test.js
@@ -5,6 +5,11 @@ const Turn = require('../src/turn');
 const Card = require('../src/Card');
 
 describe('Turn', function() {
+    let card;
+
+    beforeEach(() => {
+        card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    })
     
     it('should be an instance of Turn', () => {
       const turn = new Turn();
@@ -19,14 +24,12 @@ describe('Turn', function() {
     });
 
     it('should take in the card as an argument', () => {
-        const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
         const turn = new Turn('pug', card);
 
         expect(turn.card).to.equal(card);
     })
 
     it('should have a method that returns the guess', () => {
-        const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
         const turn = new Turn('pug', card);
 
         let userGuess = turn.returnGuess();
@@ -35,7 +38,6 @@ describe('Turn', function() {
     })
 
     it('should have a method that returns the card', () => {
-        const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
         const turn = new Turn('pug', card);
 
         let presentCard = turn.returnCard();
@@ -44,7 +46,6 @@ describe('Turn', function() {
     })
 
     it('should indicate whether the users guess was correct', () => {
-        const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
         const turn = new Turn('sea otter', card);
 
         let evaluation = turn.evaluateGuess();
@@ -53,7 +54,6 @@ describe('Turn', function() {
     })
 
     it('should indicate whether the users guess was incorrect', () => {
-        const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
         const turn = new Turn('pug', card);
 
         let evaluation = turn.evaluateGuess();
@@ -62,7 +62,6 @@ describe('Turn', function() {
     })
 
     it('should display feedback depending on whether the guess is correct', () => {
-        const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
         const turn = new Turn('sea otter', card);
 
         let evaluation = turn.evaluateGuess();
@@ -73,7 +72,6 @@ describe('Turn', function() {
     })
 
     it('should display feedback depending on whether the guess is incorrect', () => {
-        const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
         const turn = new Turn('pug', card);
 
         let evaluation = turn.evaluateGuess();
