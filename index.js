3437e04fda87c977f861c97fdab06740adfea4d3
Eleanor Ruth <ellie.ruth@me.com>
2022-10-12 19:18:20 -0600

    Remove comment

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const data = require("./src/data")
 const Game = require("./src/Game")
 
-// This is where your project starts.
 const newGame = new Game(data.prototypeData)
 newGame.start()
 console.log('Your project is running...')
