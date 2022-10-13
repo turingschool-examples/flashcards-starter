4e1ec8455d0ed1212c9fecb7b078d4532a966831
Evan Swanson <evan.sundelius@gmail.com>
2022-10-12 21:11:39 -0500

    Make huuuuuge progress on it2

--- src/Round.js
+++ src/Round.js
@@ -4,21 +4,45 @@ class Round {
     constructor(deck) {
         this.deck = deck;
         this.guesses = [];
+        //correct and incorrect separate ^^
         this.turnCount = 0;
-        this.turns = [];
+        this.currentCard = this.deck.cards[this.turnCount];
+        this.incorrectGuesses = [];
+        //use turncount for deck index num ^^
+        // this.turns = [];
     };
 
     returnCurrentCard() {
-        return this.deck.cards[0];
+        return this.currentCard;
     };
 
-    recordGuess(guess) {
-        this.guesses.push(guess);
-    };
-
-    takeTurn() {
-        this.turns.push(new Turn(this.guesses[0], this.deck.cards[0]));
+    takeTurn(userGuess) {
         this.turnCount += 1;
+        this.currentCard = this.deck.cards[this.turnCount];
+        const currentTurn = new Turn(userGuess, this.currentCard);
+        const correctfulness = currentTurn.giveFeedback();
+        if (correctfulness === 'incorrect!') {
+            this.incorrectGuesses.push(this.currentCard.id)
+        }
+        console.log(this.turnCount);
+       return correctfulness;
+    };
+
+    calculatePercentCorrect() {
+        const totalCorrect = this.deck.length - this.incorrectGuesses.length;
+        return (totalCorrect/this.deck.length) * 100;
+    };
+
+    // giveFeedback() {
+    //     //if (this.takeTurn() === 'correct!') {
+    //     //uhhhhhhhh make it move on to the next card
+    //     //Maybe that ^^ could go in the index.js file?
+    //     //}
+    //     return this.takeTurn();
+    // };
+
+    endRound() {
+        `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
     };
 };
 
