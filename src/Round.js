32c9cd289ba8f4e057d6e3f0b87c7fa68a1a4a56
Zac Walters <zacwalters4@gmail.com>
2022-10-13 12:28:45 -0600

    Add endRound functionality to round.js

--- src/Round.js
+++ src/Round.js
@@ -23,6 +23,10 @@ class Round {
     calculatePercentCorrect() {
         return this.incorrectGuesses.length / this.turns * 100
     }
+
+    endRound() {
+        return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
+    }
 }
 
 module.exports = Round
