7eb7654944713333267eadcd15de841ebff244b7
Justen Anderson <justen.anderson@gmail.com>
2022-10-13 19:26:44 -0600

    Remove comments and refactor.

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// This is where your project starts.
-// This is where I'll want to run Game.start() thingy
 const Game = require('./src/Game');
-
-
-console.log('Your project is running...'); 
 const game = new Game();
 game.start();
--- test/Game-test.js
+++ test/Game-test.js
@@ -1,14 +1,12 @@
 const chai = require('chai');
 const expect = chai.expect;
-
+const data = require('../src/data');
+const prototypeQuestions = data.prototypeData;
 const Game = require('../src/Game');
 const Round = require('../src/Round');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Turn = require('../src/Turn');
-
-// DON'T TEST GAME.START!!!!
-  // Instead extract what you can out of game.start and into a separate function, and test those functions on their own.
 
 describe('Game', () => {
   beforeEach(() => {
@@ -24,28 +22,11 @@ describe('Game', () => {
   })
 
   it('should have a method that creates cards', () => {
-    const cardDeck = game.createCards()
-    expect(cardDeck).to.equal(30)
+    expect(game.createCards()).to.deep.equal(prototypeQuestions) 
+   
   })
 
-  it('should have a method that creates a deck', () => {
-    // expect(game.createDeck()).to.equal('idk')
+  it('should have a method that puts cards in a deck', () => {
+    expect(game.createDeck()).to.deep.equal({ cards: undefined })
   })
-
-  it('should have a method that...', () => {
-    expect().to.equal()
-  })
-
-  it('should have a method that...', () => {
-    expect().to.equal()
-  })
-
-  it('should have a method that...', () => {
-    expect().to.equal()
-  })
-
-  it('should have a method that...', () => {
-    expect().to.equal()
-  })
-
 })
