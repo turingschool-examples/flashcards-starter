d3f6fbca90dd88899006c62854d5621055289901
Natalie Kinsaul <n.kinsaul@gmail.com>
2022-11-29 14:18:58 -0800

    Add Turn class methods returnGuess, returnCard, and evaluateGuess

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,20 @@
 class Turn {
-    constructor() {
-
+    constructor(guess, currentCard) {
+        this.guess = guess;
+        this.currentCard = currentCard;
+    }
+    returnGuess() {
+        return this.guess;
+    }
+    returnCard() {
+        return this.currentCard;
+    }
+    evaluateGuess() {
+        if (this.guess === this.currentCard.correctAnswer) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
---
+++ test/Turn-test.js
@@ -0,0 +1,5 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Turn');
+
