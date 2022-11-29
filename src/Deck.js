0530896f5446b915ab962792030c4d1b0d47d73b
Reid Poole <poole.reid@gmail.com>
2022-11-28 23:25:58 -0600

    add to deckClass, WIP

--- src/Deck.js
+++ src/Deck.js
@@ -1,6 +1,17 @@
 class Deck {
-    constructor() {
-        
+    constructor(cards) {
+        this.cards = cards
+    }
+
+    countCards() {
+        console.log(this.cards)
+        this.cards.reduce((amount, card) => {
+            if(card){
+               amount ++
+               console.log(amount)
+               return amount
+            }
+        }, 0)
     }
 }
 
--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,23 +6,32 @@ const Turn = require('../src/Turn');
 const Deck = require('../src/Deck');
 
 describe('Deck', function() {
+    let card1
+    let card2
+    let card3
+    beforeEach(() =>{
+        card1 = new Card( 19, "What does the callback function for filter() return?", ["boolean", "an array of the same length", "a single value of any data type"],"boolean")
+        card2 = new Card( 20, "forEach() returns an array",["true", "false"], "false")
+        card3 = new Card(21,"Which iteration method is best for DOM manipulation?", ["forEach()", "map()", "reduce()"], "forEach()")
+    });
 
     it('should be a function', function() {
+      expect(Deck).to.be.a('function')
+    })    
     
+    it('should be able to create an instantiation', function() {
         const deck = new Deck()
 
         expect(deck).to.be.an.instanceOf(Deck)
     })
 
-    it.skip('should be able to hold cards', function() {
-        const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-        const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-        const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-
+    it('should be able to hold cards', function() {
         const deck = new Deck([card1, card2, card3]);
+        expect(deck.cards).to.deep.equal([card1, card2, card3])
+    })
 
-        expect(deck).to.deep.equal([card1, card2, card3])
-
-
+    it('should be able to count the cards', function() {
+        const deck = new Deck([card1, card2, card3]);
+        expect(deck.countCards()).to.equal(3)
     })
 })
