e6dc80f42a4cefd00be724fdbca67e992493b445
Karrar Qasim <Karawr1996@gmail.com>
2022-11-28 18:40:21 -0700

    Add two more tests to return guess and card

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -26,4 +26,18 @@ describe('Turn', function() {
         const turn = new Turn('This is my guess', card);
         expect(turn.currentCard).to.equal(card);  
     })
+
+    it.skip('should be able to return the user\'s guess', function() {
+        const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+        const turn = new Turn('This is my guess', card);
+        turn.returnGuess();
+        except(turn.returnGuess()).to.equal(turn.userGuess);
+      });
+
+    it.skip('should be able to return the user\'s card', function() {
+        const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+        const turn = new Turn('This is my guess', card);
+        turn.returnCard();
+        except(turn.returnCard()).to.equal(turn.card);
+      })
 });
