53c1a7e34c51598c6e72f1486174f511d00ee7e6
Rae Gebhart <raegebhart@gmail.com>
2022-11-28 18:07:00 -0700

    test: Create method that will return guess

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -14,8 +14,14 @@ describe('Turn', () => {
   it('should be a function', () => {
     expect(Turn).to.be.a('function')
   })
-  
+
   it('should be an instance of Turn', () => {
     expect(turn).to.be.an.instanceof(Turn)
+  })
+
+  it('should return the guess', () => {
+    turn.returnGuess()
+
+    expect(turn.returnGuess()).to.equal('object')
   })
 })
