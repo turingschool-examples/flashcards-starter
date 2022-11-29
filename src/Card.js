3c9c1ed0c117e11f1fe24e6ec4f13f496e395bef
El Brewster <brewsterel3@gmail.com>
2022-11-28 16:53:37 -0800

    Add some of card class implementation

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+class Card {
+    constructor() {
+        // this.id = id;
+        // this.question = question;
+        // this.possibleAnswers = possibleAnswers;
+        // this.correctAnswer = correctAnswer;
+
+    }
+}
+
+module.exports = Card;
