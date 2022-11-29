8d6e169dad036468439ffffa353fd9b7b38d3162
Blanche Haddad <blancherachel@gmail.com>
2022-11-28 18:01:46 -0700

    feat: Add and test functionality of the turn class

--- src/turns.js
+++ src/turns.js
@@ -3,6 +3,25 @@ class Turn {
     this.guess = guess;
     this.card = card;
   }
+  returnGuess() {
+    return this.guess;
+  }
+  returnCard() {
+    return this.card;
+  }
+  evaluateGuess() {
+    if (this.guess === this.card.correctAnswer) {
+      return true;
+    }
+    return false;
+  }
+  provideFeedback() {
+    if (this.evaluateGuess() === true) {
+      return "Your answer is correct!";
+    } else if (this.evaluateGuess() === false) {
+      return "Your answer is incorrect :( ";
+    }
+  }
 }
 
 module.exports = Turn;
--- test/turns-test.js
+++ test/turns-test.js
@@ -5,9 +5,14 @@ const Card = require("../src/Card");
 
 describe("Turn", function () {
   let turn;
-  let card = new Card();
+  let card = new Card(
+    1,
+    "What color is the sky?",
+    ["blue", "yellow", "pink"],
+    "blue"
+  );
   beforeEach(() => {
-    turn = new Turn("guess", card);
+    turn = new Turn("blue", card);
   });
   it("should be a function", () => {
     expect(Turn).to.be.a("function");
@@ -16,10 +21,25 @@ describe("Turn", function () {
     expect(turn).to.be.an.instanceof(Turn);
   });
   it("should have a player guess", () => {
-    expect(turn.guess).to.equal("guess");
+    expect(turn.guess).to.equal("blue");
   });
   it("should hold the current card", () => {
     expect(turn.card).to.equal(card);
   });
-  
+  it("should return the player guess", () => {
+    expect(turn.returnGuess()).to.equal("blue");
+  });
+  it("should return the current card", () => {
+    expect(turn.returnCard()).to.equal(card);
+  });
+  it("should evaluate whether the player guess is correct or incorrect", () => {
+    expect(turn.evaluateGuess()).to.equal(true);
+  });
+  it("should provide feedback if the correct answer was chosen", () => {
+    expect(turn.provideFeedback()).to.equal("Your answer is correct!");
+  });
+  it("should also provide feedback if the incorrect answer was chosen", () => {
+    const newTurn = new Turn("pink", card);
+    expect(newTurn.provideFeedback()).to.equal("Your answer is incorrect :( ");
+  });
 });
