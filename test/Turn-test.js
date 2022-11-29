427063f31c5c1fe71a4e695444e836d172c3536a
Rae Gebhart <raegebhart@gmail.com>
2022-11-28 18:02:40 -0700

    test: Pass function test

---
+++ test/Turn-test.js
@@ -0,0 +1,17 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const Turn = require('../src/Turn')
+
+describe('Turn', () => {
+
+  let turn
+  let card
+  beforeEach(() => {
+    turn = new Turn('object', card)
+  })
+
+  it('should be a function', () => {
+    expect(Turn).to.be.a('function')
+  })
+})
