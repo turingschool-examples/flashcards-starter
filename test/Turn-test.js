76564e10ba8cbcd6219cdf6ae6446bb7382a49e3
El Brewster <brewsterel3@gmail.com>
2022-11-28 18:50:57 -0800

    Add more test functionality for Turn class

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,6 +4,7 @@ const Turn = require ('../src/Turn');
 const Card = require ('../src/Card');
 
 describe ('Turn', function() {
+
     it('should be a function', () => {
         const turn = new Turn();
         expect(Turn).to.be.a('function');
@@ -14,10 +15,31 @@ describe ('Turn', function() {
         expect(turn).to.be.an.instanceof(Turn);
     });
 
-    it('should store a the user guess as a string')
+    it('should store the user guess as a string', () => {
         const turn = new Turn('user guess', Card);
+        expect('userguess').to.be.a('string');
+        expect(turn).to.be.an.instanceof(Turn);
+    })
 
-    it('should store an instance of the Card class')
-        const turn = new Turn('user guess', Card);
+    it('should store an instance of the Card class', () => {
+        const turn2 = new Turn('user guess', Card);
+        expect(Card).to.be.an.instanceof(Card);
+        expect(turn2).to.be.an.instanceof(Turn);
+    })
+
+    it('should have a method that returns the guess', () => {
+        turn2.returnGuess();
+        expect(turn2.returnGuess()).to.equal(turn2.guess);
+    })
+    it('should have a method that returns the card', () => {
+        turn2.returnCard();
+        expect(turn2.returnCard()).to.equal(turn2.card);
+    })
+    it('should have a method that returns a boolean indicating if the user\'s guess matches the correct answer on teh card', () => {
+        turn2.evaluateGuess();
+    })
+    it('should have a method that returns either "incorrect" or "correct" based on whether the cuess is correct or incorrect', () => {
+        turn2.giveFeedback();
+    })
 
 });
