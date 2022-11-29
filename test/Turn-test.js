931768b47e1a237d92b16b2ba72236092263dd4c
Rick Vermeil <rickv85@gmail.com>
2022-11-28 18:06:57 -0700

    Add constructor and methods to Turn and pass all Turn-test tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,12 +2,15 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
-  let newTurn;
+  let newTurn = new Object();
+  let card = new Object();
 
   this.beforeEach(() => {
-    newTurn = new Turn('guess', card);
+    card = new Card(8, "What is the best coding language?", undefined, "JavaScript")
+    newTurn = new Turn('Python', card);
   });
 
   it('should instantiate a new Turn', function() {
@@ -15,7 +18,7 @@ describe('Turn', function() {
   });
 
   it('should be instantiated with a guess as its first argument', function() {
-    expect(newTurn.guess).to.equal('guess');
+    expect(newTurn.guess).to.equal('Python');
   });
 
   it('should be instantiated with the current Card as its second argument', function() {
@@ -23,7 +26,7 @@ describe('Turn', function() {
   });
 
   it('should have a method returnGuess that returns the guess', function() {
-    expect(newTurn.returnGuess()).to.equal('guess');
+    expect(newTurn.returnGuess()).to.equal('Python');
   });
 
   it('should have a method returnCard that returns the current Card', function () {
@@ -31,12 +34,10 @@ describe('Turn', function() {
   });
 
   it('should have a method evaluateGuess that compares the user guess to the correct answer on the current Card', function() {
-    // Going to need to think about how to test this. I am going to leave this as an assumed false.
     expect(newTurn.evaluateGuess()).to.equal(false);
   });
 
   it('should have a method giveFeedback that returns a string based on whether or not the user guess is correct', function() {
-    // Same as above test, assuming a false answer.
     expect(newTurn.giveFeedback()).to.equal('incorrect!');
   });
 
