3d1d415a645b6061958a4393b43ac58287607a07
Rick Vermeil <rickv85@gmail.com>
2022-11-28 17:42:32 -0700

    Create initial Turn tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,3 +2,42 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+  let newTurn;
+
+  this.beforeEach(() => {
+    newTurn = new Turn('guess', card);
+  });
+
+  it('should instantiate a new Turn', function() {
+    expect(newTurn).to.be.an.instanceOf(Turn);
+  });
+
+  it('should be instantiated with a guess as its first argument', function() {
+    expect(newTurn.guess).to.equal('guess');
+  });
+
+  it('should be instantiated with the current Card as its second argument', function() {
+    expect(newTurn.card).to.equal(card);
+  });
+
+  it('should have a method returnGuess that returns the guess', function() {
+    expect(newTurn.returnGuess()).to.equal('guess');
+  });
+
+  it('should have a method returnCard that returns the current Card', function () {
+    expect(newTurn.returnCard()).to.equal(card);
+  });
+
+  it('should have a method evaluateGuess that compares the user guess to the correct answer on the current Card', function() {
+    // Going to need to think about how to test this. I am going to leave this as an assumed false.
+    expect(newTurn.evaluateGuess()).to.equal(false);
+  });
+
+  it('should have a method giveFeedback that returns a string based on whether or not the user guess is correct', function() {
+    // Same as above test, assuming a false answer.
+    expect(newTurn.giveFeedback()).to.equal('incorrect!');
+  });
+
+});
