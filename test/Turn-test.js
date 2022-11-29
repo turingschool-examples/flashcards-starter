6b87944d6631f231cc6a768fd787a73cf3dd60b8
Jocelle Bautista <jcllebautista@gmail.com>
2022-11-28 23:19:12 -0500

    Complete Turn test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -3,37 +3,47 @@ const expect = chai.expect;
 
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
+const { prototype } = require('../src/Card');
 
 describe ('Turn', function() {
 
   it('should be a function', function() {
     const turn = new Turn()
+    
     expect(Turn).to.be.a('function')
   })
 
   it('should be an instance of a Turn', function() {
     const turn = new Turn()
+    
     expect(turn).to.be.an.instanceof(Turn)
   })
 
-  it.skip('should take the user\'s attempt', function() {
+  it('should take the user\'s attempt', function() {
     const turn = new Turn('iterators')
-    //turn.returnGuess()
+
+    expect(turn.returnGuess()).to.equal('iterators')
   })
 
-  it.skip('should take in a new Card instance', function() {
-    const turn = new Turn()
-    //turn.returnCard()
+  it('should have a method that takes in a new Card', function() {
+    const card = new Card(2, 'What is a comma-separated list of related values?', '["array", "object", "function"]', 'array')
+    const turn = new Turn('iterators', card)
+
+    expect(turn.returnCard()).to.equal(card)
   })
 
-  it.skip('should evaluate if the user\'s guess matches the correct answer' , function() {
-    const turn = new Turn()
-    //turn.evaluateGuess()
+  it('should evaluate if the user\'s guess matches the correct answer' , function() {
+    const card = new Card(2, 'What is a comma-separated list of related values?', '["array", "object", "function"]', 'array')
+    const turn = new Turn('iterators', card)
+
+    expect(turn.evaluateGuess()).to.equal(false)
   })
 
-  it.skip('should provide a feedback if answer is right'), function() {
-    const turn = new Turn()
-    //turn.giveFeedback()
-  }
+  it('should provide a feedback based on the attempted answer', function() {
+    const card = new Card(2, 'What is a comma-separated list of related values?', '["array", "object", "function"]', 'array')
+    const turn = new Turn('iterators', card)
+
+    expect(turn.giveFeedback()).to.equal('incorrect!')
+  })
 
 })
