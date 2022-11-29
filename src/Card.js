8a762a0940c0afebbc39973daec7343e5400f344
Natalie Kinsaul <n.kinsaul@gmail.com>
2022-11-29 12:59:13 -0800

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,8 @@
+class Card {
+    constructor(id, question, choices, answer) {
+        this.id = id;
+        this.question = question;
+        this.choices = choices;
+        this.answer = answer
+    }
+}
