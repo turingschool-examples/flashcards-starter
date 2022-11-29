3239328cf722ced6853b63c2df59066f696801d0
El Brewster <brewsterel3@gmail.com>
2022-11-29 08:11:27 -0800

    Add Card and Turn class file updates

--- src/Turn.js
+++ src/Turn.js
@@ -1,14 +1,42 @@
+const Card = require("./Card");
+
 class Turn {
     constructor(guess, card) {
         this.userGuess = guess;
         this.card = card;
+        this.guessedAnswer = true;
+    }
+
+    returnGuess() {
+        return this.userGuess;
+    };
+
+    returnCard() {
+        //should I create a new instance of card in here?
+        return this.card;
+    }
+    
+    evaluateGuess() {
+        if(this.userGuess === this.card.correctAnswer) {
+            this.guessedAnswer = true;
+            return true;
+        }
+        if(this.userGuess !== this.card.correctAnswer) {
+            this.guessedAnswer = false;
+            return false;
+        }
+    }
+
+    giveFeedback() {
+        if (this.guessedAnswer === true) {
+            return 'correct!';
+        }
+        if (this.guessedAnswer === false) {
+            return 'incorrect!';
+        }
     }
 }
 
 
-// returnGuess
-// returnCard
-// evaluateGuess
-// giveFeedback
 
 module.exports = Turn;
