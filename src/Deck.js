970b626bdc6129f70665c551b699280f49b5f052
Jocelle Bautista <jcllebautista@gmail.com>
2022-11-29 20:46:54 -0500

    Modify global variable in TDD

--- src/Deck.js
+++ src/Deck.js
@@ -1,3 +1,7 @@
  class Deck {
-  constructor() {}
+  constructor(cards) {
+    this.set = cards//pass in an array of cards
+  }
 }
+
+module.exports = Deck//show length of Deck array
--- test/Deck-test.js
+++ test/Deck-test.js
@@ -2,19 +2,30 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Deck = require('../src/Deck');
-const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 const { prototype } = require('../src/Card');
 
 describe ('Deck', function() {
-  // let card1, card2, card3, // --> can i go through an array of objects that is not limited to a certain number
-  // beforeEach() //defines variables
+  
+  let card1, card2, card3, deck
+  
+  beforeEach('assign values to global variables', function() {
+    card1 = new Card (2, 'What is a comma-separated list of related values?', ["array", "object", "function"],
+       'array')
+    card2 = new Card (5,
+          'What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?', ["mutator method", "accessor method", "iteration method"],
+         'iteration method')  
+    card3 = new Card (4,
+        'What type of prototype method does not modify the existing array but returns a particular representation of the array?', ["mutator method", "accessor method", "iteration method"],
+       'accessor method')
+    deck = new Deck([card1, card2, card3]) 
+    console.log('here', deck)//defines variables
+    }) 
   // const deck = new Deck()
   
-  it.skip('should be a function', function() {
-    //setup
-    //*execute
-    // expect(Deck).to.be.a('function')
+  it('should be a function', function() {
+   
+    expect(Deck).to.be.a('function')
   })
 
   it.skip('should be an instance of Deck', function() {
