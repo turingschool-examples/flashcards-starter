595acda266d965404f7a54b9471d337816161eb8
Craig Weller <crg.weller@gmail.com>
2022-11-29 19:45:57 -0700

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,10 @@
+class Card {
+    constructor(id, question, answers, correct) {
+        this.id = id;
+        this.question = question;
+        this.answers = answers;
+        this.correct - correct
+    }
+}
+
+module.exports = Card
---
+++ src/Turn.js
@@ -0,0 +1,24 @@
+class Turn { 
+    constructor (guess, card) {
+        this.guess = guess
+        this.card = card
+    }
+
+    returnGuess() {
+        return this.guess;
+    }
+
+    returnCard() {
+        return this.card;
+    }
+
+    evaluateGuess() {
+       return this.card.correct === this.guess
+    }
+
+    giveFeedback() {
+        return (this.evaluateGuess() ? 'correct' : 'incorrect')
+    }
+}
+
+module.exports = Turn
