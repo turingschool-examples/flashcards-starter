40fca1e7dbf0088c0153e766a4fca429c912fcfb
Rick Vermeil <rickv85@gmail.com>
2022-11-30 18:54:23 -0700

    Finish Game methods and tests, add code to index.js to get program running

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // This is where your project starts.
+const Game = require('./src/Game');
+// const Round = require('./src/Round');
+// const Card = require('./src/Card');
+// const Deck = require('./src/Deck');
+const data = require('./src/data');
+const prototypeQuestions = data.prototypeData;
+
+const newGame = new Game();
+
+newGame.start(prototypeQuestions);
 
 console.log('Your project is running...'); 
--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,9 @@
 const data = require('./data');
-const Round = require('./Round');
-const Card = require('./Card')
-const Deck = require('./Deck')
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('./Round');
+const Card = require('./Card');
+const Deck = require('./Deck');
 
 class Game {
   constructor() {
@@ -21,34 +21,6 @@ class Game {
       util.main(round);
   }
 
-  start(cardData) {
-    // Needs to create Cards
-    // This is going to be an array of objects that need to be instantiated as a class of
-    // Card for each element, thinking .map of .forEach might be needed here
-    // Card(id, question, answers, correctAnswer)
-    let newCards = cardData.map(card => {
-      let newCard = new Card(card.id, card.question, card.answers, card.correctAnswer);
-      return newCard;
-    });
-    // Needs to put cards in a Deck
-    // The new array of Card objects created above will then need to be added to a new Deck
-    // instance in its constructor
-    const newDeck = new Deck(newCards);
-
-    // Needs to create a new Round using the Deck
-    const newRound = new Round(newDeck);
-    this.currentRound = newRound;
-
-    // Must invoke printMessage to display in the CLI
-    this.printMessage(newRound);
-
-    // Must invoke printQuestion to kick off helper functions to allow CLI interaction
-    this.printQuestion(newDeck, newRound);
-  };
-
-// ---- Below this are methods from start() broken out for testing -----
-// Need to reflect all changes in start() above once tests are passing
-
   createCards(cardData) {
     let newCards = cardData.map(card => {
       let newCard = new Card(card.id, card.question, card.answers, card.correctAnswer);
@@ -64,6 +36,14 @@ class Game {
   createNewRound() {
     const newRound = new Round(this.deck.cards);
     this.currentRound = newRound;
+  };
+
+  start(cardData) {
+    this.createCards(cardData);
+    this.putCardsInDeck();
+    this.createNewRound();
+    this.printMessage(this.deck, this.currentRound);
+    this.printQuestion(this.currentRound);
   };
 
 };
