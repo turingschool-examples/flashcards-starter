1724a51fb00c05db830ac837fafb55bf71c8d609
Natalie Kinsaul <n.kinsaul@gmail.com>
2022-12-01 12:42:38 -0800

    Add functionality to update the current card to the next card in the deck

--- src/Round.js
+++ src/Round.js
@@ -2,13 +2,22 @@ class Round {
     constructor(deck) {
         this.deck = deck;
         this.turns = 0;
+        this.incorrectGuesses = [];
+        this.currentCard = 0;
     }
     returnCurrentCard() {
-        return this.deck.cardDeck[0];
+        return this.deck.cardDeck[this.currentCard];
     }
     takeTurn() {
         this.turns ++;
+        this.currentCard ++;
     }
 }
 
 module.exports = Round;
+
+
+// When takeTurn is called....I want the next card in the deck to become the 
+// current card.  Right now returnCurrentCard is written so that it is hard
+// coded to return the first element of deck.  I need this to be more 
+// dynamic
--- test/Round-test.js
+++ test/Round-test.js
@@ -13,9 +13,13 @@ describe('Round', function() {
         console.log(`Test #${testcounter}`);
     })
 
+    // Test #1
+
     it('should be a function', function() {
         expect(Round).to.be.a('function');
     });
+
+    // Test #2
 
     it('should be an instance of Round', function() {
         const card1 = new Card(prototypeData[0]);
@@ -29,6 +33,8 @@ describe('Round', function() {
         expect(round).to.be.an.instanceOf(Round);
     });
 
+    // Test #3
+
     it('should hold a card deck', function() {
         const card1 = new Card(prototypeData[0]);
         const card2 = new Card(prototypeData[1]);
@@ -40,6 +46,8 @@ describe('Round', function() {
     
         expect(round.deck).to.deep.equal(deck)
     });
+
+    // Test #4
 
     it('should return the first card in the deck', function() {
         const card1 = new Card(prototypeData[0]);
@@ -55,6 +63,8 @@ describe('Round', function() {
         expect(currentCard).to.deep.equal(prototypeData[0]);
     });
 
+    // Test #5
+
     it('should update the turn count', function() {
         const card1 = new Card(prototypeData[0]);
         const card2 = new Card(prototypeData[1]);
@@ -68,6 +78,8 @@ describe('Round', function() {
 
         expect(round.turns).to.equal(1);
     });
+
+    // Test #6
 
     it('should update the turn count multiple times', function() {
         const card1 = new Card(prototypeData[0]);
@@ -85,4 +97,42 @@ describe('Round', function() {
         expect(round.turns).to.equal(3);
     });
 
+    // Test #7
+
+    it('should update the current card to the next card', function() {
+        const card1 = new Card(prototypeData[0]);
+        const card2 = new Card(prototypeData[1]);
+        const card3 = new Card(prototypeData[2]);
+        
+        const deck = new Deck([card1, card2, card3]);
+        
+        const round = new Round(deck);
+
+        round.takeTurn();
+        
+        const currentCard = round.returnCurrentCard();
+
+        expect(currentCard).to.deep.equal(prototypeData[1]);
+    });
+
+    // Test #8
+
+    it('should update the current card multiple times', function() {
+        const card1 = new Card(prototypeData[0]);
+        const card2 = new Card(prototypeData[1]);
+        const card3 = new Card(prototypeData[2]);
+        const card4 = new Card(prototypeData[3]);
+        
+        const deck = new Deck([card1, card2, card3, card4]);
+        
+        const round = new Round(deck);
+
+        round.takeTurn();
+        round.takeTurn();
+        round.takeTurn();
+
+        const currentCard = round.returnCurrentCard();
+
+        expect(currentCard).to.deep.equal(prototypeData[3]);
+    });
 });
