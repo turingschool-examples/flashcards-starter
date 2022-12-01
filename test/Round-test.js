0d11edf8887dd7193fa3ba3411a27f1303058773
Natalie Kinsaul <n.kinsaul@gmail.com>
2022-11-30 16:03:03 -0800

    Test that round can be instantiated:

--- test/Round-test.js
+++ test/Round-test.js
@@ -2,6 +2,9 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Round = require('../src/Round');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const prototypeData = require('../src/data.js');
 
 describe('Round', function() {
     let testcounter = 0;
@@ -12,6 +15,18 @@ describe('Round', function() {
 
     it('should be a function', function() {
         expect(Round).to.be.a('function');
+    });
+
+    it('should be an instance of Round', function() {
+        const card1 = new Card(prototypeData[0]);
+        const card2 = new Card(prototypeData[1]);
+        const card3 = new Card(prototypeData[2]);
+
+        const deck = new Deck([card1, card2, card3]);
+
+        const round = new Round(deck);
+
+        expect(round).to.be.an.instanceOf(Round);
     });
 
 });
