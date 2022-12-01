5d51caa873c6f1ddc95898f667fa5d927cda06d7
Sarah Hampton <stoluba@gmail.com>
2022-12-01 17:44:26 -0500

    Complete Game

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,16 @@
 const data = require('./data');
+const Round = require('./Round');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+    this.cards;
+    this.deck;
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -13,6 +20,25 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
+
+  start() {
+   this.instatiateClasses();
+   this.printMessage(this.deck, this.currentRound);
+   this.printQuestion(this.currentRound);
+  }
+
+  instatiateClasses() {
+    this.cards = this.createCards(prototypeQuestions)
+    this.deck = new Deck(this.cards);
+    this.currentRound = new Round(this.deck);
+  }
+
+  createCards(prototypeCards) {
+    return prototypeCards.map(property => {
+      return new Card(property.id, property.question, property.answers, property.correctAnswer)
+    })
+  }
+
 }
 
 module.exports = Game;
---
+++ test/Game-test.js
@@ -0,0 +1,76 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Game = require('../src/Game');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+
+
+describe('Game', () => {
+  let miniPrototypeData;
+  let newDeck;
+  let newRound;
+  let newGame;
+  beforeEach(() => {
+    miniPrototypeData = [{
+      "id": 1,
+      "question": "What allows you to define a set of related information using key-value pairs?",
+      "answers": ["object", "array", "function"],
+      "correctAnswer": "object"
+    }, {
+      "id": 2,
+      "question": "What is a comma-separated list of related values?",
+      "answers": ["array", "object", "function"],
+      "correctAnswer": "array"
+    }, {
+      "id": 3,
+      "question": "What type of prototype method directly modifies the existing array?",
+      "answers": ["mutator method", "accessor method", "iteration method"],
+      "correctAnswer": "mutator method"
+    }];
+    newDeck = new Deck(miniPrototypeData);
+    newRound = new Round(newDeck);
+    newGame = new Game()
+
+  });
+
+  it('should be a function', () => {
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Game', () => {
+    expect(newGame).to.be.an.instanceof(Game);
+  }); 
+
+  it('should keep track of the current round', () => {
+    newGame.instatiateClasses();
+    expect(newGame.currentRound).to.be.an.instanceof(Round);
+  }); 
+
+  it('should create cards', () => {
+    expect(newGame.createCards(miniPrototypeData)).to.deep.equal([
+     {
+        id: 1,
+        question: 'What allows you to define a set of related information using key-value pairs?',
+        answers: [ 'object', 'array', 'function' ],
+        correctAnswer: 'object'
+      },
+     {
+        id: 2,
+        question: 'What is a comma-separated list of related values?',
+        answers: [ 'array', 'object', 'function' ],
+        correctAnswer: 'array'
+      },
+     {
+        id: 3,
+        question: 'What type of prototype method directly modifies the existing array?',
+        answers: [ 'mutator method', 'accessor method', 'iteration method' ],
+        correctAnswer: 'mutator method'
+      }
+    ])
+  });
+
+  it('should put cards in a deck', () => {
+    newGame.instatiateClasses();
+    expect(newGame.deck).to.be.an.instanceof(Deck);
+  });
+});
