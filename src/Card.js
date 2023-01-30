f138c74483768439d0a6665100d9e395f6148655
Christopher Baum <christopherabaum@gmail.com>
2023-01-30 17:51:10 -0600

    Update Card property names

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ class Card {
     constructor(id, question, possibleAnswers, correctAnswer) {
         this.id = id
         this.question = question
-        this.possibleAnswers = possibleAnswers
+        this.answers = possibleAnswers
         this.correctAnswer = correctAnswer
     }
 }
