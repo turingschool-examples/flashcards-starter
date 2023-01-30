b0eed215bf3bd841e98967362473959f8df7210b
Joshua Martin <ursa8minority@gmail.com>
2023-01-30 16:53:11 -0700

    Input javascript data model card class

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+class Card {
+    constructor () {
+        this.question = 'What allows you to define a set of related information using key-value pairs?';
+        this.answers = ['object', 'array', 'function'];
+        this.correctAnswer = 'object';
+
+
+    }
+}
+
+
+
+module.exports = Card;
