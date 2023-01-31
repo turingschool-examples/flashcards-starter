0348bffe09a966aa148aa4c230b620c49b150116
Travis Parks <travisparks0788@gmail.com>
2023-01-31 10:26:43 -0500

    Add working beforeEach hook

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,26 +5,28 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 
 describe('Turn', function() {
+  var card
+  var turn
+  
+  beforeEach(function () {
+  card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+  turn = new Turn('object', card)
+  })
+
 it('should be a function', function() {
-  const turn = new Turn();
   expect(Turn).to.be.a('function');
 });
 
 it('should be an instance of Turn', function() {
-  const turn = new Turn();
   expect(turn).to.be.an.instanceof(Turn);
 }); 
 
 it('should store a users guess', function() {
-  const turn = new Turn('object')
   expect(turn.guess).to.be.a('string')
   expect(turn.guess).to.equal('object')
 });
 
 it('should store the current card in play', function() {
-  const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-  const turn = new Turn('object', card)
-
   expect(turn.card).to.equal(card)
 })
 
