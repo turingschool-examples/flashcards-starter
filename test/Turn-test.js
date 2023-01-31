e1ccc6172290c363edf5cbe45ad13cac4447f8d4
Jason Kirchman <kirchman.jason@icloud.com>
2023-01-30 21:48:22 -0600

    feat: Create turn class and pass new turn tests

---
+++ test/Turn-test.js
@@ -0,0 +1,53 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+
+describe('Turn', () => {
+    let turn;
+    let incorrectTurn;
+    let card;
+    beforeEach(() => {
+        card = new Card(1, 
+            "Who was the first president of the USA?", 
+            ['John Adams', 'George Washington', 'Barack Obama', 'Abraham Lincoln'],
+            'George Washington');
+        turn = new Turn('George Washington', card);
+        incorrectTurn = new Turn('John Adams', card);
+    });
+
+    it('should be a function', () => {
+        expect(Turn).to.be.a('function');
+    });
+
+    it('should be an instance of Turn', () => {
+        expect(turn).to.be.instanceOf(Turn);
+    });
+
+    it('should store a user\'s guess', () => {
+        expect(turn.guess).to.equal('George Washington');
+    });
+    
+    it('should store a card', () => {
+        expect(turn.card).to.deep.equal(card);
+    });
+
+    it('should return a guess', () => {
+        expect(turn.returnGuess()).to.equal('George Washington');
+    });
+
+    it('should return a Card', () => {
+        expect(turn.returnCard()).to.deep.equal(card);
+    });
+
+    it('should be able to evaluate a guess', () => {
+        expect(turn.evaluateGuess()).to.equal(true);
+        expect(incorrectTurn.evaluateGuess()).to.equal(false);
+    });
+
+    it('should be able to give feedback on a guess', () => {
+        expect(turn.giveFeedback()).to.equal('correct!')
+        expect(incorrectTurn.giveFeedback()).to.equal('incorrect!')
+    });
+});
