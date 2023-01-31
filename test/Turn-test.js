fb56f27a75165a2ecb4f08e840eea281956ad3e8
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-01-31 13:56:10 -0800

    Add: test for givefeedback method and evaluateGuess method. Update test to include beforeEach to eliminate redundant code

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,46 +5,45 @@ const Turn = require("../src/Turn");
 const Card = require("../src/Card");
 
 describe("Turn", () => {
+  let card, turn
+
+  beforeEach(()=> {
+    card = new Card(1,"What is Robbie's favorite animal",["sea otter", "pug", "capybara"],"sea otter")
+    turn = new Turn("sea otter", card)
+  })
+  
   it("should be a function", () => {
     expect(Turn).to.be.a("function");
   });
+  
   it("should be a new instance of Turn", () => {
-    const card1 = new Card(1, "What is Robbie\'s favorite animal",["sea otter", "pug", "capybara"], "sea otter");
-    const turn = new Turn('pug', card1)
     expect(turn).to.be.an.instanceof(Turn);
   })
+  
   it("should take the player\'s guess as an argument", () => {
-    const card1 = new Card(1,"What is Robbie's favorite animal",["sea otter", "pug", "capybara"],"sea otter");
-    const turn = new Turn("sea otter", card1);
     expect(turn.guess).to.equal('sea otter');
   });
+  
   it("should have a method that returns the guess", () => {
-    const card = new Card(1,"What is Robbie's favorite animal",["sea otter", "pug", "capybara"], "sea otter");
-    const turn = new Turn("sea otter", card);
     expect(turn.returnGuess()).to.equal('sea otter');
   })
+  
   it("should have a method that returns the card", () => {
-    const card = new Card(1,"What is Robbie's favorite animal",["sea otter", "pug", "capybara"], "sea otter");
-    const turn = new Turn("sea otter", card);
-    expect(turn.returnCard()).to.deep.equal(card);
+    expect(turn.returnCard()).to.equal(card);
   });
-  it("should have a method be able to determine if the answer is correct", () => {
-    const card = new Card(1,"What is Robbie's favorite animal",["sea otter", "pug", "capybara"],"sea otter");
-    const turn1 = new Turn("sea otter", card);
-    const turn2 = new Turn("pug", card)
-
-    expect(turn1.evaluateGuess()).to.equal(false);
-    expect(turn2.evaluateGuess()).to.equal(true)
+  
+  it("should have a method that returns a boolean indicating if the user's guess is correct", () => {
+    expect(turn.evaluateGuess()).to.equal(true);
+    
   });
-  it("should have a method that returns if the answer is correct or incorrect", () => {
-    const card = new Card(1,"What is Robbie's favorite animal",["sea otter", "pug", "capybara"],"sea otter");
-    const turn1 = new Turn("sea otter", card);
-    const turn2 = new Turn("pug", card);
-
-    expect(turn1.returnFeedback()).to.equal("incorrect!");
-    expect(turn2.returnFeedback()).to.equlae("correct!")
+  it("should have a method that returns if the answer is correct", () => {
+    expect(turn.giveFeedback()).to.equal("correct!")
+    
   });
-
-
-
+  it("should have a method that returns if the answer is incorrect", () => {
+    const incorrectTurn = new Turn("pug", card)
+    expect(incorrectTurn.giveFeedback()).to.equal("incorrect!");
+    
+  });
+  
 });
