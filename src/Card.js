5181291964574d19a55e709cc680fe3cf389a55e
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-01-30 17:43:47 -0700

    Update name of the card number

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,6 @@
 class Card {
   constructor(cardNumber, question, answers, correctAnswer) {
-    this.number = cardNumber;
+    this.id = cardNumber;
     this.question = question;
     this.answers = answers;
     this.correctAnswer = correctAnswer;
