a7afed3f6979a05ef1b59dacd2989a918a9a98fc
Kirk Hauck <kirkihauck@gmail.com>
2023-01-31 16:38:35 -0700

    feat: Add test to check for instance of Deck and corrected Turn to Deck in test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,8 +6,17 @@ const expect = chai.expect;
 const Deck = require('../src/Deck');
 
 describe('Deck', () => {
+  let deck;
+
+  beforeEach(() => {
+    deck = new Deck;
+  })
 
   it('should be a function', () => {
-    expect(Turn).to.be.a('function');
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', () => {
+    expect(deck).to.be.an.instanceOf(Deck);
   });
 });
