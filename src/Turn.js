09bc72c8c07450dc4bd7988d652db52fa1434480
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-01-30 21:57:20 -0800

    Add: method is turn class to return the card, evaluate if the guess is correct, and giveFeedback to the user for correct or incorrect

--- src/Turn.js
+++ src/Turn.js
@@ -1,12 +1,28 @@
 class Turn {
-    constructor(guess, card){
-        this.guess = guess;
-        this.cardObject = card
-    }
+  constructor(guess, card){
+    this.guess = guess;
+    this.cardObject = card
+  }
 
-    returnGuess(){
-        return this.guess  
+  returnGuess(){
+    return this.guess  
+  }
+
+  returnCard(){
+    return this.card
+  }
+
+  evaluateGuess(){
+    return this.guess === this.card.correctAnswer
+  }
+
+  giveFeedback(){
+    if (!this.evaluateGuess()) {
+      return 'incorrect!';
+    } else {
+      return 'correct!'
     }
+  }
 }
 
 module.exports = Turn;
