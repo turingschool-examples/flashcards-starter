f050836c6522904d1a82b0cc5b95128d41b33fcf
Travis Parks <travisparks0788@gmail.com>
2023-01-31 09:35:18 -0500

    Add skeleton class design

---
+++ src/Turn.js
@@ -0,0 +1,28 @@
+class Turn {
+  constructor(userGuess, cardObject) {
+    this.guess = userGuess
+    this.card = cardObject
+
+  }
+
+  returnGuess() {
+    //userGuess
+  }
+  
+  returnCard() {
+    //object
+  }
+
+  eveluateGuess() {
+    //true or false
+  }
+
+  giveFeedback() {
+    //correct or incorrect
+  }
+}
+
+
+
+
+module.exports = Turn
