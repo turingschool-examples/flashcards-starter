b017de657fcfd5bddcda62251a29b554f02b63e3
trevorfitz0 <trevorfitz0@gmail.com>
2023-01-31 13:56:24 -0700

    Functioning Card Class

---
+++ src/Turn.js
@@ -0,0 +1,29 @@
+// Your Turn class should meet the following requirements:
+// Instantiated with two arguments - a string (that represents a user’s guess to the question), and a Card object for the current card in play.
+// returnGuess: method that returns the guess
+// returnCard: method that returns the Card
+// evaluateGuess: method that returns a boolean indicating if the user’s guess matches the correct answer on the card
+// giveFeedback - method that returns either ‘incorrect!’ or ‘correct!’ based on whether the guess is correct or not.
+
+
+class Turn {
+    constructor(guess, card) {
+
+    }
+
+    returnGuess() {
+
+    }
+
+    returnCard() {
+
+    }
+
+    evaluateGuess() {
+
+    }
+
+    giveFeedback() {
+        
+    }
+}
