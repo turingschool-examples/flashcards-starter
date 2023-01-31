dac94f7897846cbb4ce9b047f194bbd8d1e19736
Travis Parks <travisparks0788@gmail.com>
2023-01-31 10:46:46 -0500

    Add working beforeEach

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -11,7 +11,7 @@ describe('Turn', function() {
   beforeEach(function () {
   card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
   turn = new Turn('object', card)
-  })
+})
 
 it('should be a function', function() {
   expect(Turn).to.be.a('function');
