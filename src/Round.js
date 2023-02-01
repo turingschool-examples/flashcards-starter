97406cdc237321f0b7fe8f23da35ece9ebdafb49
Kirk Hauck <kirkihauck@gmail.com>
2023-02-01 16:11:23 -0700

    feat: Add Round.takeTurn() method to instantiate and keep track of Turns and evaluate guesses to provide appropriate feedback

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,28 @@
+const Turn = require("./Turn");
+
 class Round {
   constructor(deck) {
+    this.deck = deck;
     this.currentCard = deck.cards[0];
+    this.turn = null;
+    this.turns = 0;
+    this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard() {
+    return this.currentCard;
+  }
+
+  takeTurn(guess) {
+    this.turn = new Turn(guess, this.currentCard);
+    this.turns++;
+    this.currentCard = this.deck.cards[this.turns];
+
+    if (!this.turn.evaluateGuess()) {
+      this.incorrectGuesses.push(this.turn.card.id);
+    }
+
+    return this.turn.giveFeedBack();
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const Card = require('../src/Card');
-const Deck = require('../src/Deck')
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
 const expect = chai.expect;
 
 const Round = require('../src/Round');
@@ -32,5 +33,81 @@ describe('Round', () => {
       answers: ['rat', 'chipmunk', 'capybara'],
       correctAnswer: 'capybara'
     });
+  });
+
+  it('should be able to return the current card', () => {
+    expect(round.returnCurrentCard()).to.deep.equal({
+      id: 1,
+      question: 'What is the best rodent?',
+      answers: ['rat', 'chipmunk', 'capybara'],
+      correctAnswer: 'capybara'
+    });
+  });
+
+  it('should start with 0 turns', () => {
+    expect(round.turns).to.equal(0);
+  });
+
+  it('should be able to take a turn', () => {
+    expect(round.takeTurn).is.a('function');
+  });
+
+  it('should instantiate a new Turn when it takes a turn', () => {
+    const guess = 'random guess';
+
+    round.takeTurn(guess);
+
+    expect(round.turn).to.be.an.instanceOf(Turn);
+    expect(round.turn).to.deep.equal({guess: 'random guess', card: round.deck.cards[0]})
+  });
+
+  it('should count how many turns it takes', () => {
+    expect(round.turns).to.equal(0);
+    
+    round.takeTurn('guess argument');
+    expect(round.turns).to.equal(1);
+
+    
+    round.takeTurn('guess argument');
+    round.takeTurn('guess argument');
+
+    expect(round.turns).to.equal(3);
+  });
+
+  it('should make the next card the new current card', () => {
+    expect(round.currentCard).to.deep.equal(card1);
+
+    round.takeTurn('guess argument');
+    expect(round.currentCard).to.deep.equal(card2);
+
+    round.takeTurn('guess argument');
+    
+    expect(round.currentCard).to.deep.equal(card3);
+  });
+
+  it('should have no incorrect guesses by default', () => {
+    expect(round.incorrectGuesses).to.deep.equal([]);
+  });
+
+  it('should keep track of incorrect guess ids', () => {
+    round.takeTurn('rat');
+
+    expect(round.incorrectGuesses).to.deep.equal([1]);
+  });
+
+  it('should not keep track of correct guesses', () => {
+    round.takeTurn('rat');
+    round.takeTurn('all of it');
+    round.takeTurn('peuce');
+
+    expect(round.incorrectGuesses).to.deep.equal([1, 3])
+  });
+
+  it('should tell you if your answer is correct or incorrect', () => {
+    const correctGuess = round.takeTurn('capybara');
+    const incorrectGuess = round.takeTurn('a lot');
+
+    expect(correctGuess).to.equal('correct!')
+    expect(incorrectGuess).to.equal('incorrect!')
   });
 });
