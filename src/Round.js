f92e3d17258674324ad866d6a47af9dbad1b1c52
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-02-01 15:16:02 -0800

    Add: property for current card and incorrect guesses, add method for taking turn.

--- src/Round.js
+++ src/Round.js
@@ -1,16 +1,54 @@
-const Turn = require("./Turn")
+// const Turn = require("./Turn")
 const Deck = require("./Deck")
+const Turn = require('./Turn')
 
 class Round{
-    constructor(cards){
-        this.deck= new Deck(cards)
-        this.currentCard = this.deck.cards[0]
+    constructor(deck){
+        this.deck = deck.cards
+        this.turns = 0
+        this.currentCard = this.deck[0]
+        this.incorrectGuesses = []
     }
 
     returnCurrentCard(){
-        console.log(this.currentCard)
+       this.currentCard = this.deck[0] 
        return this.currentCard
     }
-}
+
+    takeTurn(guess){
+        let turn = new Turn (guess, this.currentCard)
+        this.turns += 1
+        turn.evaluateGuess()
+        if(!turn.evaluateGuess()){
+            this.incorrectGuesses.push(this.deck[0].id)
+            console.log(turn.giveFeedback())
+        } 
+        const feedback = turn.giveFeedback()
+            this.deck.shift()
+            console.log(feedback)
+            return feedback
+    }
+
+    calculatePercentCorrect(){
+        
+    }
+      
+
+        // turn.giveFeedback(){
+        //     if (this.evaluateGuess()) {
+        //         return 'correct!'
+        //     } else {
+        //         return 'incorrect!';
+        //     }
+  
+    }
+
+    // calculatePercentCorrect(){
+
+    // }
+
+    // endRound(){
+
+    // }
 
 module.exports = Round
