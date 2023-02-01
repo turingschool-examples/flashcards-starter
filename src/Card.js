95407c4e6b31a8406f7df1080327fc1292eae45d
Jeff Sahim <jeffreysahim@gmail.com>
2023-01-31 19:57:33 -0500

    Add Turn & Game classes

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ class Card {
   constructor(cardId, cardQuestion, possibleAnswers, correctAnswer){
     this.id = cardId,
     this.question = cardQuestion,
-    this.options = possibleAnswers,
-    this.answer = correctAnswer
+    this.answers = possibleAnswers,
+    this.correctAnswer = correctAnswer
   }
 }
 
