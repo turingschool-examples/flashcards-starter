2d497e8bac8df80072b2cea344ae99a072cf5da2
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-01-31 16:24:52 -0800

    Add: test for deck property

---
+++ test/Deck-test.js
@@ -0,0 +1,17 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const Card = require("../src/Card");
+const Deck = require("../src/Deck");
+
+describe('Deck', function() {
+  it('should be a function', function() {
+    expect(Deck).to.be.a('function');
+  });
+    
+})   
+
+    const card1 = new Card(1,"What is Robbie's favorite animal",["sea otter", "pug", "capybara"],"sea otter");
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder'));
+    const card3 = new Card(12, 'What is Sam\'s middle name?', ['Lex', 'William', 'Kay'], 'Kay');
+    const deck = new Deck([card1, card2, card3])
