cb4c7b4ede64c668dcdd398120e31dda8d3205cb
Kirk Hauck <kirkihauck@gmail.com>
2023-02-01 18:02:29 -0700

    feat: Adjust deck test to instantiate Deck with cards from data.js

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -8,14 +8,12 @@ const Deck = require('../src/Deck');
 
 describe('Deck', () => {
   let deck;
+  let cards = prototypeQuestions;
 
   beforeEach(() => {
-    deck = new Deck([
-      new Card(prototypeQuestions[0]),
-      new Card(prototypeQuestions[1]),
-      new Card(prototypeQuestions[2])
-    ]);
-  })
+    cards = cards.map(card => new Card(card));
+    deck = new Deck(cards);
+  });
 
   it('should be a function', () => {
     expect(Deck).to.be.a('function');
@@ -25,26 +23,11 @@ describe('Deck', () => {
     expect(deck).to.be.an.instanceOf(Deck);
   });
 
-  it('should have cards', () => {
-    expect(deck.cards).to.deep.equal([{
-      "id": 1,
-      "question": "What allows you to define a set of related information using key-value pairs?",
-      "answers": ["object", "array", "function"],
-      "correctAnswer": "object"
-    }, {
-      "id": 2,
-      "question": "What is a comma-separated list of related values?",
-      "answers": ["array", "object", "function"],
-      "correctAnswer": "array"
-    }, {
-      "id": 3,
-      "question": "What type of prototype method directly modifies the existing array?",
-      "answers": ["mutator method", "accessor method", "iteration method"],
-      "correctAnswer": "mutator method"
-    }]);
+  it('should have some cards', () => {
+    expect(deck.cards).to.deep.equal(prototypeQuestions)
   });
 
   it('should count how many cards it has', () => {
-    expect(deck.countCards()).to.equal(3);
+    expect(deck.countCards()).to.equal(30);
   });
 });
