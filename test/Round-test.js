2eb642640d83a729cfda64207e9377e8b2b923d7
Kirk Hauck <kirkihauck@gmail.com>
2023-02-01 18:14:00 -0700

    feature: Change Round-test.js to instantiate Card using data.js

--- test/Round-test.js
+++ test/Round-test.js
@@ -3,15 +3,21 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Turn = require('../src/Turn');
 const expect = chai.expect;
+const data = require('../src/data');
+const prototypeQuestions = data.prototypeData;
 
 const Round = require('../src/Round');
 
 describe('Round', () => {
-  const card1 = new Card(1, 'What is the best rodent?', ['rat', 'chipmunk', 'capybara'], 'capybara');
-  const card2 = new Card(2, 'How much wood would a wood chuck chuck if a wood chuck could chuck wood?', ['a little', 'a lot', 'all of it'], 'all of it');
-  const card3 = new Card(3, 'What is the color of the sky?', ['blue', 'peuce', 'salmon'], 'blue');
-  const deck = new Deck([card1, card2, card3]);
-  let round;
+  // const card1 = new Card(1, 'What is the best rodent?', ['rat', 'chipmunk', 'capybara'], 'capybara');
+  // const card2 = new Card(2, 'How much wood would a wood chuck chuck if a wood chuck could chuck wood?', ['a little', 'a lot', 'all of it'], 'all of it');
+  // const card3 = new Card(3, 'What is the color of the sky?', ['blue', 'peuce', 'salmon'], 'blue');
+  // const deck = new Deck([card1, card2, card3]);
+  // let round;
+
+  let cards = prototypeQuestions;
+  cards = cards.map(card => new Card(card));
+  let deck = new Deck(cards);
 
   beforeEach(() => {
     round = new Round(deck);
@@ -28,19 +34,19 @@ describe('Round', () => {
   it('should have a currentCard which is the first card in the deck', () => {
     expect(round.currentCard).to.be.an.instanceOf(Card);
     expect(round.currentCard).to.deep.equal({
-      id: 1,
-      question: 'What is the best rodent?',
-      answers: ['rat', 'chipmunk', 'capybara'],
-      correctAnswer: 'capybara'
+      "id": 1,
+      "question": "What allows you to define a set of related information using key-value pairs?",
+      "answers": ["object", "array", "function"],
+      "correctAnswer": "object"
     });
   });
 
   it('should be able to return the current card', () => {
     expect(round.returnCurrentCard()).to.deep.equal({
-      id: 1,
-      question: 'What is the best rodent?',
-      answers: ['rat', 'chipmunk', 'capybara'],
-      correctAnswer: 'capybara'
+      "id": 1,
+      "question": "What allows you to define a set of related information using key-value pairs?",
+      "answers": ["object", "array", "function"],
+      "correctAnswer": "object"
     });
   });
 
@@ -75,14 +81,30 @@ describe('Round', () => {
   });
 
   it('should make the next card the new current card', () => {
-    expect(round.currentCard).to.deep.equal(card1);
+    expect(round.currentCard).to.deep.equal({
+      "id": 1,
+      "question": "What allows you to define a set of related information using key-value pairs?",
+      "answers": ["object", "array", "function"],
+      "correctAnswer": "object"
+    });
 
     round.takeTurn('guess argument');
-    expect(round.currentCard).to.deep.equal(card2);
+    expect(round.currentCard).to.deep.equal({
+      "id": 2,
+      "question": "What is a comma-separated list of related values?",
+      "answers": ["array", "object", "function"],
+      "correctAnswer": "array"
+    });
 
+    round.takeTurn('guess argument');
     round.takeTurn('guess argument');
     
-    expect(round.currentCard).to.deep.equal(card3);
+    expect(round.currentCard).to.deep.equal({
+      "id": 4,
+      "question": "What type of prototype method does not modify the existing array but returns a particular representation of the array?",
+      "answers": ["mutator method", "accessor method", "iteration method"],
+      "correctAnswer": "accessor method"
+    });
   });
 
   it('should have no incorrect guesses by default', () => {
@@ -90,40 +112,40 @@ describe('Round', () => {
   });
 
   it('should keep track of incorrect guess ids', () => {
-    round.takeTurn('rat');
+    round.takeTurn('array');
 
     expect(round.incorrectGuesses).to.deep.equal([1]);
   });
 
   it('should not keep track of correct guesses', () => {
-    round.takeTurn('rat');
-    round.takeTurn('all of it');
-    round.takeTurn('peuce');
+    round.takeTurn('array');
+    round.takeTurn('array');
+    round.takeTurn('accessor method');
 
     expect(round.incorrectGuesses).to.deep.equal([1, 3])
   });
 
   it('should tell you if your answer is correct or incorrect', () => {
-    const correctGuess = round.takeTurn('capybara');
-    const incorrectGuess = round.takeTurn('a lot');
+    const correctGuess = round.takeTurn('object');
+    const incorrectGuess = round.takeTurn('function');
 
     expect(correctGuess).to.equal('correct!')
     expect(incorrectGuess).to.equal('incorrect!')
   });
 
   it('should calculate the percentage of correct guesses', () => {
-    round.takeTurn('rat');
-    round.takeTurn('all of it');
-    round.takeTurn('peuce');
+    round.takeTurn('array');
+    round.takeTurn('array');
+    round.takeTurn('accessor method');
 
-    expect(round.calculatePercentCorrect()).to.equal(33)
+    expect(round.calculatePercentCorrect()).to.equal(93)
   });
 
   it('should log a message to let the user know how they did', () => {
-    round.takeTurn('capybara');
-    round.takeTurn('a little');
-    round.takeTurn('peuce');
+    round.takeTurn('object');
+    round.takeTurn('function');
+    round.takeTurn('accessor method');
 
-    expect(round.endRound()).to.equal('** Round over! ** You answered 33% of the questions correctly!')
+    expect(round.endRound()).to.equal('** Round over! ** You answered 93% of the questions correctly!')
   });
 });
