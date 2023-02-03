194ee714d6bbe2ecb698cdfa96abdd3559ca6fb8
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-02-02 18:03:02 -0800

    Add: method for instantiating cards to data set, start round, and start game

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,16 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Round = require("./Round");
+const Card = require("./Card");
+const Deck = require("./Deck");
+//card/deck/round
+
 class Game {
-  constructor() {}
+  constructor(data) {
+    this.currentRound;
+    this.data = data
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +20,28 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  createCards(){
+    const cardInfo = prototypeQuestions.map(dataInfo => {
+      const cards = new Card(dataInfo["id"], dataInfo["question"], dataInfo["answers"], dataInfo["correctAnswer"])
+      return cards
+    })
+    return cardInfo
+  }
+
+  makeDeck(){
+    return new Deck(this.createCards())
+  }
+
+  createRound(){
+    this.currentRound = new Round(this.makeDeck())
+    return this.currentRound
+  }
+
+  start(){
+    this.printMessage(this.makeDeck(), this.createRound())
+    this.printQuestion(this.createRound())
   }
 }
 
