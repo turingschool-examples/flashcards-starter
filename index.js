5b7430b68e68ce792bbb50d402669954c33fc621
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-02-02 17:25:35 -0800

    Add: game method and start new game

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // This is where your project starts.
+const Game = require("./src/Game")
+const data = require("./src/data");
+const prototypeQuestions = data.prototypeData;
 
+const newGame = new Game(prototypeQuestions);
+newGame.start()
 console.log('Your project is running...'); 
