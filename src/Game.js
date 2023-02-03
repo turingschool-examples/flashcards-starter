304166a813f2f74bab5dcb2fdeb91ad0cb180f73
Joshua Martin <ursa8minority@gmail.com>
2023-02-02 20:22:53 -0700

    created functionality start and createcards in game data model

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,21 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null;
+  }
+  start() {
+    const cards = this.createCards();
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck);
+    this.printQuestion();
+  }
+  createCards(questions) {
+    return questions.map(question => new Card(question));
+    }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -14,5 +27,16 @@ class Game {
       util.main(round);
   }
 }
+
+const game = new Game();
+game.start();
+console.log(game.currentRound);
+
+const Game = require('../src/Game')
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+
 
 module.exports = Game;
---
+++ test/Game-test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const expect = chai.expect;
+const assert = chai.assert;
+
+const Game = require('../src/Game')
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
