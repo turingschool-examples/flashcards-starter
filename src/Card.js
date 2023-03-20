fd237b53b9dcc142d469a0200dbc56db54166321
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-03-20 15:19:50 -0700

    shows the card class created

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+class Card {
+    constructor(id,question, answers, correctAnswer) {
+        this.id = id
+        this.question = question
+        this.answers = answers
+        this.correctAnswer = correctAnswer
+    }
+}
+
+
+
+
+
+
+module.exports = Card; 
