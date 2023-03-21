00d4af5cffdb27a47ae9455508e75a240d34cca3
“Sarah <sarahcatherine311@gmail.com>
2023-03-20 20:14:31 -0600

    feat: Complete the Deck-test.js file

--- test/Card-test.js
+++ test/Card-test.js
@@ -6,7 +6,6 @@ const Card = require('../src/Card');
 describe('Card', function() {
 
   it('should be a function', function() {
-    const card = new Card();
     expect(Card).to.be.a('function');
   });
 
---
+++ test/Deck-test.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+const Card = require('../src/Card');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+
+describe('Deck', function() {
+
+  it('should be a function', function() {
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  }); 
+
+  it('should take in an array of cards', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.cards).to.equal([card1, card2, card3]);
+  });
+
+  it('should be able to count the number of cards', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.countCards()).to.equal(3);
+  });  
+});
