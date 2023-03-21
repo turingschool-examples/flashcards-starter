bd115320c5a5ed82dd8a7a12500b6fdcc73620cf
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-21 17:53:51 -0400

    Add data to test data file

---
+++ src/test-data.js
@@ -0,0 +1,40 @@
+const testData = [{
+  "id": 28,
+  "question": "Which prototype method returns an array of an object's property values?",
+  "answers": ["Object.keys()", "Object.values()", "Object.assign()"],
+  "correctAnswer": "Object.values()"
+}, {
+  "id": 29,
+  "question": "map() takes in two optional arguments: the index of the current element, and the array that map was called upon",
+  "answers": ["true", "false"],
+  "correctAnswer": "true"
+}, {
+  "id": 30,
+  "question": "What type of methods are functions that allow you to manipulate the value of a particular data type or class?",
+  "answers": ["prototype method", "object", "callback function"],
+  "correctAnswer": "prototype method"
+}, {
+  "id": 22,
+  "question": "Which prototype method is used to iterate over each property of an object?",
+  "answers": ["Object.keys()", "Object.values()", "reduce()"],
+  "correctAnswer": "Object.keys()"
+}, {
+  "id": 23,
+  "question": "Which prototype method can make a copy of a pre-existing object or merge two or more objects together?",
+  "answers": ["Object.assign()", "Object.keys()", "filter()"],
+  "correctAnswer": "Object.assign()"
+}, {
+  "id": 24,
+  "question": "Object.assign() can take in an indefinite amount of arguments",
+  "answers": ["true", "false"],
+  "correctAnswer": "true"
+}, {
+  "id": 25,
+  "question": "Arrays are specialized objects where the keys are ordered numbers.",
+  "answers": ["true", "false"],
+  "correctAnswer": "true"
+}]
+
+module.exports = {
+  testData
+}
