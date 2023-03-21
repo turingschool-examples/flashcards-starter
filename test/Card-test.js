9c1f56486cbbc5872fe7ed7cd4d5eda8dda3334c
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-21 19:33:36 -0400

    Add arrow functions

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,32 +1,33 @@
 const chai = require('chai');
 const expect = chai.expect;
-
 const Card = require('../src/Card');
+
+let card;
 
 describe('Card', () => {
 
+  beforeEach(() => {
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  });
+
   it('should be a function', () => {
-    const card = new Card();
     expect(Card).to.be.a('function');
   });
 
   it('should be an instance of Card', () => {
-    const card = new Card();
     expect(card).to.be.an.instanceof(Card);
   }); 
 
   it('should store a question', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
   });  
 
   it('should store a list of possible answers', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
   });  
 
   it('should store the correct answer', () => {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.correctAnswer).to.equal('object');
   });
 });
