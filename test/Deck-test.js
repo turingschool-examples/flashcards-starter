7576ae0811a0627621677865f906b93a7319e34b
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-21 17:59:15 -0400

    Remove console logs and arbitrary class instantiation

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,25 +4,21 @@ const expect = chai.expect;
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 const testData = require('../src/test-data.js');
-console.log(testData)
-const sampleCards = testData.testData.map(playingCard => playingCard)
-console.log(sampleCards)
+const sampleCards = testData.testData.map;(playingCard => playingCard);
 
 let deck;
-let deck2;
 
 describe('Deck', function(){
 
   beforeEach(() => {
     deck = new Deck(sampleCards);
-    deck2 = new Deck();
   })
   it('should be a function', function() {
     expect(Deck).to.be.a('function');
   });
 
   it('should be an instance of Deck', function() {
-    expect(deck2).to.be.instanceof(Deck);
+    expect(deck).to.be.instanceof(Deck);
   });
 
   it('should be initialized with an array', function() {
