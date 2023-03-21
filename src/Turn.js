4988de5347a4097edc01577badd46130df24c7a2
Adriane Sutherland <asutherland91@gmail.com>
2023-03-20 19:05:17 -0700

    Add completed Turn class

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,33 @@
 class Turn {
   constructor(guess, card) {
+   this.guess = guess;
+   this.card = card; 
+  }
 
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.card;
+  }
+
+  evaluateGuess() {
+    if(this.guess === this.card.correctAnswer) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  giveFeedback() {
+    if(this.evaluateGuess()) {
+      return "correct!";
+    }
+    else {
+      return "incorrect!"
+    }
   }
 }
 
