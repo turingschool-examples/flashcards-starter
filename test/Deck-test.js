d87d6ca79e8c0e347cb6da30b1d0343e1db611bb
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-21 18:59:45 -0400

    Change functions to arrow

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -2,30 +2,29 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Deck = require('../src/Deck');
-const Card = require('../src/Card');
 const testData = require('../src/test-data.js');
 const sampleCards = testData.testData.map;(playingCard => playingCard);
 
 let deck;
 
-describe('Deck', function(){
+describe('Deck', () => {
 
   beforeEach(() => {
     deck = new Deck(sampleCards);
-  })
-  it('should be a function', function() {
+  });
+  it('should be a function', () => {
     expect(Deck).to.be.a('function');
   });
 
-  it('should be an instance of Deck', function() {
+  it('should be an instance of Deck', () => {
     expect(deck).to.be.instanceof(Deck);
   });
 
-  it('should be initialized with an array', function() {
+  it('should be initialized with an array', () => {
     expect(deck.cards).to.deep.equal(sampleCards);
   });
 
-  it('should know how many cards it has', function() {
+  it('should know how many cards it has', () => {
 
     deck.findNumberOfCards();
 
