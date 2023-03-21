6675784226bb9d849dfd8eacd48e13f9b1973dad
Houda Jawad <houdajawad@gmail.com>
2023-03-20 19:57:45 -0700

    Add methods to turn file and tests to turn test file

--- src/Turn.js
+++ src/Turn.js
@@ -2,10 +2,31 @@ class Turn {
     constructor(userGuess, card) {
      this.userGuess = userGuess
      this.card = card
-     console.log(this.card)
-  
-    }
+   }
 
+returnGuess() {
+    return this.userGuess
+}
+
+returnCard() {
+    return this.card
+}
+
+evaluateGuess() {
+    if (this.userGuess === this.card.correctAnswer) {
+        return true
+    } else {
+        return false
+    }
+}
+
+giveFeedback() {
+    if (this.userGuess === this.card.correctAnswer) {
+        return `correct!`
+    } else {
+        return `incorrect!`
+    }
+}
 
 }
 
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,18 +6,18 @@ const Card = require('../src/Card')
 
 describe('Turn', function () {
 
-it.skip('should be a function', function() {
+it('should be a function', function() {
   const turn = new Turn();
   expect(Turn).to.be.a("function");
 })
 
-it.skip('should be an instance of Turn', function() {
+it('should be an instance of Turn', function() {
   const turn = new Turn();
   expect(turn).to.be.an.instanceOf(Turn);
 })
 
 
-it.skip('should store a users guess', function() {
+it('should store a users guess', function() {
   const turn = new Turn('pug');
   expect(turn.userGuess).to.equal('pug')
 })
@@ -28,7 +28,34 @@ it('should store a Card object', function() {
 
   expect(turn.userGuess).to.equal('pug')
   expect(turn.card).to.deep.equal(card)
+})
 
+it('should return a users guess', function() {
+  const card = new Card(1,'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  const turn = new Turn('pug', card)
+
+  expect(turn.returnGuess()).to.equal('pug')
+})
+
+it('should return a card', function() {
+   const card = new Card(1,'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+   const turn = new Turn('pug', card)
+
+   expect(turn.returnCard()).to.equal(card)
+})
+
+it('should return false when not the correct answer', function() {
+    const card = new Card(1,'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('pug', card)
+
+    expect(turn.evaluateGuess()).to.equal(false)
+})
+
+it('should return a message when not the correct answer', function() {
+    const card = new Card(1,'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('pug', card)
+
+    expect(turn.giveFeedback()).to.equal(`incorrect!`)
 })
 
 });
