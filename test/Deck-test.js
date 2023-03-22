608f58a58047b3158e47d86c6273a8864ce23a5f
Sara Park <sspark106@gmail.com>
2023-03-21 19:02:38 -0600

    feat: Add first three tests to Deck-test

---
+++ test/Deck-test.js
@@ -0,0 +1,35 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+
+describe('Turn', function() {
+    var deck, card1, card7, card14
+    beforeEach(() => {
+        card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+        card7 = new Card(7, "Which array prototype is not an accessor method?", ["join()", "slice()", "splice()"], "splice()");
+        card14 = new Card(14, "Which iteration method can turn an array into a single value of any data type?", ["reduce()", "map()", "filter()"], "reduce()");
+    
+        deck = new Deck();
+    });
+
+    it('should be a function', () => {
+        expect(Deck).to.be.a('function');
+    });
+  
+    it('should instantiate a new deck', () => {
+        expect(turn).to.be.an.instanceof(Deck);
+    });
+
+    it('should take in an array of Card objects', () => {
+        deck = new Deck([card1, card7, card14]);
+
+        expect(deck.cards).to.equal([card1, card7, card14]);
+    });
+
+    it('should know how many cards are in the deck', () => {
+        
+    })
+
+})
