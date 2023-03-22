2e9d550798ef956303568a24a03c5cccfa9cb36f
Em Lindvall <elindvall22@turing.edu>
2023-03-21 22:05:35 -0500

    Refactor deck class implementation code to include the countCards function and corresponding test code

---
+++ test/Deck-test.js
@@ -0,0 +1,37 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+const Deck = require('../src/Deck');
+
+describe('Deck', function() {
+
+  it('should be a function', function() {
+    const turn = new Deck();
+
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be able to store card objects', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+   
+    const deck = new Deck([card1, card2, card3]);
+
+    expect(deck.cards).to.deep.equal([card1, card2, card3]);
+  });
+
+  it('should be able to know how many cards are in the deck', function()  {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+   
+    const deck = new Deck([card1, card2, card3]);
+
+    deck.countCards();
+
+    expect(deck.countedCards).to.deep.equal(3);
+  });
+});
