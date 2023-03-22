b2bb9ac14ff8a8d91d847a26c486a14b0048c790
Devynne Marshall <devynnem@gmail.com>
2023-03-21 20:32:59 -0600

    Add to my turn test and turn class nearly finishing iteration 1

--- src/Turn.js
+++ src/Turn.js
@@ -2,7 +2,23 @@ class Turn {
   constructor(usersGuess, card) {
     this.guess = usersGuess;
     this.card = card;
-    console.log(this.card)
+    // console.log(this.card)
+  };
+  returnGuess() {
+    return this.guess;
+  };
+  returnCard() {
+    return this.card;
+  };
+  evaluateGuess() {
+    return this.guess === this.card.correctAnswer
+  };
+  giveFeedback() {
+    if (this.evaluateGuess()) {
+      return 'correct!'
+    } else {
+      return 'incorrect!'
+    };
   };
 };
 
