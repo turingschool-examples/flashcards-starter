b4a78131f0b66c52e55c3837f4308fcf9e959bf6
Rachel Brendel <brendelrachel@gmail.com>
2023-03-22 20:41:14 -0500

    Add class and test files with general layout

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,11 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
---
+++ test/Game-test.js
@@ -0,0 +1,7 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Game', function() {
+  it('should be a function', function() {
+  });
+})
