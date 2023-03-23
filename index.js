85708572d98685e237dada9c422d26fe3a96f818
Devynne Marshall <devynnem@gmail.com>
2023-03-23 16:24:40 -0600

    Play my game correctly in the terminal

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // This is where your project starts.
-const Game = require('../src/Game');
+const Game = require('./src/Game');
 
 var game = new Game();
 
--- src/Round.js
+++ src/Round.js
@@ -38,6 +38,7 @@ class Round {
 
   endRound() {
     this.calculatePercentCorrect()
+    console.log(`**Round over!**You answered ${this.percentCorrect} of the questions correctly!`)
     return `**Round over!**You answered ${this.percentCorrect} of the questions correctly!`
   };
 };
