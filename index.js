675875fc6b19252396cc6c0198a14f500b58292c
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-23 16:59:53 -0400

    Update index.js to instantiate an array of cards for the game

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const data = require('../flashcards/src/data');
 const prototypeQuestions = data.prototypeData;
 const Game = require('../flashcards/src/Game');
+const Card = require('../flashcards/src/Card')
 
+const gameData = prototypeQuestions.map(card => new Card(card.id, card.question, card.answers, card.correctAnswer))
 const game = new Game();
 
-game.start(prototypeQuestions);
+game.start(gameData);
 console.log('Your project is running...'); 
