a58323a5e92e165380a07321080cc35936388a8b
Robbie Jaeger <jaeger.rob@gmail.com>
2023-04-13 21:18:50 -0600

    Edit starter kit

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Card = require('../src/Card');
+const Card = require('../src/card');
 
 describe('Card', function() {
 
@@ -9,11 +9,6 @@ describe('Card', function() {
     const card = new Card();
     expect(Card).to.be.a('function');
   });
-
-  it.skip('should be an instance of Card', function() {
-    const card = new Card();
-    expect(card).to.be.an.instanceof(Card);
-  }); 
 
   it.skip('should store a question', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
