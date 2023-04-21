cb57ddca7370b480c5a926025c94a05ce79f8456
Robbie Jaeger <jaeger.rob@gmail.com>
2023-04-21 14:27:05 -0600

    Add card file and edit test boilerplate

--- test/Card-test.js
+++ test/Card-test.js
@@ -8,8 +8,9 @@ describe('card', function() {
     expect(createCard).to.be.a('function');
   });
 
-  it.skip('should store a question', function() {
+  it.skip('should create a card and its properties', function() {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    
     expect(card.id).to.equal(1);
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
