4787ea71b100d12cb2ed6d0f726be463d097f6fc
Daniel Cochico <dcochico.20@gmail.com>
2023-05-06 19:42:45 -0700

    Create turn tests

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard } = require('../src/card');
+const { createCard, evaluateGuess } = require('../src/card');
 
 describe('card', function() {
   it('should be a function', function() {
@@ -16,4 +16,19 @@ describe('card', function() {
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
     expect(card.correctAnswer).to.equal('object');
   });  
+});
+
+describe('turn', function() {
+  it.skip('should be a function', function() {
+    expect(evaluateGuess).to.be.a('function');
+  });
+
+  it.skip('should evaluate if a guess is correct or incorrect', function() {
+    const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const correctGuess = evaluateGuess('object', card.correctAnswer);
+    const incorrectGuess = evaluateGuess('array', card.correctAnswer);
+
+    expect(correctGuess).to.equal('correct!');
+    expect(incorrectGuess).to.equal('incorrect!');
+  });
 });
