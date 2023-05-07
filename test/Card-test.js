82cbb8e70cec694419406b496c41390939ef692b
Daniel Cochico <dcochico.20@gmail.com>
2023-05-06 19:58:45 -0700

    Revise turn tests

--- test/Card-test.js
+++ test/Card-test.js
@@ -25,10 +25,10 @@ describe('turn', function() {
 
   it.skip('should evaluate if a guess is correct or incorrect', function() {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const correctGuess = evaluateGuess('object', card.correctAnswer);
-    const incorrectGuess = evaluateGuess('array', card.correctAnswer);
+    const correctGuess = evaluateGuess('object', card);
+    const incorrectGuess = evaluateGuess('array', card);
 
-    expect(correctGuess).to.equal('correct!');
-    expect(incorrectGuess).to.equal('incorrect!');
+    expect(correctGuess).to.deep.equal('correct!');
+    expect(incorrectGuess).to.deep.equal('incorrect!');
   });
 });
