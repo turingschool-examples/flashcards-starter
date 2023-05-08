d86eaf0d192e6cd34e108dbff83086489b59fe6d
Jamie Caudill <j.caudill7177@gmail.com>
2023-05-08 16:17:02 -0700

    feat: Write tests for evaluateGuess function for if guess is incorrect

--- test/turn-test.js
+++ test/turn-test.js
@@ -4,10 +4,16 @@ const expect = chai.expect;
 const { evaluateGuess } = require('../src/turn');
 
 describe('evaluateGuess', () => {
-  it('should return correct if the guess is correct', () => {
+  it("should return 'correct!' if the guess is correct", () => {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    userGuess = 'object';
-    turn = evaluateGuess(userGuess, card);
-    expect(turn).to.equal('correct!')
+    const userGuess = 'object';
+    const turn = evaluateGuess(userGuess, card);
+    expect(turn).to.equal('correct!');
+  });
+  it("should return 'incorrect!' if the guess is incorrect", () => {
+    const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const userGuess = 'array';
+    const turn = evaluateGuess(userGuess, card);
+    expect(turn).to.equal('incorrect!');
   })
 });
