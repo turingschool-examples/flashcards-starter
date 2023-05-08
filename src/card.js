46b3a5829b8267bccf78525845c0c37c5e300cc8
Jamie Caudill <j.caudill7177@gmail.com>
2023-05-08 16:00:20 -0700

    feat: Write createCard function and pass tests

---
+++ src/card.js
@@ -0,0 +1,13 @@
+module.exports = {
+  createCard
+}
+
+function createCard(id, question, answers, correctAnswer) {
+  let card = {
+    id: id,
+    question: question,
+    answers: answers,
+    correctAnswer: correctAnswer
+  }
+  return card;
+}
