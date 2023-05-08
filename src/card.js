3eb9fa82446c514754de47c80e8ce15cbe7b9fae
Alec Livaditis <aleclivaditis@gmail.com>
2023-05-08 19:56:30 -0400

    createCard function

---
+++ src/card.js
@@ -0,0 +1,11 @@
+function createCard(id, question, answers, correctAnswer) {
+  card = {
+    id,
+    question,
+    answers,
+    correctAnswer
+  }
+  return card
+}
+
+module.exports = {createCard}
