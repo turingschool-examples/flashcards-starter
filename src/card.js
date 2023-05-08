c7b5088a09ea9fa2d9ca170a70d7eceaea0cf55f
Natalie Baker <nathelene76@gmail.com>
2023-05-08 17:42:40 -0600

    add createCard function

---
+++ src/card.js
@@ -0,0 +1,18 @@
+const createCard = (id,question,answers,correctAnswer) => {
+    let card = {
+        id: id,
+        question: question,
+        answers: answers,
+        correctAnswer: correctAnswer
+    }
+    return card
+}
+
+
+
+
+
+module.exports = {
+createCard,
+
+}
