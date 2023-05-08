007ad465b9ad6441db53b591809675cba6142ee2
Jamie Caudill <j.caudill7177@gmail.com>
2023-05-08 16:34:43 -0700

    feat: Write test for createDeck for different size decks

--- test/deck-test.js
+++ test/deck-test.js
@@ -11,5 +11,10 @@ describe('deck', () => {
     const card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     const deck = createDeck([card1, card2, card3]);
     expect(deck.length).to.equal(3);
-  })
+  });
+  it('should work with different size decks', () => {
+    const card1 = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const deck = createDeck([card1]);
+    expect(deck.length).to.equal(1);
+  });
 })
