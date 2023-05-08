aeb0c124db8f5ed4e500df0de11820a4c3206935
Benjamin holden <benjamin.f.holden@outlook.com>
2023-05-08 17:45:37 -0600

    Add createCard function

---
+++ src/card.js
@@ -0,0 +1,12 @@
+function createCard(id, question, answers, correctAnswer) {
+  let card = {
+    id: id,
+    question: question,
+    answers: answers,
+    correctAnswer: correctAnswer
+  }
+  return card
+}
+
+
+module.exports = { createCard };
