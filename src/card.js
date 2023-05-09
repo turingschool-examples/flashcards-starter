82baa6188181def2c08b6a5ab45f3ff3bd1abd9d
Parvin (Parveen) <119267809+Sulton88Mehron90@users.noreply.github.com>
2023-05-09 04:21:33 -0600

    Update card.js

--- src/card.js
+++ src/card.js
@@ -3,7 +3,7 @@ const createCard = (id, question, answers, correctAnswer) => {
     id: id,
     question: question,
     answers: answers,
-    correctAnswer
+    correctAnswer:correctAnswer
   };
 };
 
