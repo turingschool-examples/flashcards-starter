a03d1f872057b2a830ac69543caa0b0f81c782b3
Denzel Turambi <dturambi26@gmail.com>
2023-05-08 19:24:44 -0600

    Refactor createCard function into an arrow function

--- src/card.js
+++ src/card.js
@@ -1,12 +1,10 @@
-function createCard(id, question, answers, correctAnswer) {
+let createCard = (id, question, answers, correctAnswer) =>
   card = {
     id,
     question,
     answers,
     correctAnswer
-  }
-  return card;
-}
+  };
 
 module.exports = {
   createCard
