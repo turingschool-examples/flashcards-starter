557aef4f9b1f44a65d54100e26cd8c270cbfc7b1
Calli Herrmann <calliherrmann@gmail.com>
2023-05-08 21:44:29 -0400

    Add takeTurn function

--- src/round.js
+++ src/round.js
@@ -1,6 +1,8 @@
 // Your round will be the object that organizes guesses and 
 // records if they are correct or incorrect.
 
+const { evaluateGuess } = require("./card");
+
 const createRound = (deck) => {
     var round = {
         deck,
@@ -12,6 +14,17 @@ const createRound = (deck) => {
     return round;
 }
 
+const takeTurn = (guess, currentRound, deck) => {
+    var answer = evaluateGuess(guess, currentRound.currentCard)
+    if (answer === 'incorrect!'){
+        currentRound.incorrectGuesses.push(currentRound.currentCard.id)
+    }
+    currentRound.turns += 1;
+    currentRound.currentCard = deck[currentRound.turns]
+    return answer
+}
+
 module.exports = { 
     createRound,
+    takeTurn,
 };
--- test/round-test.js
+++ test/round-test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 
 const { createCard, evaluateGuess } = require('../src/card');
 const { createDeck, countCards } = require('../src/deck');
-const { createRound } = require('../src/round');
+const { createRound, takeTurn } = require('../src/round');
 
 describe('round', function() {
     it('should return a round', function() {
@@ -13,9 +13,26 @@ describe('round', function() {
 
         const deck = createDeck([card1, card2, card3]);
         const round = createRound(deck)
+
         expect(round.deck).to.equal(deck)
         expect(round.currentCard).to.equal(card1)
         expect(round.turns).to.equal(0)
         expect(round.incorrectGuesses).to.deep.equal([])
+    })
+})
+
+describe('take turn', function() {
+    it('should allow user to take a turn', function() {
+        const card1 = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+        const card2 = createCard(2, 'What is a comma-separated list of related values?', ['object', 'array', 'function'], 'array');
+        const card3 = createCard(3, 'What is an example of a mutator method?', ['sort()', 'map()', 'join()'], 'sort()')
+
+        const deck = createDeck([card1, card2, card3]);
+        const round = createRound(deck)
+        const guess = takeTurn('object', round, deck)
+
+        expect(guess).to.equal('correct!')
+        expect(round.currentCard).to.equal(card2)
+        expect(round.turns).to.equal(1)
     })
 })
