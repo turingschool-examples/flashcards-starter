4ed34bdad5255885d54ee710242f6c1b6e6d6e3f
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-05-09 14:02:28 -0600

    Set up mocha for round tests

---
+++ test/round-test.js
@@ -0,0 +1,35 @@
+const chai = require('chai');
+const expect = chai.expect;
+const { } = require('../src/round');
+
+/* ROUND
+- Have a deck, curentCard, incorrect guesses and turn property to track those elements
+- takeTurn function should update these elements 
+    - 
+- calculatePrecentCorrect function with round as input calcs how many were guessed correctly and returns as a precent
+- endROund function prints message that round is over and result of calc percent function
+*/
+
+describe('Checks that the deck, currentCard, incorrectGuesses, and Turn variables are all available', function(){
+    it('should have a deck that holds cards', function(){
+
+    });
+    it('should have a current card that is the first card in the deck', function(){
+
+    });
+    it('should have a turns tracker starting at 0', function(){
+
+    });
+    it('should have an empty array to store cards guess wrong', function(){
+
+    });
+});
+describe('Checks takeTurn functionality', function(){
+
+});
+describe('Checks calculatePercent functionality', function(){
+
+});
+describe('Checks endROund Functionality', function(){
+
+});
