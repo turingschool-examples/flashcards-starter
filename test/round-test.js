d4e2ba99818c23478cbc04429b9b93f62a5b77cd
Alec Livaditis <aleclivaditis@gmail.com>
2023-05-09 08:42:27 -0400

    add test that checks that incorrectGuesses are tracked by id in round

--- test/round-test.js
+++ test/round-test.js
@@ -107,7 +107,7 @@ describe('takeTurn', function(){
     const round = createRound(deck);
 
     takeTurn('sea otter', round)
-    takeTurn('pug', round)
+    takeTurn('gallbladder', round)
 
     expect(round.turns).to.be.equal(2)
   })
@@ -124,5 +124,20 @@ describe('takeTurn', function(){
     takeTurn('sea otter', round)
 
     expect(round.currentCard).to.be.deep.equal(card2)
+  })
+  
+  it('should be able to store incorrectGuesses', function() {
+    const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+    const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder')
+    const card3 = createCard(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap')
+    
+    const deck = createDeck([card1, card2, card3])
+    
+    const round = createRound(deck);
+
+    takeTurn('pug', round)
+    takeTurn('spleen', round)
+
+    expect(round.incorrectGuesses).to.be.deep.equal([1, 14])
   })
 })
