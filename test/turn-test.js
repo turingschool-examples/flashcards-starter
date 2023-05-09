9a9ef6b58a546c87d9373e7cc6e83d14a2397651
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-05-09 19:38:19 -0400

    Create turn test.

---
+++ test/turn-test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const { evaluateGuess } = require('../src/turn');
+
+describe('turn', function() {
+  it('should be a function', function() {
+    expect(evaluateGuess).to.be.a('function');
+  });
+
+  it('should evaluate if a guess is correct', () => {
+    guess = evaluateGuess('object', 'object');
+    expect(guess).to.equal('Correct!')
+  })
+
+  it('should evaluate if a guess is incorrect', () => {
+    guess2 = evaluateGuess('object', 'array');
+    expect(guess2).to.equal('Incorrect - please try again.')
+  })
+});
