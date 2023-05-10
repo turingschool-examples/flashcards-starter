186c0d80ee597128d4059caaaa3a076f28bf6291
Denzel Turambi <dturambi26@gmail.com>
2023-05-10 16:52:49 -0600

    Add createDeck function that creates a deck of cards, still a WIP

---
+++ src/deck.js
@@ -0,0 +1,15 @@
+// function createDeck(cards) {
+//   deck = [cards]
+//   return deck
+// }
+
+const createDeck = (cards) => {
+  // console.log(cards)
+  let deck = [cards]
+  console.log(deck)
+  return deck
+}
+
+module.exports = {
+  createDeck
+}
