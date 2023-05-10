371aa5410e8571d8619fb4b345091dcb03259464
Mike Gudenau <mikegudenau@gmail.com>
2023-05-10 13:04:30 -0700

    Update should statement describing deck

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -9,7 +9,7 @@ describe('deck', function() {
     expect(createDeck).to.be.a('function');
   });
   
-  it('should create an array of card objects', function() {
+  it('should create an object with an array of cards', function() {
     const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
