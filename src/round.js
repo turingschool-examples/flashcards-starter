408b0c5d93c7f7aef3bf5a02e631478747c3aa76
Matt Fowler <mbfowler@gmail.com>
2023-05-09 22:14:36 -0400

    Add initial logic for takeTurn

--- src/round.js
+++ src/round.js
@@ -1,9 +1,21 @@
-function createRound() {
-
+function createRound(deck) {
+    return {
+        deck,
+        currentCard: deck[0],
+        turns: 0,
+        incorrectGuesses: 0
+    };
 }
 
-function takeTurn() {
-
+function takeTurn(guess, round) {
+    round.turns++;
+    if (guess !== round.currentCard.correctAnswer) {
+        round.incorrectGuesses++;
+    }
+    
+    let nextCardIndex = round.deck.indexOf(round.currentCard) + 1;
+    round.currentCard = round.deck[nextCardIndex];
+    return round;
 }
 
 function endRound() {
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,17 +1,20 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const { createCard } = require('../src/card');
+const { createDeck } = require('../src/deck');
 const { createRound, takeTurn, endRound, calculatePercentCorrect } = require('../src/round');
 
 const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
 const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
 const card3 = createCard(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
 
-const deck = createDeck([card1, card2, card3]);
+const deck = createDeck(card1, card2, card3);
 
-const round = createRound(deck);
+
 
 describe('createRound', function() {
+    let round = createRound(deck);
     it('should properly create the round object', function() {
         expect(round.deck.length).to.equal(3);
         expect(round.currentCard.id).to.equal(1);
@@ -21,18 +24,19 @@ describe('createRound', function() {
 });
 
 describe('takeTurn', function() {
-    takeTurn('sea otter', round);
-    takeTurn('spleen', round);
+    let round = createRound(deck);
+    round = takeTurn('sea otter', round);
+    round = takeTurn('spleen', round);
     
-    it.skip('should properly track the number of turns taken', function() {
+    it('should properly track the number of turns taken', function() {
         expect(round.turns).to.equal(2);
     });
 
-    it.skip('should properly track the number of incorrect guesses', function() {
+    it('should properly track the number of incorrect guesses', function() {
         expect(round.incorrectGuesses).to.equal(1);
     });
 
-    it.skip('should correctly update currentCard after a guess', function() {
+    it('should correctly update currentCard after a guess', function() {
         expect(round.currentCard.id).to.equal(12);
     });
 });
