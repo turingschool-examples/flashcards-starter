9906633d15c8bf7155a1075b3f16fe50d39ffda5
Denzel Turambi <dturambi26@gmail.com>
2023-05-10 16:52:16 -0600

    Change argument being passed through into an array

--- test/deck-test.js
+++ test/deck-test.js
@@ -13,7 +13,7 @@ describe('createDeck', function() {
     const card1 = createCard(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
     const card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
     const card3 = createCard(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
-    const deck = createDeck(card1, card2, card3)
-  expect(deck).to.equal([card1, card2, card3])
+    const deck = createDeck([card1, card2, card3])
+  expect(deck).to.deep.equal([card1, card2, card3])
   });
 });
