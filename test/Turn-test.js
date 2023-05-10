73ac3db84e20356c2cba399c13c443b08e1341ee
Mike Gudenau <mikegudenau@gmail.com>
2023-05-09 17:56:54 -0700

    Add test for correct or incorrect

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -7,6 +7,14 @@ const { evaluateGuess } = require('../src/turn')
 describe('turn', function() {
   it('should be a function', function() {
     expect(evaluateGuess).to.be.a('function');
-  })
-});
+  });
 
+  it('should evaluate if user guess is correct or incorrect', function() {
+    
+    const correct = evaluateGuess("object", "object");
+    const incorrect = evaluateGuess("array", "object");
+
+    expect(correct).to.equal("Correct!");
+    expect(incorrect).to.equal("Incorrect");
+  });
+});
