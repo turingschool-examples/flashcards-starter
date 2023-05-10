ae354718a9738d514ae536f13c0994f2f4207609
Denzel Turambi <dturambi26@gmail.com>
2023-05-10 16:31:08 -0600

    Add it block to test if deck contains cards

--- test/deck-test.js
+++ test/deck-test.js
@@ -2,11 +2,18 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const { createDeck } = require('../src/deck');
+const { createCard } = require('../src/card');
 
 describe('createDeck', function() {
   it('should be a function', function() {
     expect(createDeck).to.be.a('function');
   });
 
-  
+  it('should create a deck with cards', function() {
+    const card1 = createCard(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+    const card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+    const card3 = createCard(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+    const deck = createDeck(card1, card2, card3)
+  expect(deck).to.equal([card1, card2, card3])
+  });
 });
