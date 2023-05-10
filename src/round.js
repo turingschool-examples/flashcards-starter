016d371902e3da208a57da532b6bc607857f261d
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-05-10 17:09:10 -0600

    Create functions to create, update, and end the rounds

--- src/round.js
+++ src/round.js
@@ -1,7 +1,40 @@
+const { evaluateGuess } = require('../src/turns');
 
+const createRound = (deck) => {
+    let round = {
+        deck: deck,
+        currentCard: deck[0],
+        turns: 0,
+        incorrectGuesses: []
+    }
+    return round 
+};
 
+const takeTurn = (guess, round) => {
+    result = evaluateGuess(guess, round.currentCard.correctAnswer)
+    if(result === 'incorrect!'){
+        round.incorrectGuesses.push(round.currentCard.id)
+    } 
+    round.turns++
+    round.currentCard = round.deck[round.turns]
+    return `You guessed ${result}`
+};
 
+const calculatePercentCorrect = (round) => {
+
+    const percentCorrect = Math.round(100*(1-(round.incorrectGuesses.length/round.turns)))
+
+    return `${percentCorrect}%`
+};
+
+const endRound = (round) => {
+    const percentCorrect = calculatePercentCorrect(round);
+    return `**ROUND OVER** You answered ${percentCorrect} of the questions correctly!`
+};
 
 module.exports ={
-    
+    createRound, 
+    takeTurn,
+    calculatePercentCorrect,
+    endRound
 }
--- test/round-test.js
+++ test/round-test.js
@@ -1,9 +1,8 @@
 const chai = require('chai');
 const expect = chai.expect;
-const { } = require('../src/round');
+const { createRound, takeTurn, calculatePercentCorrect, endRound } = require('../src/round');
 const { sampleData } = require('../src/sample-data');
 const { createCard } = require('../src/card');
-const { } = require('../src/turns');
 const { createDeck } = require('../src/deck');
 
 describe('checks that the deck, currentCard, incorrectGuesses, and Turn variables are all available inside the round', function(){
@@ -58,7 +57,7 @@ describe('checks that the deck, currentCard, incorrectGuesses, and Turn variable
 describe('Checks takeTurn functionality', function(){
     it('should update the turn count each time a guess is made', function(){
         let cards = []
-        for (let i=0; i<3; i++){
+        for (let i=0; i<6; i++){
           const card = createCard(sampleData[i].id, sampleData[i].question, sampleData[i].answers, sampleData[i].correctAnswer)
           cards.push(card)
         }
@@ -74,7 +73,7 @@ describe('Checks takeTurn functionality', function(){
     });
     it('should change the currentCard to the next card in the deck', function(){
         let cards = []
-        for (let i=0; i<3; i++){
+        for (let i=0; i<6; i++){
           const card = createCard(sampleData[i].id, sampleData[i].question, sampleData[i].answers, sampleData[i].correctAnswer)
           cards.push(card)
         }
@@ -89,19 +88,96 @@ describe('Checks takeTurn functionality', function(){
         expect(round.currentCard).to.deep.equal(sampleData[3])
     });
     it('guess should be evaluated, if the guess is incorrect the card is stored in the incrrectGuesses array', function(){
-        
+      let cards = []
+      for (let i=0; i<6; i++){
+        const card = createCard(sampleData[i].id, sampleData[i].question, sampleData[i].answers, sampleData[i].correctAnswer)
+        cards.push(card)
+      }
+      const deck = createDeck(cards)
+      const round = createRound(deck)
+
+      takeTurn('method', round)
+      expect(round.incorrectGuesses).to.deep.equal([1])
+
+      takeTurn('array', round)
+      takeTurn('method', round)
+      expect(round.incorrectGuesses).to.deep.equal([1, 3])
+
     });
     it('user should recieve feedback on result of guess', function(){
+      let cards = []
+      for (let i=0; i<6; i++){
+        const card = createCard(sampleData[i].id, sampleData[i].question, sampleData[i].answers, sampleData[i].correctAnswer)
+        cards.push(card)
+      }
+      const deck = createDeck(cards)
+      const round = createRound(deck)
 
+      let result = takeTurn('method', round)
+      expect(result).to.equal('You guessed incorrect!')
+
+      result = takeTurn('array', round)
+      expect(result).to.equal('You guessed correct!')
     });
 });
 describe('checks calculatePercent functionality', function(){
     it('should calculate the percent of correct guesses made in a round', function(){
+      let cards = []
+      for (let i=0; i<6; i++){
+        const card = createCard(sampleData[i].id, sampleData[i].question, sampleData[i].answers, sampleData[i].correctAnswer)
+        cards.push(card)
+      }
+      const deck = createDeck(cards)
+      const round = createRound(deck)
+      takeTurn('wrong', round)
+      takeTurn('array', round)
+      takeTurn('wrong', round)
+      takeTurn('wrong', round)
+      takeTurn('iteration method', round)
+      takeTurn('sort()', round)
 
+      const percentCorrect = calculatePercentCorrect(round)
+
+      expect(percentCorrect).to.equal('50%')
+    });
+    it('should calculate the percent of correct guesses made in a round with a different number of incorrect guesses', function(){
+      let cards = []
+      for (let i=0; i<6; i++){
+        const card = createCard(sampleData[i].id, sampleData[i].question, sampleData[i].answers, sampleData[i].correctAnswer)
+        cards.push(card)
+      }
+      const deck = createDeck(cards)
+      const round = createRound(deck)
+      takeTurn('wrong', round)
+      takeTurn('array', round)
+      takeTurn('wrong', round)
+      takeTurn('wrong', round)
+      takeTurn('iteration method', round)
+      takeTurn('wrong', round)
+
+      const percentCorrect = calculatePercentCorrect(round)
+
+      expect(percentCorrect).to.equal('33%')
     });
 });
 describe('checks endRound Functionality', function(){
     it('should return an end of round message and the precent of correct guesses', function(){
+      let cards = []
+      for (let i=0; i<6; i++){
+        const card = createCard(sampleData[i].id, sampleData[i].question, sampleData[i].answers, sampleData[i].correctAnswer)
+        cards.push(card)
+      }
+      const deck = createDeck(cards)
+      const round = createRound(deck)
+      takeTurn('object', round)
+      takeTurn('array', round)
+      takeTurn('wrong', round)
+      takeTurn('wrong', round)
+      takeTurn('iteration method', round)
+      takeTurn('sort()', round)
 
+      const endOfRoundMessage = endRound(round)
+
+      expect(endOfRoundMessage).to.equal('**ROUND OVER** You answered 67% of the questions correctly!')
     });
 });
