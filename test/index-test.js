cb16d266e20528fd2637f0c18f321690aa7dc0c0
Calli Herrmann <calliherrmann@gmail.com>
2023-05-10 18:32:51 -0400

    Add test suite for index.js

--- test/index-test.js
+++
@@ -1,9 +0,0 @@
-const chai = require('chai');
-const expect = chai.expect;
-
-const { createCard } = require('../src/card');
-const { createDeck } = require('../src/deck');
-const { createRound, takeTurn, calculatePercentCorrect, endRound } = require('../src/round');
-const { prototypeData } = require("./src/data");
-const { printMessage, printQuestion } = require("./src/game");
-
