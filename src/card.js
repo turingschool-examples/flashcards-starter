3220808557ab80f735ad405eb298b0aa87328125
Victor Sriqui <victorsriqui@gmail.com>
2023-05-10 19:24:44 -0600

    feat: Working card test and card function

---
+++ src/card.js
@@ -0,0 +1,14 @@
+const createCard = (id, question, answers, correctAnswer) => {
+    return {
+        id: id, 
+        question: question, 
+        answers: answers, 
+        correctAnswer: correctAnswer
+    }
+}
+
+
+
+  module.exports = {
+    createCard,
+    }
