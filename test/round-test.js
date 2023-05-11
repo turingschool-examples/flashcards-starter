b5995d362dc2b6b5621b7add3756d971f9e8fd40
Denzel Turambi <dturambi26@gmail.com>
2023-05-10 18:48:55 -0600

    Add beforeEach function, so that each individual test runs on the same set of objects every time

--- test/round-test.js
+++ test/round-test.js
@@ -6,27 +6,25 @@ const {createDeck} = require('../src/deck');
 const {createRound} = require('../src/round');
 
 describe('rounds', function () {
+  let round, deck, card1, card2, card3;
+
+  beforeEach(function() {
+    card1 = createCard(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+    card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+    card3 = createCard(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+    deck = createDeck([card1, card2, card3]);
+    round = createRound(deck);
+  });
+
   it('should be a function', function() {
     expect(createRound).to.be.a('function');
   });
 
   it('should create a round object with a deck', function() {
-    const card1 = createCard(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
-    const card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
-    const card3 = createCard(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
-    const deck = createDeck([card1, card2, card3]);
-    const round = createRound(deck);
-
     expect(round.deck).to.be.deep.equal([card1, card2, card3]);
   });
 
   it('should include a current card that is equal to the first card in the deck', function() {
-    const card1 = createCard(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
-    const card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
-    const card3 = createCard(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
-    const deck = createDeck([card1, card2, card3]);
-    const round = createRound(deck);
-
     expect(round.currentCard).to.be.deep.equal({
       id: 1,
       question: "What allows you to define a set of related information using key-value pairs?",
