1e27218e9f73f95e2702f5ddb84400122e9e7b71
Caitlin Cradick <caitlincradick@gmail.com>
2023-05-11 13:18:41 -0700

    feature/ game is fully playable

--- src/game.js
+++ src/game.js
@@ -1,13 +1,11 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
-
-
-
-
+const { createCard, createDeck, countDeck} = require('./card');
+const {createRound} = require('./round');
 
 function printMessage(deck) {
-  console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
+  console.log(`Welcome to FlashCards! You are playing with ${countDeck(deck)} cards.
   -----------------------------------------------------------------------`);
 }
 
@@ -15,4 +13,17 @@ function printQuestion(round) {
   util.main(round);
 }
 
-module.exports = { printMessage, printQuestion };
+function start(){
+  // console.time('Timer!')
+  const questions = []
+  prototypeQuestions.forEach(question => {
+    questions.push(createCard(question.id, question.question, question.answers, question.correctAnswer))
+  });
+  console.log(questions)
+  const deck = createDeck(questions)
+  const count = countDeck(deck)
+  const round = createRound(deck)
+  printMessage(deck)
+  printQuestion(round)
+}
+module.exports = { printMessage, printQuestion, start};
--- test/Card-test.js
+++ test/Card-test.js
@@ -1,8 +1,8 @@
 const chai = require('chai');
 const expect = chai.expect;
-const { createCard, evaluateGuess, createDeck, countDeck, createRound, calculatePercentCorrect} = require('../src/card');
+const { createCard, evaluateGuess, createDeck, countDeck, calculatePercentCorrect} = require('../src/card');
 const { sampleData } = require('../src/sub-data');
-const { takeTurn, endRound } = require('../round.js');
+const { takeTurn, endRound, createRound } = require('../src/round.js');
 
 
 // describe("Tests with beforeEach", function () {
