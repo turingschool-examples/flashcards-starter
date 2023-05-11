56a16cda41bf20b5505f420d001c657355a63c0a
Denzel Turambi <dturambi26@gmail.com>
2023-05-10 18:43:15 -0600

    Add it block to test if the current card is equal in value to the first card in the deck

--- test/round-test.js
+++ test/round-test.js
@@ -18,5 +18,20 @@ describe('rounds', function () {
     const round = createRound(deck);
 
     expect(round.deck).to.be.deep.equal([card1, card2, card3]);
-  })
+  });
+
+  it('should include a current card that is equal to the first card in the deck', function() {
+    const card1 = createCard(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+    const card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+    const card3 = createCard(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+    const deck = createDeck([card1, card2, card3]);
+    const round = createRound(deck);
+
+    expect(round.currentCard).to.be.deep.equal({
+      id: 1,
+      question: "What allows you to define a set of related information using key-value pairs?",
+      answers: ["object", "array", "function"],
+      correctAnswer: "object"
+    });
+  });
 });
