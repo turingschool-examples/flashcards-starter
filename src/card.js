d5efc8dc2840c203c3bf8ff2e9a572fc7280aada
Natalie Baker <nathelene76@gmail.com>
2023-05-10 18:10:43 -0600

    add calculatepercent and end round runctions

--- src/card.js
+++ src/card.js
@@ -8,34 +8,76 @@ const createCard = (id,question,answers,correctAnswer) => {
     return card
 }
 
-const evaluateGuess = (guess,card) => {
+const evaluateGuess = (guess,correctAnswer) => {
 
-    if (card.correctAnswer === guess) {
+    if (correctAnswer === guess) {
      return 'correct!'
     } else {
      return 'incorrect!'
     }
 }
+//returning incorrect, needs to return correct
 
 const createDeck = (cards) => {
 
-    let deck = {
-        cards: cards
-    } 
-
+     let deck = cards
     return deck
 }
 
 const countDeck = (deck) => {
 
-    return deck.cards.length
+    return deck.length
 }
 
+const createRound = (deck) => {
+    var round = {
+        deck: deck,
+        currentCard: deck[0],
+        turns: 0,
+        incorrectGuesses: []
+    }
+    return round
+}
 
+const takeTurn = (guess, round) => {
+    
+    let response = evaluateGuess(guess,round.currentCard.correctAnswer)
+    if (response === 'incorrect!'){
+    round.incorrectGuesses.push(round.currentCard.id)
+    }
+
+    round.turns += 1
+    round.currentCard = round.deck[round.turns]
+
+    //
+  
+
+    ////turns and current card start at 0 and are always on the same number so round.turn can represent the index of the card we're on
+   return response
+}
+
+const calculatePercentCorrect = (round) => {
+
+ let incorrect = round.incorrectGuesses / round.turns 
+ let percentIncorrect = incorrect * 100
+ let totalCorrect = 100 - percentIncorrect
+
+    return Math.round(totalCorrect)
+}
+
+const endRound = (round) => {
+ let percentCorrect = calculatePercentCorrect(round)
+
+ return `**Round over!** You answered ${percentCorrect}% of the questions correctly!`
+}
 
 module.exports = {
 createCard,
 evaluateGuess,
 createDeck,
-countDeck
+countDeck,
+createRound,
+takeTurn,
+calculatePercentCorrect,
+endRound
 }
--- test/Card-test.js
+++ test/Card-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard, evaluateGuess, createDeck, countDeck } = require('../src/card');
+const { createCard, evaluateGuess, createDeck, countDeck, createRound, takeTurn, calculatePercentCorrect, endRound} = require('../src/card');
 
 describe('card', function() {
   it('should be a function', function() {
@@ -26,14 +26,14 @@ describe('turn', function() {
   it('should return correct if the guess is correct', function() {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     let guess = 'object'
-    let evaluate = evaluateGuess(guess,card)
+    let evaluate = evaluateGuess(guess,card.correctAnswer)
     expect(evaluate).to.deep.equal('correct!')
   });
 
   it('should return incorrect if the guess is correct', function() {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     let guess = 'array'
-    let evaluate = evaluateGuess(guess,card)
+    let evaluate = evaluateGuess(guess,card.correctAnswer)
     expect(evaluate).to.deep.equal('incorrect!')
   });
 
@@ -46,7 +46,7 @@ describe('deck', function() {
     const card3 = createCard(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
     let cards = [card1,card2,card3];
     let deck = createDeck(cards)
-  expect(createDeck([card1,card2,card3])).to.deep.equal({cards:[card1,card2,card3]})
+  expect(createDeck([card1,card2,card3])).to.deep.equal([card1,card2,card3])
   });
 });
 
@@ -61,3 +61,60 @@ describe('countDeck', function() {
     expect(countDeck(deck)).to.deep.equal(3)
   });
 });
+
+describe('create round', function() {
+  it('should return a round object', function() {
+    const card1 = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = createCard(2, 'What is a comma-seperated list of related values?', ['array','object','function'], 'array');
+    const card3 = createCard(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    let cards = [card1,card2,card3];
+    let deck = createDeck(cards)
+    const round = createRound(deck)
+    expect(round.deck).to.deep.equal(deck)
+    expect(round.currentCard).to.deep.equal(deck[0])
+    expect(round.turns).to.deep.equal(0)
+    expect(round.incorrectGuesses).to.deep.equal([])
+   
+  })
+})
+
+describe('take turn', function() {
+  it('update turn counts, evaluate guesses, give feedback, and store id of incorrect guesses', function() {
+    const card1 = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = createCard(2, 'What is a comma-seperated list of related values?', ['array','object','function'], 'array');
+    const card3 = createCard(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    let cards = [card1,card2,card3];
+    let deck = createDeck(cards)
+    const round = createRound(deck)
+    takeTurn('array', round)
+    expect(round.turns).to.deep.equal(1)
+    expect(round.incorrectGuesses).to.deep.equal([1])
+    expect(round.currentCard).to.deep.equal({id: 2, question:'What is a comma-seperated list of related values?', answers:['array','object','function'], correctAnswer:'array'})
+    
+  })
+})
+
+describe('calculatePercentage', function() {
+  it('should calculate percentage of correct guesses', function() {
+   
+    var round = {
+      turns: 2,
+      incorrectGuesses: [1]
+  }
+
+    var totalCorrect = calculatePercentCorrect(round)
+    expect(totalCorrect).to.deep.equal(50)
+  })
+})
+
+describe('endRound', function() {
+  it('prints round over and shows percent andswered correct after each round', function() {
+    var round = {
+      turns: 2,
+      incorrectGuesses: [1]
+  }
+    // var totalCorrect = calculatePercentCorrect(round)
+    let endMessage = endRound(round)
+    expect(endMessage).to.equal('**Round over!** You answered 50% of the questions correctly!')
+  })
+})
