55cc9d3e86eb4aff53f46be764e2af1233266f2d
Victor Sriqui <victorsriqui@gmail.com>
2023-05-10 21:29:29 -0600

    feat: Creates deck, and returns deck length.

---
+++ test/deck-test.js
@@ -0,0 +1,58 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const { createCard } = require("../src/card");
+const { prototypeData } = require("../src/data");
+const { createDeck, countCards } = require("../src/deck");
+
+describe("deck", function() {
+  it("createDeck should be a function", function() {
+    expect(createDeck).to.be.a("function");
+  });
+  it("createDeck should return an array of card objects as a deck", function() {
+  
+  const testsData = prototypeData;
+  const card1 = createCard(testsData[2].id, testsData[2].question, testsData[2].answers, testsData[2].correctAnswer);
+  const card2 = createCard(testsData[3].id, testsData[3].question, testsData[3].answers, testsData[3].correctAnswer);
+  const card3 = createCard(testsData[4].id, testsData[4].question, testsData[4].answers, testsData[4].correctAnswer);
+  const cards = [card1,card2,card3];
+  const deck = createDeck(cards);
+  expect(deck[0].id).to.equal(3);
+  expect(deck[1].id).to.equal(4);
+  expect(deck[2].id).to.equal(5);
+  });
+  it("countCards should be a function", function() {
+    expect(countCards).to.be.a("function");
+  });
+
+  it("countCards should return length of deck array", function() {
+    const testsData = prototypeData;
+    const card1 = createCard(testsData[2].id, testsData[2].question, testsData[2].answers, testsData[2].correctAnswer);
+    const card2 = createCard(testsData[3].id, testsData[3].question, testsData[3].answers, testsData[3].correctAnswer);
+    const card3 = createCard(testsData[4].id, testsData[4].question, testsData[4].answers, testsData[4].correctAnswer);
+    const cards = [card1,card2,card3];
+    const deck = createDeck(cards);
+    const countDeck = countCards(deck)
+    expect(countDeck).to.equal(3);
+  });
+});
+
+
+
+
+// {
+//   "id": 3,
+//   "question": "What type of prototype method directly modifies the existing array?",
+//   "answers": ["mutator method", "accessor method", "iteration method"],
+//   "correctAnswer": "mutator method"
+// }, {
+//   "id": 4,
+//   "question": "What type of prototype method does not modify the existing array but returns a particular representation of the array?",
+//   "answers": ["mutator method", "accessor method", "iteration method"],
+//   "correctAnswer": "accessor method"
+// }, {
+//   "id": 5,
+//   "question": "What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?",
+//   "answers": ["mutator method", "accessor method", "iteration method"],
+//   "correctAnswer": "iteration method"
+// }
