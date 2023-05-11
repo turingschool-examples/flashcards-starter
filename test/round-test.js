98dba25cdb240f4a4813404368b3cb79ebff96aa
Denzel Turambi <dturambi26@gmail.com>
2023-05-10 18:33:07 -0600

    Add it block to test if createRound creates an object that includes the corresponding deck

--- test/round-test.js
+++ test/round-test.js
@@ -9,4 +9,14 @@ describe('rounds', function () {
   it('should be a function', function() {
     expect(createRound).to.be.a('function');
   });
+
+  it('should create a round object with a deck', function() {
+    const card1 = createCard(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+    const card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+    const card3 = createCard(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+    const deck = createDeck([card1, card2, card3]);
+    const round = createRound(deck);
+
+    expect(round.deck).to.be.deep.equal([card1, card2, card3]);
+  })
 });
