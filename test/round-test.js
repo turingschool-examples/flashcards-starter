01c285734cee6fbc126a8eb10d72865f7524b9ce
Denzel Turambi <dturambi26@gmail.com>
2023-05-11 14:04:44 -0600

    Add it block to test if takeTurn() updates the turn count in the round object

--- test/round-test.js
+++ test/round-test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 
 const { createCard } = require('../src/card');
 const {createDeck} = require('../src/deck');
-const {createRound} = require('../src/round');
+const {createRound, takeTurn} = require('../src/round');
 
 describe('rounds', function () {
   let round, deck, card1, card2, card3;
@@ -34,10 +34,34 @@ describe('rounds', function () {
   });
 
   it('should have a turn key with a value of 0', function() {
-    expect(round.turn).to.be.equal(0);
+    expect(round.turns).to.be.equal(0);
   });
 
   it('should have a incorrect guesses property that starts with an empty array', function() {
     expect(round.incorrectGuesses).to.be.deep.equal([]);
+  });
+});
+
+describe('take turns', function() {
+  let round, deck, card1, card2, card3;
+
+  beforeEach(function() {
+    card1 = createCard(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+    card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+    card3 = createCard(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+    deck = createDeck([card1, card2, card3]);
+    round = createRound(deck);
+  });
+
+  it('should be a function', function() {
+    expect(takeTurn).to.be.a('function');
+  });
+
+  it('should update the turn count everytime', function() {
+    takeTurn('object', round);
+    takeTurn('array', round);
+    takeTurn('mutator method', round);
+
+    expect(round.turns).to.be.equal(3);
   });
 });
