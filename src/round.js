d1faf31eeb4b8fe0373b2a4f0a5e4624b84dd8e2
Benjamin holden <benjamin.f.holden@outlook.com>
2023-05-10 20:45:40 -0600

    Add console.log to endRound function

--- src/round.js
+++ src/round.js
@@ -17,6 +17,7 @@ function calculatePercentCorrect(round){
 
 function endRound(round){
   if (round.deck.length = round.turns){
+    console.log(`** Round over! ** You answered ${calculatePercentCorrect(round)}% of the questions correctly!`)
 return `** Round over! ** You answered ${calculatePercentCorrect(round)}% of the questions correctly!`
 }
 }
