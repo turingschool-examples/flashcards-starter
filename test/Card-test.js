1be34ee706377b5161749485665fccf8d0ed7b12
Victor Sriqui <victorsriqui@gmail.com>
2023-05-11 19:47:33 -0600

    refactor: Clean up.

--- test/Card-test.js
+++ test/Card-test.js
@@ -12,12 +12,6 @@ describe("card", function() {
   it("should create a card and its properties", function() {
     const testsData = prototypeData;
     const card = createCard(testsData[2].id, testsData[2].question, testsData[2].answers, testsData[2].correctAnswer);
-    // {
-    //   "id": 3,
-    //   "question": "What type of prototype method directly modifies the existing array?",
-    //   "answers": ["mutator method", "accessor method", "iteration method"],
-    //   "correctAnswer": "mutator method"
-    // }
     expect(card.id).to.equal(3);
     expect(card.question).to.equal("What type of prototype method directly modifies the existing array?");
     expect(card.answers).to.deep.equal(["mutator method", "accessor method", "iteration method"]);
--- test/turns-test.js
+++ test/turns-test.js
@@ -13,18 +13,6 @@ describe("turns", function() {
   it("should be correct", function() {
     const testsData = prototypeData;
     const card = createCard(testsData[2].id, testsData[2].question, testsData[2].answers, testsData[2].correctAnswer);
-    // {
-    //   "id": 3,
-    //   "question": "What type of prototype method directly modifies the existing array?",
-    //   "answers": ["mutator method", "accessor method", "iteration method"],
-    //   "correctAnswer": "mutator method"
-    // }
-    // {
-    //     "id": 4,
-    //     "question": "What type of prototype method does not modify the existing array but returns a particular representation of the array?",
-    //     "answers": ["mutator method", "accessor method", "iteration method"],
-    //     "correctAnswer": "accessor method"
-    //   }
     const aGuess1 = "mutator method";
     const aCorrectGuess = evaluateGuess(aGuess1, card.correctAnswer);
 
@@ -33,18 +21,6 @@ describe("turns", function() {
   it("should be incorrect", function() {
     const testsData = prototypeData;
     const card = createCard(testsData[2].id, testsData[2].question, testsData[2].answers, testsData[2].correctAnswer);
-    // {
-    //   "id": 3,
-    //   "question": "What type of prototype method directly modifies the existing array?",
-    //   "answers": ["mutator method", "accessor method", "iteration method"],
-    //   "correctAnswer": "mutator method"
-    // }
-    // {
-    //     "id": 4,
-    //     "question": "What type of prototype method does not modify the existing array but returns a particular representation of the array?",
-    //     "answers": ["mutator method", "accessor method", "iteration method"],
-    //     "correctAnswer": "accessor method"
-    //   }
     const aGuess2 = "accessor method";
     const anIncorrectGuess = evaluateGuess(aGuess2, card.correctAnswer);
 
