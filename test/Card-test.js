aae4cc7f8d970f3fe8203a2c188aa1edca47cb6b
Renee Pinna <renee.pinna@gmail.com>
2023-07-04 12:32:34 -0600

    Add test for createDeck function

--- test/Card-test.js
+++ test/Card-test.js
@@ -18,12 +18,35 @@ describe('card', function() {
   });  
 });
 
-describe('evaluateGuess', function() {
+describe('turn', function() {
   it('should be a function', function() {
     expect(evaluateGuess).to.be.a('function');
   })
+
   it('should determine whether the guess is correct or not', function() {
     const result = evaluateGuess('object', 'object');
     expect(result).to.equal('correct!');
+  })
+})
+
+describe('deck', function(){
+  it('should be an array of cards', function() {
+    const card1 = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"],"array")
+
+    const deck = createDeck(card1, card2);
+
+    expect(deck).to.deep.equal([
+    {
+      id: 1,
+      question: "What allows you to define a set of related information using key-value pairs?",
+      answers: ["object", "array", "function"],
+      correctAnswer: "object"
+    }, {
+      id: 2,
+      question: "What is a comma-separated list of related values?",
+      answers: ["array", "object", "function"],
+      correctAnswer: "array"
+    }])
   })
 })
