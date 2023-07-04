84bb5f8fa874f33bdd9e11a176255de3e11c9a13
Jen Nguyen <jenniferannnguyen@live.com>
2023-07-03 21:04:45 -0600

    Start createRound test

--- test/Round-test.js
+++ test/Round-test.js
@@ -4,15 +4,3 @@ const expect = chai.expect;
 
 const { createRound, takeTurn } = require('../src/round');
 
-describe('round', function () {
-  it('should start a round', function(){
-    const deck = createDeck()
-    
-
-    expect(round.deck).to.be.an({});
-    expect(round.currentCard).to.equal('currentCard');
-    expect(round.turns).to.equal(0)
-    expect(round.incorrectGuesses).to.equal([])
-    
-  })
-})
