37eefc656c636d9cf865b0686ebffbd7eb5b7252
Alice Abarca <aliceabarca@otmail.com>
2023-07-03 18:06:32 -0600

    Add the function createCard passing test

---
+++ src/card.js
@@ -0,0 +1,13 @@
+function createCard(id, question, answers, correctAnswer) {
+  return {
+    id: id,
+    question: question,
+    answers: answers,
+    correctAnswer: correctAnswer
+  }
+}
+
+
+module.exports = {
+  createCard,
+}
