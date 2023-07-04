ed744c87d43cc038a56047894f542a3d475411a1
Renee Pinna <renee.pinna@gmail.com>
2023-07-04 12:20:38 -0600

    Add test for evaluate guess

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,14 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard } = require('../src/card');
+const { createCard, evaluateGuess } = require('../src/card');
 
 describe('card', function() {
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     expect(createCard).to.be.a('function');
   });
 
-  it.skip('should create a card and its properties', function() {
+  it('should create a card and its properties', function() {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     
     expect(card.id).to.equal(1);
@@ -17,3 +17,13 @@ describe('card', function() {
     expect(card.correctAnswer).to.equal('object');
   });  
 });
+
+describe('evaluateGuess', function() {
+  it('should be a function', function() {
+    expect(evaluateGuess).to.be.a('function');
+  })
+  it('should determine whether the guess is correct or not', function() {
+    const result = evaluateGuess('object', 'object');
+    expect(result).to.equal('correct!');
+  })
+})
