74e36865e2a78b1a20b69fe3a3744f89bc55adb4
Renee Pinna <renee.pinna@gmail.com>
2023-07-04 12:32:02 -0600

    Add evaluateGuess Function

--- src/card.js
+++ src/card.js
@@ -6,8 +6,14 @@ function createCard(id, question, answers, correctAnswer) {
     correctAnswer: correctAnswer
   }
 }
+  
+function evaluateGuess(guess, correctAnswer) {
+  if (guess === correctAnswer) {
+    return "correct!";
+  }
 
-
+  return "incorrect!";
+}
 
 module.exports = {
   createCard,
