8d50efa9b72e848a10f6bba5230a323d98f70a75
Josh Bennett <joshbennett793@yahoo.com>
2023-07-04 20:23:54 -0500

    Evaluate guess

--- test/Card-test.js
+++ test/Card-test.js
@@ -31,7 +31,7 @@ describe('card', () => {
     expect(card.correctAnswer).to.equal('object');
   });
 
-  it.skip('should create a card and its properties', function () {
+  it('should create a card and its properties', () => {
     expect(card.id).to.equal(1);
     expect(card.question).to.equal(
       'What allows you to define a set of related information using key-value pairs?',
@@ -40,11 +40,11 @@ describe('card', () => {
     expect(card.correctAnswer).to.equal('object');
   });
 
-  it.skip('should return Correct! if the guess is correct', () => {
-    expect(correctGuess).to.equal('Correct');
+  it('should return Correct! if the guess is correct', () => {
+    expect(correctGuess).to.equal('Correct!');
   });
 
-  it.skip('should return Incorrect! if the guess is incorrect', () => {
-    expect(incorrectGuess).to.equal('Incorrect');
+  it('should return Incorrect! if the guess is incorrect', () => {
+    expect(incorrectGuess).to.equal('Incorrect!');
   });
 });
