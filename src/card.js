b18d0c7463ddb673b3b99005ee4b0cbe0db57e4e
Jocelyn Wensloff <jewensloff@gmail.com>
2023-07-04 19:33:00 -0600

    feat: Create the createCard function

---
+++ src/card.js
@@ -0,0 +1,16 @@
+function createCard(questionNum, question, answers, correctAnswer){
+  return {
+    id: questionNum,
+    question: question,
+    answers: answers,
+    correctAnswer: correctAnswer,
+  }
+
+}
+
+
+
+
+module.exports = {
+  createCard,
+}
