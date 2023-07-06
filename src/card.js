839a53dd00bbc3cddea5127e0e624646bf378fb4
Jen Ziel <jenziel400@gmail.com>
2023-07-05 21:49:12 -0400

    Create test files.  Pass unit tests for the createCard function.

---
+++ src/card.js
@@ -0,0 +1,16 @@
+function createCard(num, question, answers, correctAnswer){
+    const card = {
+        id: num,
+        question: question,
+        answers: answers,
+        correctAnswer: correctAnswer,
+    }
+    return card
+}
+
+
+
+
+module.exports = {
+    createCard,
+}
