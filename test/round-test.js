c01b04ae8c476f50b64378588934888cf18a9143
Jen Ziel <jenziel400@gmail.com>
2023-07-06 09:32:48 -0400

    Create test for the evaluateGuess function.  Check for 2 paths through the function and check that evaluateGuess is a function.

--- test/round-test.js
+++ test/round-test.js
@@ -1,13 +1,25 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const {takeTurn, endRound } = require('../src/round');
+const {takeTurn, evaluateGuess, endRound } = require('../src/round');
 
 describe('turn', function(){
     it('should be a function', function() {
         expect(takeTurn).to.be.a('function');
     });
 });
+
+describe('check answer', function(){
+    it('should be a function', function(){
+        expect(evaluateGuess).to.be.a('function')
+    });
+    it('should return an incorrect message if guess is incorrect.', function(){
+        expect(evaluateGuess('lion', 'tiger')).to.equal('incorrect!')
+    })
+    it('should return a correct message if guess is correct.', function(){
+        expect(evaluateGuess('tiger', 'tiger')).to.equal('correct!')
+    })
+})
 
 describe('round', function(){
     it('should be a function', function(){
