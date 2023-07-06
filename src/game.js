faa3fed0b1e1decc927544b472201b44f98cef01
Jen Nguyen <jenniferannnguyen@live.com>
2023-07-05 21:39:20 -0600

    Change brackets to parens in startGame function

--- src/game.js
+++ src/game.js
@@ -1,7 +1,8 @@
 const data = require('./data');
+const { createCard } = require('../src/card')
+const { createDeck, createRound, countCards} = require('../src/round')
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
-
 function printMessage(deck) {
   console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
   -----------------------------------------------------------------------`);
@@ -11,4 +12,19 @@ function printQuestion(round) {
   util.main(round);
 }
 
-module.exports = { printMessage, printQuestion };
+function startGame() {
+  const cards = makeCards(prototypeQuestions)
+  const deck = createDeck(cards);
+  const firstRound = createRound(deck);
+  printMessage(deck)
+  printQuestion(firstRound)
+
+}
+
+const makeCards = cardsData => {
+  return cardsData.map(cardData => createCard(cardData.id, cardData.question, cardData.answers, cardData.correctAnswer));
+}
+module.exports = { 
+  printMessage, 
+  printQuestion, 
+  startGame };
