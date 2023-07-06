26eba9bf2bfd6e281c5bbc9bc9388ae94a5b164f
Jen Nguyen <jenniferannnguyen@live.com>
2023-07-05 21:36:54 -0600

    Add index file to run game

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // This is where your project starts.
+const {startGame, makeCards, printMessage, printQuestion} = require('./src/game');
+
+startGame()
 
 console.log('Your project is running...'); 
