118ba1d75e4f18a3d882c07217e730896c3e88bd
Renee Pinna <renee.pinna@gmail.com>
2023-07-06 15:01:24 -0600

    Add test for initiateDesk

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,12 +1,15 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const {printMessage, printQuestion, start} = require('../src/game');
+const {printMessage, printQuestion, start, initiateDeck} = require('../src/game');
+const data = require('../src/data');
+const prototypeQuestions = data.prototypeData;
 
 describe('Game Start', function() {
-  it.skip('should create a deck', function() {
-    const deck = initiateDeck();
+  it('should create a deck', function() {
+    const deck = initiateDeck(prototypeQuestions);
 
-    expect(deck).to.deep.equal();
+    expect(deck).to.deep.equal(prototypeQuestions);
+    
   })
 })
