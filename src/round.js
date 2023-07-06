c4bf5a16af20fe0e5a87dec81b6eca2b63940cc3
Jen Nguyen <jenniferannnguyen@live.com>
2023-07-05 21:39:48 -0600

    Refactor functions to run game

--- src/round.js
+++ src/round.js
@@ -9,31 +9,53 @@ function createRound(deck) {
   }
 }
 
+function createDeck(cards) {
+  return {
+    cards: cards
+  }
+}
+
+function countCards(deck) {
+  return deck.cards.length 
+}
+
 function takeTurn(guess, round) {
   const answer = round.currentCard.correctAnswer
   if (guess !== answer) {
     round.incorrectGuesses.push(round.currentCard.id);
   }
+
   const currentIndex = round.deck.indexOf(round.currentCard);
-  if (currentIndex < round.deck.length - 1) {
-    round.currentCard = round.deck[currentIndex + 1];
-    round.turns++;
+
+  round.turns++;
+
+  round.deck.shift();
+  round.currentCard = round.deck[0]
+  if(round.currentCard === undefined) {
+    round.currentCard = null
+  } else {
+    round.currentCard
   }
-    return evaluateGuess(guess, answer)
+
+  return evaluateGuess(guess, answer)
 }
 
 function calculatePercentageCorrect(round) {
-  const percent = Math.round((1 - (round.incorrectGuesses.length / round.turns)) * 100)
+  const incorrectPercent = Math.round((1 - (round.incorrectGuesses.length / round.turns)) * 100)
+  const percent = 100 - incorrectPercent
   return `${percent}%`
 }
 
 function endRound(round) {
   const percent = calculatePercentageCorrect(round)
-  return `** Round over! ** You answered ${percent} of the questions correctly!`
+
+  console.log(`** Round over! ** You answered ${percent} of the questions correctly!`)
 }
 
 module.exports = {
   createRound,
+  createDeck,
+  countCards,
   takeTurn,
   calculatePercentageCorrect, 
   endRound
