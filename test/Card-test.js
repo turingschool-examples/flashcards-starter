7ca8824e6104484d3c224da1369f18b80eef0540
Josh Bennett <joshbennett793@yahoo.com>
2023-07-06 20:35:40 -0500

    Remove duplicate test

--- test/Card-test.js
+++ test/Card-test.js
@@ -38,20 +38,11 @@ describe('card', () => {
     expect(card.correctAnswer).to.equal('object');
   });
 
-  it('should create a card and its properties', () => {
-    expect(card.id).to.equal(1);
-    expect(card.question).to.equal(
-      'What allows you to define a set of related information using key-value pairs?',
-    );
-    expect(card.answers).to.deep.equal(['object', 'array', 'function']);
-    expect(card.correctAnswer).to.equal('object');
-  });
-
-  it('should return Correct! if the guess is correct', () => {
+  it('should return correct! if the guess is correct', () => {
     expect(correctGuess).to.equal('correct!');
   });
 
-  it('should return Incorrect! if the guess is incorrect', () => {
+  it('should return incorrect! if the guess is incorrect', () => {
     expect(incorrectGuess).to.equal('incorrect!');
   });
 });
--- test/Game-test.js
+++ test/Game-test.js
@@ -8,26 +8,13 @@ describe('starting a game', () => {
   it('should create a list of cards from a dataset', () => {
     const cards = createCardListFromDataset(sampleData);
 
-    expect(cards).to.deep.equal([
+    expect(cards[0]).to.deep.equal([
       {
         id: 1,
         question:
           'What allows you to define a set of related information using key-value pairs?',
         answers: ['object', 'array', 'function'],
         correctAnswer: 'object',
-      },
-      {
-        id: 2,
-        question: 'What is a comma-separated list of related values?',
-        answers: ['array', 'object', 'function'],
-        correctAnswer: 'array',
-      },
-      {
-        id: 3,
-        question:
-          'What type of prototype method directly modifies the existing array?',
-        answers: ['mutator method', 'accessor method', 'iteration method'],
-        correctAnswer: 'mutator method',
       },
     ]);
   });
