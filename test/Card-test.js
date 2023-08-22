9a9591ddb527cea1cfbcbdbba958834ea1a97e2c
Ann Hochworter <ann.hochworter@gmail.com>
2023-08-22 16:13:25 -0600

    Add createRound function and matching test

--- test/Card-test.js
+++ test/Card-test.js
@@ -6,6 +6,7 @@ const {
   evaluateGuess,
   createDeck,
   countCards,
+  createRound,
 } = require('../src/card');
 
 describe('card', function () {
@@ -63,53 +64,65 @@ describe('guess', function () {
 });
 
 describe(`deck functions`, () => {
-  it('should create an array of card objects', () => {
-    const card1 = createCard(
+  let card1, card2, card3, deck;
+
+  beforeEach(() => {
+    card1 = createCard(
       1,
       "What is Robbie's favorite animal",
       ['sea otter', 'pug', 'capybara'],
       'sea otter'
     );
-    const card2 = createCard(
+    card2 = createCard(
       14,
       'What organ is Khalid missing?',
       ['spleen', 'appendix', 'gallbladder'],
       'gallbladder'
     );
-    const card3 = createCard(
+    card3 = createCard(
       12,
       "What is Travis's favorite stress reliever?",
       ['listening to music', 'watching Netflix', 'playing with bubble wrap'],
       'playing with bubble wrap'
     );
-
-    const deck = createDeck([card1, card2, card3]);
-
+    deck = createDeck([card1, card2, card3]);
+  });
+  it('should create an array of card objects', () => {
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
 
   it('should get the length of a deck', () => {
-    const card1 = createCard(
+    expect(deck.cards.length).to.equal(3);
+  });
+});
+
+describe('round functions', () => {
+  let card1, card2, card3, deck;
+
+  beforeEach(() => {
+    card1 = createCard(
       1,
       "What is Robbie's favorite animal",
       ['sea otter', 'pug', 'capybara'],
       'sea otter'
     );
-    const card2 = createCard(
+    card2 = createCard(
       14,
       'What organ is Khalid missing?',
       ['spleen', 'appendix', 'gallbladder'],
       'gallbladder'
     );
-    const card3 = createCard(
+    card3 = createCard(
       12,
       "What is Travis's favorite stress reliever?",
       ['listening to music', 'watching Netflix', 'playing with bubble wrap'],
       'playing with bubble wrap'
     );
+    deck = createDeck([card1, card2, card3]);
+  });
 
-    const deck = createDeck([card1, card2, card3]);
-
-    expect(deck.cards.length).to.equal(3);
+  it('should create a round of the game', () => {
+    const gameRound = createRound(deck, 0, 0, []);
+    // console.log('gameRound', gameRound);
   });
 });
