e37709f36087d5b20b8758e652a5a9ad4768589a
Jen Nguyen <jenniferannnguyen@live.com>
2023-08-21 19:36:05 -0600

    Add tests for createcard and evaluateGuess

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,19 +1,34 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard } = require('../src/card');
+const { createCard, evaluateGuess} = require('../src/card');
 
 describe('card', function() {
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     expect(createCard).to.be.a('function');
   });
 
-  it.skip('should create a card and its properties', function() {
+  it('should create a card and its properties', function() {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     
     expect(card.id).to.equal(1);
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
-    expect(card.answers).to.deep.equal(['object', 'array', 'function']);
+    expect(card.possibleAnswers).to.deep.equal(['object', 'array', 'function']);
     expect(card.correctAnswer).to.equal('object');
   });  
 });
+
+describe('evaluate guess', function () {
+  it('should evaluate if a guess is correct and should return correct if it is', function (){
+    const correctGuess = evaluateGuess('correct answer', 'correct answer')
+
+    expect(correctGuess).to.equal('Correct!')
+  })
+
+  it('should evaluate if a guess is incorrect and should return incorrect if it is', function (){
+    // const card = createCard (2,'What is a comma-separated list of related values?',  ['array', 'object', 'function'], 'array');
+    const incorrectGuess = evaluateGuess('correct answer', 'incorrect answer')
+
+    expect(incorrectGuess).to.equal('Incorrect!')
+  })
+})
