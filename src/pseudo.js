09a9e5b437c0c4a12564111e9b0bbd3a9f809031
Ann Hochworter <ann.hochworter@gmail.com>
2023-08-22 13:22:45 -0600

    Add pseudocode file for planning

---
+++ src/pseudo.js
@@ -0,0 +1,45 @@
+//ITERATION 1
+// createCard function
+// evaluateGuess, see if it matches with answer
+// return incorrect or correct
+
+//ITERATION 2
+//=====DECK=========
+/*What - what data do I have?
+//createDeck function
+    //an array of objects
+        //object has question (string), 
+        //answers (array of strings),
+        //correctAnswer (string)
+//*Want - what do I want to do with that data?
+    //take the array and create an object of objects
+        //why? - Sarah saw this right away
+//*How - How will I make this happen?
+    //iterate over the deckArray and create an object of objects
+//*Methods - What methods will I need to make that happen?*/
+//forEach or map?
+//How do I determine which one
+
+//========ROUND========
+
+//*What - what data do I have?
+//createRound - is this a function?
+//round should be an object
+//organizes guess and records if they are correct or incorrect
+// has a key value pair for the deck object
+
+//*Want - what do I want/need to do with that data?
+//*How - How will I make that happen?
+//*Methods - What methods will I need to make that happen?
+// create round object
+// deck property
+// currentCard property that takes in first card of deck array
+
+// takeTurns property that starts as 0
+// incorrectGuesses to store guesses in array
+
+// takeTurn to increment turns, evaluates guesses, gives feedback and stores id of incorrect guesses
+
+// calculatePercentCorrect(round) calculates and returns the percentage of correct guesses
+
+//endRound(round) prints Round over! You answered ### of the questions correctly
