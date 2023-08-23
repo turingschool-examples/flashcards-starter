8d11e195cc82ba03b91f5b40f68e78006071ed8c
Logan Matheny <loganpaulmatheny@gmail.com>
2023-08-23 09:52:56 -0400

    Complete the console application for flashcards

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
+const data = require("./src/data");
+const prototypeQuestions = data.prototypeData;
+
+const { createCard, createDeck } = require("./src/card");
+
+const { createRound } = require("./src/round");
+
+const { printMessage, printQuestion } = require("./src/game");
+
 // This is where your project starts.
 function start() {
   console.log("Your project is running...");
+  // const cards = prototypeQuestions.map((card) =>
+  //   createCard(card.id, card.question, card.answers, card.correctAnswer)
+  // );
+  let cards = [];
+  for (let i = 0; i < 3; i++) {
+    let card = createCard(
+      prototypeQuestions[i].id,
+      prototypeQuestions[i].question,
+      prototypeQuestions[i].answers,
+      prototypeQuestions[i].correctAnswer
+    );
+    cards.push(card);
+  }
+
+  const deck = createDeck(cards);
+
+  const round = createRound(deck);
+
+  printMessage(deck);
+
+  printQuestion(round);
 }
 
 start();
--- src/game.js
+++ src/game.js
@@ -1,59 +1,21 @@
 const data = require("./data");
 const prototypeQuestions = data.prototypeData;
-// const util = require("./util");
+const util = require("./util");
 
-const { evaluateGuess } = require("../src/card");
+const { countCards } = require("../src/card");
 
-// function printMessage(deck) {
-//   console.log(`Welcome to FlashCards! You are playing with ${countCards(
-//     deck
-//   )} cards.
-//   -----------------------------------------------------------------------`);
-// }
-
-// function printQuestion(round) {
-//   util.main(round);
-// }
-
-function createRound(deck) {
-  let round = {
-    deck: deck,
-    currentCard: deck[0],
-    turns: 0,
-    incorrectGuesses: [],
-  };
-  return round;
+function printMessage(deck) {
+  console.log(`Welcome to FlashCards! You are playing with ${countCards(
+    deck
+  )} cards.
+  -----------------------------------------------------------------------`);
 }
 
-function takeTurn(guess, round) {
-  round.turns += 1;
-  let turn = round.turns;
-  let feedback = evaluateGuess(guess, round.currentCard);
-  if (feedback === `incorrect!`) {
-    round.incorrectGuesses.push(round.currentCard.id);
-  }
-  round.currentCard = round.deck[turn];
-  return feedback;
-}
-
-function calculatePercentCorrect(round) {
-  let percentageCorrect = Math.floor(
-    ((round.turns - round.incorrectGuesses.length) / round.turns) * 100
-  );
-  return percentageCorrect;
-}
-
-function endRound(round) {
-  return `** Round over! ** You answered ${calculatePercentCorrect(
-    round
-  )}% of the questions correctly!`;
+function printQuestion(round) {
+  util.main(round);
 }
 
 module.exports = {
-  // printMessage,
-  // printQuestion,
-  createRound,
-  takeTurn,
-  calculatePercentCorrect,
-  endRound,
+  printMessage,
+  printQuestion,
 };
