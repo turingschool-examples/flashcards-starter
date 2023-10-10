34e55e557fb23775276e27c016507f5a9bcca856
Chris Butler <butlertree@gmail.com>
2023-10-10 14:17:14 -0600

    Add game functions for iteration 2

--- src/card.js
+++ src/card.js
@@ -1,5 +1,16 @@
+// const { prototypeData } = require('./data');
+
+
+// const cards = prototypeData.map((data) => {
+//     return createCard(data.id, data.question, data.answers, data.correctAnswer);
+// });
+
+// console.log(cards);
+
+
+
 function createCard(id, question, answers, correctAnswer){
-    card = {
+   let card = {
         id: id,
         question: question,
         answers: answers,
@@ -16,9 +27,71 @@ function evaluateGuess(guess, correctAnswer){
     return 'incorrect!'
 }
 
+function createDeck(cards){
+    return cards;
+}
+
+function countCards(deck){
+    return deck.length;
+}
+
+
+// Creating a round
+function createRound(deck) {
+    let round = {
+        deck: deck,
+        currentCard: deck[0],
+        turns: 0,
+        incorrectGuesses: [],
+        
+        takeTurn: function(guess) {
+            console.log(guess)
+            return takeTurn(this, guess);
+           
+        }
+    };
+    return round;
+}
+
+
+// Taking a turn
+function takeTurn(round, guess) {
+   
+
+    let feedback = evaluateGuess(guess, round.currentCard.correctAnswer);
+    
+    if (feedback === 'incorrect!') {
+        round.incorrectGuesses.push(round.currentCard.id);
+    }
+
+    if (round.turns < round.deck.length - 1) {1
+        round.turns++;
+        round.currentCard = round.deck[round.turns];
+    } else {
+    round.currentCard = undefined;
+    }
+    return feedback  
+}
+
+
+function endRound(round) {
+    let score = ((round.deck.length - round.incorrectGuesses.length) / round.deck.length) * 100;
+    if(score < 90) {
+        console.log(`Your score is ${score}%. You need more practice! Restart the round with node index.js`);
+    } else {
+        console.log(`Your score is ${score}%. Well done!`);
+    }
+}
+
 
 
 module.exports = {
     createCard,
-    evaluateGuess
+    evaluateGuess,
+    createDeck,
+    countCards,
+    createRound,
+    takeTurn,
+    endRound
+
 }
--- test/Card-test.js
+++ test/Card-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard, evaluateGuess } = require('../src/card');
+const { createCard, evaluateGuess, createDeck, countCards, createRound, takeTurn } = require('../src/card');
 
 describe('card', function() {
   it('should be a function', function() {
@@ -40,4 +40,43 @@ describe('evaluateGuess', function() {
     expect(evaluateGuess(guess, correctAnswer)).to.equal('incorrect!');
   });
 });
+});
+
+
+describe('createDeck', function() {
+  it('should return the same array passed into it', function() {
+    const cards = [{id: 1}, {id: 2}];
+    const result = createDeck(cards);
+    expect(result).to.equal(cards);  // Checking if they reference the same array
+  });
+});
+
+describe('countCards', function() {
+  it('should return the number of cards in the deck', function() {
+    const cards = [{id: 1}, {id: 2}];
+    const result = countCards(cards);
+    expect(result).to.equal(2);
+  });
+});
+
+describe('createRound', function() {
+  it('should initialize a round with the first card as currentCard', function() {
+    const cards = [{id: 1}, {id: 2}];
+    const round = createRound(cards);
+    expect(round.currentCard).to.equal(cards[0]);
+  });
+  
+  // You can add more 'it' blocks here to test other aspects of the createRound function
+});
+
+describe('takeTurn', function() {
+  it('should increase the turn count by one', function() {
+    const cards = [{id: 1, correctAnswer: 'Chris rules'}, {id: 2, correctAnswer: 'Wally rules'}];
+    const round = createRound(cards);
+    const initialTurns = round.turns;
+    takeTurn(round, 'Chris rules');
+    expect(round.turns).to.equal(initialTurns + 1);
+  });
+  
+  // Similarly, you can add more 'it' blocks here to test other aspects of the takeTurn function
 });
