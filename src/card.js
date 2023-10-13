d82f9fff2dd788762b22d243f181dcec7d1bc0db
Joseph Stewart <JosephStewart1752@gmail.com>
2023-10-12 22:25:09 -0400

    Page cleanup

--- src/card.js
+++ src/card.js
@@ -9,11 +9,11 @@ function createCard(id, question, answers = [], correctAnswer = ' ') {
 }
 
 function evaluateGuess(guess, card) {
-    return guess === card.correctAnswer ? 'Correct!' : 'Incorrect!';
+    return guess === card.correctAnswer ? 'Correct!' : 'Incorrect!'
 }
 
 function createDeck(cards) {
-    return cards.map((cardData, index) => createCard(index + 1, ...cardData));
+    return cards.map((cardData, index) => createCard(index + 1, ...cardData))
 }
 
 const cardData = [
@@ -27,34 +27,33 @@ const cardData = [
 const deck = createDeck(cardData)
 
 function createRound(deck) {
-    let currentCard = deck[0];
-    let turns = 0;
-    const incorrectGuesses = [];
+    let currentCard = deck[0]
+    let turns = 0
+    const incorrectGuesses = []
 
     function takeTurn(guess) {
         turns++;
         currentCard = deck[turns];
-        const result = evaluateGuess(guess, currentCard);
+        const result = evaluateGuess(guess, currentCard)
         if (result === 'Incorrect!') {
             incorrectGuesses.push(currentCard.id);
         }
-        return result;
+        return result
     }
 
     function calculatePercentCorrect() {
         const totalCards = deck.length;
-        const correctGuesses = turns - incorrectGuesses.length;
-        const percentCorrect = (correctGuesses / totalCards) * 100;
-        return percentCorrect;
+        const correctGuesses = turns - incorrectGuesses.length
+        const percentCorrect = (correctGuesses / totalCards) * 100
+        return percentCorrect
     }
 
     function endRound() {
-        const percentCorrect = calculatePercentCorrect();
-        return `The round has ended with ${percentCorrect}% questioned answered correctly.`;
+        const percentCorrect = calculatePercentCorrect()
+        return `The round has ended with ${percentCorrect}% questioned answered correctly.`
     }
 
     return {
-        deck,
         takeTurn,
         calculatePercentCorrect,
         endRound
