efad1f837a641274e8c29f4c5c56870b11d1d827
Jamie Francisco <0jamiefrancisco@gmail.com>
2023-11-27 17:07:56 -0800

    Create tests and functions for createCard and evaluateGuess

---
+++ src/card.js
@@ -0,0 +1,21 @@
+function createCard(id, question, answers, correctAnswer) {
+let card = {
+  id: id,
+  question: question,
+  answers: answers,
+  correctAnswer: correctAnswer
+}
+return card;
+}
+
+function evaluateGuess(guess, correctAnswer) {
+  if (guess === correctAnswer) {
+    return 'Correct';
+  } else {
+    return 'Incorrect';
+  }
+}
+
+module.exports = { createCard, 
+  evaluateGuess
+};
