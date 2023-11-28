7ad8e50cf67ddc3371e1982e7ea5c2950d548769
Eric Kendrick <eric.c.kendrick@gmail.com>
2023-11-27 18:53:03 -0600

    Add evaluateGuess function

---
+++ src/turn.js
@@ -0,0 +1,9 @@
+function evaluateGuess(guess, correctAnswer) {
+  if (guess === correctAnswer) {
+    return 'correct!';
+  } else {
+    return 'incorrect!';
+  }
+}
+
+module.exports = { evaluateGuess };
