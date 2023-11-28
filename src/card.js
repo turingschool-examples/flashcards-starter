4726635c6c20cfdd3afd992574f0e4f2a64ad60e
Jamie Francisco <0jamiefrancisco@gmail.com>
2023-11-28 13:58:46 -0800

    Add functions to create deck and count cards in deck

--- src/card.js
+++ src/card.js
@@ -16,6 +16,27 @@ function evaluateGuess(guess, correctAnswer) {
   }
 }
 
+// function createDeck(cards) {
+// const deck = [];
+// cards.forEach(card => {
+//   deck.push(card);
+// });
+// return deck;
+// }
+function createDeck(cards) {
+ const deck = cards;
+ return deck;
+}
+
+function countCards(deck) {
+  return deck.length;
+}
+
+
+
 module.exports = { createCard, 
-  evaluateGuess
+  evaluateGuess,
+  createDeck,
+  countCards
+
 };
