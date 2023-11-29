7a0b9e7bf5b9cb0689a2e5b014639cdf61e08a54
Dana Zack <danaezack@gmail.com>
2023-11-29 15:30:02 -0700

    Feat: begin to build startGame function

--- src/game.js
+++ src/game.js
@@ -2,6 +2,12 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const { createRound, takeTurn, calculatePercentCorrect, endRound } = require('../src/round');
+const { createCard } = require('../src/card');
+const { createDeck } = require('../src/deck');
+const { evaluateGuess } = require('../src/turn');
+const { prototypeData } = require ('../src/data')
+
 function printMessage(deck) {
   console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
   -----------------------------------------------------------------------`);
@@ -11,4 +17,18 @@ function printQuestion(round) {
   util.main(round);
 }
 
-module.exports = { printMessage, printQuestion };
+function startGame() {
+  const deck = prototypeData.map(card => {
+    return createCard(card)
+  })
+  // for (var i = 0; i < prototypeData.length; i++) {
+  //   deck.push(createCard(prototypeData[i]))
+  // }
+  console.log(deck)
+  // createRound
+  // printMessage
+  // printQuestion
+}
+console.log(startGame())
+
+module.exports = { printMessage, printQuestion, startGame };
