44431921724a084012a7e7401b77151c7666c701
Dana Zack <danaezack@gmail.com>
2023-11-28 18:19:34 -0700

    Feat: build createRound function

--- src/round.js
+++ src/round.js
@@ -1,4 +1,24 @@
+function createRound(deck) {
+  const round = {
+    deck: deck,
+    currentCard: deck[0],
+    turns: 0,
+    incorrectGuesses: []  
+  }
+  return round;
+}
 
+function takeTurn() {
+
+}
+
+function calculatePercentCorrect() {
+
+}
+
+function endRound() {
+
+}
 
 module.exports = {
   createRound,
--- test/round-test.js
+++ test/round-test.js
@@ -13,14 +13,13 @@ describe('createRound', function() {
   });
 
   it('should create a round and its properties', function() {
-    //SETUP
     const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = createCard(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
     const deck = createDeck([card1, card2, card3]);
-    //EXECUTION
+
     const round = createRound(deck);
-    //ASSERTION
+
     expect(round.deck).to.deep.equal([card1, card2, card3]);
     expect(round.currentCard).to.deep.equal({
           id: 1,
@@ -44,14 +43,14 @@ describe('createRound', function() {
 
 
 
-describe('', function() {
-  it('', function() {
-    expect().to.be.a('function');
-  });
+// describe('', function() {
+//   it.skip('', function() {
+//     expect().to.be.a('function');
+//   });
 
-  it('should...', function() {
-    //SETUP
-    //EXECUTION
-    //ASSERTION
-  });  
-});
+//   it.skip('should...', function() {
+//     //SETUP
+//     //EXECUTION
+//     //ASSERTION
+//   });  
+// });
