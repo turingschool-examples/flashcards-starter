5c6ae81286722dc2dfc77eec188f0ad9c4127d12
Dana Zack <danaezack@gmail.com>
2023-11-29 18:08:55 -0700

    Refactor: update evaluateGuess function tests

--- test/turn-test.js
+++ test/turn-test.js
@@ -4,22 +4,25 @@ const expect = chai.expect;
 const { evaluateGuess } = require('../src/turn');
 
 describe('evaluateGuess', function() {
+  let correctAnswer;
+  beforeEach(function () {
+    correctAnswer = 'potato'
+  });
+  
   it('should be a function', function() {
     expect(evaluateGuess).to.be.a('function');
   });
 
   it('should declare correct if user guess is correct', function() {
-    const guess = "potato";
-    const correctAnswer = "potato";
-
+    const guess = 'potato'
+    
     const turnOutcome = evaluateGuess(guess, correctAnswer);
 
     expect(turnOutcome).to.equal("Correct!");
   });  
 
   it('should declare incorrect if user guess is incorrect', function() {
-    const guess = "potato";
-    const correctAnswer = "tomato";
+    const guess = "tomato"
 
     const turnOutcome = evaluateGuess(guess, correctAnswer);
 
