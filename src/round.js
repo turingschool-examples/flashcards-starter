64ae5fea993f3d562e068cde466511c189672088
Eric Kendrick <eric.c.kendrick@gmail.com>
2023-11-30 13:22:00 -0600

    Add function to calculate and return percentage correct and console log results of the round

--- src/round.js
+++ src/round.js
@@ -18,7 +18,20 @@ function takeTurn(guess, round) {
   return evaluateGuess(guess, round.currentCard.correctAnswer);
 };
 
+function calculatePercentCorrect(round) {
+  const numberCorrect = round.turns - round.incorrectGuesses.length;
+  const percentageCorrect = (numberCorrect / round.turns) * 100;
+  return percentageCorrect;
+};
+
+function showRoundResult(round) {
+  console.log(`** Round over! ** You answered ${calculatePercentCorrect(round)}% of the questions correctly!`);
+  return(`** Round over! ** You answered ${calculatePercentCorrect(round)}% of the questions correctly!`);
+}
+
 module.exports = { 
   createRound,
-  takeTurn
+  takeTurn,
+  calculatePercentCorrect,
+  showRoundResult
 };
