2b9b99697f5eded91d0988f2dd2c18796a7035b1
Eric Kendrick <eric.c.kendrick@gmail.com>
2023-11-30 20:55:56 -0600

    Add start function to begin new game

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-// This is where your project starts.
+const { createCard } = require ('./src/card');
+const { createDeck, countCards } = require ('./src/deck');
+const { createRound, takeTurn, calculatePercentCorrect, endRound } = require ('./src/round');
+const { prototypeData } = require ( './src/data');
+const { printMessage, printQuestion } = require ('./src/game');
+
+function start() {
+  const deck = createDeck(prototypeData);
+  printMessage(deck);
+  const round = createRound(deck);
+  printQuestion(round);
+  takeTurn(0,round);
+  calculatePercentCorrect(round);
+  if (round.turns > 30) {
+    endRound(round);
+  };
+};
+
+start();
 
 console.log('Your project is running...'); 
