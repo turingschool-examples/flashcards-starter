8ef64913d06bf14d382e7110d1702445fb65d319
Adam Bedient <adambedient@gmail.com>
2024-01-30 11:00:51 -0500

    Create test for evaluateGuess

--- test/card-test.js
+++ test/card-test.js
@@ -1,14 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard } = require('../src/card');
+const { createCard, evaluateGuess } = require('../src/card');
 
 describe('card', function() {
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     expect(createCard).to.be.a('function');
   });
 
-  it.skip('should create a card and its properties', function() {
+  it('should create a card and its properties', function() {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     
     expect(card.id).to.equal(1);
@@ -16,4 +16,21 @@ describe('card', function() {
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
     expect(card.correctAnswer).to.equal('object');
   });  
+});
+
+describe('guess', function() {
+  it('should be a function', function() {
+    expect(evaluateGuess).to.be.a('function');
+  });
+
+  it('should identify whether answer is correct', function() {
+    const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const guess1 = 'object';
+    const guess2 = 'banana';
+    const correctGuess = evaluateGuess(guess1, card.correctAnswer);
+    const incorrectGuess = evaluateGuess(guess2, card.correctAnswer);
+
+    expect(correctGuess).to.equal('correct!');
+    expect(incorrectGuess).to.equal('incorrect!');
+  });
 });
