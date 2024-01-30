1017c32174e6cfe844136012b4257ce294526aa0
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-01-30 15:19:30 -0600

    Add createDeck function and pass tests

--- src/deck.js
+++ src/deck.js
@@ -1,6 +1,15 @@
-
+function createDeck(cardsArray) {
+    let deck = [];
+    cardsArray.forEach((card) => {
+        // console.log(deck.includes(card))
+        if (!deck.includes(card)) {
+            deck.push(card)
+        };
+    });
+    return deck
+};
 
 module.exports = {
-    // createDeck,
+    createDeck,
     // countCards,
 }
