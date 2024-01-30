afcaeb4082e35ef2af135d8e9ebfc1dad5a143af
Cory Sanders <corysanders3@yahoo.com>
2024-01-30 08:27:30 -0700

    Finalize the createCard function and create a turn and turn-test file.

---
+++ src/card.js
@@ -0,0 +1,11 @@
+function createCard(id, question, answers, correctAnswer) {
+    const newCard = {
+        id,
+        question,
+        answers,
+        correctAnswer
+    };
+    return newCard
+}
+
+module.exports = { createCard }
