8b010cf9daff9d783670441ad90d17ab63e33e58
Erin Kelley <erin.kelley01@gmail.com>
2024-01-29 17:27:12 -0700

    Add chai assertions for card and response tests

--- test/card-test.js
+++ test/card-test.js
@@ -1,14 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard } = require('../src/card');
+const { createCard, evaluateGuess } = require('../src/card');
 
 describe('card', function() {
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     expect(createCard).to.be.a('function');
   });
 
-  it.skip('should create a card and its properties', function() {
+  it('should create a card and its properties', function() {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     
     expect(card.id).to.equal(1);
@@ -17,3 +17,24 @@ describe('card', function() {
     expect(card.correctAnswer).to.equal('object');
   });  
 });
+
+describe('response', function() {
+  it('should be a function', function(){
+    expect(evaluateGuess).to.be.a('function');
+  });
+
+  it('should return incorrect if guess is not correct', function(){
+    const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const response = evaluateGuess('array', card);
+
+    expect(response).to.equal('incorrect!')
+  })
+
+  it('should return correct if guess is correct', function(){
+    const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const response = evaluateGuess('object', card);
+
+    expect(response).to.equal('correct!')
+
+  })
+})
