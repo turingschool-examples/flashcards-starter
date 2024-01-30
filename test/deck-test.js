0ba6ad96acd21cfd6e4641f99c772c6dbd61c511
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-01-30 09:40:45 -0600

    Add 'Deck'  describe block

--- test/deck-test.js
+++ test/deck-test.js
@@ -4,47 +4,42 @@ const expect = chai.expect;
 const { createDeck, countCards } = require('../src/deck');
 const { createCard } = require('../src/card');
 
-describe('Create Deck', function() {
+describe('Deck', function () {
     let card1, card2, card3;
     beforeEach(() => {
         card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
         card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
         card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     });
-
-    it.skip('should be a function', function() {
-        expect(createDeck).to.be.a('function');
+    
+    describe('Create Deck', function() {
+        it.skip('should be a function', function() {
+            expect(createDeck).to.be.a('function');
+        });
+    
+        it.skip('should return an array of card objects', function() {
+            let deck = createDeck([card1, card2, card3]);
+    
+            expect(deck).to.deep.equal([card1, card2, card3]);
+            expect(deck[0].id).to.equal(1);
+        });
+    
+        it.skip('should not add duplicate cards', function() {
+            let deck = createDeck([card1, card2, card3, card2]);
+    
+            expect(deck).to.deep.equal([card1, card2, card3]);
+        })
     });
-
-    it.skip('should return an array of card objects', function() {
-        let deck = createDeck([card1, card2, card3]);
-
-        expect(deck).to.deep.equal([card1, card2, card3]);
-        expect(deck[0].id).to.equal(1);
-    });
-
-    it.skip('should not add duplicate cards', function() {
-        let deck = createDeck([card1, card2, card3, card2]);
-
-        expect(deck).to.deep.equal([card1, card2, card3]);
-    })
-});
-
-describe('Count Cards', function() {
-    let card1, card2, card3;
-    beforeEach(() => {
-        card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-        card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-        card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    });
-
-    it.skip('should be a function', function() {
-        expect(countCards).to.be.a('function');
-    });
-
-    it.skip('should return the length of the deck array', function() {
-        let deck = createDeck([card1, card2, card3]);
-
-        expect(countCards(deck)).to.equal(3);
+    
+    describe('Count Cards', function() {
+        it.skip('should be a function', function() {
+            expect(countCards).to.be.a('function');
+        });
+    
+        it.skip('should return the length of the deck array', function() {
+            let deck = createDeck([card1, card2, card3]);
+    
+            expect(countCards(deck)).to.equal(3);
+        });
     });
 });
