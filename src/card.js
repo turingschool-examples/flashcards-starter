1f8c8855ad90f945420ff0fa30ced125460f8a5c
Adam Bedient <adambedient@gmail.com>
2024-01-30 10:36:17 -0500

    Define createCard function

---
+++ src/card.js
@@ -0,0 +1,14 @@
+function createCard(id, question, answers, correctAnswer) {
+    const card = {
+        id: id,
+        question: question,
+        answers: answers,
+        correctAnswer: correctAnswer
+    }
+
+    return card;
+}
+
+module.exports = {
+    createCard
+}
