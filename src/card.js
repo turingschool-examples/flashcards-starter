5be69150289ca9b5efc5cdeee9f0370a7b361eee
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-01-29 22:15:20 -0600

    Remove pseudocode

--- src/card.js
+++ src/card.js
@@ -2,9 +2,6 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 
 function createCard(id, question, answers, correctAnswer) {
-//takes 4 arguments
-//input object OR number, string, array of strings, & string
-//output object
   return {
     id,
     question,
