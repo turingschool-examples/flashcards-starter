77b3329acab2c0caca073539a2624ba205509a3c
Adam Bedient <adambedient@gmail.com>
2024-01-30 11:01:24 -0500

    Define evaluateGuess

--- src/card.js
+++ src/card.js
@@ -9,6 +9,15 @@ function createCard(id, question, answers, correctAnswer) {
     return card;
 }
 
+function evaluateGuess(guess, correctAnswer) {
+    if (guess === correctAnswer) {
+        return 'correct!'
+    } else {
+        return 'incorrect!'
+    }
+}
+
 module.exports = {
-    createCard
+    createCard,
+    evaluateGuess
 }
