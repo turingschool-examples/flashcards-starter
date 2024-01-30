850363c7df09482cab7b229800ac15907cb45404
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-01-30 15:26:30 -0600

    Add assertions to createDeck block

--- test/deck-test.js
+++ test/deck-test.js
@@ -11,20 +11,23 @@ describe('Deck', function () {
         card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
         card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     });
-    
+
     describe('Create Deck', function() {
-        it.skip('should be a function', function() {
+        it('should be a function', function() {
             expect(createDeck).to.be.a('function');
         });
     
-        it.skip('should return an array of card objects', function() {
+        it('should return an array of card objects', function() {
             let deck = createDeck([card1, card2, card3]);
     
             expect(deck).to.deep.equal([card1, card2, card3]);
             expect(deck[0].id).to.equal(1);
+            expect(deck[0].question).to.equal('What is Robbie\'s favorite animal');
+            expect(deck[0].answers).to.deep.equal(['sea otter', 'pug', 'capybara']);
+            expect(deck[0].correctAnswer).to.equal('sea otter');
         });
     
-        it.skip('should not add duplicate cards', function() {
+        it('should not add duplicate cards', function() {
             let deck = createDeck([card1, card2, card3, card2]);
     
             expect(deck).to.deep.equal([card1, card2, card3]);
@@ -32,11 +35,11 @@ describe('Deck', function () {
     });
     
     describe('Count Cards', function() {
-        it.skip('should be a function', function() {
+        it('should be a function', function() {
             expect(countCards).to.be.a('function');
         });
     
-        it.skip('should return the length of the deck array', function() {
+        it('should return the length of the deck array', function() {
             let deck = createDeck([card1, card2, card3]);
     
             expect(countCards(deck)).to.equal(3);
