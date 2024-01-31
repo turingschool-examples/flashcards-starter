b33f77d6e65f475e5654c2ef2237d55c31814bdd
Adam Bedient <adambedient@gmail.com>
2024-01-31 08:25:34 -0500

    Implement takeTurn source code

--- src/round.js
+++ src/round.js
@@ -1,17 +1,25 @@
+const { createCard, evaluateGuess } = require('../src/card');
+
 function createRound(deck) {
     const newRound = {
         deck: deck,
         currentCard: deck[0],
         turns: 0,
         incorrectGuesses: [],
-        message: ''
     }
 
     return newRound;
 }
 
 function takeTurn(guess, round) {
-round.message = evaluateGuess(guess, round.currentCard.correctAnswer);
+    message = evaluateGuess(guess, round.currentCard.correctAnswer);
+    if (message === 'incorrect!') {
+        round.incorrectGuesses.push(round.currentCard.id);
+    }
+    round.turns++;
+    round.currentCard = round.deck[round.turns];
+
+    return message;
 }
 
 module.exports = {
