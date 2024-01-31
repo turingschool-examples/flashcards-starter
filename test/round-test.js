1ddcb5031308e4eeabd8943e485d3c02dcac499e
Erin Kelley <erin.kelley01@gmail.com>
2024-01-31 15:29:13 -0700

    Finish start function and confirm play of game

--- test/round-test.js
+++ test/round-test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 
 const { createDeck, countCards } = require('../src/deck');
 const { createCard, evaluateGuess } = require('../src/card');
-const { createRound, takeTurn, trackGuess, calculatePercentCorrect, endRound } = require('../src/round');
+const { createRound, takeTurn, calculatePercentCorrect, endRound } = require('../src/round');
 
 describe('round', function() {
     it('should be a function', function(){
@@ -26,38 +26,6 @@ describe('turn', function(){
     it('should be a function', function() {
         expect(takeTurn).to.be.a('function')
     })
-it('should increase the number of turns when a guess is taken', function() {
-const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-const card3 = createCard(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-const deck = createDeck([card1, card2, card3]);
-const round = createRound(deck);
-expect(round.turns).to.equal(0)
-expect(round.currentCard).to.equal(card1)
-var turn = takeTurn('sea otter', round)
-expect(turn.turns).to.equal(1)
-expect(turn.currentCard).to.equal(card2)
-var turn2 = takeTurn('spleen', round)
-expect(turn2.turns).to.equal(2)
-var turn3 = takeTurn('playing with bubble wrap', round)
-expect(turn3.turns).to.equal(3)
-})
-it('should move on to the next card when a guess is taken', function() {
-    const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = createCard(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-    
-    const deck = createDeck([card1, card2, card3]);
-    const round = createRound(deck);
-    expect(round.currentCard).to.equal(card1)
-    expect(round.turns).to.equal(0)
-    var turn = takeTurn('sea otter', round)   
-    expect(turn.currentCard).to.equal(card2)
-    expect(turn.turns).to.equal(1)
-    var turn2 = takeTurn('spleen', round)
-    expect(turn2.currentCard).to.equal(card3)
-    expect(turn2.turns).to.equal(2)
-})
 it('should give message', function() {
         const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
         const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
@@ -66,8 +34,7 @@ it('should give message', function() {
         var deck = createDeck([card1, card2, card3]);
         var round = createRound(deck);
         var turn = takeTurn('sea otter', round)  
-        var message = trackGuess('sea otter', round)
-        expect(message).to.equal('correct!')
+        expect(turn).to.equal('correct!')
         
     })
     it('should return incorrect if guess is incorrect', function() {
@@ -77,9 +44,10 @@ it('should give message', function() {
         
         const deck = createDeck([card1, card2, card3]);
         const round = createRound(deck);
-        var turn = takeTurn('spleen', round)  
-        var message = trackGuess('spleen', round)
-        expect(message).to.equal('incorrect!')
+        var turn = takeTurn('sea otter', round)  
+        expect(turn).to.equal('correct!')
+        var turn2 = takeTurn('spleen', round)  
+        expect(turn2).to.equal('incorrect!')
        
 })
 it('should track incorrect guesses', function() {
@@ -89,14 +57,15 @@ it('should track incorrect guesses', function() {
     
     const deck = createDeck([card1, card2, card3]);
     const round = createRound(deck);
-    var turn = takeTurn('spleen', round)  
-    var message = trackGuess('spleen', round)
-    expect(message).to.equal('incorrect!')
+    var turn = takeTurn('sea otter', round)  
+    expect(turn).to.equal('correct!')
+    var turn2 = takeTurn('spleen', round)  
+    expect(turn2).to.equal('incorrect!')
     expect(round.incorrectGuesses).to.deep.equal([14])
 })
 });
 
-describe('round', function() {
+describe('percentCorrect', function() {
     it('should be a function', function(){
         expect(calculatePercentCorrect).to.be.a('function')
     })
@@ -107,15 +76,13 @@ describe('round', function() {
         const deck = createDeck([card1, card2, card3]);
         const round = createRound(deck);
         var turn = takeTurn('sea otter', round)  
-        var message = trackGuess('sea otter', round)
         var turn = takeTurn('spleen', round)  
-        var message = trackGuess('spleen', round)
         var percentCorrect = calculatePercentCorrect(round);
         expect(percentCorrect.correctPercent).to.equal(50)
     })
 
 })
-describe('round', function() {
+describe('endRound', function() {
     it('should be a function', function(){
         expect(endRound).to.be.a('function')
     })
