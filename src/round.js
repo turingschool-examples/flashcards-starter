a3cb96aff878b0bf5c3b7af344279f336f97d2ed
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-01-30 19:44:08 -0600

    Pass final takeTurn test

--- src/round.js
+++ src/round.js
@@ -22,14 +22,23 @@ function createRound(deckArray) {
 
 function takeTurn(guess, round) {
     let evaluation = evaluateGuess(guess, round.currentCard);
-    // console.log('guess:', guess)
+    // console.log('guess:', guess);
+    // console.log('current card: ',round.currentCard);
     if (evaluation === 'Incorrect!') {
-        console.log(round.currentCard.id)
-        console.log('type of current card id:', typeof (round.currentCard.id))
         round.incorrectGuesses.push(round.currentCard.id)
-        console.log(round.incorrectGuesses[0])
-        console.log(typeof (round.incorrectGuesses[0]))
-    }
+    };
+    if (evaluation !== 'Please choose a valid option!') {
+        round.turns++;
+        // round.deck.shift()
+        round.deck.push(round.deck.shift());
+        round.currentCard = round.deck[0];
+    };
+
+    console.log('deck array [0]: ', round.deck[0] )
+    console.log('round deck after shift:', round.deck)
+    console.log('current card: ', round.currentCard) /*why is the current card not updating?*/ 
+    console.log('round object: ', round)
+    
     return evaluation
 };
 
