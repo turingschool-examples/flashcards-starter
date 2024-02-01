03880d03b557e8300c5506c6f74e3b40b62c1e81
Erin Kelley <erin.kelley01@gmail.com>
2024-01-31 19:21:07 -0700

    Clean up indentation in test files

--- test/card-test.js
+++ test/card-test.js
@@ -4,39 +4,35 @@ const expect = chai.expect;
 const { createCard, evaluateGuess } = require('../src/card');
 
 describe('card', function() {
-  it('should be a function', function() {
+    it('should be a function', function() {
     expect(createCard).to.be.a('function');
-  });
+    });
 
-  it('should create a card and its properties', function() {
+    it('should create a card and its properties', function() {
     const card = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    
     expect(card.id).to.equal(1);
     expect(card.question).to.equal('What is Robbie\'s favorite animal');
     expect(card.answers).to.deep.equal(['sea otter', 'pug', 'capybara']);
     expect(card.correctAnswer).to.equal('sea otter');
-  });  
+    });  
 });
 
 describe('response', function() {
-  it('should be a function', function(){
+    it('should be a function', function(){
     expect(evaluateGuess).to.be.a('function');
-  });
+    });
 
-  it('should return incorrect if guess is not correct', function(){
+    it('should return incorrect if guess is not correct', function(){
     const card = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const response = evaluateGuess('pug', card);
-
     expect(response).to.equal('incorrect!')
-  })
+    })
 
-  it('should return correct if guess is correct', function(){
+    it('should return correct if guess is correct', function(){
     const card = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const response = evaluateGuess('sea otter', card);
-
     expect(response).to.equal('correct!')
-
-  })
+    })
 })
 
   
--- test/round-test.js
+++ test/round-test.js
@@ -5,24 +5,22 @@ const { createDeck, countCards } = require('../src/deck');
 const { createCard, evaluateGuess } = require('../src/card');
 const { createRound, takeTurn, calculatePercentCorrect, endRound } = require('../src/round');
 
-
-
 describe('round', function() {
     it('should be a function', function(){
-        expect(createRound).to.be.a('function')
+    expect(createRound).to.be.a('function')
     })
     it('should create a round and its properties', function() {
-        const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-        const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-        const card3 = createCard(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-        const deck = createDeck([card1, card2, card3]);
-        const round = createRound(deck)
-        expect(round.deck).to.deep.equal([card1, card2, card3]);
-        expect(round.currentCard).to.equal(card1);
-        expect(round.turns).to.equal(0);
-        expect(round.incorrectGuesses).to.deep.equal([])
-      })  
-    })
+    const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = createCard(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = createDeck([card1, card2, card3]);
+    const round = createRound(deck)
+    expect(round.deck).to.deep.equal([card1, card2, card3]);
+    expect(round.currentCard).to.equal(card1);
+    expect(round.turns).to.equal(0);
+    expect(round.incorrectGuesses).to.deep.equal([])
+    })  
+})
 
 describe('beforeEach', () => {
     var card1; 
@@ -32,46 +30,45 @@ describe('beforeEach', () => {
     var round; 
 
     beforeEach(() => {
-     card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     card3 = createCard(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-     deck = createDeck([card1, card2, card3]);
-     round = createRound(deck);
+    deck = createDeck([card1, card2, card3]);
+    round = createRound(deck);
     });
-
-
 
 describe('turn', function(){
     it('should be a function', function() {
-        expect(takeTurn).to.be.a('function')
+    expect(takeTurn).to.be.a('function')
     })
-it('should give message', function() {
+
+    it('should give message', function() {
     expect(takeTurn('sea otter', round)).to.equal('correct!')
-        
     })
+
     it('should return incorrect if guess is incorrect', function() {
-        expect(takeTurn('sea otter', round) ).to.equal('correct!')  
-        expect(takeTurn('spleen', round) ).to.equal('incorrect!')
-       
-})
-it('should track incorrect guesses', function() {
+    expect(takeTurn('sea otter', round) ).to.equal('correct!')  
+     expect(takeTurn('spleen', round) ).to.equal('incorrect!')
+    })
+
+    it('should track incorrect guesses', function() {
     expect(takeTurn('sea otter', round)).to.equal('correct!')
     expect(takeTurn('spleen', round) ).to.equal('incorrect!')
     expect(round.incorrectGuesses).to.deep.equal([14])
-})
+    })
 });
 
 describe('percentCorrect', function() {
     it('should be a function', function(){
-        expect(calculatePercentCorrect).to.be.a('function')
-    })
-    it('should calculate the percentage correct', function() {
-        takeTurn('sea otter', round)  
-        takeTurn('spleen', round)  
-        var percentCorrect = calculatePercentCorrect(round);
-        expect(percentCorrect).to.equal(50)
+    expect(calculatePercentCorrect).to.be.a('function')
     })
 
+    it('should calculate the percentage correct', function() {
+    takeTurn('sea otter', round)  
+    takeTurn('spleen', round)  
+    const percentCorrect = calculatePercentCorrect(round);
+    expect(percentCorrect).to.equal(50)
+    })
 })
 });
 
