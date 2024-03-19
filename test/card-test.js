7c009846f49e27c4e2778414288463c0040ba427
David Swatzell <thatsstrikethree@gmail.com>
2024-03-19 16:17:45 -0500

    add initial round test

--- test/card-test.js
+++ test/card-test.js
@@ -49,8 +49,20 @@ expect(deck).to.deep.equal([{id:1, question: 'What is Robbie\'s favorite animal'
 {id:12, question:'What is Travis\'s middle name?', answers:['Lex', 'William', 'Fitzgerald'], correctAnswer:'Fitzgerald'}])
 
 })
+})
 
+describe('round', function(){
+  it('should be a function',function(){
+    expect(createRound).to.be.a('function')
+  });
+  it('should have a deck object', function(){
+    const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = createCard(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
 
+    const deck = createDeck([card1, card2, card3]);
 
-
+    const round = createRound(deck);
+    expect(round.deck).to.deep.equal(deck)
+  })
 })
