61afb08916be1dd54d4a6f84041a6f15eb580e92
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-18 21:59:52 -0700

    Set start function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // This is where your project starts.
+const { start } = require("./src/game");
 
-console.log('Your project is running...'); 
+console.log("Your project is running...");
+start();
--- src/game.js
+++ src/game.js
@@ -1,9 +1,21 @@
-const data = require('./data');
+const data = require("./data");
 const prototypeQuestions = data.prototypeData;
-const util = require('./util');
+const util = require("./util");
+
+const { createDeck, countCards } = require("./deck");
+const { createRound } = require("./round");
+
+function start() {
+  const deck = createDeck(prototypeQuestions);
+  const round = createRound(deck);
+  printMessage(deck);
+  printQuestion(round);
+}
 
 function printMessage(deck) {
-  console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
+  console.log(`Welcome to FlashCards! You are playing with ${countCards(
+    deck
+  )} cards.
   -----------------------------------------------------------------------`);
 }
 
@@ -11,4 +23,4 @@ function printQuestion(round) {
   util.main(round);
 }
 
-module.exports = { printMessage, printQuestion };
+module.exports = { start, printMessage, printQuestion };
