c3c4e91cd75221d6ba8445a1421c3552f36f8bd4
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-03-21 13:17:12 -0600

    refactor: changes round from class with constructor to funcitons that returns object in accordance with new tests.

--- src/round.js
+++ src/round.js
@@ -1,12 +1,26 @@
-class round{
-    constructor(deck = [null]){
-        this.deck = deck
-        this.currentCard = this.deck[0]
-        this.turns = 0
-        this.incorrectGuesses = [null]
+const {createDeck, countCards} = require('./deck')
+const {createCard, evaluateGuess} = require('./card')
+function round(deck = [null]){
+    return {
+        deck,
+        currentCard : deck[0],
+        turns : 0,
+        incorrectGuesses : [null]
     };
 };
 function takeTurn(guess, round){
+    const turnCard = createCard(round.currentCard);
+    round.turns += 1;
+    round.currentCard = round.deck[round.turns];
+    const judgement = evaluateGuess(guess, turnCard.correctAnswer);
+    console.log(judgement);
+    if (judgement === 'incorrect!'){
+        if (round.incorrectGuesses[0] === null){
+            round.incorrectGuesses = []
+        };
+        round.incorrectGuesses.push(guess)
+    };
+    return round
 };
 function calculatePercentCorrect(round){
 };
