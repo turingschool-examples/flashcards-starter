2b74e45f7222b62ce09b2a3dbcc74d3aad587ed3
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-03-21 20:10:48 -0600

    feat: Will add the basic structure of start, some errors persist when called, print question does not function.

--- src/game.js
+++ src/game.js
@@ -1,7 +1,14 @@
 const data = require('./data');
+const { createDeck, countCards } = require('./deck');
+const { round } = require('./round');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
-
+function start(){
+  const deck = createDeck(prototypeQuestions)
+  console.log(deck)
+  printMessage(newRound.deck)
+  printQuestion(newRound)
+};
 function printMessage(deck) {
   console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
   -----------------------------------------------------------------------`);
@@ -11,4 +18,4 @@ function printQuestion(round) {
   util.main(round);
 }
 
-module.exports = { printMessage, printQuestion };
+module.exports = { printMessage, printQuestion, start };
