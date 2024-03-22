8f520d0833705b833e04355196492f14b9591716
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-03-21 20:55:04 -0600

    refactor: changes some functions to be impure so that they co-operate with the provided util.js file. This breaks tests but the code that will pass them is left commented.

--- src/game.js
+++ src/game.js
@@ -1,13 +1,15 @@
 const data = require('./data');
 const { createDeck, countCards } = require('./deck');
-const { round } = require('./round');
+const { round, calculatePercentCorrect } = require('./round');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 function start(){
   const deck = createDeck(prototypeQuestions)
+  const newRound = round(deck)
   console.log(deck)
   printMessage(newRound.deck)
-  printQuestion(newRound)
+  let results = printQuestion(newRound)
+
 };
 function printMessage(deck) {
   console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
